021a176fb2ade3d44f4ed02ad19f31511a6b1b9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:33:01 +0000

    Add package ezaws/0.12/ezaws-0.12.tar.gz

---
+++ packages/ezaws/ezaws-0.12.tar.gz/ezaws-0.12/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup  # type: ignore
+
+setup(
+    name="ezaws",
+    version="0.12",
+    packages=["ezaws"],
+    description="An easy to use interface to AWS, ezaws.",
+    url="https://github.com/saidvandeklundert/ezaws",
+    classifiers=["Programming Language :: Python :: 3"],
+    python_requires=">=3.10, <4",
+    project_urls={
+        "Bug Reports": "https://github.com/saidvandeklundert/ezaws/issues",
+        "Source": "https://github.com/saidvandeklundert/ezaws",
+        "Documentation": "https://github.com/saidvandeklundert/ezaws",
+    },
+)
