c96e789e725f0fae321270c57dbceef06634a3ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:45 +0000

    Add package project-done/0.1.3/project-done-0.1.3.tar.gz

---
+++ packages/project-done/project-done-0.1.3.tar.gz/project-done-0.1.3/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['project_done']
+
+package_data = \
+{'': ['*'], 'project_done': ['sound_effects/*']}
+
+install_requires = \
+['ipython>=8.1.1,<9.0.0']
+
+setup_kwargs = {
+    'name': 'project-done',
+    'version': '0.1.3',
+    'description': '',
+    'long_description': None,
+    'author': None,
+    'author_email': None,
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
