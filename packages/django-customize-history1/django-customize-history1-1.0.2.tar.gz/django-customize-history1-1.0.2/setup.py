b6aab518f03e2935cf9cb2ee8c9e3a308bf43a68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:38 +0000

    Add package django-customize-history1/1.0.2/django-customize-history1-1.0.2.tar.gz

---
+++ packages/django-customize-history1/django-customize-history1-1.0.2.tar.gz/django-customize-history1-1.0.2/setup.py
@@ -0,0 +1,34 @@
+import os, sys
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+if sys.argv[-1] == 'publish':
+    try:
+        import wheel
+
+        print("Wheel version: ", wheel.__version__)
+    except ImportError:
+        print('Wheel library missing. Please run "pip install wheel"')
+        sys.exit()
+    os.system('python setup.py sdist upload')
+    os.system('python setup.py bdist_wheel upload')
+    sys.exit()
+
+setup(
+    name='django-customize-history1',
+    version='1.0.2',
+    license='MIT',
+    author="Mayur Ariwala",
+    author_email='mayur@softices.com',
+    packages=['django_customize_history'],
+    include_package_data=True,
+    url='https://github.com/mayur-softices/djnago-customize-history/',
+    keywords='Django Customize History',
+    install_requires=[
+        'django',
+    ],
+    zip_safe=False,
+)
