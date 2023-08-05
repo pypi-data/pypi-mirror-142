4f982bc445ead7eafe17c4233f8d097e80da5260
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:31:20 +0000

    Add package bq-test-pypy/3.0.3/bq-test-pypy-3.0.3.tar.gz

---
+++ packages/bq-test-pypy/bq-test-pypy-3.0.3.tar.gz/bq-test-pypy-3.0.3/README.md
@@ -0,0 +1,3 @@
+# Testing bqapi for Bisque
+
+
---
+++ packages/bq-test-pypy/bq-test-pypy-3.0.3.tar.gz/bq-test-pypy-3.0.3/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='bq-test-pypy',
+    version='3.0.3',
+    license='MIT',
+    author="Bisque Team",
+    author_email='ifa@ucsb.edu',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    url='https://github.com/gmyrianthous/example-publish-pypi',
+    keywords='example project',
+    install_requires=[
+          'six', 'lxml', 'requests==2.10.0', 'request-toolbelt',
+      ],
+
+)
