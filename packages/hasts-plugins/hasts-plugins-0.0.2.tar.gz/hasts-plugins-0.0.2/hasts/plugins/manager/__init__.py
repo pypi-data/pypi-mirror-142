6d2f42b81947b68863bec755af5a09fdbcf9d9bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:33:11 +0000

    Add package hasts-plugins/0.0.2/hasts-plugins-0.0.2.tar.gz

---
+++ packages/hasts-plugins/hasts-plugins-0.0.2.tar.gz/hasts-plugins-0.0.2/hasts/plugins/manager/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+### IMPORTS ###
+# Wrapping the kneedeepio plugins manager so there doesn't have to be knowledge/import of the kneedeepio library.
+from kneedeepio.plugins.manager import PluginFactory
+
+### GLOBALS ###
+
+### FUNCTIONS ###
+
+### CLASSES ###
