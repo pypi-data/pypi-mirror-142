e9ad0a6b4d1985748aa90dc1043fcaf4c530a2e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:33:25 +0000

    Add package urw/0.0.3/urw-0.0.3.tar.gz

---
+++ packages/urw/urw-0.0.3.tar.gz/urw-0.0.3/setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+
+from setuptools import setup, find_packages
+
+setup(
+    name="urw",
+    py_modules=['urw'],
+    version="0.0.3",
+    keywords=["urdu", "random", "words", "generator"],
+    description="Urdu word/phrase generator.",
+    long_description=open('README.md').read(),
+
+    project_urls={
+        'Homepage': 'https://www.techtum.dev',
+        'Issue Tracker': 'https://github.com/siphr/urw.git',
+    },
+    author="siphr",
+    author_email="pypi@techtum.dev",
+
+    packages=['urw'],
+    package_data = {
+        'urw':['wordlist.txt']
+        },
+    platforms="any",
+    install_requires=[]
+)
