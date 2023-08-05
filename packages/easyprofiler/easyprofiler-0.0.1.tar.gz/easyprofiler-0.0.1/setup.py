075790fdf486698cc00ae4d97ac41e452c130fe0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:33:05 +0000

    Add package easyprofiler/0.0.1/easyprofiler-0.0.1.tar.gz

---
+++ packages/easyprofiler/easyprofiler-0.0.1.tar.gz/easyprofiler-0.0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+with open("README.md", "r", encoding="utf8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="easyprofiler",
+    version="0.0.1",
+    description="",
+)
