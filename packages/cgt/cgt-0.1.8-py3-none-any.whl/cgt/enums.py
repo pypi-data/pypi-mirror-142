eba4cf68dd82360619dcb70ede9b4e30da544af0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:31:58 +0000

    Add package cgt/0.1.8/cgt-0.1.8-py3-none-any.whl

---
+++ packages/cgt/cgt-0.1.8-py3-none-any.whl/cgt/enums.py
@@ -0,0 +1,49 @@
+"""
+"""
+
+from enum import Enum, auto
+
+class MODEL(Enum):
+    one_region_inversions  = "inversions of a single region"
+    two_region_inversions  = "inversions of two adjacent regions"
+    all_inversions		   = "all inversions"
+    one_region_swaps       = "adjacent transpositions"
+    two_region_adjacent_transpositions = "transpositions of two adjacent regions"
+
+class TYPE(Enum):
+    reg_to_signed_pos = auto()
+    pos_to_signed_reg = auto()
+    signed_reg_to_pos = auto()
+    signed_pos_to_reg = auto()
+
+class FORMAT(Enum):
+    formal_sum     = auto()
+    equiv_classes  = auto()
+    dictionary     = auto()
+    only_reps      = auto()
+
+class SET(Enum):
+    signed_cycles 	= auto()
+    unsigned_cycles = auto()
+    one_row			= auto()
+    wreath			= auto()
+    wreath_s2		= auto()
+    
+class SYMMETRY(Enum):
+    circular = dihedral = D_n              = auto()
+    linear = S_2 = C_2 = flip = reflection = auto()
+
+class CLASSES(Enum):
+    conjugacy_classes = auto()
+    cosets            = auto()
+    double_cosets     = auto()
+
+class DISPLAY(Enum):
+    arrows  = auto()
+    one_row = auto()
+    cycles  = auto()
+
+class ALGEBRA(Enum):
+    group        = auto()
+    genome       = auto()
+    genome_class = auto()
