e21629b0afc91bf0ea34ef1ae2b20aeb8f34ec30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:33 +0000

    Add package mytestlib-hk/1.0.0/mytestlib-hk-1.0.0.tar.gz

---
+++ packages/mytestlib-hk/mytestlib-hk-1.0.0.tar.gz/mytestlib-hk-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mytestlib-hk',
+    version='1.0.0',
+    url='https://pypi.org/',
+    author='kashiki_h',
+    author_email='kashiki_h@gaia.eonet.ne.jp',
+    description='My Test Library.',
+    packages=find_packages()
+)
