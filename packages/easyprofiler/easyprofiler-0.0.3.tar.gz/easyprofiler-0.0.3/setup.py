e4dc87e66ef60633e9d8e45980b014d7830fbc8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:33:07 +0000

    Add package easyprofiler/0.0.3/easyprofiler-0.0.3.tar.gz

---
+++ packages/easyprofiler/easyprofiler-0.0.3.tar.gz/easyprofiler-0.0.3/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name="easyprofiler",
+    version="0.0.3",
+    description="easyprofiler",
+    keywords=['easyprofiler', 'easy-profiler', 'easy', 'profiler',
+              'line', 'profile', 'profiling', 'line-profiler'],
+)
