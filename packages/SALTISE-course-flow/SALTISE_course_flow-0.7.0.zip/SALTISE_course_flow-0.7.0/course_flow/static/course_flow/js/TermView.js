f912bbcfb0d299b349298d743de805bf88ec4f39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:28 +0000

    Add package SALTISE-course-flow/0.7.0/SALTISE_course_flow-0.7.0.zip

---
+++ packages/SALTISE-course-flow/SALTISE_course_flow-0.7.0.zip/SALTISE_course_flow-0.7.0/course_flow/static/course_flow/js/TermView.js
@@ -0,0 +1,85 @@
+import * as React from "react";
+import {Provider, connect} from "react-redux";
+import {ComponentJSON, TitleText} from "./ComponentJSON.js";
+import {WeekViewUnconnected} from "./WeekView.js";
+import NodeWeekView from "./NodeWeekView.js";
+import {getTermByID} from "./FindState.js";
+
+
+//Basic component to represent a Week
+class TermView extends WeekViewUnconnected{
+    
+    render(){
+        let data = this.props.data;
+        var node_blocks = [];
+        for(var i=0;i<this.props.column_order.length;i++){
+            let col=this.props.column_order[i];
+            let nodeweeks = [];
+            for(var j=0;j<data.nodeweek_set.length;j++){
+                let nodeweek = data.nodeweek_set[j];
+                if(this.props.nodes_by_column[col].indexOf(nodeweek)>=0){
+                    nodeweeks.push(
+                        <NodeWeekView key={nodeweek} objectID={nodeweek} parentID={data.id} renderer={this.props.renderer} column_order={this.props.column_order}/>
+                    );
+                }
+            }
+            if(nodeweeks.length==0)nodeweeks.push(
+                <div class="node-week placeholder" style={{height:"100%"}}></div>
+            )
+            node_blocks.push(
+                <div class={"node-block term column-"+col} id={this.props.objectID+"-node-block-column-"+col} key={col} >
+                    {nodeweeks}
+                </div>
+            );
+        }
+        
+        
+        let style={};
+        if(data.lock){
+            style.border="2px solid "+data.lock.user_colour;
+        }
+        
+        let mouseover_actions = [];
+        if(!read_only){
+            mouseover_actions.push(this.addInsertSibling(data));
+            mouseover_actions.push(this.addDuplicateSelf(data));
+            mouseover_actions.push(this.addDeleteSelf(data));
+        }
+        mouseover_actions.push(this.addCommenting(data));
+        
+        return (
+            <div style={style} class={"week"} ref={this.maindiv} onClick={(evt)=>this.props.renderer.selection_manager.changeSelection(evt,this)}>
+                <div class="mouseover-container-bypass">
+                    <div class="mouseover-actions">
+                        {mouseover_actions}
+                    </div>
+                </div>
+                <TitleText text={data.title} defaultText={data.week_type_display+" "+(this.props.rank+1)}/>
+                <div class="node-block" id={this.props.objectID+"-node-block"} ref={this.node_block}>
+                    {node_blocks}
+                </div>
+                {this.addEditable(data)}
+            </div>
+        );
+    }
+
+    makeDragAndDrop(){
+        //Makes the nodeweeks in the node block draggable
+        this.makeSortableNode($(this.node_block.current).children().children(".node-week").not(".ui-draggable"),
+          this.props.objectID,
+          "nodeweek",
+          ".node-week",
+          false,
+          [200,1],
+          ".node-block",
+          ".node");
+    }
+}
+const mapTermStateToProps = (state,own_props)=>(
+    getTermByID(state,own_props.objectID)
+)
+const mapTermDispatchToProps = {};
+export default connect(
+    mapTermStateToProps,
+    null
+)(TermView)
---
+++ packages/SALTISE-course-flow/SALTISE_course_flow-0.7.0.zip/SALTISE_course_flow-0.7.0/course_flow/tasks.py
@@ -0,0 +1,104 @@
+from smtplib import SMTPException
+
+import pandas as pd
+from celery import shared_task
+from django.conf import settings
+from django.core.cache import cache
+from django.core.mail import EmailMessage
+from django.utils import timezone
+from django.utils.translation import gettext as _
+
+from course_flow import export_functions, import_functions
+from course_flow import redux_actions as actions
+
+from .celery import try_async
+from .models import User
+from .utils import dateTimeFormatNoSpace, get_model_from_str
+
+
+@try_async
+@shared_task
+def async_send_export_email(
+    user_email, pk, object_type, task_type, email_subject, email_text
+):
+    model_object = get_model_from_str(object_type).objects.get(pk=pk)
+    if task_type == "outcomes_excel":
+        file = export_functions.get_outcomes_excel(model_object, object_type)
+        file_type = "xlsx"
+    elif task_type == "outcomes_csv":
+        file = export_functions.get_outcomes_csv(model_object, object_type)
+        file_type = "csv"
+    elif task_type == "frameworks_excel":
+        file = export_functions.get_course_frameworks_excel(
+            model_object, object_type
+        )
+        file_type = "xlsx"
+    elif task_type == "matrix_excel":
+        file = export_functions.get_program_matrix_excel(
+            model_object, object_type
+        )
+        file_type = "xlsx"
+    elif task_type == "matrix_csv":
+        file = export_functions.get_program_matrix_csv(
+            model_object, object_type
+        )
+        file_type = "csv"
+    elif task_type == "nodes_excel":
+        file = export_functions.get_nodes_excel(model_object, object_type)
+        file_type = "xlsx"
+    elif task_type == "nodes_csv":
+        file = export_functions.get_nodes_csv(model_object, object_type)
+        file_type = "csv"
+    filename = (
+        object_type
+        + "_"
+        + str(pk)
+        + "_"
+        + timezone.now().strftime(dateTimeFormatNoSpace())
+        + "."
+        + file_type
+    )
+    email = EmailMessage(
+        email_subject, email_text, settings.DEFAULT_FROM_EMAIL, [user_email],
+    )
+    if file_type == "csv":
+        file_data = "text/csv"
+    elif file_type == "xlsx":
+        file_data = (
+            "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+        )
+
+    email.attach(
+        filename, file, file_data,
+    )
+    try:
+        email.send()
+    except SMTPException:
+        print("Email could not be sent")
+
+
+@try_async
+@shared_task
+def async_import_file_data(pk, object_type, task_type, file_json, user_id):
+    model_object = get_model_from_str(object_type).objects.get(pk=pk)
+    user = User.objects.get(pk=user_id)
+    if object_type == "workflow":
+        actions.dispatch_wf(
+            model_object,
+            actions.changeField(pk, "workflow", {"importing": True}, False),
+        )
+    cache.set(object_type + str(pk) + "importing", True, 300)
+    df = pd.read_json(file_json)
+    try:
+        if task_type == "outcomes":
+            import_functions.import_outcomes(df, model_object, user)
+        if task_type == "nodes":
+            import_functions.import_nodes(df, model_object, user)
+    except:
+        pass
+    cache.delete(object_type + str(pk) + "importing")
+    if object_type == "workflow":
+        actions.dispatch_wf(
+            model_object,
+            actions.changeField(pk, "workflow", {"importing": False}, False),
+        )
