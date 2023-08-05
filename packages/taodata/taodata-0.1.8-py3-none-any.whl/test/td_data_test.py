32799d8c096c9784985d086bba966283945eabc7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:23 +0000

    Add package taodata/0.1.8/taodata-0.1.8-py3-none-any.whl

---
+++ packages/taodata/taodata-0.1.8-py3-none-any.whl/test/td_data_test.py
@@ -0,0 +1,27 @@
+import taodata as td
+import pandas as pd
+
+import json
+
+if __name__ == '__main__':
+    api = td.get_api('123456', 30)
+
+    api_name = td.WbBlog.name
+
+    bodyHelper = td.BodyHelper()
+    bodyHelper.match_phrase(td.WbBlog.fields.blog_raw_text, '疫情形势').exists(td.WbBlog.fields.blog_retweeted_status_id, 'must_not')
+    body = bodyHelper.to_json()
+    print(body)
+    fields = [td.WbBlog.fields.blog_id, td.WbBlog.fields.blog_raw_text]
+    params = {
+        'body': body,
+        'archive_day': '20220301'
+    }
+    data = api.query(api_name=api_name, fields=fields, **params)
+    print(data['info'])
+
+    df = pd.DataFrame(columns=data['alias_fields'], data=data['items'])
+    print(df)
+
+    api.export('export.xlsx', 'xlsx', api_name, fields, **params)
+
