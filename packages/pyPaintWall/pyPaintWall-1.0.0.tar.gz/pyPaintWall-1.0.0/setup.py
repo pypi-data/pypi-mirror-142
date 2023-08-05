6edb35e0fd8a2c5d54afb3abba03103eeda4d203
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:31:40 +0000

    Add package pyPaintWall/1.0.0/pyPaintWall-1.0.0.tar.gz

---
+++ packages/pyPaintWall/pyPaintWall-1.0.0.tar.gz/pyPaintWall-1.0.0/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+    name = 'pyPaintWall',
+    version = '1.0.0',
+    author = 'Maycon Cypriano Batestin',
+    author_email = 'mayconcipriano@gmail.com',
+    packages = ['pyPaintWall'],
+    description = 'A way to create script python with beatiful header',
+    long_description = 'file: README.md',
+    url = 'https://github.com/batestin1/',
+    project_urls = {
+        'Código fonte': 'https://github.com/batestin1/',
+        'Download': 'https://github.com/batestin1/'
+    },
+    keywords = 'A way to create script python with beatiful header',
+    classifiers = [
+
+
+    ]
+)
