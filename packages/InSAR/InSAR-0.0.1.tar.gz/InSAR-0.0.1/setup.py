f1fd4a5acccce6d3ed9b1b7d76263c0a57c84297
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:23 +0000

    Add package InSAR/0.0.1/InSAR-0.0.1.tar.gz

---
+++ packages/InSAR/InSAR-0.0.1.tar.gz/InSAR-0.0.1/setup.py
@@ -0,0 +1,27 @@
+from __future__ import print_function
+from setuptools import setup
+
+setup(
+    name="InSAR",
+    version="0.0.1",
+    author="Mi BoYang",
+    author_email="",
+    description="InSAR Python",
+    # long_description=open("README.rst").read(),
+    license="Apache2 License",
+    url="",
+    packages=['module'],
+    install_requires=[
+    ],
+    classifiers=[
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Topic :: Text Processing :: Indexing",
+        "Topic :: Utilities",
+        "Topic :: Internet",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Programming Language :: Python",
+        'Programming Language :: Python :: 3.6',
+    ],
+)
