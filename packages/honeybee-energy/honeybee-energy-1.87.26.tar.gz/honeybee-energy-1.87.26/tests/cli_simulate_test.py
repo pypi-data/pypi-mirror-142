fc0b4242f8e77cc4f42a71ba7e4089125af54b38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:03 +0000

    Add package honeybee-energy/1.87.26/honeybee-energy-1.87.26.tar.gz

---
+++ packages/honeybee-energy/honeybee-energy-1.87.26.tar.gz/honeybee-energy-1.87.26/tests/cli_simulate_test.py
@@ -0,0 +1,57 @@
+"""Test cli simulate module."""
+from click.testing import CliRunner
+from honeybee_energy.cli.simulate import simulate_model, simulate_osm, simulate_idf
+from honeybee.config import folders
+from ladybug.futil import nukedir
+
+import os
+
+
+def test_simulate_idf():
+    runner = CliRunner()
+    input_idf = './tests/idf/test_shoe_box.idf'
+    input_epw = './tests/epw/chicago.epw'
+
+    result = runner.invoke(simulate_idf, [input_idf, input_epw])
+    assert result.exit_code == 0
+
+    folder = os.path.join(folders.default_simulation_folder, 'test_shoe_box')
+    output_sql = os.path.join(folder, 'eplusout.sql')
+    assert os.path.isfile(output_sql)
+    nukedir(folder)
+
+
+def test_simulate_osm():
+    runner = CliRunner()
+    input_osm = './tests/osm/shoe_box.osm'
+    input_epw = './tests/epw/chicago.epw'
+
+    result = runner.invoke(simulate_osm, [input_osm, input_epw])
+    assert result.exit_code == 0
+
+    folder = os.path.join(folders.default_simulation_folder, 'shoe_box')
+    output_sql = os.path.join(folder, 'run', 'eplusout.sql')
+    assert os.path.isfile(output_sql)
+    nukedir(folder)
+
+
+def test_simulate_model():
+    runner = CliRunner()
+    input_model = './tests/json/ShoeBox.json'
+    input_epw = './tests/epw/chicago.epw'
+    input_ground = './tests/idf/monthly_ground.idf'
+
+    in_args = [
+        input_model, input_epw, '--additional-string',
+        'Output:Variable, *, Surface Window System Solar Transmittance, Timestep;',
+        '--additional-idf', input_ground
+    ]
+
+    result = runner.invoke(simulate_model, in_args)
+    print(result.output)
+    assert result.exit_code == 0
+
+    folder = os.path.join(folders.default_simulation_folder, 'shoebox')
+    output_sql = os.path.join(folder, 'openstudio', 'run', 'eplusout.sql')
+    assert os.path.isfile(output_sql)
+    nukedir(folder)
