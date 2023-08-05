c15151d5872956a9d0d34ca50aa4580da423d583
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:17 +0000

    Add package clock-tracking/0.0.6/clock_tracking-0.0.6-py3-none-any.whl

---
+++ packages/clock-tracking/clock_tracking-0.0.6-py3-none-any.whl/clock_tracking/clock.py
@@ -0,0 +1,94 @@
+from clock_tracking import logger
+from clock_tracking import report
+from clock_tracking import options
+from clock_tracking import filters
+from os.path import expanduser
+
+class Clock():
+    def __init__(self):
+        self.reader = logger.ClockReader()
+        self.writer = logger.ClockLogger()
+        self.arg = options.ClockArguments()
+
+        self.filters = []
+        self.reports = []
+        self.file = expanduser('~') + '/clock.txt'
+
+    # Parses the arguments from the options.ClockArguments() and 
+    # fills in the self.file, self.filters and self.reports
+    def parse_arguments(self):
+        self.arg.parse()
+        if self.arg.options.file is not None and self.arg.options.file != '':
+            self.file = self.arg.options.file
+        self.filters = self.arg.get_filters()
+        if self.arg.options.command != 'show':
+            return
+
+        if self.arg.options.details:
+            self.reports.append(report.DetailsReport())
+        elif self.arg.options.timeline and self.arg.options.today:
+            self.reports.append(report.DayTimelineReport())
+        elif len(self.arg.arguments) > 0 and self.arg.arguments[0] == 'tags':
+            self.arg.options.categories = False
+            self.reports.append(report.TagsReport())
+        elif self.arg.options.categories:
+            self.reports.append(report.CategoriesReport(len(self.arg.arguments)))
+        if self.arg.options.details or self.arg.options.timeline or self.arg.options.categories:
+            self.reports.append(report.TotalTimeReport(self.arg.get_target_time()))
+
+    # Filters issues according to self.filters and shows reports in self.reports
+    def show(self):
+        self.reader.read_file(self.file)
+        collection = self.reader.parse()
+        for f in self.filters:
+            collection = f.apply_to(collection)
+        for r in self.reports:
+            r.print_report(collection)
+
+    # Add new entry at given time (at) with given description (description)
+    def add(self, at, description):
+        self.writer.read_file(self.file)
+        self.writer.add(at, description)
+        self.writer.write_file(self.file)
+
+    # Edits current entry by replacing its description by given description
+    def edit(self, description):
+        self.writer.read_file(self.file)
+        self.writer.edit_current(description)
+        self.writer.write_file(self.file)
+
+    def restart(self, at):
+        self.writer.read_file(self.file)
+        desc = self.writer.get_last()
+        self.writer.add(at, desc)
+        self.writer.write_file(self.file)
+
+    # Shows current issue report
+    @staticmethod
+    def report_current(file):
+        c = Clock()
+        c.file = file
+        c.filters.append(filters.LastFilter(1))
+        c.reports.append(report.DetailsReport())
+        c.show()
+
+    # Single static method to run script according to command line arguments
+    @staticmethod
+    def run():
+        clock = Clock()
+        clock.parse_arguments()
+        options = clock.arg.options
+
+        if options.command == 'show':
+            clock.show()
+        elif options.command == 'add':
+            clock.add(options.at, ' '.join(clock.arg.arguments))
+        elif options.command == 'edit':
+            clock.edit(' '.join(clock.arg.arguments))
+        elif options.command == 'stop':
+            clock.add(options.at, '[Stop]')
+        elif options.command == 'restart':
+            clock.restart(options.at)
+
+        if options.command in ('add', 'edit', 'stop', 'restart'):
+            Clock.report_current(options.file)
---
+++ packages/clock-tracking/clock_tracking-0.0.6-py3-none-any.whl/clock_tracking/logger.py
@@ -0,0 +1,106 @@
+from datetime import date
+from datetime import datetime
+from clock_tracking.datetimeutils import DateTimeUtils
+from clock_tracking import task
+
+class ClockLogger:
+    def __init__(self):
+        self.lines = []
+
+    def read_file(self, file):
+        try:
+            with open(file, 'r') as f:
+                self.lines = f.readlines()
+            return True
+        except Exception:
+            return False
+        
+    def write_file(self, file):
+        try:
+            with open(file, 'w') as f:
+                f.writelines(self.lines)
+            return True
+        except Exception as e:
+            print('Could not write to file ' + str(file) + ': ' + str(e))
+            return False
+
+    def date_exists(self):
+        today = date.today()
+        for l in self.lines:
+            if DateTimeUtils.is_date(l) and DateTimeUtils.get_date(l).date() == today:
+                return True
+
+    def add_today(self):
+        self.lines.append(date.today().strftime('[%Y-%m-%d]') + '\n')
+
+    def edit_current(self, description):
+        if len(self.lines) == 0:
+            return
+        new_description = self.lines[-1][0:5] + ' ' + description + '\n'
+        self.lines[-1] = new_description
+        print('Edited: ' + self.lines[-1])
+
+    def get_last(self):
+        if len(self.lines) == 0:
+            return 'Starting'
+        
+        description = self.lines[-1][6:]
+        if '[Stop]' in description and len(self.lines) > 2:
+            description = self.lines[-2][6:]
+
+        return description.replace('\r', '').replace('\n', '')
+
+    # Finds position of entry given the 'at' hour (today)
+    def find_position(self, at):
+        found_today = False
+        at_time = DateTimeUtils.parse_time(date.today(), at)
+        index = 0
+        for line in self.lines:
+            if DateTimeUtils.is_date(line) and DateTimeUtils.get_date(line).date() == date.today():
+                found_today = True
+            elif found_today and not DateTimeUtils.is_date(line):
+                current_time = DateTimeUtils.parse_time(date.today(), line[0:5])
+                if current_time > at_time:
+                    return index
+            
+            index = index + 1
+        return index
+
+    def add(self, at, description):
+        if not self.date_exists():
+            self.add_today()
+
+        new_line = at + ' ' + description + '\n'
+        self.lines.insert(self.find_position(at), new_line)
+        print('Added: ' + new_line)
+
+class ClockReader:
+    def __init__(self):
+        self.lines = []
+    
+    def read_file(self, filename):
+        try:
+            file = open(filename, 'r')
+            self.lines = [line for line in file]
+        except Exception:
+            self.lines = []
+            print('Could not open file ' + str(filename))
+
+    def parse(self):
+        collection = task.TaskCollection()
+        current_day = datetime(1900, 1, 1)
+        current_task = None
+        for line in self.lines:
+            if DateTimeUtils.is_date(line):
+                current_task and current_task.discard_last()
+                current_day = DateTimeUtils.get_date(line)
+                current_task = None
+                continue
+
+            time = DateTimeUtils.parse_time(current_day, line[0:5])
+            current_task and current_task.finish(time)
+            current_task = collection.add_task(task.Task(line[6:].replace('\n', ''), time))
+
+        current_task and current_task.finish(datetime.now())
+        return collection
+
