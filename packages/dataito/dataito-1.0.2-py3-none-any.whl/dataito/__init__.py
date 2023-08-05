9daa52c2137b8b143024d35d44184ec6fb090b19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:17:16 +0000

    Add package dataito/1.0.2/dataito-1.0.2-py3-none-any.whl

---
+++ packages/dataito/dataito-1.0.2-py3-none-any.whl/dataito/__init__.py
@@ -0,0 +1,6 @@
+'''DataITO模块'''
+'''Python数据输入(Input)、转换(transform)、输出(output)，一行代码读取/转换多种格式的数据文件'''
+
+from dataito.read import read
+from dataito.transform import transform
+from dataito.save import save
