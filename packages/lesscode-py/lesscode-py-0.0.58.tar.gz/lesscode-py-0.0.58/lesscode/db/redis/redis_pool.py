701dd7972f32b2f50173e70f86054d80813bbdfe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:55 +0000

    Add package lesscode-py/0.0.58/lesscode-py-0.0.58.tar.gz

---
+++ packages/lesscode-py/lesscode-py-0.0.58.tar.gz/lesscode-py-0.0.58/lesscode/db/redis/redis_pool.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+# author:chao.yy
+# email:yuyc@ishangqi.com
+# date:2022/2/17 2:45 下午
+# Copyright (C) 2022 The lesscode Team
+
+import aioredis
+
+from lesscode.db.base_connection_pool import BaseConnectionPool
+
+
+class RedisPool(BaseConnectionPool):
+    """
+    mysql 数据库链接创建类
+    """
+
+    def create_pool(self):
+        """
+        创建mysql 异步连接池
+        :return:
+        """
+        if self.conn_info.async_enable:
+            pool = aioredis.ConnectionPool.from_url(f"redis://{self.conn_info.host}", port=self.conn_info.port,
+                                                    password=self.conn_info.password,
+                                                    db=self.conn_info.db_name, encoding="utf-8", decode_responses=True)
+            return pool
+        else:
+            raise NotImplementedError
