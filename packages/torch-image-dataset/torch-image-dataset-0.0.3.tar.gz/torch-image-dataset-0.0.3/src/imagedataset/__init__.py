b5fbe61c7c8d35734f1f1eaad0f1323822e55dec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:33:16 +0000

    Add package torch-image-dataset/0.0.3/torch-image-dataset-0.0.3.tar.gz

---
+++ packages/torch-image-dataset/torch-image-dataset-0.0.3.tar.gz/torch-image-dataset-0.0.3/src/imagedataset/__init__.py
@@ -0,0 +1,5 @@
+from .datasets import BasicImageFolder, AdvancedImageFolder, from_database
+from .imageloaders import *
+from .imagedecoders import *
+from .interpolations import Interpolation
+from .imageloaders import *
