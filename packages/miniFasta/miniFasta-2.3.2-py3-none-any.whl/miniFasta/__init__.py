e26246111adb396194878f55770b6d75463df803
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:20 +0000

    Add package miniFasta/2.3.2/miniFasta-2.3.2-py3-none-any.whl

---
+++ packages/miniFasta/miniFasta-2.3.2-py3-none-any.whl/miniFasta/__init__.py
@@ -0,0 +1,5 @@
+from ._miniFasta import print_fasta, translate_seq, reverse_comp
+from ._reader import read
+from ._writer import write, fasta_object
+
+__all__ = ["fasta_object", "read", "write", "print_fasta", "translate_seq", "reverse_comp"]
