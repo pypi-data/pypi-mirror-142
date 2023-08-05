be379076082bbf4d853a251aeca18a8af1af7914
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:16:47 +0000

    Add package django-customize-history1/1.0.4/django-customize-history1-1.0.4.tar.gz

---
+++ packages/django-customize-history1/django-customize-history1-1.0.4.tar.gz/django-customize-history1-1.0.4/setup.py
@@ -0,0 +1,55 @@
+import os, sys
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+if sys.argv[-1] == 'publish':
+    try:
+        import wheel
+
+        print("Wheel version: ", wheel.__version__)
+    except ImportError:
+        print('Wheel library missing. Please run "pip install wheel"')
+        sys.exit()
+    os.system('python setup.py sdist upload')
+    os.system('python setup.py bdist_wheel upload')
+    sys.exit()
+
+with open("README.rst") as readme:
+    setup(
+        name='django-customize-history1',
+        version='1.0.4',
+        license='MIT',
+        description="Store model history and view/revert changes from admin site.",
+        long_description="\n".join((readme.read())),
+        long_description_content_type="text/markdown",
+        author="Mayur Ariwala",
+        author_email='mayur@softices.com',
+        packages=['django_customize_history'],
+        include_package_data=True,
+        url='https://github.com/mayur-softices/djnago-customize-history/',
+        keywords='Django Customize History',
+        install_requires=[
+            'django',
+        ],
+        classifiers=[
+            "Development Status :: 5 - Production/Stable",
+            "Framework :: Django",
+            "Environment :: Web Environment",
+            "Intended Audience :: Developers",
+            "Framework :: Django",
+            "Framework :: Django :: 2.2",
+            "Framework :: Django :: 3.2",
+            "Framework :: Django :: 4.0",
+            "Programming Language :: Python",
+            "Programming Language :: Python :: 3",
+            "Programming Language :: Python :: 3.7",
+            "Programming Language :: Python :: 3.8",
+            "Programming Language :: Python :: 3.9",
+            "Programming Language :: Python :: 3.10",
+            "License :: OSI Approved :: MIT License",
+        ],
+        zip_safe=False,
+    )
