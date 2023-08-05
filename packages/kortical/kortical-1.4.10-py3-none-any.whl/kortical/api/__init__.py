49036abb42de5486eca77cdaa78e24b0a8d8e5a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:28 +0000

    Add package kortical/1.4.10/kortical-1.4.10-py3-none-any.whl

---
+++ packages/kortical/kortical-1.4.10-py3-none-any.whl/kortical/api/__init__.py
@@ -0,0 +1,28 @@
+from kortical.api import _api, code, data, deployment, experiment_framework, instance, model, superhuman_calibration
+
+
+def init(url=None, credentials=None, reset_config=False):
+    if reset_config:
+        _api.reset_config(url, credentials)
+    return _api.init(url, credentials)
+
+
+def get(url, *args, throw=True, **kwargs):
+    kwargs['throw'] = throw
+    return _api.get(url, *args, **kwargs)
+
+
+def post(url, *args, throw=True, **kwargs):
+    kwargs['throw'] = throw
+    return _api.post(url, *args, **kwargs)
+
+
+def delete(url, *args, throw=True, **kwargs):
+    kwargs['throw'] = throw
+    return _api.delete(url, *args, **kwargs)
+
+
+def post_file(url, fields, filename, filepath, *args, description=None, throw=True, **kwargs):
+    kwargs['throw'] = throw
+    kwargs['description'] = description
+    return _api.post_file(url, fields, filename, filepath, *args, **kwargs)
