8bdec6bc70b72fa880fb663f157b31a2aa818ee6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:47 +0000

    Add package mcpybox/0.0.0/mcpybox-0.0.0.tar.gz

---
+++ packages/mcpybox/mcpybox-0.0.0.tar.gz/mcpybox-0.0.0/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '0.0.0'
+DESCRIPTION = 'Minecraft python code tools'
+LONG_DESCRIPTION = 'Just some useful stuff for creating python scripts for Minecraft.'
+
+# Setting up
+setup(
+    name="mcpybox",
+    version=VERSION,
+    author="Prisemut (Tristan H.)",
+    author_email="<tristan.helas@protonmail.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python', 'minecraft', 'bedrock'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
---
+++ packages/mcpybox/mcpybox-0.0.0.tar.gz/mcpybox-0.0.0/text_animation/debug.py
@@ -0,0 +1,2 @@
+def debug(message):
+    print(message)
