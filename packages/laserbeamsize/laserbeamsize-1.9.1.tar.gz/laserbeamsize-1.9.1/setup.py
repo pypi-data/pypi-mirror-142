ed8b6e9cbe9e2705417af84bd02c31f518bdb05a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:33:30 +0000

    Add package laserbeamsize/1.9.1/laserbeamsize-1.9.1.tar.gz

---
+++ packages/laserbeamsize/laserbeamsize-1.9.1.tar.gz/laserbeamsize-1.9.1/setup.py
@@ -0,0 +1,21 @@
+import os.path
+from setuptools import setup
+
+def read(rel_path):
+    here = os.path.abspath(os.path.dirname(__file__))
+    with open(os.path.join(here, rel_path), 'r', encoding='utf-8') as fp:
+        return fp.read()
+
+def get_version(rel_path):
+    for line in read(rel_path).splitlines():
+        if line.startswith('__version__'):
+            delim = '"' if '"' in line else "'"
+            return line.split(delim)[1]
+    else:
+        raise RuntimeError("Unable to find version string.")
+
+setup(
+    long_description = read('README.rst'),
+    long_description_content_type = 'text/x-rst',
+    version = get_version("laserbeamsize/__init__.py")
+)
