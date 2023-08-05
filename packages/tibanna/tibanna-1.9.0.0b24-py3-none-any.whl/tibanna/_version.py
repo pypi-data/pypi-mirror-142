5b7d8a7e74d105ff98ad0d9e1deda8faf2f93306
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:31:18 +0000

    Add package tibanna/1.9.0.0b24/tibanna-1.9.0.0b24-py3-none-any.whl

---
+++ packages/tibanna/tibanna-1.9.0.0b24-py3-none-any.whl/tibanna/_version.py
@@ -0,0 +1,16 @@
+"""Version information."""
+import tomlkit
+
+from pathlib import Path
+
+
+def _get_project_meta():
+    toml_path = Path(__file__).parent.parent.joinpath('pyproject.toml')
+    with open(toml_path) as pyproject:
+        file_contents = pyproject.read()
+
+    return tomlkit.parse(file_contents)['tool']['poetry']
+
+pkg_meta = _get_project_meta()
+
+__version__ = str(pkg_meta['version'])
