8cf90906272eeb3fe0bead419ef165971beb7684
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:27 +0000

    Add package aiomisc/15.7.2/aiomisc-15.7.2-py3-none-any.whl

---
+++ packages/aiomisc/aiomisc-15.7.2-py3-none-any.whl/aiomisc/cron.py
@@ -0,0 +1,174 @@
+import asyncio
+import logging
+from datetime import datetime, timezone
+from functools import partial
+from typing import Any, Awaitable, Callable, Optional, Tuple, Type, Union
+
+from croniter import croniter
+
+from . import utils
+from .counters import Statistic
+
+
+log = logging.getLogger(__name__)
+
+
+class CronCallbackStatistic(Statistic):
+    call_count: int
+    sum_time: float
+    call_ok: int
+    call_failed: int
+    call_suppressed: int
+
+
+class CronCallback:
+    """
+    .. note::
+
+        When the cron function executes longer then execution interval a
+        next call will be skipping and warning will be logged.
+
+    """
+
+    __slots__ = (
+        "_cb", "_closed", "_task", "_loop", "_handle", "__name",
+        "_croniter", "_statistic",
+    )
+
+    def __init__(
+        self,
+        coroutine_func: Callable[..., Union[Any, Awaitable[Any]]],
+        *args: Any, **kwargs: Any
+    ):
+        self.__name = repr(coroutine_func)
+        self._cb = partial(
+            utils.awaitable(coroutine_func), *args, **kwargs
+        )
+        self._statistic = CronCallbackStatistic()
+        self._closed = False
+        self._handle: Optional[asyncio.Handle] = None
+        self._task: Optional[asyncio.Future] = None
+
+    async def _run(
+        self,
+        suppress_exceptions: Tuple[Type[Exception], ...] = (),
+    ) -> None:
+        delta = -self._loop.time()
+        try:
+            await self._cb()
+            self._statistic.call_ok += 1
+        except asyncio.CancelledError:
+            self._statistic.call_failed += 1
+            raise
+        except suppress_exceptions:
+            self._statistic.call_failed += 1
+            self._statistic.call_suppressed += 1
+            return
+        except Exception:
+            self._statistic.call_failed += 1
+            log.exception("Cron task error:")
+        finally:
+            delta += self._loop.time()
+            self._statistic.sum_time += delta
+            self._statistic.call_count += 1
+
+    def get_next(self) -> float:
+        if not self._loop or not self._croniter:
+            raise asyncio.InvalidStateError
+        loop_time = self._loop.time()
+        timestamp = datetime.now(timezone.utc).timestamp()
+        interval = self._croniter.get_next(float) - timestamp
+        if interval < 0:
+            raise asyncio.InvalidStateError
+        return loop_time + interval
+
+    def get_current(self) -> float:
+        if not self._loop or not self._croniter:
+            raise asyncio.InvalidStateError
+        loop_time = self._loop.time()
+        timestamp = datetime.now(timezone.utc).timestamp()
+        interval = self._croniter.get_current(float) - timestamp
+        if interval < 0:
+            raise asyncio.InvalidStateError
+        return loop_time + interval
+
+    def start(
+        self,
+        spec: str,
+        loop: asyncio.AbstractEventLoop = None,
+        *, shield: bool = False,
+        suppress_exceptions: Tuple[Type[Exception], ...] = ()
+    ) -> None:
+        if self._task and not self._task.done():
+            raise asyncio.InvalidStateError
+
+        current_loop = loop or asyncio.get_event_loop()
+        # noinspection PyAttributeOutsideInit
+        self._loop = current_loop       # type: asyncio.AbstractEventLoop
+
+        # noinspection PyAttributeOutsideInit
+        self._croniter = croniter(
+            spec, start_time=datetime.now(timezone.utc).timestamp(),
+        )
+
+        self._closed = False
+
+        def cron() -> None:
+            if self._loop.is_closed():
+                return
+
+            if self._task and not self._task.done():
+                log.warning("Task %r still running skipping", self)
+                call_next()
+                return
+
+            loop = self._loop   # type: asyncio.AbstractEventLoop
+
+            del self._task
+            self._task = None
+
+            if self._closed:
+                return
+
+            runner: Callable[..., Awaitable[Any]] = self._run
+            if shield:
+                runner = utils.shield(runner)
+
+            self._task = loop.create_task(runner(suppress_exceptions))
+
+            call_next()
+
+        def call_next(*_: Any) -> None:
+            if self._handle is not None:
+                self._handle.cancel()
+                del self._handle
+
+            self._handle = self._loop.call_at(self.get_next(), cron)
+        self._loop.call_at(
+            self.get_next(), self._loop.call_soon_threadsafe, cron,
+        )
+
+    def stop(self) -> asyncio.Future:
+        self._closed = True
+
+        if self._task is None:
+            self._task = self._loop.create_future()
+            self._task.set_exception(RuntimeError("Callback not started"))
+
+        elif not self._task.done():
+            self._task.cancel()
+
+        if self._handle:
+            self._handle.cancel()
+
+        return self._task
+
+    def __repr__(self) -> str:
+        return "%s(%s)" % (self.__class__.__name__, self.__name)
+
+    def __str__(self) -> str:
+        return "%s(%s)" % (self.__class__.__name__, self._cb.func.__name__)
+
+    @property
+    def task(self) -> Optional[asyncio.Future]:
+        return self._task
---
+++ packages/aiomisc/aiomisc-15.7.2-py3-none-any.whl/aiomisc/periodic.py
@@ -0,0 +1,148 @@
+import asyncio
+import logging
+from functools import partial
+from typing import Any, Awaitable, Callable, Optional, Tuple, Type, Union
+
+from . import utils
+from .counters import Statistic
+
+
+log = logging.getLogger(__name__)
+ExceptionsType = Tuple[Type[Exception], ...]
+CallbackType = Callable[..., Union[Awaitable[Any], Any]]
+
+
+class PeriodicCallbackStatistic(Statistic):
+    call_count: int
+    done: int
+    fail: int
+    sum_time: float
+
+
+class PeriodicCallback:
+    """
+    .. note::
+
+        When the periodic function executes longer then execution interval a
+        next call will be skipping and warning will be logged.
+
+    """
+
+    __slots__ = (
+        "_cb", "_closed", "_task", "_loop", "_handle", "__name",
+        "_statistic",
+    )
+
+    _closed: Optional[bool]
+    _handle: Optional[asyncio.Handle]
+    _task: Optional[asyncio.Future]
+
+    def __init__(
+        self, coroutine_func: CallbackType,
+        *args: Any, **kwargs: Any
+    ):
+
+        self._statistic = PeriodicCallbackStatistic()
+        self.__name = repr(coroutine_func)
+        self._cb = partial(
+            utils.awaitable(coroutine_func), *args, **kwargs
+        )
+        self._closed = False
+        self._handle = None
+        self._task = None
+
+    async def _run(self, suppress_exceptions: ExceptionsType = ()) -> None:
+        try:
+            await self._cb()
+        except suppress_exceptions:
+            return
+        except asyncio.CancelledError:
+            raise
+        except Exception:
+            log.exception("Periodic task error:")
+
+    def start(
+        self, interval: Union[int, float],
+        loop: asyncio.AbstractEventLoop = None, *,
+        delay: Union[int, float] = 0,
+        shield: bool = False,
+        suppress_exceptions: ExceptionsType = ()
+    ) -> None:
+        assert interval
+
+        if self._task and not self._task.done():
+            raise asyncio.InvalidStateError
+
+        current_loop = loop or asyncio.get_event_loop()
+        # noinspection PyAttributeOutsideInit
+        self._loop = current_loop   # type: asyncio.AbstractEventLoop
+        self._closed = False
+
+        def periodic() -> None:
+            if self._loop.is_closed():
+                return
+
+            if self._task and not self._task.done():
+                log.warning("Task %r still running skipping", self)
+                return
+
+            del self._task
+            self._task = None
+
+            if self._closed:
+                return
+
+            runner = utils.shield(self._run) if shield else self._run
+            self._task = self._loop.create_task(
+                runner(suppress_exceptions),        # type: ignore
+            )
+
+            start_time = self._loop.time()
+
+            self._task.add_done_callback(call)
+            self._task.add_done_callback(lambda t: do_stat(t, start_time))
+
+        def do_stat(task: asyncio.Task, start_time: float) -> None:
+            self._statistic.call_count += 1
+            self._statistic.sum_time += self._loop.time() - start_time
+
+            if task.cancelled():
+                self._statistic.fail += 1
+            elif task.exception():
+                self._statistic.fail += 1
+            else:
+                self._statistic.done += 1
+
+        def call(*_: Any) -> None:
+            if self._handle is not None:
+                self._handle.cancel()
+                del self._handle
+
+            self._handle = self._loop.call_later(
+                interval, periodic,
+            )
+
+        self._loop.call_later(delay, self._loop.call_soon_threadsafe, periodic)
+
+    def stop(self) -> asyncio.Future:
+        self._closed = True
+
+        if self._task is None:
+            self._task = self._loop.create_future()
+            self._task.set_exception(
+                RuntimeError("Callback not started"),
+            )
+        elif not self._task.done():
+            self._task.cancel()
+
+        if self._handle:
+            self._handle.cancel()
+
+        return self.task
+
+    def __repr__(self) -> str:
+        return "%s(%s)" % (self.__class__.__name__, self.__name)
+
+    @property
+    def task(self) -> asyncio.Future:
+        return self._task   # type: ignore
