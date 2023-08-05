f38dccf5aceeceb6b2803b20e37d652807c1e8a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:33:30 +0000

    Add package airtable-async-ti/0.0.1b11/airtable-async-ti-0.0.1b11.tar.gz

---
+++ packages/airtable-async-ti/airtable-async-ti-0.0.1b11.tar.gz/airtable-async-ti-0.0.1b11/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+
+setup(
+    name="airtable-async-ti",
+    packages=["airbase", "airbase.utils"],
+    description="Forked asynchronous Python API Wrapper for the Airtable API - return id in responses",
+    author="Jennifer Trippett",
+    author_email="lfparis@gmail.com",
+    url="https://github.com/tangibleintelligence/airbase",
+    version="0.0.1b11",
+    install_requires=["aiohttp"],
+    extras_require={"tools": ["pandas"]},
+    package_data={"airbase": ["py.typed"]},
+    zip_safe=False,
+    python_requires=">=3.7",
+    keywords=["airtable", "api", "async", "async.io"],
+    license="The MIT License (MIT)",
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Environment :: Console",
+        "Framework :: AsyncIO",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python",
+        "Topic :: Software Development",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: Implementation :: CPython",
+    ],
+)
