7d98cd66c2871705d0b476a1fefaf5b37b04305b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:17:15 +0000

    Add package scalum/1.2.1/scalum-1.2.1-py2.py3-none-any.whl

---
+++ packages/scalum/scalum-1.2.1-py2.py3-none-any.whl/scalum/__init__.py
@@ -0,0 +1,7 @@
+__version__ = "1.2.1"
+
+
+from .Scale import Scale
+from .ScaleWithPolyAnnotation import ScaleWithPolyAnnotation
+from .ScaleWithPolyAnnotation import ScaleWithPolyAnnotation
+from .ScaleWithInverse import ScaleWithInverse
