ab36b503189be0919a2be4c6216a236b14311911
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:24 +0000

    Add package pyTGR/1.0.1/pyTGR-1.0.1.tar.gz

---
+++ packages/pyTGR/pyTGR-1.0.1.tar.gz/pyTGR-1.0.1/setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '1.0.1'
+DESCRIPTION = 'A minimal lib for rendering things on Terminal.'
+
+# Setting up
+setup(
+    name="pyTGR",
+    version=VERSION,
+    author="Merwin",
+    author_email="<merwinmathews1001@gmail.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    license='MIT',
+    url="https://github.com/mastercodermerwin/TGR",
+    project_urls={
+        "Bug Tracker": "https://github.com/mastercodermerwin/TGR/issues",
+    },
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=['opencv-python', 'sty','pynput'],
+    keywords=['python', 'terminal','color','graphics','shapes','terminal events'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
