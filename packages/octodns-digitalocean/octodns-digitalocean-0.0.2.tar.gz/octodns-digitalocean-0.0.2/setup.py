d60040e34fb8f154446988d8fdaf1bf0f67d516a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:31:24 +0000

    Add package octodns-digitalocean/0.0.2/octodns-digitalocean-0.0.2.tar.gz

---
+++ packages/octodns-digitalocean/octodns-digitalocean-0.0.2.tar.gz/octodns-digitalocean-0.0.2/setup.py
@@ -0,0 +1,52 @@
+from setuptools import find_packages, setup
+
+
+def descriptions():
+    with open('README.md') as fh:
+        ret = fh.read()
+        first = ret.split('\n', 1)[0].replace('#', '')
+        return first, ret
+
+
+def version():
+    with open('octodns_digitalocean/__init__.py') as fh:
+        for line in fh:
+            if line.startswith('__VERSION__'):
+                return line.split("'")[1]
+
+
+description, long_description = descriptions()
+
+tests_require = (
+    'pytest',
+    'pytest-network',
+    'requests_mock',
+)
+
+setup(
+    author='Ross McFarland',
+    author_email='rwmcfa1@gmail.com',
+    description=description,
+    extras_require={
+        'dev': tests_require + (
+            'build>=0.7.0',
+            'pycodestyle>=2.6.0',
+            'pyflakes>=2.2.0',
+            'readme_renderer[md]>=26.0',
+            'twine>=3.4.2',
+        ),
+    },
+    install_requires=(
+        'octodns>=0.9.16',
+        'requests>=2.27.0',
+    ),
+    license='MIT',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    name='octodns-digitalocean',
+    packages=find_packages(),
+    python_requires='>=3.6',
+    tests_require=tests_require,
+    url='https://github.com/octodns/octodns-digitalocean',
+    version=version(),
+)
