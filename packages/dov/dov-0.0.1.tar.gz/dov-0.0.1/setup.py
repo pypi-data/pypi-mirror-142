75bd026ddf7bf14d3c86a8876b2b00b674d39796
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:05 +0000

    Add package dov/0.0.1/dov-0.0.1.tar.gz

---
+++ packages/dov/dov-0.0.1.tar.gz/dov-0.0.1/setup.py
@@ -0,0 +1,45 @@
+import sys
+from setuptools import setup
+from setuptools import find_packages
+
+NAME = "dov"
+VERSION = "0.0.1"
+AUTHOR = "Ailln"
+EMAIL = "kinggreenhall@gmail.com"
+URL = ""
+LICENSE = "MIT License"
+DESCRIPTION = "dovolopor"
+
+if sys.version_info[:2] < (3, 6):
+    raise RuntimeError("Python version >= 3.6 required.")
+
+if __name__ == "__main__":
+    setup(
+        name=NAME,
+        version=VERSION,
+        author=AUTHOR,
+        author_email=EMAIL,
+        url=URL,
+        license=LICENSE,
+        description=DESCRIPTION,
+        packages=find_packages(),
+        include_package_data=True,
+        install_requires=open("./requirements.txt", "r").read().splitlines(),
+        long_description=open("./README.md", "r").read(),
+        long_description_content_type='text/markdown',
+        entry_points={
+            "console_scripts": [
+                "dov=dov.shell:run"
+            ]
+        },
+        package_data={
+            "dov": ["src/*.txt"]
+        },
+        zip_safe=True,
+        classifiers=[
+            "Programming Language :: Python :: 3",
+            f"License :: OSI Approved :: {LICENSE}",
+            "Operating System :: OS Independent",
+        ],
+        python_requires=">=3.6"
+    )
