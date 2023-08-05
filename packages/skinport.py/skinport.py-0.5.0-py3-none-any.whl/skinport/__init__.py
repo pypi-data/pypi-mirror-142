5316758c3e34a97d075e8def26a710c540059bc0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:02 +0000

    Add package skinport.py/0.5.0/skinport.py-0.5.0-py3-none-any.whl

---
+++ packages/skinport.py/skinport.py-0.5.0-py3-none-any.whl/skinport/__init__.py
@@ -0,0 +1,40 @@
+"""
+Skinport API Wrapper
+~~~~~~~~~~~~~~~~~~~
+A basic wrapper for the Skinport API.
+:copyright: (c) 2022 PaxxPatriot
+:license: MIT, see LICENSE for more details.
+"""
+__title__ = "skinport"
+__author__ = "PaxxPatriot"
+__license__ = "MIT"
+__copyright__ = "Copyright 2022 PaxxPatriot"
+__version__ = "0.5.0"
+
+__path__ = __import__("pkgutil").extend_path(__path__, __name__)
+
+import logging
+from typing import NamedTuple
+
+from .client import *
+from .color import *
+from .enums import *
+from .errors import *
+from .item import *
+from .sale import *
+from .transaction import *
+from .iterators import *
+from .salefeed import *
+
+
+class VersionInfo(NamedTuple):
+    major: int
+    minor: int
+    micro: int
+    releaselevel: str
+    serial: int
+
+
+version_info: VersionInfo = VersionInfo(major=0, minor=5, micro=0, releaselevel="final", serial=0)
+
+logging.getLogger(__name__).addHandler(logging.NullHandler())
