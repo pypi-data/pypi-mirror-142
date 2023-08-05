7909ce2b4220aa9a24817c98b122d7841be497d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:10:21 +0000

    Add package notramp/1.0.4/notramp-1.0.4.tar.gz

---
+++ packages/notramp/notramp-1.0.4.tar.gz/notramp-1.0.4/notramp/amp_cov.py
@@ -0,0 +1,104 @@
+# Copyright 2022 Simon Magin.
+# Licensed under the BSD 2-Clause License (https://opensource.org/licenses/BSD-2-Clause)
+# This file may not be copied, modified, or distributed except according to those terms.
+
+
+from os import path
+import logging
+import logging.config
+
+
+log_file_path = path.join(path.dirname(__file__), "resources", "logging.conf")
+logging.config.fileConfig(log_file_path, disable_existing_loggers=False)
+logger = logging.getLogger(__name__)
+
+
+def bin_mappings(amp_bins, mappings, max_cov, margins):
+    """sort mappings to amplicons"""
+    logger.info("sorting mappings to amplicons")
+    binned = []
+    not_av = []
+    logger.info("mappings of %s reads available", str(len(mappings)))
+    while len(amp_bins) > 0:
+        if len(mappings) > 0:
+            if mappings[0].tend <= amp_bins[0].end + margins:
+                if mappings[0].tstart >= amp_bins[0].start - margins:
+                    amp_bins[0].reads_dct[mappings[0].qname] = mappings[0].qname
+                    mappings.pop(0)
+                else:
+                    not_av.append(mappings[0].qname)
+                    mappings.pop(0)
+            else:
+                binned.append(amp_bins[0])
+                amp_bins.pop(0)
+        else:
+            binned.append(amp_bins[0])
+            break
+
+    num_selected = 0
+    for amp_bin in binned:
+        amp_bin.random_sample(max_cov)
+        amn = amp_bin.name
+        rct = str(len(amp_bin.reads_dct))
+        slr = str(len(amp_bin.selected))
+        logger.info("Amp_%s reads available %s selected: %s", amn, rct, slr)
+        num_selected += len(amp_bin.selected)
+    logger.debug("%s reads could not be binned to an Amp", str(len(not_av)))
+    return binned
+
+
+def write_read_to_file(rname, read_fobj, out_fobj, infile_fastq, outfile_type):
+    """Write reads from in_file to out_file"""
+    if infile_fastq:
+        if outfile_type == "fasta":
+            rname = rname.replace("@", ">")
+            out_fobj.write(rname + "\n")
+            out_fobj.write(next(read_fobj))
+        else:
+            try:
+                out_fobj.write(rname + "\n")
+                out_fobj.write(next(read_fobj))
+                out_fobj.write(next(read_fobj))
+                out_fobj.write(next(read_fobj))
+            except StopIteration:
+                logger.exception("Unexpected eof during writing of fastq")
+    else:
+        out_fobj.write(rname + "\n")
+        out_fobj.write(next(read_fobj))
+
+
+def write_capped_from_file(binned, reads, fa_out, kw):
+    """write subsample to outfile using reads-file as source"""
+    logger.info("writing subsample to outfile using reads-file as source")
+    fastq = kw["fq_in"]
+    hin = "@" if fastq else ">"
+    all_picks = [hin + name for amp in binned for name in amp.selected]
+    with open(reads, "r", encoding="utf-8") as rfi, open(fa_out, "w", encoding="utf-8") as ofo:
+        for line in rfi:
+            if line.startswith(hin):
+                readname = line.split(" ")[0]
+                if readname in all_picks:
+                    write_read_to_file(readname, rfi, ofo, fastq, kw["fq_out"])
+
+
+def write_capped_from_loaded(binned, loaded_reads, out_file, kw):
+    """write subsample to outfile using loaded reads as source"""
+    logger.info("writing subsample to outfile using loaded reads as source")
+    all_picks = [name for amp in binned for name in amp.selected]
+    with open(out_file, "w", encoding="utf-8") as outf:
+        for name in all_picks:
+            try:
+                if not kw["fq_out"]:
+                    header = ">" + name + "\n"
+                    seq = loaded_reads[name].seq + "\n"
+                    outf.write(header)
+                    outf.write(seq)
+                else:
+                    fq_read = loaded_reads[name]
+                    header = "@" + name + "\n"
+                    outf.write(header)
+                    outf.write(fq_read.seq + "\n")
+                    outf.write("+\n")
+                    outf.write(fq_read.qstr + "\n")
+            except KeyError:
+                logging.exception("Error: read %s was not found in loaded reads", name)
---
+++ packages/notramp/notramp-1.0.4.tar.gz/notramp-1.0.4/notramp/nta_aux.py
@@ -0,0 +1,92 @@
+from os import path
+import logging
+import logging.config
+import psutil
+
+
+log_file_path = path.join(path.dirname(__file__),  "resources", "logging.conf")
+logging.config.fileConfig(log_file_path , disable_existing_loggers=False)
+logger = logging.getLogger(__name__)
+
+
+def fastq_autoscan(read_file):
+    """Scanning readfile to determine filetype"""
+    logger.debug("Scanning readfile to determine filetype")
+    with open(read_file, "r", encoding="utf-8") as rfl:
+        line_ct = 0
+        fastq = False
+        while line_ct < 100:
+            for line in rfl:
+                line_ct += 1
+                if line.startswith("@"):
+                    fastq = True
+                    break
+    return fastq
+
+
+def chk_mem_fit(kw):
+    """Check for available memory"""
+    logger.info("Checking for available memory")
+    fastq = fastq_autoscan(kw["reads"])
+    rf_size = path.getsize(kw["reads"])
+    if fastq:
+        load_size = rf_size if kw["fq_out"] else rf_size/2
+    else:
+        load_size = rf_size
+    avail_mem = psutil.virtual_memory()[1]
+    if avail_mem / load_size > 4:
+        return True
+    else:
+        return False
+
+
+class BedColumnError(Exception):
+    """raised when number of columns in bed file is not matching"""
+
+    def __init__(self):
+        self.message = "Not all lines in primer bed have same number of" \
+                    " columns. File may be corrupt, please check integrity" \
+                    " of this file. Columns may not be empty. Take care that" \
+                    " headers or comments begin with any of the following:" \
+                    " 'track', 'browser', '#'."
+        super().__init__(self.message)
+
+
+def bed_scan(bed_file):
+    """Scan bed-file to get col-num"""
+    logger.debug("Scanning bed-file")
+    col_num = 6
+    headers = []
+    with open(bed_file, "r", encoding="utf-8") as bfl:
+        line_ct = 0
+        line_lens = []
+        for line in bfl:
+            line_ct += 1
+            lst = line.strip()
+            if lst.startswith("#"):
+                headers.append("#")
+            elif lst.startswith("browser"):
+                headers.append("browser")
+            elif lst.startswith("track"):
+                headers.append("track")
+            else:
+                if len(lst) > 0:
+                    lsp = lst.split("\t")
+                    line_lens.append(len(lsp))
+        len_set = set(line_lens)
+        if len(len_set) == 1:
+            col_num = list(len_set)[0]
+        else:
+            raise BedColumnError
+        if len(headers) == 0:
+            headers = False
+    return col_num, headers
+
+
+def bed_chk_header(line, headers):
+    """Check if line conforms to header type in headers list"""
+    head = False
+    for string in headers:
+        if line.startswith(string):
+            head = True
+    return head
