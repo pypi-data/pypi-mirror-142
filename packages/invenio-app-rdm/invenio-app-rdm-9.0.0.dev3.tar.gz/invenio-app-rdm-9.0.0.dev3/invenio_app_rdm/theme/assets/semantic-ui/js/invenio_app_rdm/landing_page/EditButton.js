1593cee2ee90f88234ef37e12b6b9fb2bfd869ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:39 +0000

    Add package invenio-app-rdm/9.0.0.dev3/invenio-app-rdm-9.0.0.dev3.tar.gz

---
+++ packages/invenio-app-rdm/invenio-app-rdm-9.0.0.dev3.tar.gz/invenio-app-rdm-9.0.0.dev3/invenio_app_rdm/theme/assets/semantic-ui/js/invenio_app_rdm/landing_page/EditButton.js
@@ -0,0 +1,42 @@
+// This file is part of InvenioRDM
+// Copyright (C) 2021 Northwestern University.
+// Copyright (C) 2021 Graz University of Technology.
+//
+// Invenio RDM Records is free software; you can redistribute it and/or modify it
+// under the terms of the MIT License; see LICENSE file for more details.
+
+import React, { useState } from "react";
+import { Icon, Button } from "semantic-ui-react";
+import { i18next } from "@translations/invenio_app_rdm/i18next";
+import { axiosWithconfig } from "../utils";
+
+export const EditButton = (props) => {
+  const [loading, setLoading] = useState(false);
+  const recid = props.recid;
+  const handleError = props.onError;
+  const handleClick = () => {
+    setLoading(true);
+    axiosWithconfig
+      .post(`/api/records/${recid}/draft`)
+      .then((response) => {
+        window.location = `/uploads/${recid}`;
+      })
+      .catch((error) => {
+        setLoading(false);
+        handleError(error.response.data.message);
+      });
+  };
+
+  return (
+    <Button
+      fluid
+      color="orange"
+      size="mini"
+      onClick={handleClick}
+      loading={loading}
+    >
+      <Icon name="edit" />
+      {i18next.t("Edit")}
+    </Button>
+  );
+};
---
+++ packages/invenio-app-rdm/invenio-app-rdm-9.0.0.dev3.tar.gz/invenio-app-rdm-9.0.0.dev3/invenio_app_rdm/theme/assets/semantic-ui/js/invenio_app_rdm/landing_page/ShareButton.js
@@ -0,0 +1,45 @@
+// This file is part of InvenioRDM
+// Copyright (C) 2021 CERN.
+// Copyright (C) 2021 Northwestern University.
+// Copyright (C) 2021 Graz University of Technology.
+//
+// Invenio RDM Records is free software; you can redistribute it and/or modify it
+// under the terms of the MIT License; see LICENSE file for more details.
+
+import React, { useState } from "react";
+import { Icon, Button, Popup } from "semantic-ui-react";
+import { ShareModal } from "./ShareModal";
+import { i18next } from "@translations/invenio_app_rdm/i18next";
+
+export const ShareButton = (props) => {
+  const [modalOpen, setModalOpen] = useState(false);
+  const handleOpen = () => setModalOpen(true);
+  const handleClose = () => setModalOpen(false);
+
+  return (
+    <>
+      <Popup
+        content={i18next.t("You don't have permissions to share this record.")}
+        disabled={!props.disabled}
+        trigger={
+          <Button
+            fluid
+            onClick={handleOpen}
+            disabled={props.disabled}
+            primary
+            size="mini"
+            aria-haspopup="dialog"
+          >
+            <Icon name="share square" />
+            {i18next.t("Share")}
+          </Button>
+        }
+      />
+      <ShareModal
+        open={modalOpen}
+        handleClose={handleClose}
+        recid={props.recid}
+      />
+    </>
+  );
+};
