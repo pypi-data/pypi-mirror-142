62b758accbeb97eef359b375967f094f361b3498
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:39 +0000

    Add package conductor-python/1.0.14/conductor_python-1.0.14-py3-none-any.whl

---
+++ packages/conductor-python/conductor_python-1.0.14-py3-none-any.whl/conductor/client/worker/sample/simple_python_worker.py
@@ -0,0 +1,9 @@
+from conductor.client.worker.worker_interface import WorkerInterface
+
+
+class SimplePythonWorker(WorkerInterface):
+    def execute(self, task):
+        task_result = self.get_task_result_from_task(task)
+        task_result.add_output_data('key', 'value')
+        task_result.status = 'COMPLETED'
+        return task_result
