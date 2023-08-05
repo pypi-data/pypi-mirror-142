437248f77474a572d6ab32bde35233f1612f15aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:31:11 +0000

    Add package NinjaTools/0.2.0/NinjaTools-0.2.0.tar.gz

---
+++ packages/NinjaTools/NinjaTools-0.2.0.tar.gz/NinjaTools-0.2.0/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="NinjaTools",
+    version="0.2.0",
+    author="Nikko Gonzales",
+    author_email="nikkoxgonzales@gmail.com",
+    description="Bunch of useful tools",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/nikkoxgonzales/ninja-tools",
+    install_requires=[
+        'pywin32>=303',
+        'numpy>=1.22.3',
+        'opencv-python==4.5.5.62'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: Microsoft :: Windows",
+    ],
+    packages=setuptools.find_packages(),
+    python_requires='>=3.9'
+)
