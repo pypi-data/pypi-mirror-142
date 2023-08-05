37272e6e8c3ca308c34215db604081a80228b4a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:25 +0000

    Add package lumo/0.1.13.9/lumo-0.1.13.9.tar.gz

---
+++ packages/lumo/lumo-0.1.13.9.tar.gz/lumo-0.1.13.9/setup.py
@@ -0,0 +1,42 @@
+from setuptools import setup, find_packages
+from lumo import __version__
+
+# from rename_templete import copy_templete
+
+# copy_templete()
+
+"""
+python3 setup.py sdist bdist_wheel; 
+python3 setup.py sdist bdist_wheel; pip3 install dist/$(python3 install.py);
+python3 setup.py sdist bdist_wheel; pip install dist/$(python3 install.py) --user
+python3 setup.py sdist bdist_wheel; pip install dist/$(python3 install.py) 
+python3 setup.py sdist bdist_wheel; pip3 install dist/$(python3 install.py) 
+sudo pip install dist/$(python3 install.py);
+pip install dist/$(python3 install.py) --user
+"""
+
+setup(
+    name='lumo',
+    version=__version__,
+    description='torch kit for programing your dl experiments code elegant.',
+    url='https://github.com/sailist/lumo',
+    author='sailist',
+    author_email='sailist@outlook.com',
+    license='Apache License 2.0',
+    include_package_data=True,
+    install_requires=[
+        'fire', 'psutil', 'joblib', 'accelerator'
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+    keywords='lumo',
+    packages=find_packages('.', exclude=('tests', 'lumo_frontend')),
+    entry_points={
+        'console_scripts': [
+            'lumo = lumo.cli.cli:main'
+        ]
+    },
+)
