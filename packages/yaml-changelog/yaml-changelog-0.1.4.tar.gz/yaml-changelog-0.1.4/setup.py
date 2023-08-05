173d88ee4a8822d6a9f6df5b4c38d009560a83a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:17:15 +0000

    Add package yaml-changelog/0.1.4/yaml-changelog-0.1.4.tar.gz

---
+++ packages/yaml-changelog/yaml-changelog-0.1.4.tar.gz/yaml-changelog-0.1.4/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="yaml-changelog",
+    version="0.1.4",
+    author="PolicyEngine",
+    license="http://www.fsf.org/licensing/licenses/agpl-3.0.html",
+    url="https://github.com/policyengine/policyengine",
+    install_requires=[
+        "argparse",
+        "datetime",
+        "pathlib",
+        "pyyaml",
+        "requests",
+        "black",
+        "autopep8",
+        "wheel",
+    ],
+    entry_points={
+        "console_scripts": [
+            "build-changelog = yaml_changelog.build:main",
+            "bump-version = yaml_changelog.bump:main",
+        ],
+    },
+    packages=find_packages(),
+)
