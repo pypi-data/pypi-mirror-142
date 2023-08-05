bccbf0cd066b2976cbb81e62f8fffd9b6ae1cd9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:31:24 +0000

    Add package pieqc/0.0.1/pieqc-0.0.1.tar.gz

---
+++ packages/pieqc/pieqc-0.0.1.tar.gz/pieqc-0.0.1/setup.py
@@ -0,0 +1,9 @@
+import setuptools
+
+setuptools.setup(
+        name="pieqc",
+        version="0.0.1",
+        author="fmalone",
+        description="test pie",
+        packages=["pieqc"],
+        )
