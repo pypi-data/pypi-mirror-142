cf58f4e295f053280c80ef5ff2d812e6dc8fe6a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:14 +0000

    Add package hestia-earth-models/0.24.7/hestia-earth-models-0.24.7.tar.gz

---
+++ packages/hestia-earth-models/hestia-earth-models-0.24.7.tar.gz/hestia-earth-models-0.24.7/hestia_earth/models/stehfestBouwman2006GisImplementation/noxToAirCropResidueDecomposition.py
@@ -0,0 +1,29 @@
+from hestia_earth.schema import EmissionMethodTier, EmissionStatsDefinition
+
+from hestia_earth.models.log import logRequirements
+from hestia_earth.models.utils.emission import _new_emission
+from .noxToAirSoilFlux import _should_run, _get_value
+from . import MODEL
+
+TERM_ID = 'noxToAirCropResidueDecomposition'
+TIER = EmissionMethodTier.TIER_1.value
+
+
+def _emission(value: float):
+    emission = _new_emission(TERM_ID, MODEL)
+    emission['value'] = [value]
+    emission['methodTier'] = TIER
+    emission['statsDefinition'] = EmissionStatsDefinition.MODELLED.value
+    return emission
+
+
+def _run(country_id: str, N_total: float, residue: float):
+    noxToAirSoilFlux = _get_value(country_id, N_total)
+    logRequirements(model=MODEL, term=TERM_ID,
+                    noxToAirSoilFlux=noxToAirSoilFlux)
+    return [_emission(residue * noxToAirSoilFlux / N_total)]
+
+
+def run(cycle: dict):
+    should_run, country_id, N_total, residue = _should_run(cycle, TERM_ID, TIER)
+    return _run(country_id, N_total, residue) if should_run else []
