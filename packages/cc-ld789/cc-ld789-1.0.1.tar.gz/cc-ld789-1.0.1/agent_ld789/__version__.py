e222ce48f1077ae626668e38598ff02d76d9290d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:09:34 +0000

    Add package cc-ld789/1.0.1/cc-ld789-1.0.1.tar.gz

---
+++ packages/cc-ld789/cc-ld789-1.0.1.tar.gz/cc-ld789-1.0.1/agent_ld789/__version__.py
@@ -0,0 +1,14 @@
+"""
+__version__.py
+~~~~~~~~~~~~~~
+
+Information about the current version of the py-package-template package.
+"""
+
+__title__ = 'cc-ld789'
+__description__ = 'cc-ld789 - ld789 Agent API'
+__version__ = '1.0.1'
+__author__ = 'Cockroach Creative'
+__author_email__ = 'support@cockreative.com'
+__license__ = 'Apache 2.0'
+__url__ = 'https://cockreative.com/'
