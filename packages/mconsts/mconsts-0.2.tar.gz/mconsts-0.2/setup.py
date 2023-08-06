94648c8c54d15487d6b7d75cedfa851102c1895b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:18:04 +0000

    Add package mconsts/0.2/mconsts-0.2.tar.gz

---
+++ packages/mconsts/mconsts-0.2.tar.gz/mconsts-0.2/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='mconsts',
+    version='0.2',
+    license='MIT',
+    description='mathematical constants',
+    author='github.com/qque',
+    url='https://github.com/qque/mconsts',
+    keywords=['math']
+)
