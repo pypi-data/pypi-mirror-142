c5244f42aa03c400aa3409d1d8ac976cdd4c40ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:36 +0000

    Add package pymediaroom/0.6.5.1/pymediaroom-0.6.5.1.tar.gz

---
+++ packages/pymediaroom/pymediaroom-0.6.5.1.tar.gz/pymediaroom-0.6.5.1/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+version=open("VERSION").read()
+long_description=open("README.rst").read()
+
+setup(name='pymediaroom',
+      version=version,
+      description='Remote control your Mediaroom Set-up-box',
+      long_description=long_description,
+      long_description_content_type='text/x-rst',
+      url='http://github.com/dgomes/pymediaroom',
+      author='Diogo Gomes',
+      author_email='diogogomes@gmail.com',
+      license='MIT',
+      packages=['pymediaroom'],
+      install_requires=[
+        'async_timeout>=4.0',
+        'xmltodict',
+      ],
+      python_requires='>=3.5',
+      download_url= 'https://github.com/dgomes/pymediaroom/tarball/0.6.4',
+      )
