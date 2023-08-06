5f9380119250de0cb54f64b0ffab52acc2f73f18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:13:52 +0000

    Add package basicmatrix/0.1/basicmatrix-0.1.tar.gz

---
+++ packages/basicmatrix/basicmatrix-0.1.tar.gz/basicmatrix-0.1/basicmatrix/__init__.py
@@ -0,0 +1,2 @@
+from .basicmatrix import BasicMatrix
+
---
+++ packages/basicmatrix/basicmatrix-0.1.tar.gz/basicmatrix-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='basicmatrix',
+      version='0.1',
+      description='Basic matrix arithmetic',
+      packages=['basicmatrix'],
+      author='Christiaan van Eeden',
+      author_email='christiaanveeden@gmail.com',
+      zip_safe=False)
+
+
