f252b61531e2d4583c271c71ec89227f1bfa1236
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:09:40 +0000

    Add package asynckivy/0.5.3/asynckivy-0.5.3-py3-none-any.whl

---
+++ packages/asynckivy/asynckivy-0.5.3-py3-none-any.whl/asynckivy/_animation.py
@@ -0,0 +1,152 @@
+__all__ = ('animate', )
+from functools import partial
+from kivy.clock import Clock
+from kivy.animation import AnimationTransition
+from asynckivy import sleep_forever
+
+
+async def animate(target, **kwargs):  # noqa:C901
+    '''
+    animate
+    =======
+
+    An async version of ``kivy.animation.Animation``.
+
+    Usage
+    -----
+
+    .. code-block:: python
+
+       import asynckivy as ak
+
+       async def some_async_func(widget):
+           # case #1: start an animation and wait for its completion
+           await ak.animate(widget, x=100, d=2, s=.2, t='in_cubic')
+
+       # case #2: start an animation but not wait for its completion
+       ak.start(ak.animate(widget, ...))
+
+    Difference from kivy.animation.Animation
+    ----------------------------------------
+
+    ``kivy.animation.Animation`` requires the object you wanna animate to
+    have an attribute named ``uid`` but ``asynckivy`` does not. When you have
+    an object like this:
+
+    .. code-block:: python
+
+       class MyClass: pass
+       obj = MyClass()
+       obj.value = 100
+
+    you already can animate it by ``asynckivy.animate(obj, value=200)``.
+    Therefore, ``asynckivy.animate()`` is more broadly applicable than
+    ``kivy.animation.Animation``.
+
+    Sequence and Parallel
+    ---------------------
+
+    Kivy has two compound animations: ``Sequence`` and ``Parallel``.
+    You can achieve the same functionality in asynckivy as follows:
+
+    .. code-block:: python
+
+       def kivy_Sequence(widget):
+           anim = Animation(x=100) + Animation(x=0)
+           anim.repeat = True
+           anim.start(widget)
+
+       async def asynckivy_Sequence(widget):
+           while True:
+               await ak.animate(widget, x=100)
+               await ak.animate(widget, x=0)
+
+       def kivy_Parallel(widget):
+           anim = Animation(x=100) & Animation(y=100, d=2)
+           anim.start(widget)
+           anim.bind(on_complete=lambda *args: print("completed"))
+
+       async def asynckivy_Parallel(widget):
+           await ak.and_(
+               ak.animate(widget, x=100),
+               ak.animate(widget, y=100, d=2),
+           )
+           print("completed")
+    '''
+    from asyncgui import get_step_coro
+    duration = kwargs.pop('d', kwargs.pop('duration', 1.))
+    transition = kwargs.pop('t', kwargs.pop('transition', 'linear'))
+    step = kwargs.pop('s', kwargs.pop('step', 0))
+    animated_properties = kwargs
+    if not duration:
+        for key, value in animated_properties.items():
+            setattr(target, key, value)
+        return
+    if isinstance(transition, str):
+        transition = getattr(AnimationTransition, transition)
+
+    # get current values
+    properties = {}
+    for key, value in animated_properties.items():
+        original_value = getattr(target, key)
+        if isinstance(original_value, (tuple, list)):
+            original_value = original_value[:]
+        elif isinstance(original_value, dict):
+            original_value = original_value.copy()
+        properties[key] = (original_value, value)
+
+    try:
+        ctx = {
+            'target': target,
+            'time': 0.,
+            'duration': duration,
+            'transition': transition,
+            'properties': properties,
+            'step_coro': await get_step_coro(),
+        }
+        clock_event = Clock.schedule_interval(partial(_update, ctx), step)
+        await sleep_forever()
+    finally:
+        clock_event.cancel()
+
+
+def _update(ctx, dt):
+    time = ctx['time'] + dt
+    ctx['time'] = time
+
+    # calculate progression
+    progress = min(1., time / ctx['duration'])
+    t = ctx['transition'](progress)
+
+    # apply progression on target
+    target = ctx['target']
+    for key, values in ctx['properties'].items():
+        a, b = values
+        value = _calculate(a, b, t)
+        setattr(target, key, value)
+
+    # time to stop ?
+    if progress >= 1.:
+        ctx['step_coro']()
+        return False
+
+
+def _calculate(a, b, t):
+    if isinstance(a, list) or isinstance(a, tuple):
+        if isinstance(a, list):
+            tp = list
+        else:
+            tp = tuple
+        return tp([_calculate(a[x], b[x], t) for x in range(len(a))])
+    elif isinstance(a, dict):
+        d = {}
+        for x in a:
+            if x not in b:
+                # User requested to animate only part of the dict.
+                # Copy the rest
+                d[x] = a[x]
+            else:
+                d[x] = _calculate(a[x], b[x], t)
+        return d
+    else:
+        return (a * (1. - t)) + (b * t)
---
+++ packages/asynckivy/asynckivy-0.5.3-py3-none-any.whl/asynckivy/_interpolate.py
@@ -0,0 +1,121 @@
+__all__ = ('interpolate', 'fade_transition', )
+from contextlib import asynccontextmanager
+import types
+from functools import partial
+from kivy.clock import Clock
+from kivy.animation import AnimationTransition
+
+
+async def interpolate(start, end, **kwargs):
+    '''
+    interpolate
+    ===========
+
+    Interpolates between the values ``start`` and ``end`` in an async-manner.
+    Inspired by wasabi2d's interpolate_.
+
+    Usage
+    -----
+
+    .. code-block:: python
+
+       async for v in asynckivy.interpolate(0, 100, d=1., s=.3, t='linear'):
+           print(int(v))
+
+    The code above prints as follows:
+
+    =========== =====
+    progress    print
+    =========== =====
+    0 sec       0
+    0.3 sec     30
+    0.6 sec     60
+    0.9 sec     90
+    **1.2 sec** 100
+    =========== =====
+
+    Keyword-arguments are the same as ``kivy.animation.Animation``'s.
+
+    .. _interpolate: https://wasabi2d.readthedocs.io/en/stable/coros.html#clock.coro.interpolate  # noqa: E501
+    '''
+    from asyncgui import get_step_coro
+    duration = kwargs.pop('d', kwargs.pop('duration', 1.))
+    transition = kwargs.pop('t', kwargs.pop('transition', 'linear'))
+    step = kwargs.pop('s', kwargs.pop('step', 0))
+    if isinstance(transition, str):
+        transition = getattr(AnimationTransition, transition)
+    if kwargs:
+        raise ValueError(f"unrecognizable keyword-arguments: {kwargs}")
+
+    yield start
+    if not duration:
+        yield end
+        return
+    try:
+        ctx = {
+            'start': start,
+            'end': end,
+            'time': 0.,
+            'duration': duration,
+            'transition': transition,
+            'step_coro': await get_step_coro(),
+        }
+        clock_event = Clock.schedule_interval(partial(_update, ctx), step)
+
+        get_current_value = _get_current_value
+        while True:
+            # TODO: refactor when drop python 3.7
+            value = await get_current_value()
+            if value is None:
+                break
+            else:
+                yield value
+    finally:
+        clock_event.cancel()
+
+
+def _update(ctx, dt):
+    time = ctx['time'] + dt
+    ctx['time'] = time
+
+    # calculate progression
+    progress = min(1., time / ctx['duration'])
+    t = ctx['transition'](progress)
+
+    value = (ctx['start'] * (1. - t)) + (ctx['end'] * t)
+    ctx['step_coro'](value)
+
+    # time to stop ?
+    if progress >= 1.:
+        ctx['step_coro'](None)
+        return False
+
+
+@types.coroutine
+def _get_current_value() -> int:
+    return (yield lambda step_coro: None)[0][0]
+
+
+@asynccontextmanager
+async def fade_transition(*widgets, **kwargs):
+    '''Fade-out/in the given widgets.
+
+    Available keyword-arguments are 'd', 'duration', 's' and 'step'.
+    '''
+    half_d = kwargs.pop('d', kwargs.pop('duration', 1.)) / 2.
+    s = kwargs.pop('s', kwargs.pop('step', 0))
+    if kwargs:
+        raise ValueError("surplus keyword-arguments were given:", kwargs)
+    original_opacities = tuple(w.opacity for w in widgets)
+    try:
+        async for v in interpolate(1.0, 0.0, d=half_d, s=s):
+            for w, o in zip(widgets, original_opacities):
+                w.opacity = v * o
+        yield
+        async for v in interpolate(0.0, 1.0, d=half_d, s=s):
+            for w, o in zip(widgets, original_opacities):
+                w.opacity = v * o
+    except GeneratorExit:
+        for w, o in zip(widgets, original_opacities):
+            w.opacity = o
+        raise
