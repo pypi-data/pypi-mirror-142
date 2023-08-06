cf923e456b95a86751cf503ac7a1b8291d2aa2b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:16:39 +0000

    Add package th2-data-services/1.0.0.dev1993893421/th2_data_services-1.0.0.dev1993893421.tar.gz

---
+++ packages/th2-data-services/th2_data_services-1.0.0.dev1993893421.tar.gz/th2_data_services-1.0.0.dev1993893421/th2_data_services/provider/v5/command_resolver.py
@@ -0,0 +1,32 @@
+from typing import Union
+
+from th2_data_services.provider.v5.commands.grpc import GetEventsById as GetEventsByIdFromGRPC
+from th2_data_services.provider.v5.commands.http import GetEventsById as GetEventsByIdFromHTTP
+
+from th2_data_services.provider.v5.commands.grpc import GetEventById as GetEventByIdFromGRPC
+from th2_data_services.provider.v5.commands.http import GetEventById as GetEventByIdFromHTTP
+
+from th2_data_services.provider.v5.data_source.grpc import GRPCProvider5DataSource
+from th2_data_services.provider.v5.data_source.http import HTTPProvider5DataSource
+
+
+def resolver_get_event_by_id(
+    data_source: Union[GRPCProvider5DataSource, HTTPProvider5DataSource]
+) -> Union[GetEventByIdFromGRPC, GetEventByIdFromHTTP]:
+    if isinstance(data_source, GRPCProvider5DataSource):
+        return GetEventByIdFromGRPC
+    elif isinstance(data_source, HTTPProvider5DataSource):
+        return GetEventByIdFromHTTP
+    else:
+        ValueError("Unknown DataSource Object.")
+
+
+def resolver_get_events_by_id(
+    data_source: Union[GRPCProvider5DataSource, HTTPProvider5DataSource]
+) -> Union[GetEventsByIdFromHTTP, GetEventsByIdFromGRPC]:
+    if isinstance(data_source, GRPCProvider5DataSource):
+        return GetEventsByIdFromGRPC
+    elif isinstance(data_source, HTTPProvider5DataSource):
+        return GetEventsByIdFromHTTP
+    else:
+        raise ValueError("Unknown DataSource Object.")
