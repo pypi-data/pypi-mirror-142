fe8783f95f242ed9fe8a045fa877a47d7d1630bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:35:11 +0000

    Add package confite/1.0.2/confite-1.0.2-py3-none-any.whl

---
+++ packages/confite/confite-1.0.2-py3-none-any.whl/confite/output_formatter.py
@@ -0,0 +1,11 @@
+def format_multiline_string(
+        text: str,
+        break_line_character: str):
+    lines_list: list[str] = text.split(
+        break_line_character
+    )
+    multiline_text: str
+    if len(lines_list) > 0:
+        for index in range(len(lines_list) - 1):
+            lines_list[index] += '\n'
+    return lines_list
