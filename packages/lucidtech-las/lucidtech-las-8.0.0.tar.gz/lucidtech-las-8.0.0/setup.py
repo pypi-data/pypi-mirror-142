0bcba36e42540256fccafe51a7512742e0fc5a18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:25:28 +0000

    Add package lucidtech-las/8.0.0/lucidtech-las-8.0.0.tar.gz

---
+++ packages/lucidtech-las/lucidtech-las-8.0.0.tar.gz/lucidtech-las-8.0.0/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup
+
+with open('requirements.txt') as fp:
+    install_requires = fp.readlines()
+
+
+with open('README.md') as fp:
+    readme = fp.read()
+
+
+setup(
+    name='lucidtech-las',
+    version='8.0.0',
+    description='Python SDK for Lucidtech AI Services',
+    long_description=readme,
+    long_description_content_type='text/markdown',
+    license='Apache 2.0',
+    platforms='Posix; MacOS X; Windows',
+    author='Lucidtech',
+    maintainer='August Kvernmo',
+    maintainer_email='august@lucidtech.ai',
+    url='https://github.com/LucidtechAI/las-sdk-python',
+    packages=['las'],
+    install_requires=install_requires,
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Topic :: Internet'
+    ]
+)
