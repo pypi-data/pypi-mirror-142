77c46c95581b5b7c1eb71de066451a30d1b814fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:17:24 +0000

    Add package utsavpc/0.2/utsavpc-0.2.tar.gz

---
+++ packages/utsavpc/utsavpc-0.2.tar.gz/utsavpc-0.2/setup.py
@@ -0,0 +1,10 @@
+
+from setuptools import setup
+setup(name = "utsavpc",
+version = "0.2",
+description = "This is Utsav's First package",
+long_description = " This is a package whcih sums the value of two numbers.",
+author = "Utsav Chabhadiya",
+packages = ['utsavpc'],
+install_requires = []
+)
