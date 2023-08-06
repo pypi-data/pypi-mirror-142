5e4409fd80236d6bca11ca91d4196945aa9706a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:19:26 +0000

    Add package git-search-command/1.0.1/git_search_command-1.0.1-py3-none-any.whl

---
+++ packages/git-search-command/git_search_command-1.0.1-py3-none-any.whl/gsc/command_line/gitlab_cli.py
@@ -0,0 +1,217 @@
+import click
+from gsc.utils import is_valid_environment_name
+from gsc.command_line import keep_main_thread_running
+from gsc.command_line.print_observer import (
+    ConsoleGroupResultObserver,
+    ConsoleProjectResultObserver,
+)
+from gsc.config import Env, EnvConfig, GitlabConfig
+from gsc.use_cases.gitlab_search_proj_use_case import GitLabSearchProjUseCase
+from gsc.use_cases.gitlab_search_group_use_case import GitLabSearchGroupUseCase
+
+
+@click.group("gl", help="Search in GitLab repositories.")
+def gitlab_cli():
+    pass
+
+
+@gitlab_cli.command(
+    "env", help="Setup the environment for searching, support multiple environments."
+)
+@click.option(
+    "-n",
+    "--new",
+    type=str,
+    metavar="<environment>",
+    help="Create new or override the env information if it was existed.",
+)
+@click.option(
+    "-d",
+    "--default",
+    type=str,
+    metavar="<environment>",
+    help="Set the environment as default.",
+)
+@click.option(
+    "-r",
+    "--remove",
+    type=str,
+    metavar="<environment>",
+    help="Remove the environment.",
+)
+@click.option(
+    "-i",
+    "--info",
+    type=str,
+    metavar="<environment>",
+    help="Display the environment info.",
+)
+@click.option(
+    "-l",
+    "--list",
+    "show_list",
+    is_flag=True,
+    default=False,
+    help="List all environment.",
+)
+@click.pass_context
+def environment(ctx, **kwargs):
+    config = GitlabConfig()
+    if kwargs.get("show_list"):
+        __show_list_envs(config)
+    elif kwargs.get("new"):
+        __create_new_environment(config, kwargs.get("new"))
+    elif kwargs.get("default"):
+        __set_default_environment(config, kwargs.get("default"))
+    elif kwargs.get("remove"):
+        __remove_environment(config, kwargs.get("remove"))
+    elif kwargs.get("info"):
+        __show_detail_environment(config, kwargs.get("info"))
+    else:
+        click.secho(environment.get_help(ctx))
+
+
+@gitlab_cli.command("search", help="Search the content in GitLab repositories.")
+@click.argument("keyword", type=str, metavar="<keyword>")
+@click.option(
+    "-g",
+    "--group",
+    type=str,
+    metavar="<string>",
+    help="Search in the specified project group, input group id or group path.",
+)
+@click.option(
+    "-p",
+    "--project",
+    type=int,
+    metavar="<int>",
+    help="Search in the specified project, input project id.",
+)
+@click.option(
+    "-e",
+    "--environment",
+    "env",
+    type=str,
+    metavar="<string>",
+    help="Select the environment for searching, if not declare, default environment has been used.",
+)
+@click.option(
+    "-v",
+    "show_preview",
+    is_flag=True,
+    default=False,
+    help="Show result preview, available only when searching project, not in group.",
+)
+@click.option(
+    "-o",
+    "--output",
+    type=str,
+    metavar="<file_path>",
+    help="Export the search result to file, only support text file.",
+)
+@click.pass_context
+def search(ctx, **kwargs):
+    config = GitlabConfig()
+    config.set_session_env("")
+    if not config.get_default_env():
+        click.secho("There is no environment.")
+        click.secho("Try `gsc gl env --new <environment_name>` before searching.")
+        return
+
+    if kwargs.get("keyword"):
+        keyword = kwargs.get("keyword")
+        env = kwargs.get("env")
+        output = kwargs.get("output")
+        if env:
+            config.set_session_env(env)
+
+        click.clear()
+        if kwargs.get("project"):
+            project = kwargs.get("project")
+            show_preview = kwargs.get("show_preview")
+            __search_in_project(keyword, project, show_preview, output)
+        elif kwargs.get("group"):
+            group = kwargs.get("group")
+            __search_in_group(keyword, group, output)
+    else:
+        click.secho(search.get_help(ctx))
+
+
+@keep_main_thread_running
+def __search_in_group(keyword: str, group_name: str, output_path: str):
+    usecase = GitLabSearchGroupUseCase()
+    usecase.on_searching().subscribe(
+        ConsoleGroupResultObserver(
+            group=group_name, keyword=keyword, output_path=output_path
+        )
+    )
+    usecase.search(group_name, keyword)
+
+
+@keep_main_thread_running
+def __search_in_project(
+    keyword: str, project_id: int, show_preview: bool, output_path: str
+):
+    usecase = GitLabSearchProjUseCase()
+    usecase.on_searching().subscribe(
+        ConsoleProjectResultObserver(
+            id=project_id,
+            keyword=keyword,
+            preview=show_preview,
+            output_path=output_path,
+        )
+    )
+    usecase.search(project_id, keyword)
+
+
+def __show_list_envs(config: EnvConfig):
+    envs = config.get_all_envs()
+    if not envs:
+        click.secho("There is no environment.")
+        return
+
+    for env in envs:
+        name = env.name
+        if config.is_default_env(name):
+            click.secho(f"* {name}")
+        else:
+            click.secho(f"  {name}")
+
+
+def __create_new_environment(config: EnvConfig, name: str):
+    if not is_valid_environment_name(name):
+        click.secho(
+            "Environment name only contains letters, numbers, underscores, dashes..., not space"
+        )
+        return
+
+    new_env = Env(name=name)
+
+    host = click.prompt(f'Please input host name for "{name}" environment ', type=str)
+    if host:
+        new_env.host_name = host
+
+    token = click.prompt(f'Please input token for "{name}" environment ', type=str)
+    if token:
+        new_env.private_token = token
+
+    config.set_env(new_env)
+
+
+def __remove_environment(config: EnvConfig, env_name: str):
+    config.remove_env(env_name)
+
+
+def __show_detail_environment(config: EnvConfig, name: str):
+    if not config.is_env_existed(name):
+        click.secho(f'"{name}" environment is not found.')
+        return
+
+    environ = config.get_env(name)
+    click.secho(f'"{name}" environment :')
+    click.secho(f"Host name : {environ.host_name}")
+    click.secho(f"Private token : {environ.private_token}")
+
+
+def __set_default_environment(config: EnvConfig, env_name: str):
+    config.set_default_env(env_name)
---
+++ packages/git-search-command/git_search_command-1.0.1-py3-none-any.whl/gsc/command_line/print_observer.py
@@ -0,0 +1,158 @@
+from typing import Any, Optional
+import click
+from rx.core import typing, Observer
+from gsc.entities.gitlab_model import Project, FileName
+from gsc.command_line import finish_main_thread
+
+
+class PrintObserver(Observer):
+    def __init__(
+        self,
+        on_next: Optional[typing.OnNext] = None,
+        on_error: Optional[typing.OnError] = None,
+        on_completed: Optional[typing.OnCompleted] = None,
+        **kwargs,
+    ) -> None:
+        super().__init__(on_next, on_error, on_completed)
+        self._keyword = kwargs.get("keyword")
+        self._output_path = kwargs.get("output_path")
+        self._export_file = (
+            click.open_file(self._output_path, "w") if self._output_path else None
+        )
+        self._project = None
+        self._on_print_start()
+
+    def _on_print_start(self) -> None:
+        pass
+
+    def _on_print_project(self, project: Project) -> None:
+        pass
+
+    def _on_print_file(self, file: FileName) -> None:
+        pass
+
+    def _on_print_end(self) -> None:
+        pass
+
+    def _on_print_error(self, error: Exception) -> None:
+        pass
+
+    def _echo(self, msg, **styles):
+        click.secho(msg, **styles)
+        if self._export_file:
+            self._export_file.write(msg + "\n")
+
+    def on_next(self, value: Any) -> None:
+        if isinstance(value, Project):
+            # Save project object to print it later
+            self._project = value
+        elif isinstance(value, FileName):
+            # Ensure that searching is not empty
+            if self._project is not None:
+                self._on_print_project(self._project)
+                self._project = None
+            self._on_print_file(value)
+        else:
+            pass
+
+    def on_completed(self) -> None:
+        self._on_print_end()
+        if self._export_file:
+            self._export_file.close()
+            self._export_file = None
+
+    def on_error(self, error: Exception) -> None:
+        self._on_print_error(error)
+        if self._export_file:
+            self._export_file.close()
+            self._export_file = None
+
+    def dispose(self) -> None:
+        self._keyword = None
+        return super().dispose()
+
+
+class ConsoleProjectResultObserver(PrintObserver):
+    def __init__(
+        self,
+        on_next: Optional[typing.OnNext] = None,
+        on_error: Optional[typing.OnError] = None,
+        on_completed: Optional[typing.OnCompleted] = None,
+        **kwargs,
+    ) -> None:
+        self._project_id = kwargs.get("id")
+        self._show_code_preview = kwargs.get("preview")
+        self._file_count = 0
+        super().__init__(on_next, on_error, on_completed, **kwargs)
+
+    def _on_print_start(self) -> None:
+        self._echo(
+            f'Searching for "{self._keyword}" in project with id "{self._project_id}" ...'
+        )
+
+    def _on_print_project(self, project: Project) -> None:
+        self._file_count = 0
+        self._echo("*************************")
+        self._echo(f"[{project.id}] {project.name}", fg="bright_magenta")
+
+    def _on_print_file(self, file: FileName) -> None:
+        self._file_count += 1
+        self._echo(f"{file.path}")
+        if self._show_code_preview:
+            self._echo(f"{file.data_preview}", dim=True)
+
+    @finish_main_thread
+    def _on_print_end(self) -> None:
+        if self._file_count == 0:
+            self._echo(f'There is NO file containing "{self._keyword}".')
+            return
+
+        self._echo("*************************")
+        self._echo(
+            f'There are {self._file_count} file(s) containing "{self._keyword}".'
+        )
+        self._file_count = 0
+
+    @finish_main_thread
+    def _on_print_error(self, error: Exception) -> None:
+        self._echo(f"[Error] {error}", fg="bright_red")
+
+    def dispose(self) -> None:
+        self._file_count = 0
+        return super().dispose()
+
+
+class ConsoleGroupResultObserver(ConsoleProjectResultObserver):
+    def __init__(
+        self,
+        on_next: Optional[typing.OnNext] = None,
+        on_error: Optional[typing.OnError] = None,
+        on_completed: Optional[typing.OnCompleted] = None,
+        **kwargs,
+    ) -> None:
+        self._group_name = kwargs.get("group")
+        self._project_count = 0
+        super().__init__(on_next, on_error, on_completed, **kwargs)
+
+    def _on_print_start(self) -> None:
+        self._echo(f'Searching for "{self._keyword}" in "{self._group_name}" group ...')
+
+    def _on_print_project(self, project: Project) -> None:
+        self._project_count += 1
+        return super()._on_print_project(project)
+
+    @finish_main_thread
+    def _on_print_end(self) -> None:
+        if self._project_count == 0:
+            self._echo(f'There is NO project containing "{self._keyword}".')
+            return
+
+        self._echo("*************************")
+        self._echo(
+            f'There are {self._project_count} project(s) containing "{self._keyword}".'
+        )
+        self._project_count = 0
+
+    def dispose(self) -> None:
+        self._project_count = 0
+        return super().dispose()
