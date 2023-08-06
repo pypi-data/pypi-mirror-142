e3d534301340ead31bbd6a4eb04748f914beec65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:16:10 +0000

    Add package protectonce-native/0.1.18/protectonce_native-0.1.18.tar.gz

---
+++ packages/protectonce-native/protectonce_native-0.1.18.tar.gz/protectonce_native-0.1.18/protectonce_native/core/lib/reports/report.js
@@ -0,0 +1,11 @@
+const ReportType = {
+  REPORT_TYPE_REPORT: 'report',
+  REPORT_TYPE_ALERT: 'alert',
+  REPORT_TYPE_BLOCK: 'block',
+  REPORT_TYPE_ABORT: 'abort',
+  REPORT_TYPE_NONE: 'none'
+};
+
+module.exports = {
+  ReportType
+};
