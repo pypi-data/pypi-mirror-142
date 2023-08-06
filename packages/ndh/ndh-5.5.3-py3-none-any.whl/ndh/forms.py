6cd8e898466cc62e969fbdb8f1145c30bcfbc226
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:09:56 +0000

    Add package ndh/5.5.3/ndh-5.5.3-py3-none-any.whl

---
+++ packages/ndh/ndh-5.5.3-py3-none-any.whl/ndh/forms.py
@@ -0,0 +1,8 @@
+"""General Form-related helpers for NDH."""
+from django import forms
+
+
+class AccessibleDateTimeField(forms.SplitDateTimeField):
+    """Set date & time HTML types."""
+
+    widget = forms.SplitDateTimeWidget(date_attrs={"type": "date"}, time_attrs={"type": "time"})  # type: ignore
