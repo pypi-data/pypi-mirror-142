cd0866a4e21ba537df0e7f9a373f85351a10fc0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:36:52 +0000

    Add package d365fw/0.8.0/d365fw-0.8.0.tar.gz

---
+++ packages/d365fw/d365fw-0.8.0.tar.gz/d365fw-0.8.0/D365API/Constant.py
@@ -0,0 +1,14 @@
+"""
+D365API.Constant
+~~~~~~~~~~~~~~~~
+"""
+
+D365_API_V = '9.2'
+
+TEST_FILE = 'TestData.json'
+
+HTTP_GET = 'GET'
+HTTP_POST = 'POST'
+HTTP_PUT = 'PUT'
+HTTP_DELETE = 'DELETE'
+HTTP_PATCH = 'PATCH'
