d01728f0fa89bad9764f7d5f42afdea3dc22bad0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:17:25 +0000

    Add package utsavpc/1.0/utsavpc-1.0.tar.gz

---
+++ packages/utsavpc/utsavpc-1.0.tar.gz/utsavpc-1.0/setup.py
@@ -0,0 +1,10 @@
+
+from setuptools import setup
+setup(name = "utsavpc",
+version = "1.0",
+description = "SUM/SUB/NUM/DIV",
+long_description = " This is a package whcih sums the value of two numbers.",
+author = "Utsav Chabhadiya",
+packages = ['utsavpc'],
+install_requires = []
+)
