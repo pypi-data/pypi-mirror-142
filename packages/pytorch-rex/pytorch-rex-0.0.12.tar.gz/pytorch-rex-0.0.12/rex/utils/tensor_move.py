33135f4e530ce0eda4c0045ff0e79e193e282f80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:17:49 +0000

    Add package pytorch-rex/0.0.12/pytorch-rex-0.0.12.tar.gz

---
+++ packages/pytorch-rex/pytorch-rex-0.0.12.tar.gz/pytorch-rex-0.0.12/rex/utils/tensor_move.py
@@ -0,0 +1,14 @@
+from typing import Optional, Union
+
+import torch
+
+
+def move_to_cuda_device(
+    obj: Union[torch.Tensor, dict], device: Optional[torch.device] = None
+):
+    if isinstance(obj, torch.Tensor):
+        obj = obj.cuda(device)
+    elif isinstance(obj, dict):
+        for key in obj:
+            obj[key] = move_to_cuda_device(obj[key], device)
+    return obj
