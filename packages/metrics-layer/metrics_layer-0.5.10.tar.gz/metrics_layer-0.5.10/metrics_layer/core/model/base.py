3c477a334f93735ce22341e4f451554853a8f312
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:11:20 +0000

    Add package metrics-layer/0.5.10/metrics_layer-0.5.10.tar.gz

---
+++ packages/metrics-layer/metrics_layer-0.5.10.tar.gz/metrics_layer-0.5.10/metrics_layer/core/model/base.py
@@ -0,0 +1,43 @@
+import re
+
+
+class AccessDeniedOrDoesNotExistException(Exception):
+    def __init__(self, message: str, object_name: str, object_type: str):
+        self.message = message
+        self.object_name = object_name
+        self.object_type = object_type
+
+    def __str__(self):
+        return self.message
+
+
+class MetricsLayerBase:
+    def __init__(self, definition: dict = {}) -> None:
+        self._definition = definition
+
+    def __getattr__(self, attr: str):
+        return self._definition.get(attr, None)
+
+    def __repr__(self):
+        return f"<{self.__class__.__name__} name={self.name}>"
+
+    def to_dict(self):
+        return self._definition
+
+    @staticmethod
+    def field_name_parts(field_name: str):
+        explore_name, view_name = None, None
+        if field_name.count(".") == 2:
+            explore_name, view_name, name = field_name.split(".")
+        elif field_name.count(".") == 1:
+            view_name, name = field_name.split(".")
+        else:
+            name = field_name
+        return explore_name, view_name, name
+
+
+class SQLReplacement:
+    @staticmethod
+    def fields_to_replace(text: str):
+        matches = re.finditer(r"\$\{(.*?)\}", text, re.MULTILINE)
+        return [match.group(1) for match in matches]
