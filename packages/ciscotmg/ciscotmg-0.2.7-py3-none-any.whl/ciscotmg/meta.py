78fcca8cbcf81f5fdf8c41af6e1023c833c774e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:10:12 +0000

    Add package ciscotmg/0.2.7/ciscotmg-0.2.7-py3-none-any.whl

---
+++ packages/ciscotmg/ciscotmg-0.2.7-py3-none-any.whl/ciscotmg/meta.py
@@ -0,0 +1,12 @@
+class Meta:
+    """App Information"""
+
+    __title__ = "ciscotmg"
+    __description__ = "Private Python SDK for Cisco TMG APIs"
+    __url__ = "https://github.com/robertcsapo/ciscotmg"
+    __download_url__ = "https://pypi.python.org/pypi/ciscotmg"
+    __author__ = "Robert Csapo"
+    __email__ = "rcsapo@cisco.com"
+    __copyright__ = "Copyright (c) 2022 Cisco Systems, Inc."
+    __license__ = "MIT"
+    __version__ = "0.2.7"
