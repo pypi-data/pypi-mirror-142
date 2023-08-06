f4c417cd30428f0c2a72408e8d30239c12c61829
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:42 +0000

    Add package DuIvyTools/0.3.3/DuIvyTools-0.3.3-py3-none-any.whl

---
+++ packages/DuIvyTools/DuIvyTools-0.3.3-py3-none-any.whl/DuIvyTools/__init__.py
@@ -0,0 +1,14 @@
+"""``DuIvyTools`` package for analysis of GMX MD results files. Written by Charles Hahn.
+
+Python modules
+----------------
+The package consists of the following Python modules:
+* DIT : the CLI application.
+* XPM : module to process XPM files.
+* XVG : module to process XVG files.
+* NDX : module to process NDX files.
+* MDP : module to generate MDP files.
+* HELP : module to provide help messages.
+
+"""
+
