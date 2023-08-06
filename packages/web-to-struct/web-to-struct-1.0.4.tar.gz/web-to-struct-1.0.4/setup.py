75cbe916ded4e075ad76185c5585757aee4a38b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:18:21 +0000

    Add package web-to-struct/1.0.4/web-to-struct-1.0.4.tar.gz

---
+++ packages/web-to-struct/web-to-struct-1.0.4.tar.gz/web-to-struct-1.0.4/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+import pathlib
+
+here = pathlib.Path(__file__).parent.resolve()
+long_description = (here / 'README.md').read_text(encoding='utf-8')
+
+setup(
+    name="web-to-struct",
+    version="1.0.4",
+    description="A tool for data structuring, mainly for web data.",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url="https://github.com/BD777/web-to-struct",
+    author="BD777",
+    author_email="mis_tletoe@foxmail.com",
+    license="MIT",
+    packages=find_packages(),
+    python_requires=">=3.6, <4",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.8",
+    ],
+    install_requires=[
+        "bs4",
+    ]
+)
