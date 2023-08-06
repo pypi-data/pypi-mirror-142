56fbd0fb50cbeceb868e5946745cf17580667a00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:39:05 +0000

    Add package wombopy/0.1.2.2/wombopy-0.1.2.2.tar.gz

---
+++ packages/wombopy/wombopy-0.1.2.2.tar.gz/wombopy-0.1.2.2/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="wombopy",
+    version="0.1.2.2",
+    description="A module to generate wombo.art images",
+    author="Holy Tanuki",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    install_requires=[
+        "typer==0.4.0",
+        "colorama==0.4.4",
+        "shellingham==1.4.0",
+        "pytest==7.1.0",
+        "requests==2.27.1",
+        "Pillow==9.0.1",
+    ],
+    package_dir={"": "wombopy"},
+    packages=setuptools.find_packages(where="wombopy"),
+    python_requires=">=3.9",
+)
