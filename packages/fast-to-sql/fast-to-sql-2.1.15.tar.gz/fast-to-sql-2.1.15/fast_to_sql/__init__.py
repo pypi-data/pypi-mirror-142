f98903ceb5ffedb0501842ad96beb3a02c4c5763
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:12:04 +0000

    Add package fast-to-sql/2.1.15/fast-to-sql-2.1.15.tar.gz

---
+++ packages/fast-to-sql/fast-to-sql-2.1.15.tar.gz/fast-to-sql-2.1.15/fast_to_sql/__init__.py
@@ -0,0 +1,19 @@
+__project__ = "fast-to-sql"
+__version__ = "2.1.15"
+__keywords__ = ["pandas", "to_sql", "fast", "sql"]
+__author__ = "Jarred Glaser"
+__author_email__ = "jarred.glaser@gmail.com"
+__url__ = "https://github.com/jdglaser/fast-to-sql"
+__platforms__ = "ALL"
+__requires__=['pandas', 'pyodbc']
+__doc__="An improved way to upload pandas dataframes to Microsoft SQL Server."
+
+__classifiers__ = [
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Developers',
+    'Programming Language :: Python :: 3.8',
+    'License :: OSI Approved :: MIT License'
+]
+
+__extra_requires__ = {
+}
