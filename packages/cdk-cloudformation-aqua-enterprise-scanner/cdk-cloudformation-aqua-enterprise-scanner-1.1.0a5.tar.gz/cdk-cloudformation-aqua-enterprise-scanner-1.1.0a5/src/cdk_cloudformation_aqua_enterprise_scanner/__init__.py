dc716603883f65e77ac7e63f90543c3b6ede705d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:19:38 +0000

    Add package cdk-cloudformation-aqua-enterprise-scanner/1.1.0a5/cdk-cloudformation-aqua-enterprise-scanner-1.1.0a5.tar.gz

---
+++ packages/cdk-cloudformation-aqua-enterprise-scanner/cdk-cloudformation-aqua-enterprise-scanner-1.1.0a5.tar.gz/cdk-cloudformation-aqua-enterprise-scanner-1.1.0a5/src/cdk_cloudformation_aqua_enterprise_scanner/__init__.py
@@ -0,0 +1,396 @@
+'''
+# aqua-enterprise-scanner
+
+> AWS CDK [L1 construct](https://docs.aws.amazon.com/cdk/latest/guide/constructs.html) and data structures for the [AWS CloudFormation Registry](https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/registry.html) type `Aqua::Enterprise::Scanner` v1.1.0.
+
+## Description
+
+A resource provider for Aqua Enterprise Scanner.
+
+## References
+
+* [Source](https://github.com/aquasecurity/aqua-helm.git)
+
+## Usage
+
+In order to use this library, you will need to activate this AWS CloudFormation Registry type in your account. You can do this via the AWS Management Console or using the [AWS CLI](https://aws.amazon.com/cli/) using the following command:
+
+```sh
+aws cloudformation activate-type \
+  --type-name Aqua::Enterprise::Scanner \
+  --publisher-id 4f06bc39af5f4b984dd46ad17f10316e6258d9fa \
+  --type RESOURCE \
+  --execution-role-arn ROLE-ARN
+```
+
+Alternatively:
+
+```sh
+aws cloudformation activate-type \
+  --public-type-arn arn:aws:cloudformation:us-east-1::type/resource/4f06bc39af5f4b984dd46ad17f10316e6258d9fa/Aqua-Enterprise-Scanner \
+  --execution-role-arn ROLE-ARN
+```
+
+You can find more information about activating this type in the [AWS CloudFormation documentation](https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/registry-public.html).
+
+## Feedback
+
+This library is auto-generated and published to all supported programming languages by the [cdklabs/cdk-cloudformation](https://github.com/cdklabs/cdk-cloudformation) project based on the API schema published for `Aqua::Enterprise::Scanner`.
+
+* Issues related to this generated library should be [reported here](https://github.com/cdklabs/cdk-cloudformation/issues/new?title=Issue+with+%40cdk-cloudformation%2Faqua-enterprise-scanner+v1.1.0).
+* Issues related to `Aqua::Enterprise::Scanner` should be reported to the [publisher](https://github.com/aquasecurity/aqua-helm.git).
+
+## License
+
+Distributed under the Apache-2.0 License.
+'''
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import publication
+import typing_extensions
+
+from ._jsii import *
+
+import aws_cdk
+import constructs
+
+
+class CfnScanner(
+    aws_cdk.CfnResource,
+    metaclass=jsii.JSIIMeta,
+    jsii_type="@cdk-cloudformation/aqua-enterprise-scanner.CfnScanner",
+):
+    '''A CloudFormation ``Aqua::Enterprise::Scanner``.
+
+    :cloudformationResource: Aqua::Enterprise::Scanner
+    :link: https://github.com/aquasecurity/aqua-helm.git
+    '''
+
+    def __init__(
+        self,
+        scope: constructs.Construct,
+        id: builtins.str,
+        *,
+        cluster_id: typing.Optional[builtins.str] = None,
+        kube_config: typing.Optional[builtins.str] = None,
+        name: typing.Optional[builtins.str] = None,
+        namespace: typing.Optional[builtins.str] = None,
+        role_arn: typing.Optional[builtins.str] = None,
+        time_out: typing.Optional[jsii.Number] = None,
+        value_override_url: typing.Optional[builtins.str] = None,
+        values: typing.Any = None,
+        value_yaml: typing.Optional[builtins.str] = None,
+        version: typing.Optional[builtins.str] = None,
+        vpc_configuration: typing.Optional["CfnScannerPropsVpcConfiguration"] = None,
+    ) -> None:
+        '''Create a new ``Aqua::Enterprise::Scanner``.
+
+        :param scope: - scope in which this resource is defined.
+        :param id: - scoped id of the resource.
+        :param cluster_id: EKS cluster name.
+        :param kube_config: Secrets Manager ARN for kubeconfig file.
+        :param name: Name for the helm release.
+        :param namespace: Namespace to use with helm. Created if doesn't exist and default will be used if not provided
+        :param role_arn: IAM to use with EKS cluster authentication, if not resource execution role will be used.
+        :param time_out: Timeout for resource provider. Default 60 mins
+        :param value_override_url: Custom Value Yaml file can optionally be specified.
+        :param values: Custom Values can optionally be specified.
+        :param value_yaml: String representation of a values.yaml file.
+        :param version: Version can be specified, if not latest will be used.
+        :param vpc_configuration: For network connectivity to Cluster inside VPC.
+        '''
+        props = CfnScannerProps(
+            cluster_id=cluster_id,
+            kube_config=kube_config,
+            name=name,
+            namespace=namespace,
+            role_arn=role_arn,
+            time_out=time_out,
+            value_override_url=value_override_url,
+            values=values,
+            value_yaml=value_yaml,
+            version=version,
+            vpc_configuration=vpc_configuration,
+        )
+
+        jsii.create(self.__class__, self, [scope, id, props])
+
+    @jsii.python.classproperty # type: ignore[misc]
+    @jsii.member(jsii_name="CFN_RESOURCE_TYPE_NAME")
+    def CFN_RESOURCE_TYPE_NAME(cls) -> builtins.str:
+        '''The CloudFormation resource type name for this resource class.'''
+        return typing.cast(builtins.str, jsii.sget(cls, "CFN_RESOURCE_TYPE_NAME"))
+
+    @builtins.property # type: ignore[misc]
+    @jsii.member(jsii_name="attrId")
+    def attr_id(self) -> builtins.str:
+        '''Attribute ``Aqua::Enterprise::Scanner.ID``.
+
+        :link: https://github.com/aquasecurity/aqua-helm.git
+        '''
+        return typing.cast(builtins.str, jsii.get(self, "attrId"))
+
+    @builtins.property # type: ignore[misc]
+    @jsii.member(jsii_name="props")
+    def props(self) -> "CfnScannerProps":
+        '''Resource props.'''
+        return typing.cast("CfnScannerProps", jsii.get(self, "props"))
+
+
+@jsii.data_type(
+    jsii_type="@cdk-cloudformation/aqua-enterprise-scanner.CfnScannerProps",
+    jsii_struct_bases=[],
+    name_mapping={
+        "cluster_id": "clusterId",
+        "kube_config": "kubeConfig",
+        "name": "name",
+        "namespace": "namespace",
+        "role_arn": "roleArn",
+        "time_out": "timeOut",
+        "value_override_url": "valueOverrideUrl",
+        "values": "values",
+        "value_yaml": "valueYaml",
+        "version": "version",
+        "vpc_configuration": "vpcConfiguration",
+    },
+)
+class CfnScannerProps:
+    def __init__(
+        self,
+        *,
+        cluster_id: typing.Optional[builtins.str] = None,
+        kube_config: typing.Optional[builtins.str] = None,
+        name: typing.Optional[builtins.str] = None,
+        namespace: typing.Optional[builtins.str] = None,
+        role_arn: typing.Optional[builtins.str] = None,
+        time_out: typing.Optional[jsii.Number] = None,
+        value_override_url: typing.Optional[builtins.str] = None,
+        values: typing.Any = None,
+        value_yaml: typing.Optional[builtins.str] = None,
+        version: typing.Optional[builtins.str] = None,
+        vpc_configuration: typing.Optional["CfnScannerPropsVpcConfiguration"] = None,
+    ) -> None:
+        '''A resource provider for Aqua Enterprise Scanner.
+
+        :param cluster_id: EKS cluster name.
+        :param kube_config: Secrets Manager ARN for kubeconfig file.
+        :param name: Name for the helm release.
+        :param namespace: Namespace to use with helm. Created if doesn't exist and default will be used if not provided
+        :param role_arn: IAM to use with EKS cluster authentication, if not resource execution role will be used.
+        :param time_out: Timeout for resource provider. Default 60 mins
+        :param value_override_url: Custom Value Yaml file can optionally be specified.
+        :param values: Custom Values can optionally be specified.
+        :param value_yaml: String representation of a values.yaml file.
+        :param version: Version can be specified, if not latest will be used.
+        :param vpc_configuration: For network connectivity to Cluster inside VPC.
+
+        :schema: CfnScannerProps
+        '''
+        if isinstance(vpc_configuration, dict):
+            vpc_configuration = CfnScannerPropsVpcConfiguration(**vpc_configuration)
+        self._values: typing.Dict[str, typing.Any] = {}
+        if cluster_id is not None:
+            self._values["cluster_id"] = cluster_id
+        if kube_config is not None:
+            self._values["kube_config"] = kube_config
+        if name is not None:
+            self._values["name"] = name
+        if namespace is not None:
+            self._values["namespace"] = namespace
+        if role_arn is not None:
+            self._values["role_arn"] = role_arn
+        if time_out is not None:
+            self._values["time_out"] = time_out
+        if value_override_url is not None:
+            self._values["value_override_url"] = value_override_url
+        if values is not None:
+            self._values["values"] = values
+        if value_yaml is not None:
+            self._values["value_yaml"] = value_yaml
+        if version is not None:
+            self._values["version"] = version
+        if vpc_configuration is not None:
+            self._values["vpc_configuration"] = vpc_configuration
+
+    @builtins.property
+    def cluster_id(self) -> typing.Optional[builtins.str]:
+        '''EKS cluster name.
+
+        :schema: CfnScannerProps#ClusterID
+        '''
+        result = self._values.get("cluster_id")
+        return typing.cast(typing.Optional[builtins.str], result)
+
+    @builtins.property
+    def kube_config(self) -> typing.Optional[builtins.str]:
+        '''Secrets Manager ARN for kubeconfig file.
+
+        :schema: CfnScannerProps#KubeConfig
+        '''
+        result = self._values.get("kube_config")
+        return typing.cast(typing.Optional[builtins.str], result)
+
+    @builtins.property
+    def name(self) -> typing.Optional[builtins.str]:
+        '''Name for the helm release.
+
+        :schema: CfnScannerProps#Name
+        '''
+        result = self._values.get("name")
+        return typing.cast(typing.Optional[builtins.str], result)
+
+    @builtins.property
+    def namespace(self) -> typing.Optional[builtins.str]:
+        '''Namespace to use with helm.
+
+        Created if doesn't exist and default will be used if not provided
+
+        :schema: CfnScannerProps#Namespace
+        '''
+        result = self._values.get("namespace")
+        return typing.cast(typing.Optional[builtins.str], result)
+
+    @builtins.property
+    def role_arn(self) -> typing.Optional[builtins.str]:
+        '''IAM to use with EKS cluster authentication, if not resource execution role will be used.
+
+        :schema: CfnScannerProps#RoleArn
+        '''
+        result = self._values.get("role_arn")
+        return typing.cast(typing.Optional[builtins.str], result)
+
+    @builtins.property
+    def time_out(self) -> typing.Optional[jsii.Number]:
+        '''Timeout for resource provider.
+
+        Default 60 mins
+
+        :schema: CfnScannerProps#TimeOut
+        '''
+        result = self._values.get("time_out")
+        return typing.cast(typing.Optional[jsii.Number], result)
+
+    @builtins.property
+    def value_override_url(self) -> typing.Optional[builtins.str]:
+        '''Custom Value Yaml file can optionally be specified.
+
+        :schema: CfnScannerProps#ValueOverrideURL
+        '''
+        result = self._values.get("value_override_url")
+        return typing.cast(typing.Optional[builtins.str], result)
+
+    @builtins.property
+    def values(self) -> typing.Any:
+        '''Custom Values can optionally be specified.
+
+        :schema: CfnScannerProps#Values
+        '''
+        result = self._values.get("values")
+        return typing.cast(typing.Any, result)
+
+    @builtins.property
+    def value_yaml(self) -> typing.Optional[builtins.str]:
+        '''String representation of a values.yaml file.
+
+        :schema: CfnScannerProps#ValueYaml
+        '''
+        result = self._values.get("value_yaml")
+        return typing.cast(typing.Optional[builtins.str], result)
+
+    @builtins.property
+    def version(self) -> typing.Optional[builtins.str]:
+        '''Version can be specified, if not latest will be used.
+
+        :schema: CfnScannerProps#Version
+        '''
+        result = self._values.get("version")
+        return typing.cast(typing.Optional[builtins.str], result)
+
+    @builtins.property
+    def vpc_configuration(self) -> typing.Optional["CfnScannerPropsVpcConfiguration"]:
+        '''For network connectivity to Cluster inside VPC.
+
+        :schema: CfnScannerProps#VPCConfiguration
+        '''
+        result = self._values.get("vpc_configuration")
+        return typing.cast(typing.Optional["CfnScannerPropsVpcConfiguration"], result)
+
+    def __eq__(self, rhs: typing.Any) -> builtins.bool:
+        return isinstance(rhs, self.__class__) and rhs._values == self._values
+
+    def __ne__(self, rhs: typing.Any) -> builtins.bool:
+        return not (rhs == self)
+
+    def __repr__(self) -> str:
+        return "CfnScannerProps(%s)" % ", ".join(
+            k + "=" + repr(v) for k, v in self._values.items()
+        )
+
+
+@jsii.data_type(
+    jsii_type="@cdk-cloudformation/aqua-enterprise-scanner.CfnScannerPropsVpcConfiguration",
+    jsii_struct_bases=[],
+    name_mapping={"security_group_ids": "securityGroupIds", "subnet_ids": "subnetIds"},
+)
+class CfnScannerPropsVpcConfiguration:
+    def __init__(
+        self,
+        *,
+        security_group_ids: typing.Optional[typing.Sequence[builtins.str]] = None,
+        subnet_ids: typing.Optional[typing.Sequence[builtins.str]] = None,
+    ) -> None:
+        '''For network connectivity to Cluster inside VPC.
+
+        :param security_group_ids: Specify one or more security groups.
+        :param subnet_ids: Specify one or more subnets.
+
+        :schema: CfnScannerPropsVpcConfiguration
+        '''
+        self._values: typing.Dict[str, typing.Any] = {}
+        if security_group_ids is not None:
+            self._values["security_group_ids"] = security_group_ids
+        if subnet_ids is not None:
+            self._values["subnet_ids"] = subnet_ids
+
+    @builtins.property
+    def security_group_ids(self) -> typing.Optional[typing.List[builtins.str]]:
+        '''Specify one or more security groups.
+
+        :schema: CfnScannerPropsVpcConfiguration#SecurityGroupIds
+        '''
+        result = self._values.get("security_group_ids")
+        return typing.cast(typing.Optional[typing.List[builtins.str]], result)
+
+    @builtins.property
+    def subnet_ids(self) -> typing.Optional[typing.List[builtins.str]]:
+        '''Specify one or more subnets.
+
+        :schema: CfnScannerPropsVpcConfiguration#SubnetIds
+        '''
+        result = self._values.get("subnet_ids")
+        return typing.cast(typing.Optional[typing.List[builtins.str]], result)
+
+    def __eq__(self, rhs: typing.Any) -> builtins.bool:
+        return isinstance(rhs, self.__class__) and rhs._values == self._values
+
+    def __ne__(self, rhs: typing.Any) -> builtins.bool:
+        return not (rhs == self)
+
+    def __repr__(self) -> str:
+        return "CfnScannerPropsVpcConfiguration(%s)" % ", ".join(
+            k + "=" + repr(v) for k, v in self._values.items()
+        )
+
+
+__all__ = [
+    "CfnScanner",
+    "CfnScannerProps",
+    "CfnScannerPropsVpcConfiguration",
+]
+
+publication.publish()
