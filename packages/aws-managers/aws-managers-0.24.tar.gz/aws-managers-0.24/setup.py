c6b03c5561bea4f194a10d47e06dd93916b8312a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:24:21 +0000

    Add package aws-managers/0.24/aws-managers-0.24.tar.gz

---
+++ packages/aws-managers/aws-managers-0.24.tar.gz/aws-managers-0.24/setup.py
@@ -0,0 +1,36 @@
+from distutils.core import setup
+
+from setuptools import find_packages
+
+setup(
+    name='aws-managers',
+    packages=find_packages(),
+    package_data={
+        'aws_managers': ['./templates/athena/ddl/*.jinja2',
+                         './templates/athena/dml/*.jinja2']
+    },
+    include_package_data=False,
+    version='0.024',
+    license='MIT',
+    description='Wrappers around boto3 and sagemaker',
+    author='Vahndi Minah',
+    url='https://github.com/vahndi/aws-managers',
+    keywords=['boto3', 'sagemaker'],
+    install_requires=[
+        'awswrangler',
+        'botocore',
+        'boto3',
+        'jinja2',
+        'numpy',
+        'pandas',
+        'sagemaker',
+        'tqdm'
+    ],
+    classifiers=[
+      'Development Status :: 3 - Alpha',
+      'Intended Audience :: Developers',
+      'Topic :: Software Development :: Build Tools',
+      'License :: OSI Approved :: MIT License',
+      'Programming Language :: Python :: 3',
+    ]
+)
