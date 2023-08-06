d7117b743d49162a0997e8eacf55c852f9afbb0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:54:22 +0000

    Add package toolkit-py/0.12.8/toolkit_py-0.12.8-py3-none-any.whl

---
+++ packages/toolkit-py/toolkit_py-0.12.8-py3-none-any.whl/project_scaffold/templates/qt5/core.qml.h
@@ -0,0 +1,57 @@
+{{SLASH_COMMENTS}}
+
+#ifndef {{APP_NAME_UPPER}}__CORE_H
+#define {{APP_NAME_UPPER}}__CORE_H
+
+#include <QDebug>
+#include <QObject>
+#include <QSettings>
+#include <QTimer>
+#include <QWebSocket>
+
+class Core : public QObject {
+    Q_OBJECT
+
+public:
+    explicit Core(QObject *parent = nullptr);
+
+    bool DebugMode;
+
+    void InitConfig(QSettings *);
+
+    Q_PROPERTY(bool debugMode MEMBER DebugMode);
+    Q_PROPERTY(QString exportProperty MEMBER exportProperty);
+
+    Q_INVOKABLE static QString getUuid();
+    Q_INVOKABLE void connectToWebsocketServer(const QString &);
+    Q_INVOKABLE void sendTextMessageToWebsocketServer(const QString &);
+
+    static std::string AESEncryptStr(const QString &msgStr, const QString &keyStr);
+    static std::string AESDecryptStr(const QString &msgStr, const QString &keyStr);
+
+signals:
+    void signalsExample();
+
+public slots:
+    void onExit();
+
+private Q_SLOTS:
+    void onWebsocketTimeout();
+    void onWebsocketConnected();
+    void onWebsocketDisconnected();
+    void onWebsocketTextMessageReceived(const QString &message);
+
+private:
+    // variables from config file
+    QSettings *settings{};
+    QString exportProperty;
+
+    QString remoteServerSocket;
+
+    QWebSocket *websocketClient;
+    QString websocketUri;
+    QString websocketUrl;
+    QTimer websocketTimer;
+};
+
+#endif//{{APP_NAME_UPPER}}__CORE_H
