b1019e04f7d3993f165e643d43a88d1eaefdf0ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:14:14 +0000

    Add package betanegbinfit/0.42/betanegbinfit-0.42.tar.gz

---
+++ packages/betanegbinfit/betanegbinfit-0.42.tar.gz/betanegbinfit-0.42/betanegbinfit/long_pvalues.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""Arbitrary precision arithmetics for p-value calculation."""
+import numpy as np
+import gmpy2
+
+
+__PRECISION = 128
+
+
+def get_cdf(model, params):
+    return
+
+
+
+
