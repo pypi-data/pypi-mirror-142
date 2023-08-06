a947ecc022cc265251878d65f9db023a53fc9c1b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:28:04 +0000

    Add package yadg/4.1.0rc3/yadg-4.1.0rc3.tar.gz

---
+++ packages/yadg/yadg-4.1.0rc3.tar.gz/yadg-4.1.0rc3/setup.py
@@ -0,0 +1,51 @@
+import setuptools
+
+with open("VERSION", "r") as infile:
+    version = infile.read().strip()
+
+with open("README.md", "r", encoding="utf-8") as infile:
+    readme = infile.read()
+
+packagedir = "src"
+
+setuptools.setup(
+    name="yadg",
+    version=version,
+    author="Peter Kraus",
+    author_email="peter@tondon.de",
+    description="yet another datagram",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    url="https://github.com/dgbowl/yadg",
+    project_urls={
+        "Bug Tracker": "https://github.com/dgbowl/yadg/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": packagedir},
+    packages=setuptools.find_packages(where=packagedir),
+    python_requires=">=3.8",
+    install_requires=[
+        "numpy",
+        "scipy",
+        "pint",
+        "uncertainties",
+        "striprtf",
+        "tzlocal",
+        "python-dateutil",
+    ],
+    extras_require={
+        "testing": [
+            "pytest"
+        ],
+        "docs": [
+            "sphinx",
+            "sphinx-rtd-theme",
+            "sphinx-autodoc-typehints"
+        ]
+    },
+    entry_points={"console_scripts": ["yadg=yadg:run_with_arguments"]},
+)
