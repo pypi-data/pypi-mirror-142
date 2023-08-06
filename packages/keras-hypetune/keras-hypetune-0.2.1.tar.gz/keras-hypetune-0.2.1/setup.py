0d7c2abe8723d773b9259ed0fc9f1155441f2b50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:16:21 +0000

    Add package keras-hypetune/0.2.1/keras-hypetune-0.2.1.tar.gz

---
+++ packages/keras-hypetune/keras-hypetune-0.2.1.tar.gz/keras-hypetune-0.2.1/setup.py
@@ -0,0 +1,34 @@
+import pathlib
+from setuptools import setup, find_packages
+
+HERE = pathlib.Path(__file__).parent
+
+VERSION = '0.2.1'
+PACKAGE_NAME = 'keras-hypetune'
+AUTHOR = 'Marco Cerliani'
+AUTHOR_EMAIL = 'cerlymarco@gmail.com'
+URL = 'https://github.com/cerlymarco/keras-hypetune'
+
+LICENSE = 'MIT'
+DESCRIPTION = 'A friendly python package for Keras Hyperparameters Tuning based only on NumPy and Hyperopt.'
+LONG_DESCRIPTION = (HERE / "README.md").read_text()
+LONG_DESC_TYPE = "text/markdown"
+
+INSTALL_REQUIRES = [
+      'numpy',
+      'hyperopt==0.2.5'
+]
+
+setup(name=PACKAGE_NAME,
+      version=VERSION,
+      description=DESCRIPTION,
+      long_description=LONG_DESCRIPTION,
+      long_description_content_type=LONG_DESC_TYPE,
+      author=AUTHOR,
+      license=LICENSE,
+      author_email=AUTHOR_EMAIL,
+      url=URL,
+      install_requires=INSTALL_REQUIRES,
+      python_requires='>=3',
+      packages=find_packages()
+      )
