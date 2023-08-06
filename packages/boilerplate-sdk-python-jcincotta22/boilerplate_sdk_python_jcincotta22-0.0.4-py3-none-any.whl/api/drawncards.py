2899d2feab8d9b9fb5190148f29d61c082ca4365
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:13:25 +0000

    Add package boilerplate-sdk-python-jcincotta22/0.0.4/boilerplate_sdk_python_jcincotta22-0.0.4-py3-none-any.whl

---
+++ packages/boilerplate-sdk-python-jcincotta22/boilerplate_sdk_python_jcincotta22-0.0.4-py3-none-any.whl/api/drawncards.py
@@ -0,0 +1,8 @@
+from ..api.card import Card
+
+
+class DrawnCards(object):
+    def __init__(self, cards: list[Card], deck_id: str, remaining: int):
+        self.cards = cards
+        self.deck_id = deck_id
+        self.remaining = remaining
