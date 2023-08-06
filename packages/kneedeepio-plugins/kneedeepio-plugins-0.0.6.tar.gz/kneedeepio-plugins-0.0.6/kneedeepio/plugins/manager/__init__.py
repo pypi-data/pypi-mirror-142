b7fb4dddd0f024d847bfe908b2d015cf1bad9fe1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:11:58 +0000

    Add package kneedeepio-plugins/0.0.6/kneedeepio-plugins-0.0.6.tar.gz

---
+++ packages/kneedeepio-plugins/kneedeepio-plugins-0.0.6.tar.gz/kneedeepio-plugins-0.0.6/kneedeepio/plugins/manager/__init__.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+# NOTE: This core module contains the loading and calling functionality.
+from .pluginfactory import PluginFactory
+from .exceptions import KneeDeepIOPluginsManagerException
+from .exceptions import ServiceAlreadyRegistered
+from .exceptions import ServiceNotRegistered
+from .exceptions import PluginAlreadyLoadedException
+from .exceptions import PluginNotLoadedException
+
+### IMPORTS ###
+
+### GLOBALS ###
+
+### FUNCTIONS ###
+
+### CLASSES ###
