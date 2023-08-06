633b6924c47c0b48565feb436c20b6393e7e58e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:49:30 +0000

    Add package bistring/0.5.0/bistring-0.5.0-py3-none-any.whl

---
+++ packages/bistring/bistring-0.5.0-py3-none-any.whl/bistring/_typing.py
@@ -0,0 +1,19 @@
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT license.
+
+from typing import Callable, Match, Pattern, Tuple, Union
+
+
+BiIndex = Tuple[int, int]
+
+Bounds = Tuple[int, int]
+
+AnyBounds = Union[int, range, slice, Bounds]
+
+Index = Union[int, slice]
+
+Range = Union[range, slice, Bounds]
+
+Regex = Union[str, Pattern[str]]
+
+Replacement = Union[str, Callable[[Match[str]], str]]
