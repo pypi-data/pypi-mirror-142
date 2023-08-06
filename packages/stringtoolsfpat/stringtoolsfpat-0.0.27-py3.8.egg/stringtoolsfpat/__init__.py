92b17617cce76c464ae542c1fb471c71c8076da7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:09:54 +0000

    Add package stringtoolsfpat/0.0.27/stringtoolsfpat-0.0.27-py3.8.egg

---
+++ packages/stringtoolsfpat/stringtoolsfpat-0.0.27-py3.8.egg/stringtoolsfpat/__init__.py
@@ -0,0 +1,10 @@
+class StringToolsFpat:
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def lower(string: str):
+        """
+        to lower case
+        """
+        return string.lower()
