1b4ffef64b8259e81d0373f91f70d2c9dc75374a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:40:02 +0000

    Add package ikarus/0.0.2/ikarus-0.0.2.tar.gz

---
+++ packages/ikarus/ikarus-0.0.2.tar.gz/ikarus-0.0.2/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+with open("README.rst", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='ikarus',
+    version='0.0.2',
+    packages=['ikarus'],
+    desription='Machine Learning classifier of tumor cells',
+    install_requires=[
+        'numpy',
+        'pandas',
+        'scipy',
+        'scanpy',
+        'anndata',
+        'pyscenic'
+    ]
+)
