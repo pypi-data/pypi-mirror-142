1aa1cd76a89e314b48a3d5459534b4ccbd020143
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:56:15 +0000

    Add package pyoxigraph/0.3.0rc1/pyoxigraph-0.3.0_rc.1.tar.gz

---
+++ packages/pyoxigraph/pyoxigraph-0.3.0_rc.1.tar.gz/pyoxigraph-0.3.0_rc.1/src/lib.rs
@@ -0,0 +1,40 @@
+#![deny(
+    future_incompatible,
+    nonstandard_style,
+    rust_2018_idioms,
+    trivial_casts,
+    trivial_numeric_casts,
+    unsafe_code,
+    unused_qualifications
+)]
+
+mod io;
+mod model;
+mod sparql;
+mod store;
+
+use crate::model::*;
+use crate::sparql::*;
+use crate::store::*;
+use pyo3::prelude::*;
+
+/// Oxigraph Python bindings
+#[pymodule]
+fn pyoxigraph(_py: Python<'_>, module: &PyModule) -> PyResult<()> {
+    module.add("__package__", "pyoxigraph")?;
+    module.add("__version__", env!("CARGO_PKG_VERSION"))?;
+    module.add("__author__", env!("CARGO_PKG_AUTHORS").replace(':', "\n"))?;
+
+    module.add_class::<PyNamedNode>()?;
+    module.add_class::<PyBlankNode>()?;
+    module.add_class::<PyLiteral>()?;
+    module.add_class::<PyDefaultGraph>()?;
+    module.add_class::<PyTriple>()?;
+    module.add_class::<PyQuad>()?;
+    module.add_class::<PyStore>()?;
+    module.add_class::<PyVariable>()?;
+    module.add_class::<PyQuerySolutions>()?;
+    module.add_class::<PyQuerySolution>()?;
+    module.add_class::<PyQueryTriples>()?;
+    io::add_to_module(module)
+}
