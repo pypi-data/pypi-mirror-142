471129308d40256c6b161e55d1feb02cfb68634b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:09:19 +0000

    Add package panther/0.1.4/panther-0.1.4.tar.gz

---
+++ packages/panther/panther-0.1.4.tar.gz/panther-0.1.4/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='panther',
+    version='0.1.4',
+    license='MIT',
+    author='Ali RajabNezhad',
+    author_email='alirn76@yahoo.com',
+    packages=find_packages('panther'),
+    package_dir={'': 'panther'},
+    url='https://github.com/alirn76/panther',
+    keywords='fast friendly web framework',
+    install_requires=[],
+)
