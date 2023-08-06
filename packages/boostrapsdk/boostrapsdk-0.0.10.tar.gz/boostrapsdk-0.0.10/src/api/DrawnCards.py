83c1495bc57371983ab676497c48447c21d9fa8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:28:20 +0000

    Add package boostrapsdk/0.0.10/boostrapsdk-0.0.10.tar.gz

---
+++ packages/boostrapsdk/boostrapsdk-0.0.10.tar.gz/boostrapsdk-0.0.10/src/api/DrawnCards.py
@@ -0,0 +1,7 @@
+from src.api.Card import Card
+
+class DrawnCards(object):
+    def __init__(self, cards: list[Card], deck_id: str, remaining: int):
+        self.cards = cards
+        self.deck_id = deck_id
+        self.remaining = remaining
