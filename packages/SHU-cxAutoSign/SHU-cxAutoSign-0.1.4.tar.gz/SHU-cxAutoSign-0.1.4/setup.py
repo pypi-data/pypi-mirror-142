3cb58f1690259bd2ed86ece02f5bd2e8b1dae5d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:09:20 +0000

    Add package SHU-cxAutoSign/0.1.4/SHU-cxAutoSign-0.1.4.tar.gz

---
+++ packages/SHU-cxAutoSign/SHU-cxAutoSign-0.1.4.tar.gz/SHU-cxAutoSign-0.1.4/setup.py
@@ -0,0 +1,30 @@
+
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="SHU-cxAutoSign",
+    version="0.1.4",
+    author="XHLin",
+    author_email="xhaughearl@gmail.com",
+    description="",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    project_urls={
+        "Bug Tracker": "https://github.com/pypa/sampleproject/issues",
+    },
+    install_requires = [
+        'requests',
+        'beautifulsoup4'
+        ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages=["SHU_cxAutoSign"],
+    python_requires=">=3.6",
+)
