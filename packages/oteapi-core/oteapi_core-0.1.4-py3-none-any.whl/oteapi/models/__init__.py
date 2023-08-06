df8df6d69f177dbcff92f8480cba522a5eecde3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:38:30 +0000

    Add package oteapi-core/0.1.4/oteapi_core-0.1.4-py3-none-any.whl

---
+++ packages/oteapi-core/oteapi_core-0.1.4-py3-none-any.whl/oteapi/models/__init__.py
@@ -0,0 +1,33 @@
+"""`oteapi.models` module.
+
+This module contains all the `pydantic` configuration models.
+"""
+from typing import Union
+
+from .datacacheconfig import DataCacheConfig
+from .filterconfig import FilterConfig
+from .functionconfig import FunctionConfig
+from .genericconfig import AttrDict, GenericConfig
+from .mappingconfig import MappingConfig, RDFTriple
+from .resourceconfig import ResourceConfig
+from .sessionupdate import SessionUpdate
+from .transformationconfig import TransformationConfig, TransformationStatus
+
+__all__ = (
+    "AttrDict",
+    "DataCacheConfig",
+    "FilterConfig",
+    "FunctionConfig",
+    "GenericConfig",
+    "MappingConfig",
+    "RDFTriple",
+    "ResourceConfig",
+    "SessionUpdate",
+    "StrategyConfig",
+    "TransformationConfig",
+    "TransformationStatus",
+)
+
+StrategyConfig = Union[
+    FilterConfig, FunctionConfig, MappingConfig, ResourceConfig, TransformationConfig
+]
