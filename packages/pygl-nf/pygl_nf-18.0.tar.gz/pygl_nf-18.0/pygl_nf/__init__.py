42cef50cd666ab80b0e9b3ddd89238ee726bf1c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:32 +0000

    Add package pygl-nf/18.0/pygl_nf-18.0.tar.gz

---
+++ packages/pygl-nf/pygl_nf-18.0.tar.gz/pygl_nf-18.0/pygl_nf/__init__.py
@@ -0,0 +1,4 @@
+import colorama
+print(colorama.Fore.BLUE+'Pygl version - (17.5)'+colorama.Fore.RESET)
+print(colorama.Fore.CYAN+colorama.Style.BRIGHT+'My Github --- ( https://github.com/dedisvoin/GRPgraph-versions )'+colorama.Fore.RESET,colorama.Style.RESET_ALL)
+__version__ = '18.0'
