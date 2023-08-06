0c0f40ba61bd44ea0376830dfa043bda5cb8f0fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:51:15 +0000

    Add package calculadora-kevin/0.0.2/calculadora_kevin-0.0.2-py3-none-any.whl

---
+++ packages/calculadora-kevin/calculadora_kevin-0.0.2-py3-none-any.whl/project/calc.py
@@ -0,0 +1,8 @@
+from project.calc_kevin.calc_kevin import Calculator
+
+sum = lambda a, b: Calculator.sum(a, b)
+sub = lambda a, b: Calculator.sub(a, b)
+mul = lambda a, b: Calculator.mul(a, b)
+exp = lambda a, b: Calculator.exp(a, b)
+root = lambda a, b: Calculator.root(a, b)
+div = lambda a, b: Calculator.div(a, b)
