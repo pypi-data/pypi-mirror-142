e6dadad622ba8a73f92c2feb6f4c73d271dcb69f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:26:21 +0000

    Add package class-composer/0.1.0/class_composer-0.1.0-py3-none-any.whl

---
+++ packages/class-composer/class_composer-0.1.0-py3-none-any.whl/composer/__init__.py
@@ -0,0 +1,16 @@
+__author__ = "Andy"
+__license__ = "MIT"
+__version__ = "0.1.0-alpha"
+
+from typing import Literal, NamedTuple
+
+
+class Version(NamedTuple):
+    major: int
+    minor: int
+    patch: int
+
+    id: Literal["alpha", "beta", "final"]
+
+
+version = Version(0, 1, 0, "alpha")
