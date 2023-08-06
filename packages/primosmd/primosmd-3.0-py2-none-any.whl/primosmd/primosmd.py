6d1cfcc369bb11cc1002365c736610f9c54d1b06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:11:42 +0000

    Add package primosmd/3.0/primosmd-3.0-py2-none-any.whl

---
+++ packages/primosmd/primosmd-3.0-py2-none-any.whl/primosmd/primosmd.py
@@ -0,0 +1,13 @@
+
+"""
+Created for @author: Mario
+"""
+def primos(n):
+	for i in range(2, n):
+		es_primo = True
+		for j in range(2, i):
+			if(i%j == 0):
+				es_primo = False
+		if(es_primo):
+			print(f"{i} es primo")
+print(primos(30))
