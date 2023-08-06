1042242823c8ba1dc6fb6587713918f0ff4cd4e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:10:05 +0000

    Add package pangoro/0.35/pangoro-0.35.tar.gz

---
+++ packages/pangoro/pangoro-0.35.tar.gz/pangoro-0.35/pangoro/__init__.py
@@ -0,0 +1,4 @@
+#pangoro.preprocessing.PangoroDataFrame
+#from pangoro.preprocessing import PangoroDataFrame
+__all__ =["preprocessing"]
+
---
+++ packages/pangoro/pangoro-0.35.tar.gz/pangoro-0.35/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pangoro',
+    version='0.35',
+    packages=find_packages(exclude=['tests*']),
+    license='MIT',
+    description='pangoro python package',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+
+    install_requires=['numpy','pandas','sklearn','seaborn','matplotlib'],
+    url='https://github.com/ieuTeamD',
+    author='Team D',
+    author_email='ieuTeamD@gmail.com'
+)
