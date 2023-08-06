dccba99930d930ee62c097b2351efc48cc0b5c92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:27:57 +0000

    Add package xcpythontool/0.0.6/xcpythontool-0.0.6.tar.gz

---
+++ packages/xcpythontool/xcpythontool-0.0.6.tar.gz/xcpythontool-0.0.6/setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+
+#############################################
+# File Name: setup.py
+# Author: xuxuechao
+# Mail: 932829703@qq.com
+# Created Time:  2022-03-14 29:25: AM
+#############################################
+
+
+from setuptools import setup, find_packages
+
+setup(
+    name = "xcpythontool",
+    version = "0.0.6",
+    keywords = ["xxc","play"],
+    description = "测试上传pip",
+    long_description = "测试上传pip...",
+    license = "MIT Licence",
+    url = "https://github.com/19933139420/xcpythontest.git",
+    author = "xuxuechao",
+    author_email = "932829703@qq.com",
+    packages = find_packages(),
+    include_package_data = True,
+    platforms = "any",
+)
---
+++ packages/xcpythontool/xcpythontool-0.0.6.tar.gz/xcpythontool-0.0.6/xcpythontool/__init__.py
@@ -0,0 +1,1 @@
+from xcpythontool.playGame import PlayGame
