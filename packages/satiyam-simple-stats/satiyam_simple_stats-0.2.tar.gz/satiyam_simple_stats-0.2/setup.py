59b4c0bc2edff2b00e30ba49a59923d9366a5b13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:16:23 +0000

    Add package satiyam-simple-stats/0.2/satiyam_simple_stats-0.2.tar.gz

---
+++ packages/satiyam-simple-stats/satiyam_simple_stats-0.2.tar.gz/satiyam_simple_stats-0.2/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='satiyam_simple_stats',
+      version='0.2',
+      description='Gaussian and Binomial Distributions',
+      packages=['satiyam_simple_stats'],
+      zip_safe=False)
