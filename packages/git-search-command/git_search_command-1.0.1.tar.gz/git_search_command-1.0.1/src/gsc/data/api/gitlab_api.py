643d92f248bb3e63be00af46df413c421c6b4eec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:19:26 +0000

    Add package git-search-command/1.0.1/git_search_command-1.0.1.tar.gz

---
+++ packages/git-search-command/git_search_command-1.0.1.tar.gz/git_search_command-1.0.1/src/gsc/data/api/gitlab_api.py
@@ -0,0 +1,40 @@
+from gsc.request.request_wrapper import Api, GetRequest, GetRequestAutoFetchPagination
+from gsc.config import GitlabConfig
+
+
+class GitLabApi(Api):
+    def __init__(self) -> None:
+        config = GitlabConfig()
+        selected_env = config.get_session_env() or config.get_default_env()
+        super().__init__(
+            selected_env.host_name,
+            {
+                "Content-Type": "application/json;charset=UTF-8",
+                "PRIVATE-TOKEN": selected_env.private_token,
+            },
+        )
+
+
+class ProjectApi(GitLabApi):
+    @GetRequestAutoFetchPagination(path="api/v4/groups/{group_name}/projects")
+    def project_list(self, group_name: str, limit: int):
+        return {"group_name": group_name}, {
+            "page": 1,
+            "per_page": limit,
+            "order_by": "id",
+            "sort": "asc",
+        }
+
+    @GetRequest(path="api/v4/projects/{proj_id}")
+    def project_info(self, proj_id: int):
+        return {"proj_id": proj_id}, None
+
+
+class SearchApi(GitLabApi):
+    @GetRequest(path="api/v4/projects/{proj_id}/search")
+    def search_in_project(self, proj_id: int, keyword: str):
+        return {"proj_id": proj_id}, {
+            "scope": "blobs",
+            "search": keyword,
+            "per_page": 100,
+        }
