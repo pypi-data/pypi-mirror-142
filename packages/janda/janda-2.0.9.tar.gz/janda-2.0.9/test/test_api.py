c2409fe761282a42a45d0f0999b8f61c005165d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:54:19 +0000

    Add package janda/2.0.9/janda-2.0.9.tar.gz

---
+++ packages/janda/janda-2.0.9.tar.gz/janda-2.0.9/test/test_api.py
@@ -0,0 +1,11 @@
+import json
+import requests
+
+with open('janda/base/Base.json', 'r', encoding='utf-8') as f:
+    site = json.load(f)
+    api_list = []
+    for key, value in site.items():
+        api_list.append(value['api'])
+        ## print(value['api'])
+        r = requests.get(value['api'])
+        print(value['api'], r.status_code)
