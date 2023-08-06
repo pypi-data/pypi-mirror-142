c9419b469a5761c9d61666475cea9f49a904e0dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:28:04 +0000

    Add package zdbpydra/0.1.0/zdbpydra-0.1.0.tar.gz

---
+++ packages/zdbpydra/zdbpydra-0.1.0.tar.gz/zdbpydra-0.1.0/zdbpydra/__init__.py
@@ -0,0 +1,39 @@
+"""
+This package allows to access JSON-LD data (with PICA+ data embedded)
+from the German Union Catalogue of Serials (ZDB)
+"""
+
+__author__ = "Donatus Herre <donatus.herre@slub-dresden.de>"
+__version__ = "0.1.0"
+
+from .client import Hydra
+from .docs import PicaParser
+
+
+def context(headers={}, loglevel=0):
+    hydra = Hydra(loglevel=loglevel)
+    return hydra.context(headers=headers)
+
+
+def title(id, pica=False, headers={}, loglevel=0):
+    hydra = Hydra(loglevel=loglevel)
+    return hydra.title(id, pica=pica, headers=headers)
+
+
+def search(query, size=10, page=1, headers={}, loglevel=0):
+    hydra = Hydra(loglevel=loglevel)
+    return hydra.search(query, size=size, page=page, headers={})
+
+
+def scroll(query, size=10, page=1, headers={}, loglevel=0):
+    hydra = Hydra(loglevel=loglevel)
+    return hydra.scroll(query, size=size, page=page, headers=headers)
+
+
+def stream(query, size=10, page=1, headers={}, loglevel=0):
+    hydra = Hydra(loglevel=loglevel)
+    return hydra.stream(query, size=size, page=page, headers=headers)
+
+
+def parse_pica(data):
+    return PicaParser(data)
