e19ed62f20831ee4b9f4bda72ec25503466d37ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:13:16 +0000

    Add package jaws-scripts/4.0.2/jaws_scripts-4.0.2-py3-none-any.whl

---
+++ packages/jaws-scripts/jaws_scripts-4.0.2-py3-none-any.whl/jaws_scripts/registry/describe_schemas.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+
+"""
+    Describe JAWS AVRO schemas in to Schema Registry
+"""
+
+import os
+import json
+import pkgutil
+
+from confluent_kafka.schema_registry import SchemaRegistryClient
+
+
+def describe_schemas() -> None:
+    """
+        Describe JAWS AVRO schemas in to Schema Registry
+    """
+    sr_conf = {'url':  os.environ.get('SCHEMA_REGISTRY', 'http://localhost:8081')}
+    client = SchemaRegistryClient(sr_conf)
+
+    conf = pkgutil.get_data("jaws_libp", "avro/schema-registry.json")
+
+    records = json.loads(conf)
+
+    lookup = {}
+
+    for r in records:
+        lookup[r['subject']] = r['file']
+
+    subjects = client.get_subjects()
+
+    for subject in subjects:
+        registered = client.get_latest_version(subject)
+
+        filename = 'avro/schemas/' + registered.subject + '.avsc'
+
+        if registered.subject in lookup:
+            filename = lookup[registered.subject]
+
+        path = "/tmp/" + filename
+        os.makedirs(os.path.dirname(path), exist_ok=True)
+        with open(path, "w") as text_file:
+            print(json.dumps(json.loads(registered.schema.schema_str), indent=4), file=text_file)
+
+        print("ID: {}, Subject: {}, Version: {}, File: {}".format(registered.schema_id, registered.subject, registered.version, path))
+
+
+if __name__ == "__main__":
+    describe_schemas()
