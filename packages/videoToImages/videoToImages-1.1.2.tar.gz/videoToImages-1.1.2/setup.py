941225d4417812c970d9a429db84c318e1d23e36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:09:45 +0000

    Add package videoToImages/1.1.2/videoToImages-1.1.2.tar.gz

---
+++ packages/videoToImages/videoToImages-1.1.2.tar.gz/videoToImages-1.1.2/setup.py
@@ -0,0 +1,34 @@
+import setuptools
+import os
+
+def read(rel_path):
+    base_path = os.path.abspath(os.path.dirname(__file__))
+    with open(os.path.join(base_path, rel_path), 'r') as f:
+        return f.read()
+
+setuptools.setup(
+    name="videoToImages",
+    version="1.1.2",
+    author="Ali Waqas",
+    author_email="engrr.ali.waqas@gmail.com",
+    description="A easy to use, video to images converter",
+    long_description=read('README.md'),
+    long_description_content_type="text/markdown",
+    url="https://github.com/aliwaqas333/VideoToImages",
+    project_urls={
+        "Bug Tracker": "https://github.com/aliwaqas333/VideoToImages/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+    entry_points={
+        'console_scripts': [
+            'videoToImages = videoToImages.videoToImages:main',
+        ],
+    },
+)
