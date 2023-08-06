2b7414b0463f7f69420386b9de669bb3e50b8035
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:12:32 +0000

    Add package janda/1.0.8/janda-1.0.8.tar.gz

---
+++ packages/janda/janda-1.0.8.tar.gz/janda-1.0.8/setup.py
@@ -0,0 +1,55 @@
+import re
+
+from setuptools import setup
+
+
+version = ''
+with open('janda/__init__.py') as f:
+    version = re.search(
+        r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]', f.read(), re.MULTILINE).group(1)
+
+
+requirements = []
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+
+if not version:
+    raise RuntimeError('version is not set')
+
+readme = ''
+with open('README.md') as f:
+    readme = f.read()
+
+
+setup(
+    name='janda',
+    author='sinkaroid',
+    author_email='anakmancasan@gmail.com',
+    version='1.0.8',
+    long_description=readme,
+    url='https://github.com/sinkaroid/janda',
+    packages=['janda'],
+    license='MIT',
+    classifiers=[
+        "Framework :: AsyncIO",
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        'Programming Language :: Python :: 3.7',
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Software Development :: Libraries :: Application Frameworks",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+
+    ],
+    description='Doujin wrapper',
+    include_package_data=True,
+    keywords=['janda', 'doujin', 'wrapper'],
+    install_requires=requirements
+)
