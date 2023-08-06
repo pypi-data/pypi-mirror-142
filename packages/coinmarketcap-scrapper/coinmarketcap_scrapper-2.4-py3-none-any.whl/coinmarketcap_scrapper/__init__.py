8c2b9abdc522f1d310dfb91e2ca2a22965bfd204
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:42 +0000

    Add package coinmarketcap-scrapper/2.4/coinmarketcap_scrapper-2.4-py3-none-any.whl

---
+++ packages/coinmarketcap-scrapper/coinmarketcap_scrapper-2.4-py3-none-any.whl/coinmarketcap_scrapper/__init__.py
@@ -0,0 +1,30 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+# ------------------------------------------------------------------------------
+#+ Autor:  	Ran#
+#+ Creado: 	2022/02/05 22:42:06.864533
+#+ Editado:	2022/02/20 21:16:57.587083
+# ------------------------------------------------------------------------------
+
+'''
+info coinmarketcap_scrapper
+'''
+
+# ------------------------------------------------------------------------------
+
+from .coinmarketcap_scrapper import CoinMarketCap
+from .__variables import RAIZ
+
+# ------------------------------------------------------------------------------
+
+__nome__ = 'coinmarketcap_scrapper'
+__version__ = '2.4'
+__autor__ = 'Ran#'
+__creditos__ = 'Ran#'
+__licenza__ = 'GPLv3'
+__raiz__ = RAIZ
+__descricion__ = 'Scrapper of the CoinMarketCap website'
+__ligazon__ = 'https://wwww.github.com/Ran-n/coinmarketcap_scrapper'
+__problemas__ = 'https://wwww.github.com/Ran-n/coinmarketcap_scrapper/issues'
+
+# ------------------------------------------------------------------------------
