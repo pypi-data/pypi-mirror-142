f95d020f932e2a150c401affa7d935e43f245164
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:09:38 +0000

    Add package libreflow.pianoplayer/2.0.2/libreflow.pianoplayer-2.0.2.tar.gz

---
+++ packages/libreflow.pianoplayer/libreflow.pianoplayer-2.0.2.tar.gz/libreflow.pianoplayer-2.0.2/src/libreflow/pianoplayer/flow/site.py
@@ -0,0 +1,9 @@
+from kabaret import flow
+from libreflow.baseflow.site import WorkingSite as BaseWorkingSite
+
+
+class WorkingSite(BaseWorkingSite):
+
+    package_source_dir = flow.Param()
+    package_target_dir = flow.Param()
+    target_sites       = flow.OrderedStringSetParam()
