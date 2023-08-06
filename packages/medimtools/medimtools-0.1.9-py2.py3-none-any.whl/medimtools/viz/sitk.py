af9b91cfbdf1d6cd1c76607597be86344035685b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:25:06 +0000

    Add package medimtools/0.1.9/medimtools-0.1.9-py2.py3-none-any.whl

---
+++ packages/medimtools/medimtools-0.1.9-py2.py3-none-any.whl/medimtools/viz/sitk.py
@@ -0,0 +1,38 @@
+import SimpleITK as sitk
+import matplotlib.pyplot as plt
+from medimtools.viz.ops import *
+
+def quick_view(
+    image: sitk.Image,
+    display: bool = True,
+    isotropic: bool = True,
+    cmap: str = "gray",
+    mask=None,
+    contour=None,
+):
+
+    # [0,255] for visualization purposes
+    image = sitk.Cast(sitk.RescaleIntensity(image, 0, 255), sitk.sitkUInt8)
+
+    if isotropic:
+        image = make_isotropic(image)
+
+        if mask:
+            mask = make_isotropic(mask, interpolator=sitk.sitkNearestNeighbor)
+
+        if contour:
+            contour = make_isotropic(contour, interpolator=sitk.sitkNearestNeighbor)
+
+    if mask:
+        image = overlay_mask(image, mask)
+
+    if contour:
+        image = overlay_mask(image, contour, contour=True)
+
+    image = get_image_preview(image)
+
+    if display:
+        plt.imshow(image, cmap=cmap)
+        plt.show()
+
+    return image
