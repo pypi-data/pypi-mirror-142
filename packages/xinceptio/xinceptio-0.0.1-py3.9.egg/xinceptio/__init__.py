aea0cb7db18021163e1ad7ef561bc34cc7ea715f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:38:12 +0000

    Add package xinceptio/0.0.1/xinceptio-0.0.1-py3.9.egg

---
+++ packages/xinceptio/xinceptio-0.0.1-py3.9.egg/xinceptio/__init__.py
@@ -0,0 +1,10 @@
+'''
+Author: Xin Zong
+Date: 2022-03-11 16:38:30
+LastEditors: Xin Zong
+LastEditTime: 2022-03-11 19:30:27
+FilePath: /pip_package/xinceptio/__init__.py
+'''
+from .fuzzy_variable_input import FuzzyInputVariable as Input 
+from .fuzzy_variable_output import FuzzyOutputVariable as Output
+from .fuzzy_system import FuzzySystem as Oracle
