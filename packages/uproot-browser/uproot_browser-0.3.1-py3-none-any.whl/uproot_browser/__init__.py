45e3429d7f18dc32c36bdb054d95bbb7c8b4c7fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:21:27 +0000

    Add package uproot-browser/0.3.1/uproot_browser-0.3.1-py3-none-any.whl

---
+++ packages/uproot-browser/uproot_browser-0.3.1-py3-none-any.whl/uproot_browser/__init__.py
@@ -0,0 +1,9 @@
+"""
+This is uproot-browser. There is no user accessible API; only a terminal
+interface is provided currently.
+"""
+from __future__ import annotations
+
+__version__ = "0.3.1"
+
+__all__ = ("__version__",)
