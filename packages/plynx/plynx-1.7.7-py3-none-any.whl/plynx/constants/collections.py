f93fe424691ac6cb0b9259ecf0540ac5ef638654
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:10:42 +0000

    Add package plynx/1.7.7/plynx-1.7.7-py3-none-any.whl

---
+++ packages/plynx/plynx-1.7.7-py3-none-any.whl/plynx/constants/collections.py
@@ -0,0 +1,11 @@
+class Collections:
+    GROUPS = 'groups'
+    NODE_CACHE = 'node_cache'
+    RUN_CANCELLATIONS = 'run_cancellations'
+    RUNS = 'runs'
+    TEMPLATES = 'templates'
+    USERS = 'users'
+    WORKER_STATE = 'worker_state'
+
+    # virtual collections:
+    HUB_NODE_REGISTRY = 'hub_node_registry'
