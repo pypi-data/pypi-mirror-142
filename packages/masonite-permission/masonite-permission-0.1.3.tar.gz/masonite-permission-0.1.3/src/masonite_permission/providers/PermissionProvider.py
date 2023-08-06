088793b7e5986af6d9b88509bd61a3d51765c424
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:27:54 +0000

    Add package masonite-permission/0.1.3/masonite-permission-0.1.3.tar.gz

---
+++ packages/masonite-permission/masonite-permission-0.1.3.tar.gz/masonite-permission-0.1.3/src/masonite_permission/providers/PermissionProvider.py
@@ -0,0 +1,23 @@
+"""A PermissionProvider Service Provider."""
+
+from masonite.packages import PackageProvider
+
+from ..masonite_permission import MasonitePermission
+
+
+class PermissionProvider(PackageProvider):
+    def configure(self):
+        """Register objects into the Service Container."""
+        (
+            self.root("masonite_permission")
+            .name("masonite-permission")
+            .config("config/masonite_permission.py", publish=True)
+            .migrations("migrations/create_permissions_table.py")
+        )
+
+    def register(self):
+        super().register()
+
+    def boot(self):
+        """Boots services required by the container."""
+        pass
