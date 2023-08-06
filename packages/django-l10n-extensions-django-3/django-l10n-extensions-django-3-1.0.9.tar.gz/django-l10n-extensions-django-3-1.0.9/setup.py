c1d0b5367f2f10b3e08420eb467712e98a2bb022
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:34:46 +0000

    Add package django-l10n-extensions-django-3/1.0.9/django-l10n-extensions-django-3-1.0.9.tar.gz

---
+++ packages/django-l10n-extensions-django-3/django-l10n-extensions-django-3-1.0.9.tar.gz/django-l10n-extensions-django-3-1.0.9/setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+
+with open("DESCRIPTION.md", "r") as fh:
+    description = fh.read()
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='django-l10n-extensions-django-3',
+    version='1.0.9',
+    author=u'Jon Miller',
+    author_email='iamjonamiller@gmail.com',
+    package_dir={'': 'src'},
+    packages=find_packages(where='./src'),
+    include_package_data=True,
+    install_requires=['Django>=3.2.12', 'polib>=1.0'],
+    url='https://github.com/iamjonmiller/django-l10n-extensions',
+    license='',
+    description=description,
+    description_content_type='text/markdown',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    zip_safe=False,
+    key_words=['django', 'l10n', ]
+)
