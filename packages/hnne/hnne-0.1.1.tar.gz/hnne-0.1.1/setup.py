1f67df5e9a127efc165ee403521c0fdd368117a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:26:20 +0000

    Add package hnne/0.1.1/hnne-0.1.1.tar.gz

---
+++ packages/hnne/hnne-0.1.1.tar.gz/hnne-0.1.1/setup.py
@@ -0,0 +1,33 @@
+import setuptools
+
+
+setuptools.setup(
+    name="hnne",
+    version="0.1.1",
+    author="Marios Koulakis, Saquib Sarfraz",
+    author_email="marios.koulakis@gmail.com",
+    description="A fast hierarchical dimensionality reduction algorithm.",
+    packages=['hnne'],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "License :: Free for non-commercial use",
+        "Operating System :: Unix",
+        "Operating System :: MacOS",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
+    keywords="dimension dimensionality reduction t-sne umap hierarchical clustering finch",
+    install_requires=[
+        "scipy",
+        "numpy==1.20",
+        "sklearn",
+        "tqdm",
+        "pynndescent",
+        "typer",
+        "pandas",
+        "cython",
+    ],
+    test_suite="pytest",
+    tests_require=["pytest"],
+)
