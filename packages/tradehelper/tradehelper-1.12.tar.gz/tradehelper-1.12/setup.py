71e2b759830de72a17a0dab58087436a03abd85a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:20:23 +0000

    Add package tradehelper/1.12/tradehelper-1.12.tar.gz

---
+++ packages/tradehelper/tradehelper-1.12.tar.gz/tradehelper-1.12/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+setuptools.setup(
+    name='tradehelper',
+    version='1.12',
+    license='MIT',
+    author="preneeshav",
+    author_email='preneesh.av@gmail.com ',
+    # packages=find_packages('src'),
+    # package_dir={'': 'src'},
+        packages=setuptools.find_packages(),
+    url='https://github.com/preneeshav/tradehelp',
+    
+    keywords='trade help',
+    install_requires=[
+          'pandas','datetime'
+      ],
+
+)
