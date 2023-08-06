5b71864f794c5a1137dbbb84e7b34ec2dc6efe4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:43:04 +0000

    Add package soft-files/0.3.0/soft-files-0.3.0.tar.gz

---
+++ packages/soft-files/soft-files-0.3.0.tar.gz/soft-files-0.3.0/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['soft', 'soft.client', 'soft.server']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['PyNaCl>=1.5.0,<2.0.0', 'loguru>=0.6.0,<0.7.0']
+
+setup_kwargs = {
+    'name': 'soft-files',
+    'version': '0.3.0',
+    'description': 'A homegrown file transfer protocol.',
+    'long_description': None,
+    'author': 'Kronifer',
+    'author_email': '44979306+Kronifer@users.noreply.github.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
---
+++ packages/soft-files/soft-files-0.3.0.tar.gz/soft-files-0.3.0/soft/client/safe.py
@@ -0,0 +1,34 @@
+import nacl.secret
+import nacl.utils
+
+
+class Safe:
+    def __init__(self, key: str):
+        self.key = bytes.fromhex(key)
+        self.box = nacl.secret.SecretBox(self.key)
+
+    def encrypt_userinfo(self, userinfo: list) -> bytes:
+        return self.box.encrypt(f"{userinfo[0]}, {userinfo[1]}".encode())
+
+    def decrypt_userinfo_resp(self, encrypted_data: bytes) -> bool:
+        return bool(self.box.decrypt(encrypted_data))
+
+    def encrypt_cmd(self, command: str, args: list = None) -> bytes:
+        if command == "pwd":
+            return self.box.encrypt(command.encode())
+        elif command == "cd":
+            return self.box.encrypt(f"{command}, {' '.join(args)}".encode())
+        elif command == "ls":
+            return self.box.encrypt(command.encode())
+        elif command == "dir":
+            return self.box.encrypt(command.encode())
+        elif command == "put":
+            print(args[0])
+            return self.box.encrypt(
+                f"{command}, {args[0]}".encode() + b", " + args[1].encode()
+            )
+        elif command == "get":
+            return self.box.encrypt(f"{command}, {args[0]}".encode())
+
+    def decrypt_cmd_resp(self, encrypted: bytes) -> str:
+        return self.box.decrypt(encrypted).decode()
