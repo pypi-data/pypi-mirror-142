65ac3abb8cd23baaf6cc2e28ab4dc5012d53a0d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:09:27 +0000

    Add package londiste/3.10/londiste-3.10.tar.gz

---
+++ packages/londiste/londiste-3.10.tar.gz/londiste-3.10/tests/run.sh
@@ -0,0 +1,24 @@
+#! /bin/bash
+
+set -e
+set -x
+
+tests="qtable"
+tests="
+    simple
+    qtable
+    obfuscate
+    shard
+    register
+    takeover-merge
+"
+tests="shard"
+
+PAGER=cat
+export PAGER
+
+cd $(dirname $0)
+
+for tst in $tests; do
+    (cd $tst; ../zstop.sh; ./init.sh; ./regen.sh; ../zstop.sh)
+done
