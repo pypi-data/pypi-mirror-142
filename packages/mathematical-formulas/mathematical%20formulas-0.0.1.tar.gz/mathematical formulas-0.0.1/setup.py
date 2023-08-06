0fee6c69bf758cfc3bd90d1d04947afe9db7b4b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:19:07 +0000

    Add package mathematical-formulas/0.0.1/mathematical%20formulas-0.0.1.tar.gz

---
+++ packages/mathematical-formulas/mathematical%20formulas-0.0.1.tar.gz/mathematical formulas-0.0.1/setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+setuptools.setup(
+    name="mathematical formulas",
+    version="0.0.1",
+    author="szp",
+    author_email="18635154534@163.com",
+    description="这是一份关于存储数学公式的库，通过语音识别调用",
+    long_description_content_type="text/markdown",
+    url="https://github.com/",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
