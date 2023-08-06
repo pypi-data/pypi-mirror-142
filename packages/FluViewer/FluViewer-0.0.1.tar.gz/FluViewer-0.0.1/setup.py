50b00eae1c8443f79310bf3807fd0661eb3385fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:22:30 +0000

    Add package FluViewer/0.0.1/FluViewer-0.0.1.tar.gz

---
+++ packages/FluViewer/FluViewer-0.0.1.tar.gz/FluViewer-0.0.1/setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="FluViewer",
+    version="0.0.1",
+    author="Kevin Kuchinski",
+    author_email="kevin.kuchinski@bccdc.ca",
+    description="A tool for generating influenza A virus genome sequences from FASTQ data",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/KevinKuchinski/FluViewer",
+    project_urls={
+        "Bug Tracker": "https://github.com/KevinKuchinski/FluViewer/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.7",
+    entry_points={
+    'console_scripts': [
+        'FluViewer = FluViewer.FluViewer_v_0_0_1:main',
+    ],
+    }
+)
