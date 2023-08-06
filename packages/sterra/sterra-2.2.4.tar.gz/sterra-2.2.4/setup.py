4c3bf5cc2a7b2ef0bd65ce1aad45f4a41f94477c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:06:26 +0000

    Add package sterra/2.2.4/sterra-2.2.4.tar.gz

---
+++ packages/sterra/sterra-2.2.4.tar.gz/sterra-2.2.4/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'sterra',
+    version = '2.2.4',
+    description = 'A SOCMINT tool to analyse instagram followers | following of a target, and get informations from it.',
+    author = 'novitae',
+    url = 'https://github.com/novitae/sterraxcyl',
+    license = 'GNU General Public License v3 (GPLv3)',
+    classifiers = [
+        'Programming Language :: Python :: 3.9',
+    ],
+    packages = find_packages(),
+    install_requires = ['aiohttp', 'argparse', 'datetime', 'openpyxl', 'requests', 'string-color', 'tqdm'],
+    entry_points = {'console_scripts': ['sterra = sterra.core:main']}
+)
