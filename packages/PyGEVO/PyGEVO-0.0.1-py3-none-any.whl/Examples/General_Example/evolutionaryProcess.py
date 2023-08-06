c24659d1a61df9331ea9e2b0fab1431d0a7bc075
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:35:18 +0000

    Add package PyGEVO/0.0.1/PyGEVO-0.0.1-py3-none-any.whl

---
+++ packages/PyGEVO/PyGEVO-0.0.1-py3-none-any.whl/Examples/General_Example/evolutionaryProcess.py
@@ -0,0 +1,18 @@
+#from compiler import Compiler
+#comp=Compiler()
+#comp.enableCython()
+#comp.compile()
+import sys
+sys.path.append('../../')
+from core.domain.population import Population
+from core.domain.algorithms import Algorithms
+from core.fitnessFunctions.syntheticFunctions import FitnessFunctions
+import time
+
+s = time.time()
+grammar="grammar.bnf"
+pop = Population(grammar,numberIndividuals=10000, individualSize=8, fitness_function=FitnessFunctions.rosenbrock)
+population = pop.generatePop()
+algo = Algorithms(grammar, initBNF=56)
+evolvedPop = algo.evolveWithGE(population, populationFactory=pop, gen=400, porcentSelect=0.6, staticSelection=250,debug=True, cacheScore=False)
+print("Time taken:", time.time() - s)
