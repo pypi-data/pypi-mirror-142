b3fe393e6c26eccd3798e2ef88306aac5fcb2b18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:54:09 +0000

    Add package fief-server/0.3.0/fief-server-0.3.0.tar.gz

---
+++ packages/fief-server/fief-server-0.3.0.tar.gz/fief-server-0.3.0/fief/dependencies/global_managers.py
@@ -0,0 +1,34 @@
+from fastapi import Depends
+
+from fief.db import AsyncSession, get_global_async_session
+from fief.managers import (
+    AccountManager,
+    AccountUserManager,
+    AdminAPIKeyManager,
+    AdminSessionTokenManager,
+    get_manager,
+)
+
+
+async def get_account_manager(
+    session: AsyncSession = Depends(get_global_async_session),
+) -> AccountManager:
+    return get_manager(AccountManager, session)
+
+
+async def get_admin_session_token_manager(
+    session: AsyncSession = Depends(get_global_async_session),
+) -> AdminSessionTokenManager:
+    return get_manager(AdminSessionTokenManager, session)
+
+
+async def get_admin_api_key_manager(
+    session: AsyncSession = Depends(get_global_async_session),
+) -> AdminAPIKeyManager:
+    return get_manager(AdminAPIKeyManager, session)
+
+
+async def get_account_user_manager(
+    session: AsyncSession = Depends(get_global_async_session),
+) -> AccountUserManager:
+    return get_manager(AccountUserManager, session)
---
+++ packages/fief-server/fief-server-0.3.0.tar.gz/fief-server-0.3.0/fief/models/__init__.py
@@ -0,0 +1,33 @@
+from fief.models.account import Account
+from fief.models.account_user import AccountUser
+from fief.models.admin_api_key import AdminAPIKey
+from fief.models.admin_session_token import AdminSessionToken
+from fief.models.authorization_code import AuthorizationCode
+from fief.models.base import AccountBase, GlobalBase
+from fief.models.client import Client
+from fief.models.generics import M_UUID, M
+from fief.models.grant import Grant
+from fief.models.login_session import LoginSession
+from fief.models.refresh_token import RefreshToken
+from fief.models.session_token import SessionToken
+from fief.models.tenant import Tenant
+from fief.models.user import User
+
+__all__ = [
+    "Account",
+    "AccountBase",
+    "AccountUser",
+    "AdminAPIKey",
+    "AdminSessionToken",
+    "AuthorizationCode",
+    "Client",
+    "GlobalBase",
+    "Grant",
+    "LoginSession",
+    "RefreshToken",
+    "SessionToken",
+    "M",
+    "M_UUID",
+    "Tenant",
+    "User",
+]
