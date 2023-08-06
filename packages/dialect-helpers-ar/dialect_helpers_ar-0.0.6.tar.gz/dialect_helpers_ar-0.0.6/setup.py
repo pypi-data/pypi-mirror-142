e7572f6feb894b9babdeee7957a837e94b00745d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:24:01 +0000

    Add package dialect-helpers-ar/0.0.6/dialect_helpers_ar-0.0.6.tar.gz

---
+++ packages/dialect-helpers-ar/dialect_helpers_ar-0.0.6.tar.gz/dialect_helpers_ar-0.0.6/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.6' 
+DESCRIPTION = 'Some helper functions to help with arabic dialects classification proplem'
+
+setup(
+	name='dialect_helpers_ar',
+	version=VERSION,
+	author='Ahmed Elshehawy',
+	author_email='a.elshehawy64@gmail.com',
+	packages=find_packages()
+)
