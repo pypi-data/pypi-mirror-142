daf0c39a77064b3aeae732405a0720dd97e460e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:09:31 +0000

    Add package plateau/4.0.4/plateau-4.0.4.tar.gz

---
+++ packages/plateau/plateau-4.0.4.tar.gz/plateau-4.0.4/tests/io/eager/test_gc.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+import pytest
+
+from plateau.io.eager import garbage_collect_dataset
+from plateau.io.testing.gc import *  # noqa: F4
+
+
+@pytest.fixture()
+def garbage_collect_callable():
+    return garbage_collect_dataset
---
+++ packages/plateau/plateau-4.0.4.tar.gz/plateau-4.0.4/tests/io/iter/test_write.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+import pytest
+
+from plateau.io.iter import store_dataframes_as_dataset__iter
+from plateau.io.testing.write import *  # noqa
+
+
+def _store_dataframes(df_list, *args, **kwargs):
+    df_generator = (x for x in df_list)
+    return store_dataframes_as_dataset__iter(df_generator, *args, **kwargs)
+
+
+@pytest.fixture()
+def bound_store_dataframes():
+    return _store_dataframes
