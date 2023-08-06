68aad4db104d808dd75f736e97cef10fd90a7826
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:03 +0000

    Add package linefreq/0.1.0/linefreq-0.1.0.tar.gz

---
+++ packages/linefreq/linefreq-0.1.0.tar.gz/linefreq-0.1.0/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['fq']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['rich>=12.0.0,<13.0.0']
+
+entry_points = \
+{'console_scripts': ['fq = fq.cli:run']}
+
+setup_kwargs = {
+    'name': 'linefreq',
+    'version': '0.1.0',
+    'description': 'a real time line frequency utility',
+    'long_description': None,
+    'author': 'redraw',
+    'author_email': 'redraw@sdf.org',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/redraw/fq',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.6.2,<4.0',
+}
+
+
+setup(**setup_kwargs)
