cdf677edfb22ea0dc0f8f5e8d8d14d7531767221
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:24:37 +0000

    Add package AthenaColor/2.0.0/AthenaColor-2.0.0-py3-none-any.whl

---
+++ packages/AthenaColor/AthenaColor-2.0.0-py3-none-any.whl/AthenaColor/__init__.py
@@ -0,0 +1,9 @@
+import AthenaColor.Objects
+import AthenaColor.Predefined
+import AthenaColor.ConsolePrinter
+
+Colors = AthenaColor.Predefined.Colors
+
+# DOCU
+import AthenaColor.Help
+from AthenaColor.readme import readme
