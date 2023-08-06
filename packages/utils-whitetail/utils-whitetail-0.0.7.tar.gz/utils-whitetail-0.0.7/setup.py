184242ce7bd1d3f8c4d588b9d295db78fb438ee9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:27:18 +0000

    Add package utils-whitetail/0.0.7/utils-whitetail-0.0.7.tar.gz

---
+++ packages/utils-whitetail/utils-whitetail-0.0.7.tar.gz/utils-whitetail-0.0.7/setup.py
@@ -0,0 +1,32 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+with open("requirements.txt", "r", encoding="utf-8") as file:
+    reqs=file.read()
+    
+reqs=reqs.split("\n")
+
+setuptools.setup(
+    name="utils-whitetail",
+    version="0.0.7",
+    author="Ankesh Gautam",
+    author_email="ankesh.gautam@whitetail.in",
+    description="Common utils",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://bitbucket.org/mediblock/lib-web-scrape-utils/",
+    project_urls={
+        "Bug Tracker": "https://bitbucket.org/mediblock/lib-web-scrape-utils/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    install_requires=reqs,
+    python_requires=">=3.6",
+)
