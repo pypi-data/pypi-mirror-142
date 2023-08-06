fc136ad28259b6a055835f93848fc3c88d477cf8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:26:10 +0000

    Add package lcapy/1.4/lcapy-1.4.tar.gz

---
+++ packages/lcapy/lcapy-1.4.tar.gz/lcapy-1.4/lcapy/dexpr.py
@@ -0,0 +1,15 @@
+"""This modules provides the DiscreteExpression class to provide
+common methods for the discrete-time representations.
+
+Copyright 2020 Michael Hayes, UCECE
+
+"""
+
+from .expr import Expr
+
+class DiscreteExpression(Expr):
+
+    """Superclass of discrete-time, discrete-frequency, and z-domain
+    expressions."""
+
+    pass
