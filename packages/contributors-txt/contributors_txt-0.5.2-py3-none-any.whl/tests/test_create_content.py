446529eba93095fa1607cc3f7ad60018d354d957
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:17:58 +0000

    Add package contributors-txt/0.5.2/contributors_txt-0.5.2-py3-none-any.whl

---
+++ packages/contributors-txt/contributors_txt-0.5.2-py3-none-any.whl/tests/test_create_content.py
@@ -0,0 +1,33 @@
+import logging
+
+import pytest
+
+from contributors_txt.create_content import create_content
+
+
+@pytest.mark.parametrize(
+    "shortlog_output,expected",
+    [
+        [
+            "1 name <email@net.com>",
+            "<email@net.com>",
+        ],
+        [
+            "1 another_name <email@net.com>",
+            "- another_name",
+        ],
+        [
+            "\n1 name <aemail@net.com>\n2 another_name <email@net.com>",
+            """- another_name <email@net.com>
+- name <aemail@net.com>
+""",
+        ],
+    ],
+)
+def test_basic(shortlog_output: str, expected: str, caplog):
+    caplog.set_level(logging.DEBUG)
+    result = create_content(
+        aliases=[], shortlog_output=shortlog_output, configuration_file="foo.conf"
+    )
+    assert expected in result
+    assert "using the configuration in 'foo.conf'" in result
