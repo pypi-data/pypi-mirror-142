b264b2c50c9adbf4131bdfbc888c526a6dbfcc0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:10:15 +0000

    Add package utsavcalc/0.2/utsavcalc-0.2-py3-none-any.whl

---
+++ packages/utsavcalc/utsavcalc-0.2-py3-none-any.whl/utsavcalc/__init__.py
@@ -0,0 +1,23 @@
+class calculator:
+    def __init__(self):
+        print('''           
+                          Welcome to Utsav's Basic Calculator
+              
+Guide : 1) To Add two number use code [o.sum(a,b)](o is the object of the class) --> returns a + b
+        2) To Subtract two number use code [o.sub(a,b)] --> returns a - b
+        3) To Multiply two number use code [o.mul(a,b)] --> returns a * b
+        4) To Divide two numbers use code [o.div(a,b)] --> returns a/b ''')
+              
+              
+        
+    def sum(self,num1,num2):
+        return num1 + num2
+
+    def sub(self,num1,num2):
+        return num1 - num2
+
+    def mul(self,num1,num2):
+        return num1 * num2
+    
+    def div(self,num1,num2):
+        return num1 / num2
