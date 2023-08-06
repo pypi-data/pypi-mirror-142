a8abdb18df87acc50116677f9396df9b7d034a7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:25:13 +0000

    Add package awesome-sso/0.2.19/awesome-sso-0.2.19.tar.gz

---
+++ packages/awesome-sso/awesome-sso-0.2.19.tar.gz/awesome-sso-0.2.19/awesome_sso/mail/mailgun.py
@@ -0,0 +1,66 @@
+import json
+from io import TextIOWrapper
+from typing import List
+
+import requests
+from pydantic import EmailStr, HttpUrl
+
+
+class MailGun:
+    def __init__(
+        self,
+        base_url: HttpUrl,
+        api_key: str,
+    ):
+        self.base_url = base_url
+        self.api_key = api_key
+
+    def send_simple_message(
+        self,
+        from_name: str,
+        from_email: EmailStr,
+        to: List[EmailStr],
+        subject: str,
+        text: str,
+        files: List[TextIOWrapper] = [],
+        cc: List[EmailStr] = [],
+        bcc: List[EmailStr] = [],
+    ):
+        return requests.post(
+            self.base_url + "/messages",
+            auth=("api", self.api_key),
+            files=files,
+            data={
+                "from": "%s <%s>" % (from_name, from_email),
+                "to": to,
+                "cc": cc,
+                "bcc": bcc,
+                "subject": subject,
+                "text": text,
+            },
+        )
+
+    def send_template(
+        self,
+        from_name: str,
+        from_email: EmailStr,
+        to: List[EmailStr],
+        subject: str,
+        template: str,
+        data: dict,
+        cc: List[EmailStr] = [],
+        bcc: List[EmailStr] = [],
+    ):
+        return requests.post(
+            self.base_url + "/messages",
+            auth=("api", self.api_key),
+            data={
+                "from": "%s <%s>" % (from_name, from_email),
+                "to": to,
+                "cc": cc,
+                "bcc": bcc,
+                "subject": subject,
+                "template": template,
+                "h:X-Mailgun-Variables": json.dumps(data),
+            },
+        )
