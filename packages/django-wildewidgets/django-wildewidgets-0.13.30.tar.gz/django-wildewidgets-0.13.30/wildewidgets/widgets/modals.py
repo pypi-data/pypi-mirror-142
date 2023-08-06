e4f180f88c4b466cbd1c429d1831dc8ab12db9e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:50:58 +0000

    Add package django-wildewidgets/0.13.30/django-wildewidgets-0.13.30.tar.gz

---
+++ packages/django-wildewidgets/django-wildewidgets-0.13.30.tar.gz/django-wildewidgets-0.13.30/wildewidgets/widgets/modals.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from wildewidgets.widgets.structure import CrispyFormWidget
+from .base import TemplateWidget
+
+from django.core.exceptions import ImproperlyConfigured
+
+
+class ModalWidget(TemplateWidget):
+    template_name = 'wildewidgets/modal.html'
+    modal_id = None
+    modal_title = None
+    modal_body = None
+
+    def __init__(self, modal_id=None, modal_title=None, modal_body=None, **kwargs):
+        self.modal_id = modal_id if modal_id else self.modal_id
+        self.modal_title = modal_title if modal_title else self.modal_title
+        self.modal_body = modal_body if modal_body else self.modal_body
+        super().__init__(**kwargs)
+
+    def get_context_data(self, **kwargs):
+        kwargs = super().get_context_data(**kwargs)
+        kwargs['modal_id'] = self.modal_id
+        kwargs['modal_title'] = self.modal_title
+        kwargs['modal_body'] = self.modal_body
+        return kwargs
+
+
+class CrispyFormModalWidget(ModalWidget):
+    form_class = None
+    form = None
+
+    def __init__(self, form=None, form_class=None, **kwargs):
+        if form_class:
+            modal_form = form_class()
+        elif form:
+            modal_form = form
+        else:
+            raise ImproperlyConfigured("Either 'form_class' or 'form' must be set")
+        modal_body = CrispyFormWidget(form=modal_form)
+        kwargs['modal_body'] = modal_body
+        super().__init__(**kwargs)
+
+
