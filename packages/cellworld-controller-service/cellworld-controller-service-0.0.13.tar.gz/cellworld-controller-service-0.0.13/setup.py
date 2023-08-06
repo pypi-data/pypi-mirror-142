a9dcfc916012ee3d49c24039c56b918b8df93529
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:20:16 +0000

    Add package cellworld-controller-service/0.0.13/cellworld-controller-service-0.0.13.tar.gz

---
+++ packages/cellworld-controller-service/cellworld-controller-service-0.0.13.tar.gz/cellworld-controller-service-0.0.13/setup.py
@@ -0,0 +1,3 @@
+from setuptools import setup
+
+setup(name='cellworld-controller-service',description='Controller client',author='German Espinosa',author_email='germanespinosa@gmail.com',packages=['cellworld_controller_service'],install_requires=['cellworld_experiment_service'],license='MIT',version='0.0.13',zip_safe=False)
