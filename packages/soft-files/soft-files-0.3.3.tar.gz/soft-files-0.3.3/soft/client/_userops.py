3f56f6b3e2017f05a7639f20bf80fb1bf51b1bb9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:46:25 +0000

    Add package soft-files/0.3.3/soft-files-0.3.3.tar.gz

---
+++ packages/soft-files/soft-files-0.3.3.tar.gz/soft-files-0.3.3/soft/client/_userops.py
@@ -0,0 +1,29 @@
+from .safe import Safe
+import socket
+
+
+def get_userinfo():
+    username = input("Username: ")
+    if username == "":
+        print("Username cannot be empty!")
+        get_userinfo()
+    password = input("Password: ")
+    if password == "":
+        print("Password cannot be empty!")
+        get_userinfo()
+    return [username, password]
+
+
+def login(safe: Safe, server: socket.socket):
+    while True:
+        uinfo = get_userinfo()
+        uinfo = safe.encrypt_userinfo(uinfo)
+        server.sendall(uinfo)
+        resp = safe.decrypt_userinfo_resp(server.recv(1024))
+        if not resp:
+            print("Username or password is incorrect!")
+            continue
+        else:
+            print("Login successful!")
+            break
+    return
---
+++ packages/soft-files/soft-files-0.3.3.tar.gz/soft-files-0.3.3/soft/server/safe.py
@@ -0,0 +1,20 @@
+import nacl.secret
+import nacl.utils
+
+
+class Safe:
+    def __init__(self, key: str):
+        self.key = bytes.fromhex(key)
+        self.box = nacl.secret.SecretBox(self.key)
+
+    def decrypt_userinfo(self, encrypted: bytes) -> list:
+        return self.box.decrypt(encrypted).decode().split(", ")
+
+    def encrypt_userinfo_resp(self, resp: bool) -> bytes:
+        return self.box.encrypt(bytes(resp))
+
+    def encrypt_cmd_resp(self, resp: str) -> bytes:
+        return self.box.encrypt(resp.encode())
+
+    def decrypt_cmd(self, encrypted: bytes) -> str:
+        return self.box.decrypt(encrypted).decode().split(", ")
