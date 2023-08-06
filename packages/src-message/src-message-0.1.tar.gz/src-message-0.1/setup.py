69210194ded138c5e297ef3c49f8363e266aceb0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:20:08 +0000

    Add package src-message/0.1/src-message-0.1.tar.gz

---
+++ packages/src-message/src-message-0.1.tar.gz/src-message-0.1/setup.py
@@ -0,0 +1,18 @@
+# 通过 setuptools 模块导入所需要的函数
+from setuptools import setup,find_packages
+setup(
+    name="src-message",
+    version="0.1",
+    author="ljj",
+    # url="ckh.handsome.com",
+    packages = find_packages("src"),
+    package_dir = {"":"src"},
+    package_data = {
+        # 定义打包除了 .py 之外的文件类型，此处 .py 其实可以不写
+        "":[".txt",".info","*.properties",".py"],
+        # 包含 data 文件夹下所有的 *.dat 文件
+        "":["data/*.*"],
+    },
+# 取消所有测试包
+    exclude = ["*.test","*.test.*","test.*","test"]
+)
---
+++ packages/src-message/src-message-0.1.tar.gz/src-message-0.1/src/ljj/handsome/boy/message.py
@@ -0,0 +1,2 @@
+def get_info():
+    return "ckh_handsome_boy"
