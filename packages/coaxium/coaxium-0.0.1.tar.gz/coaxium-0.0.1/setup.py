03c379eb28f254827de74145c306209f7f5dadd6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:40:29 +0000

    Add package coaxium/0.0.1/coaxium-0.0.1.tar.gz

---
+++ packages/coaxium/coaxium-0.0.1.tar.gz/coaxium-0.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='coaxium',
+    version='0.0.1',
+    description='',
+    long_description='',
+    url='https://github.com/Coaxium/coaxium',
+    author='Miles Adkins',
+    author_email='milescadkins@gmail.com',
+    packages=[],
+    classifiers=['Development Status :: 1 - Planning'],
+)
