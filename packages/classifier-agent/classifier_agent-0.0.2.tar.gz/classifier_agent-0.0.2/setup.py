d961b85d671db393193b30bc148ed6fcf967679a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:25:33 +0000

    Add package classifier-agent/0.0.2/classifier_agent-0.0.2.tar.gz

---
+++ packages/classifier-agent/classifier_agent-0.0.2.tar.gz/classifier_agent-0.0.2/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+import setuptools
+ 
+with open("Readme.md", "r") as myfile:
+  long_description = myfile.read()
+ 
+setup(
+  name='classifier_agent',
+  version='0.0.2',
+  description='A simple package to perform classification on a given dataset in csv or excel format',
+  long_description = long_description,
+  long_description_content_type="text/markdown",
+  url='https://github.com/adnanmushtaq1996/ML-Classifier-Python-Package',  
+  author='Adnan Karol',
+  author_email='adnanmushtaq5@gmail.com',
+  license='MIT', 
+  classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",],
+  keywords= ['machine learning', 'classification', 'random forest', 
+             'xgboost', 'svm', 'logistic regression', 'naive bayes', 'knn', 'decision tree'], 
+  packages=find_packages(),
+  install_requires=['sklearn', 'pandas']
+)
