06bed7e6336da099de6f34a316155a6d55bc1803
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:12:30 +0000

    Add package archipelagos/0.0.4/archipelagos-0.0.4.tar.gz

---
+++ packages/archipelagos/archipelagos-0.0.4.tar.gz/archipelagos-0.0.4/setup.py
@@ -0,0 +1,38 @@
+import pathlib
+from setuptools import setup
+
+HERE = pathlib.Path(__file__).parent
+README = (HERE / "README.md").read_text()
+
+setup(
+    name="archipelagos",
+    version="0.0.4",
+    description="The Python Client for the Archipelagos platform.",
+
+    long_description=README,
+    long_description_content_type="text/markdown",
+
+    url="https://www.archipelagos-labs.com",
+    license="Apache License, Version 2.0",
+
+    author="Archipelagos Labs",
+    author_email="support@archipelagos-labs.com",
+
+    classifiers=["Programming Language :: Python :: 3",
+                 "Programming Language :: Python :: 3.7",
+                 "Programming Language :: Python :: 3.8",
+                 "Programming Language :: Python :: 3.9",
+                 "Programming Language :: Python :: 3.10"],
+
+    packages=["archipelagos",
+              "archipelagos.common",
+              "archipelagos.common.data"],
+
+    install_requires=["geopandas>=0.10.2",
+                      "numpy>=1.21.2",
+                      "pandas>=1.3.5",
+                      "protobuf>=3.19.1",
+                      "requests>=2.27.1"],
+
+    include_package_data=True
+)
