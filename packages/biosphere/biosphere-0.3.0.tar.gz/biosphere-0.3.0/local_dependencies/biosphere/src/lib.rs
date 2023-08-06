399cf00d84612339e3e49dd5ec01c139186504e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:55:47 +0000

    Add package biosphere/0.3.0/biosphere-0.3.0.tar.gz

---
+++ packages/biosphere/biosphere-0.3.0.tar.gz/biosphere-0.3.0/local_dependencies/biosphere/src/lib.rs
@@ -0,0 +1,12 @@
+// Allow capital X for arrays.
+#![allow(non_snake_case)]
+pub use forest::RandomForest;
+pub use forest::RandomForestParameters;
+pub use tree::{DecisionTree, DecisionTreeParameters, MaxFeatures};
+mod forest;
+mod quick_sort;
+mod tree;
+pub mod utils; // This is public for benchmarking only.
+
+#[cfg(test)]
+mod testing;
---
+++ packages/biosphere/biosphere-0.3.0.tar.gz/biosphere-0.3.0/src/lib.rs
@@ -0,0 +1,12 @@
+mod decision_tree;
+mod random_forest;
+mod utils;
+
+use pyo3::prelude::{pymodule, PyModule, PyResult, Python};
+
+#[pymodule]
+fn biosphere(_py: Python, m: &PyModule) -> PyResult<()> {
+    m.add_class::<decision_tree::DecisionTree>()?;
+    m.add_class::<random_forest::RandomForest>()?;
+    Ok(())
+}
