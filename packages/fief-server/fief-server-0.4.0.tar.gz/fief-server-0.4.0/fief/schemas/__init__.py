1ffdb9092716f4ad61ebd57518e6c85aaee7c6af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:14:14 +0000

    Add package fief-server/0.4.0/fief-server-0.4.0.tar.gz

---
+++ packages/fief-server/fief-server-0.4.0.tar.gz/fief-server-0.4.0/fief/schemas/__init__.py
@@ -0,0 +1,23 @@
+from fief.schemas import (
+    account,
+    admin_api_key,
+    auth,
+    client,
+    generics,
+    reset,
+    tenant,
+    user,
+    well_known,
+)
+
+__all__ = [
+    "account",
+    "admin_api_key",
+    "auth",
+    "client",
+    "generics",
+    "reset",
+    "tenant",
+    "user",
+    "well_known",
+]
