7a22b08fc06471f3a0c4292c99f861745886971b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:09:14 +0000

    Add package fast-dash/0.1.3/fast_dash-0.1.3-py3-none-any.whl

---
+++ packages/fast-dash/fast_dash-0.1.3-py3-none-any.whl/fast_dash/__init__.py
@@ -0,0 +1,10 @@
+"""Top-level package for Fast Dash."""
+
+__author__ = """Kedar Dabhadkar"""
+__email__ = "kdabhadk@gmail.com"
+__version__ = "0.1.3"
+
+from fast_dash.fast_dash import FastDash
+from fast_dash.Components import Fastify
+
+__all__ = ["FastDash", "Fastify"]
