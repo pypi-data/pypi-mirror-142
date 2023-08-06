68f48ed82a7a3571c80dc0e5c45e704546ff6968
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:18:05 +0000

    Add package tickstore-grpc/1.0/tickstore-grpc-1.0.tar.gz

---
+++ packages/tickstore-grpc/tickstore-grpc-1.0.tar.gz/tickstore-grpc-1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='tickstore-grpc',
+    version='1.0',
+    packages=[''],
+    url='https://gitlab.com/alphaticks/tickstore-grpc',
+    license='copyright',
+    author='Laurent Meunier',
+    description='gRPC code for tickstore client'
+)
