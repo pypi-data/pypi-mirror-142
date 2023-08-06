7d00ade062a83994ff9f3571e8b47a4aa5c28a21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:09:22 +0000

    Add package ipytablewidgets/0.2.4/ipytablewidgets-0.2.4.tar.gz

---
+++ packages/ipytablewidgets/ipytablewidgets-0.2.4.tar.gz/ipytablewidgets-0.2.4/js/webpack.config.js
@@ -0,0 +1,51 @@
+const path = require('path');
+const nbextPass = path.resolve(__dirname, '../ipytablewidgets/static');
+const distPass = path.resolve(__dirname, 'dist');
+
+const outputLibraryTarget = "amd";
+const rules = [];
+
+module.exports = [
+  {
+    // Notebook extension
+    entry: './lib/extension.js',
+      output: {
+      hashFunction: "sha256",
+      filename: 'extension.js',
+      path: nbextPass,
+      libraryTarget: outputLibraryTarget
+    },
+    externals: ['@jupyter-widgets/base'],
+    mode: 'production',
+  },
+  {
+    entry: './lib/index.js',
+      output: {
+      hashFunction: "sha256",
+      filename: 'index.js',
+      path: nbextPass,
+      libraryTarget: outputLibraryTarget
+    },
+    devtool: 'source-map',
+    module: {
+      rules: rules,
+    },
+    externals: ['@jupyter-widgets/base'],
+    mode: 'production',
+  },
+  {
+    entry: './lib/index.js',
+      output: {
+      hashFunction: "sha256",
+      filename: 'index.js',
+      path: distPass,
+      libraryTarget: outputLibraryTarget
+    },
+    devtool: 'source-map',
+    module: {
+      rules: rules,
+    },
+    externals: ['@jupyter-widgets/base'],
+    mode: 'production',
+  },
+];
