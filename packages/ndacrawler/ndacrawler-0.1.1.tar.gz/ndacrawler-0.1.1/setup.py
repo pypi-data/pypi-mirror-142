56d13c9056dc7a88807c68af948a1256adf7d536
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:26:21 +0000

    Add package ndacrawler/0.1.1/ndacrawler-0.1.1.tar.gz

---
+++ packages/ndacrawler/ndacrawler-0.1.1.tar.gz/ndacrawler-0.1.1/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="ndacrawler",
+    version="0.1.1",
+    author="Antonio Mande",
+    author_email="mandetonny@gmail.com",
+    description="A simple crawler for the NDA website",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/AntonioMande/ndacrawler",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src", exclude=['tests*']),
+    python_requires=">=3.6",
+    install_requires=["requests", "beautifulsoup4==4.10", "lxml", "cchardet"]
+)
