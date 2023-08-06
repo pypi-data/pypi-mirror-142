fc94773bd1e3fe40d38738d73d1e77b575aeb1ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:27:58 +0000

    Add package frc-livescore-ng/0.0.1rc106/frc-livescore-ng-0.0.1rc106.tar.gz

---
+++ packages/frc-livescore-ng/frc-livescore-ng-0.0.1rc106.tar.gz/frc-livescore-ng-0.0.1rc106/livescore/__init__.py
@@ -0,0 +1,9 @@
+# flake8: noqa
+from .LivescoreBase import NoOverlayFoundException
+from .LivescoreBase import InvalidScaleException
+from .Livescore2017 import Livescore2017
+from .Livescore2018 import Livescore2018
+from .Livescore2019 import Livescore2019
+from .Livescore2020 import Livescore2020
+from .Livescore2021 import Livescore2021
+from .Livescore2022 import Livescore2022
---
+++ packages/frc-livescore-ng/frc-livescore-ng-0.0.1rc106.tar.gz/frc-livescore-ng-0.0.1rc106/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='frc-livescore-ng',
+    version='0.0.1-pre106',
+    description='Get FRC scores from an image',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author='Blake Bourque',
+    url='https://github.com/TechplexEngineer/frc-livescore',
+    keywords=['frc', 'score', 'robotics'],
+    license='MIT',
+    packages=find_packages(exclude=('tests', 'docs')),
+    package_data={'livescore': ['templates/*.png', 'tessdata/*.traineddata', 'training_data/*.pkl']}
+)
