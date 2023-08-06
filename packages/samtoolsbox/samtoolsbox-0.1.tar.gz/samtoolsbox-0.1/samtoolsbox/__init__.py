429e335efa5c227f634428f499a36523baaa0fce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:18:14 +0000

    Add package samtoolsbox/0.1/samtoolsbox-0.1.tar.gz

---
+++ packages/samtoolsbox/samtoolsbox-0.1.tar.gz/samtoolsbox-0.1/samtoolsbox/__init__.py
@@ -0,0 +1,2 @@
+def hello_sam():
+    print(114514)
---
+++ packages/samtoolsbox/samtoolsbox-0.1.tar.gz/samtoolsbox-0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='samtoolsbox',
+    version=0.1,
+    description=(
+        'sam\'s tools box'
+    ),
+    #long_description=open('README.rst').read(),
+    author='sam',
+    author_email='1102566608@qq.com',
+    license='BSD License',
+    packages=find_packages(),
+    platforms=["all"],
+    url='http://b.isam.top/',
+)
