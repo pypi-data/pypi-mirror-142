60f316a200db7e8b5451a8d07cc87907b8f43c55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:45:09 +0000

    Add package cping/0.1.6/cping-0.1.6.tar.gz

---
+++ packages/cping/cping-0.1.6.tar.gz/cping-0.1.6/setup.py
@@ -0,0 +1,27 @@
+from setuptools import find_packages, setup
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+setup(
+    name='cping',
+    author='hSaria',
+    author_email='sariahajjar@gmail.com',
+    classifiers=[
+        'Intended Audience :: System Administrators',
+        'Intended Audience :: Telecommunications Industry',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3', 'Topic :: Internet',
+        'Topic :: System :: Networking :: Monitoring', 'Topic :: Utilities'
+    ],
+    description='Concurrent multi-host ping (ICMP and TCP)',
+    entry_points={'console_scripts': ['cping = cping.__main__:main']},
+    install_requires=['windows-curses; sys_platform == "win32"'],
+    license='MIT',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    packages=find_packages(),
+    python_requires='>=3.6.0',
+    url='https://github.com/hSaria/cPing',
+    version='0.1.6',
+)
