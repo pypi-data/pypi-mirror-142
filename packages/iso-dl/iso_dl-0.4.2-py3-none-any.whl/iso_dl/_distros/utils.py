74b8ef03c5951ddfbd9a769e1ff4304b7a7f00a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:34:49 +0000

    Add package iso-dl/0.4.2/iso_dl-0.4.2-py3-none-any.whl

---
+++ packages/iso-dl/iso_dl-0.4.2-py3-none-any.whl/iso_dl/_distros/utils.py
@@ -0,0 +1,37 @@
+# iso-dl # distros # utils #
+
+import re
+import requests
+from bs4 import BeautifulSoup
+
+__all__ = ("parse_key_value_pairs", "dict_keys_to_lowercase", "get_links_from_url", "by_end")
+
+
+def parse_key_value_pairs(key_value_pairs: str) -> dict[str, str]:
+    """
+    Parser for this format:
+    key1 = value1
+    key2 = value2
+    """
+    return dict(re.findall(r"^(.*\S)\s*=\s*[\"'`]{0,1}(.*[^\"'`\n])[\"'`]{0,1}$", key_value_pairs, re.M))
+
+
+def dict_keys_to_lowercase(dict: dict) -> dict:
+    return {key.lower(): value for key, value in dict.items()}
+
+
+def get_links_from_url(url: str) -> list[str]:
+    """
+    Get all links from a given url.
+    """
+
+    r = requests.get(url)
+    soup = BeautifulSoup(r.text, "html.parser")
+    return [link.get("href") for link in soup.find_all("a")]
+
+
+def by_end(data: list, end: str) -> list:
+    """
+    Filter a list by the end of the string.
+    """
+    return [i for i in data if i.endswith(end)]
