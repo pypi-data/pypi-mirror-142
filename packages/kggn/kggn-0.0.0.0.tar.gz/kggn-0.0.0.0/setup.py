6c29cace09bf61319e7abbbcdabbf0a6be5440c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:09:49 +0000

    Add package kggn/0.0.0.0/kggn-0.0.0.0.tar.gz

---
+++ packages/kggn/kggn-0.0.0.0.tar.gz/kggn-0.0.0.0/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+ 
+classifiers = [
+]
+ 
+setup(
+  name="kggn",
+  version="0.0.0.0",
+  description="",
+  long_description="",
+  url="",  
+  author="kggn",
+  author_email="",
+  license="MIT", 
+  classifiers=classifiers,
+  keywords="", 
+  packages=find_packages(),
+  install_requires=[""] 
+)
