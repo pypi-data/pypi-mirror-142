ad7ff2a0c97ef35d6298f96fbb1e64d65345a584
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:16:23 +0000

    Add package nonebot-plugin-setu/1.0.7/nonebot_plugin_setu-1.0.7-py3-none-any.whl

---
+++ packages/nonebot-plugin-setu/nonebot_plugin_setu-1.0.7-py3-none-any.whl/nonebot_plugin_setu/dao/image_dao.py
@@ -0,0 +1,42 @@
+import json
+import sqlite3
+
+
+class ImageDao:
+    def __init__(self):
+        self.db_path = 'data/lolicon.db'
+
+    def add_images(self, datas):
+        conn = sqlite3.connect(self.db_path)
+        cursor = conn.cursor()
+        for data in datas:
+            pid = data['pid']
+            uid = data['uid']
+            title = data['title']
+            author = data['author']
+            r18 = data['r18']
+            width = data['width']
+            height = data['height']
+            ext = data['ext']
+            urls = json.dumps(data['urls'])
+            upload_date = data['uploadDate']
+            sql = "INSERT OR IGNORE INTO lolicon_images(pid, uid, title, author, r18, width, height, ext, urls, upload_date) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"
+            cursor.execute(sql, (pid, uid, title, author, r18, width, height, ext, urls, upload_date))
+            tags = data['tags']
+            for tag in tags:
+                sql = "INSERT OR IGNORE INTO lolicon_tags(pid, tags) VALUES (?, ?)"
+                cursor.execute(sql, (pid, tag))
+        conn.commit()
+        conn.close()
+
+    def get_images(self, pid):
+        conn = sqlite3.connect(self.db_path)
+        cursor = conn.cursor()
+        sql = f"select * from lolicon_images where pid='{pid}'"
+        cursor.execute(sql)
+        img_info = cursor.fetchone()
+        data = {}
+        for idx, col in enumerate(cursor.description):
+            data[col[0]] = img_info[idx]
+        conn.close()
+        return data
