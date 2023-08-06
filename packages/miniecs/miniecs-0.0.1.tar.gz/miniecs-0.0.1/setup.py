02b9b9ff51eb633f9bfb7e58e182f0c33e74d511
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:55:40 +0000

    Add package miniecs/0.0.1/miniecs-0.0.1.tar.gz

---
+++ packages/miniecs/miniecs-0.0.1.tar.gz/miniecs-0.0.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+VERSION = '0.0.1'
+DESCRIPTION = 'N/A'
+LONG_DESCRIPTION = 'N/A'
+
+# Setting up
+setup(
+    name="miniecs",
+    version=VERSION,
+    author="Juan Felipe",
+    author_email="juanfelipe22103@gmail.com",
+    description=DESCRIPTION,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python', 'sockets'],
+    classifiers=[],
+)
