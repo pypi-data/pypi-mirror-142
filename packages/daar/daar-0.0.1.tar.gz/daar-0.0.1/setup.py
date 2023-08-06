d31cc66ba1b7187b1883649d9ecfea98b83dbd35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:19:29 +0000

    Add package daar/0.0.1/daar-0.0.1.tar.gz

---
+++ packages/daar/daar-0.0.1.tar.gz/daar-0.0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.1' 
+DESCRIPTION = 'Some helper functions to help with arabic dialects classification proplem'
+
+setup(
+	name='daar',
+	version=VERSION,
+	author='Ahmed Elshehawy',
+	author_email='a.elshehawy64@gmail.com',
+	packages=['daar']
+)
