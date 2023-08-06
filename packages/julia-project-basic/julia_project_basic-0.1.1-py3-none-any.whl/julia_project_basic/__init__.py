23591cc3ec7e63de6bd06804d8f2985848bf60bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:53:28 +0000

    Add package julia-project-basic/0.1.1/julia_project_basic-0.1.1-py3-none-any.whl

---
+++ packages/julia-project-basic/julia_project_basic-0.1.1-py3-none-any.whl/julia_project_basic/__init__.py
@@ -0,0 +1,5 @@
+"""Tools for ensuring that Julia registries and packages are installed correctly and that PyCall works"""
+
+__version__ = '0.1.1'
+
+from .basic import ensure_project_ready, ensure_project_ready_fix_pycall, test_pycall, rebuild_pycall, need_resolve
