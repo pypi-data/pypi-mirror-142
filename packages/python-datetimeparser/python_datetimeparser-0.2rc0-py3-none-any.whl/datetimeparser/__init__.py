b5a8d0709c5048e1ebc266283ad30001d2df8195
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:17:25 +0000

    Add package python-datetimeparser/0.2rc0/python_datetimeparser-0.2rc0-py3-none-any.whl

---
+++ packages/python-datetimeparser/python_datetimeparser-0.2rc0-py3-none-any.whl/datetimeparser/__init__.py
@@ -0,0 +1,7 @@
+from datetimeparser import parser
+from datetimeparser import evaluator
+from datetimeparser import enums
+from datetimeparser import baseclasses
+from datetimeparser import parsermethods
+
+from datetimeparser.datetimeparser import parse
