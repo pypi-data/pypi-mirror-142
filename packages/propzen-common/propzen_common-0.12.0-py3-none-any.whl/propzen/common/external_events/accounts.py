25dacb28fcc8b593d46df8b55ff89bb40f25d14e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:10:13 +0000

    Add package propzen-common/0.12.0/propzen_common-0.12.0-py3-none-any.whl

---
+++ packages/propzen-common/propzen_common-0.12.0-py3-none-any.whl/propzen/common/external_events/accounts.py
@@ -0,0 +1,17 @@
+from uuid import UUID
+from dataclasses import dataclass
+from propzen.common.service_layer.externalbus import ExternalEvent
+
+
+@dataclass
+class AccountRegistered(ExternalEvent):
+    fullname: str
+    email: str
+    token: str
+
+
+@dataclass
+class AccountConfirmed(ExternalEvent):
+    account_id: UUID
+    fullname: str
+    email: str
