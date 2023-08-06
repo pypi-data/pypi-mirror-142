522a933159c6e598a8246f26c6ff9b90c308b2b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:51:14 +0000

    Add package blpapi-stubs/0.0.2/blpapi-stubs-0.0.2.tar.gz

---
+++ packages/blpapi-stubs/blpapi-stubs-0.0.2.tar.gz/blpapi-stubs-0.0.2/README.md
@@ -0,0 +1,15 @@
+# blpapi-stubs
+
+Mypy stubs for [blpapi](https://www.bloomberg.com/professional/support/api-library/).
+
+## Note
+
+The latest version `3.18.0` of blpapi (2022-03-03) is typed; you really only need these stubs if using an older version: `3.17.1` or earlier.
+
+## Installation
+
+````console
+python -m pip install blpapi-stubs
+````
+
+It is not necessary to modify your `config.ini` as the files are placed alongside their `.py` counterparts.
---
+++ packages/blpapi-stubs/blpapi-stubs-0.0.2.tar.gz/blpapi-stubs-0.0.2/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(
+    name="blpapi-stubs",
+    version="0.0.2",
+    author="Stiofán Fordham",
+    author_email="stiofan.fordham@gmail.com",
+    description="Mypy stubs for blpapi",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/sdfordham/blpapi-stubs",
+    project_urls={
+        "Bug Tracker": "https://github.com/sdfordham/blpapi-stubs/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=["mypy>=0.790", "typing-extensions>=3.7.4"],
+    packages=["blpapi"],
+    package_data={"": ["*.pyi", "py.typed"]},
+)
