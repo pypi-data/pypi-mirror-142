15f5a9d3d81c2d07782c543dd98d7bb149282ab5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:45:17 +0000

    Add package wolfhece/1.3.0/wolfhece-1.3.0.tar.gz

---
+++ packages/wolfhece/wolfhece-1.3.0.tar.gz/wolfhece-1.3.0/src/wolfhece/apps/Optimisation_hydro.py
@@ -0,0 +1,20 @@
+import sys
+import numpy as np
+import matplotlib.pyplot as plt
+from numpy.core.shape_base import block
+from numpy.testing._private.utils import measure
+from wolfhece.hydrology.Optimisation import Optimisation
+
+from .hydrology.PostProcessHydrology import PostProcessHydrology
+from .hydrology.Catchment import *
+from .PyParams import*
+
+
+app = wx.App()
+
+myOpti = Optimisation()
+
+# %% Show  graphs
+myOpti.Show()
+app.MainLoop()
+print("That's all folks! ")
