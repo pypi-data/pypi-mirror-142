49cb1674207eb62dcf8c4d443d308aab4e2182db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:08:45 +0000

    Add package jovialengine/0.4.2/jovialengine-0.4.2-py3-none-any.whl

---
+++ packages/jovialengine/jovialengine-0.4.2-py3-none-any.whl/jovialengine/__init__.py
@@ -0,0 +1,7 @@
+from . import utility
+from . import shared
+from .saveable import Saveable
+from .animsprite import AnimSprite
+from .fontwrap import FontWrap
+from .modebase import ModeBase
+from .game import Game
