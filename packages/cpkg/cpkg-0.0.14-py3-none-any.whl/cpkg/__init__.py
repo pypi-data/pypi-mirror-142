de8c9dc9c26f8bce8dfd34489e61ee1f02b7e5b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:04 +0000

    Add package cpkg/0.0.14/cpkg-0.0.14-py3-none-any.whl

---
+++ packages/cpkg/cpkg-0.0.14-py3-none-any.whl/cpkg/__init__.py
@@ -0,0 +1,11 @@
+from .createPkg import *
+from cpkgtab import *
+__author__ = 'xiongtianshuo'
+__version__ = '0.0.13'
+__all__ = [
+    'NameNotSpecificationError',
+    'PathNotFoundError',
+    'PathNotFoundError',
+    'create'
+]
+ctab(__version__, create)
