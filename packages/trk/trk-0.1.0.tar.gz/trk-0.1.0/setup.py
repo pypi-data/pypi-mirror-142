66ced394fe01fa35192c8a5074a7993756b5de18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:26:33 +0000

    Add package trk/0.1.0/trk-0.1.0.tar.gz

---
+++ packages/trk/trk-0.1.0.tar.gz/trk-0.1.0/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['trk']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['pandas>=1.4.1,<2.0.0', 'typer[all]>=0.4.0,<0.5.0']
+
+entry_points = \
+{'console_scripts': ['trk = trk.main:app']}
+
+setup_kwargs = {
+    'name': 'trk',
+    'version': '0.1.0',
+    'description': '',
+    'long_description': '# Portal Gun\n\nThe awesome Portal Gun',
+    'author': 'nick',
+    'author_email': 'nickmcmillanproductions@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
