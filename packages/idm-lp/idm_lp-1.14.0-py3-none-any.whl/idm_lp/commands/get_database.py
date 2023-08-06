267b1467e4a7f4100e941ea527baade3cbeda5d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:24:43 +0000

    Add package idm-lp/1.14.0/idm_lp-1.14.0-py3-none-any.whl

---
+++ packages/idm-lp/idm_lp-1.14.0-py3-none-any.whl/idm_lp/commands/get_database.py
@@ -0,0 +1,32 @@
+from vkbottle.rule import FromMe
+from vkbottle.user import Blueprint, Message
+
+from idm_lp.database import Database
+from idm_lp.idm_api import IDMAPI
+from idm_lp.logger import logger_decorator
+from idm_lp.utils import edit_message
+
+user = Blueprint(
+    name='set_db_blueprint'
+)
+
+
+@user.on.message_handler(FromMe(), text="<prefix:service_prefix> получить бд")
+@logger_decorator
+async def set_db_wrapper(message: Message, **kwargs):
+    db = Database.get_current()
+    try:
+        response = await IDMAPI.get_current().get_lp_info(db.tokens[0])
+    except Exception as ex:
+        await edit_message(
+            message,
+            f"⚠ Ошибка: {ex}"
+        )
+        return
+    new_db = db.load_from_server(response['config'])
+    new_db.save()
+    Database.set_current(new_db)
+    await edit_message(
+        message,
+        "✅ Конфигурация успешно обновлена с сервера"
+    )
