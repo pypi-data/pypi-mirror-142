1d9ee39bdf1345e58230cae1ef42ad33206720f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:19:44 +0000

    Add package scriptable/0.1.4/scriptable-0.1.4.tar.gz

---
+++ packages/scriptable/scriptable-0.1.4.tar.gz/scriptable-0.1.4/scriptable/typescript_engine.py
@@ -0,0 +1,43 @@
+from typing import Dict, Any
+
+# noinspection PyUnresolvedReferences
+from antlr4 import InputStream, CommonTokenStream
+
+from scriptable.abstract_engine import ScriptableEngine
+from scriptable.antlr.TypescriptLexer import TypescriptLexer
+from scriptable.antlr.TypescriptParser import TypescriptParser
+from scriptable.api.ast_binding import ASTBinding
+from scriptable.api.ast_restrictions import ASTRestrictions
+from scriptable.listener.error_listener import ScriptableErrorListener
+from scriptable.runtime.buildin.typescript.console import Console
+from scriptable.runtime.buildin.typescript.process import Process
+from scriptable.typescript_visitor import TypescriptVisitorImpl
+
+
+class TypescriptEngine(ScriptableEngine):
+
+    @staticmethod
+    def parse(schema, restrictions: ASTRestrictions = ASTRestrictions()) -> 'TypescriptEngine':
+        lexer = TypescriptLexer(TypescriptEngine.stream(schema))
+        stream = CommonTokenStream(lexer)
+        parser = TypescriptParser(stream)
+
+        error_listener = ScriptableErrorListener()
+        parser.removeErrorListeners()
+        parser.addErrorListener(error_listener)
+        lexer.removeErrorListeners()
+        lexer.addErrorListener(error_listener)
+
+        visitor = TypescriptVisitorImpl(restrictions)
+        tree = visitor.visit(parser.sAll())
+
+        if len(error_listener.errors) > 0:
+            raise ValueError(str(error_listener.errors[0]))
+
+        return TypescriptEngine(tree, restrictions)
+
+    def _create_binding(self, properties: Dict[str, Any], restrictions: ASTRestrictions):
+        binding = ASTBinding(restrictions=restrictions)
+        binding.add_property("console", Console())
+        binding.add_property("process", Process(properties))
+        return binding
