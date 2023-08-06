f9ee308fdf1e34bf5ef876a62a9230b89fcb8077
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:20:23 +0000

    Add package polly-python/0.0.9/polly-python-0.0.9.tar.gz

---
+++ packages/polly-python/polly-python-0.0.9.tar.gz/polly-python-0.0.9/polly/constants.py
@@ -0,0 +1,24 @@
+DATA_TYPES = {
+    "Mutation": [
+        {
+            "format": ["maf"],
+            "supported_repo": [
+                {
+                    "name": "cbioportal",
+                    "header_mapping": {
+                        "gene": "Hugo_Symbol",
+                        "chr": "Chromosome",
+                        "startPosition": "Start_Position",
+                        "endPosition": "End_Position",
+                        "referenceAllele": "Reference_Allele",
+                        "variantAllele": "Tumor_Seq_Allele2",
+                        "mutationType": "Variant_Classification",
+                        "variantType": "Variant_Type",
+                        "uniqueSampleKey": "Tumor_Sample_Barcode",
+                    },
+                },
+                {"name": "tcga", "header_mapping": {}},
+            ],
+        }
+    ]
+}
