243ef901889504998dc1d10ab33774fc53491e6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:18:18 +0000

    Add package wordrank/0.1.5/wordrank-0.1.5.tar.gz

---
+++ packages/wordrank/wordrank-0.1.5.tar.gz/wordrank-0.1.5/wordrank/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+@author:XuMing(xuming624@qq.com)
+@description: 
+"""
+from wordrank import config
+from wordrank.wordrank import WordRank
+from wordrank.utils.logger import set_log_level
+
+WR = WordRank()
+rank = WR.rank
