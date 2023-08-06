22dc0d9e2ecd98e14a5f5800e9de6281c16f569a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:20:57 +0000

    Add package steam-sdk/0.0.20/steam_sdk-0.0.20-py3-none-any.whl

---
+++ packages/steam-sdk/steam_sdk-0.0.20-py3-none-any.whl/steam_sdk/drivers/DriverLEDET.py
@@ -0,0 +1,51 @@
+import os
+import numpy as np
+import pandas as pd
+from dataclasses import dataclass, asdict
+import subprocess
+
+from steam_nb_api.ledet.ParametersLEDET import ParametersLEDET
+import steam_nb_api.ledet.ParameterSweep
+
+
+def RunSimulationsLEDET(LEDETFolder, LEDETExe, MagnetName, Simulations = 'All', RunSimulations = False):
+    # ExcelFolder = LEDETFolder + "//LEDET//" + MagnetName + "//Input//"  # original
+    ExcelFolder = LEDETFolder + "/LEDET/" + MagnetName + "/Input/"  # edited to pass tests on Gitlab
+    StartFile = LEDETFolder + "//startLEDET.xlsx"
+    SimNumbers = []
+
+    #1. Prepare everything
+    if len(Simulations)==3:
+        if Simulations =='All':
+            items = os.listdir(ExcelFolder)
+            for item in items:
+                if item.startswith(MagnetName) and item.endswith('.xlsx'):
+                    if ".sys" not in item:
+                        num = item.replace('.xlsx', '')
+                        num = num.replace(MagnetName+'_', '')
+                        num = int(num)
+                        SimNumbers.append(num)
+    else:
+        SimNumbers = Simulations
+
+    df = pd.read_excel(StartFile, header=None)
+    df.rename(columns={0: 'a', 1: 'b', 2: 'c'}, inplace=True)
+    df.loc[df['b'] == 'currFolder', 'c'] = LEDETFolder + "\\LEDET"
+    df.loc[df['b'] == 'nameMagnet', 'c'] = MagnetName
+    df.loc[df['b'] == 'simsToRun',  'c'] = str(SimNumbers)
+    writer = pd.ExcelWriter(StartFile)
+    df.to_excel(writer, index=False, index_label=False, header=False, sheet_name='startLEDET')
+    writer.save()
+
+    #2. Run Executable
+    if RunSimulations:
+        os.chdir(LEDETFolder)
+        os.system(LEDETExe)
+
+
+def run_LEDET(self, LEDET_exe_full_path):
+    RunSimulationsLEDET(self.base_folder, LEDET_exe_full_path, self.nameCircuit, Simulations=self.model_no,
+                   RunSimulations=False)
+    LEDET_exe_path = os.path.join(self.base_folder, LEDET_exe_full_path)
+    os.chdir(self.base_folder)
+    subprocess.call([LEDET_exe_path])
