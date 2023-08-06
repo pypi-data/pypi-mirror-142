496703292ff753294fb3818f3245592496c6ca0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:11:30 +0000

    Add package naclo/0.1.7/naclo-0.1.7.tar.gz

---
+++ packages/naclo/naclo-0.1.7.tar.gz/naclo-0.1.7/src/naclo/mol_conversion.py
@@ -0,0 +1,47 @@
+from rdkit import Chem
+
+
+def mols_2_smiles(mols):  # *
+    """Generates SMILES strings from list of rdkit Mol objects.
+
+    Args:
+        mols (iter[rdkit Mol]): Contains RDKit Mols.
+
+    Returns:
+        list[str]: Contains SMILES strings.
+    """
+    return [Chem.MolToSmiles(mol) for mol in mols]
+
+def smiles_2_mols(smiles):  # *
+    """Generates rdkit Mol objects from SMILES strings.
+
+    Args:
+        smiles (iter[str]): Contains SMILES strings.
+
+    Returns:
+        list[rdkit Mol]: Contains RDKit Mols.
+    """
+    return [Chem.MolFromSmiles(smile) for smile in smiles]
+
+def mols_2_inchi_keys(mols):  # *
+    """Generates InChI key strings from rdkit Mol objects.
+
+    Args:
+        mols (iter[rdkit Mols]): Contains rdkit Mols.
+
+    Returns:
+        list[str]: Contains InChI key strings.
+    """
+    return [Chem.MolToInchiKey(mol) for mol in mols]
+
+def smiles_2_inchi_keys(smiles):  # *
+    """Generates InChI key strings from SMILES strings.
+
+    Args:
+        smiles (iter[str]): Contains SMILES strings.
+
+    Returns:
+        list[str]: Contains InChI key strings.
+    """
+    mols = smiles_2_mols(smiles)
+    return mols_2_inchi_keys(mols)
