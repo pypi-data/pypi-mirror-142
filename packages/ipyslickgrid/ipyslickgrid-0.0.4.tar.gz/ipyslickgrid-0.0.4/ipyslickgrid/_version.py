662289b7dae04069af014864cb6fa4813df5200a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:12:31 +0000

    Add package ipyslickgrid/0.0.4/ipyslickgrid-0.0.4.tar.gz

---
+++ packages/ipyslickgrid/ipyslickgrid-0.0.4.tar.gz/ipyslickgrid-0.0.4/ipyslickgrid/_version.py
@@ -0,0 +1,10 @@
+# Modifications copyright (C) 2021 8080 Labs GmbH
+
+# - remove version_info to __version__ mapping
+# - set __version__ as string and add comments
+
+# alpha example: 0.0.3a0
+# beta example: 0.0.3b
+# release candidate: 0.0.3rc
+# final release: 0.0.3
+__version__ = "0.0.4"
