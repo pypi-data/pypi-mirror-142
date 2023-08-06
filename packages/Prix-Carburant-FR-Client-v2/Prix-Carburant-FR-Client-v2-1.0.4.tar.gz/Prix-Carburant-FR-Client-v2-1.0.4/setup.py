e2de3ffcc3b89affa66dda6b157c6ad0aafb4232
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:19:38 +0000

    Add package Prix-Carburant-FR-Client-v2/1.0.4/Prix-Carburant-FR-Client-v2-1.0.4.tar.gz

---
+++ packages/Prix-Carburant-FR-Client-v2/Prix-Carburant-FR-Client-v2-1.0.4.tar.gz/Prix-Carburant-FR-Client-v2-1.0.4/setup.py
@@ -0,0 +1,28 @@
+import pathlib
+from setuptools import setup
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = ""
+
+# This call to setup() does all the work
+setup(
+    name="Prix-Carburant-FR-Client-v2",
+    version="1.0.4",
+    description="StationEssence client",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Marc-06/essence",
+    author="Maxime Wantiez, Yann RITTER",
+    author_email=" ",
+    license="MIT",
+    packages=["prixCarburantClient"],
+    include_package_data=True,
+    entry_points={
+        "console_scripts": [
+            "realpython=reader.__main__:main",
+        ]
+    },
+)
