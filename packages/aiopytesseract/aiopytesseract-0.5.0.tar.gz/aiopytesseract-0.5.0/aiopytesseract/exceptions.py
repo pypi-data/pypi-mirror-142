c6fea633d83e421f5e35a3697d242e4b981171e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:10:45 +0000

    Add package aiopytesseract/0.5.0/aiopytesseract-0.5.0.tar.gz

---
+++ packages/aiopytesseract/aiopytesseract-0.5.0.tar.gz/aiopytesseract-0.5.0/aiopytesseract/exceptions.py
@@ -0,0 +1,22 @@
+class TesseractNotFoundError(Exception):
+    pass
+
+
+class TesseractRuntimeError(RuntimeError):
+    pass
+
+
+class PSMInvalidException(Exception):
+    pass
+
+
+class OEMInvalidException(Exception):
+    pass
+
+
+class NoSuchFileException(Exception):
+    pass
+
+
+class LanguageInvalidException(Exception):
+    pass
