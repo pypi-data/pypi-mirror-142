ede8773769d725537eedb31169abc1b454a81522
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:06 +0000

    Add package pyxcelframe/1.6/pyxcelframe-1.6-py3-none-any.whl

---
+++ packages/pyxcelframe/pyxcelframe-1.6-py3-none-any.whl/pyxcelframe/__init__.py
@@ -0,0 +1,5 @@
+from pyxcelframe.pyxcelframe import insert_frame,\
+                                    insert_columns,\
+                                    copy_cell_style,\
+                                    sheet_to_sheet,\
+                                    column_last_row
