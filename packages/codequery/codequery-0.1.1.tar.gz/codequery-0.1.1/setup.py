b16048a4a6ffa68bf37153f8731af3672517933b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:50:38 +0000

    Add package codequery/0.1.1/codequery-0.1.1.tar.gz

---
+++ packages/codequery/codequery-0.1.1.tar.gz/codequery-0.1.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='codequery',
+    version='0.1.1',
+    description='Queries and code facts checking support for python source code',
+    url='https://github.com/daltonserey/codequery',
+    author='Dalton Serey',
+    author_email='daltonserey@gmail.com',
+    maintainer='Dalton Serey',
+    maintainer_email='daltonserey@gmail.com',
+    license='MIT',
+    py_modules=['codequery'],
+    python_requires='>3.6',
+)
