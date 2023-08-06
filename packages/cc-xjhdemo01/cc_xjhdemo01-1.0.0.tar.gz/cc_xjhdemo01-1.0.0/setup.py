942459da4736a7b4013f3dba0c93c3abf7a80426
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:20:18 +0000

    Add package cc-xjhdemo01/1.0.0/cc_xjhdemo01-1.0.0.tar.gz

---
+++ packages/cc-xjhdemo01/cc_xjhdemo01-1.0.0.tar.gz/cc_xjhdemo01-1.0.0/setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+
+#############################################
+# File Name: setup.py
+# Author: LiangjunFeng
+# Mail: zhumavip@163.com
+# Created Time:  2018-4-16 19:17:34
+#############################################
+
+from setuptools import setup, find_packages            #这个包没有的可以pip一下
+
+setup(
+    name = "cc_xjhdemo01",      #这里是pip项目发布的名称
+    version = "1.0.0",  #版本号，数值大的会优先被pip
+    keywords = ("pip", "cc_xjhdemo01","featureextraction"),
+    description = "An feature extraction algorithm",
+    long_description = "An feature extraction algorithm, improve the FastICA",
+    license = "MIT Licence",
+
+    url = "https://github.com/LiangjunFeng/SICA",     #项目相关文件地址，一般是github
+    author = "xjh815",
+    author_email = "xujinghua815@qq.com",
+
+    packages = find_packages(),
+    include_package_data = True,
+    platforms = "any",
+    install_requires = ["numpy"]          #这个项目需要的第三方库
+)
+
