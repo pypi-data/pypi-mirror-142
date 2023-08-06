e3b76e8f5dfa8f18d969d9aa238ec014ce184447
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:14:47 +0000

    Add package PyCDSL/0.6.4/PyCDSL-0.6.4-py2.py3-none-any.whl

---
+++ packages/PyCDSL/PyCDSL-0.6.4-py2.py3-none-any.whl/pycdsl/__init__.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+PyCDSL
+
+Python Interface to Cologne Digital Sanskrit Lexicon (CDSL).
+"""
+# Created on Sat Apr 17 18:47:35 2021
+
+###############################################################################
+
+__author__ = "Hrishikesh Terdalkar"
+__email__ = 'hrishikeshrt@linuxmail.org'
+__version__ = '0.6.4'
+
+###############################################################################
+
+from .corpus import CDSLCorpus               # noqa
+from .lexicon import CDSLDict                # noqa
+from .shell import CDSLShell                 # noqa
+from .constants import (                     # noqa
+    SERVER_URL,
+    DEFAULT_SCHEME, INTERNAL_SCHEME,
+    DEFAULT_CORPUS_DIR,
+    DEFAULT_DICTIONARIES, ENGLISH_DICTIONARIES
+)
