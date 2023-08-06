c5e443485595298cf0521c49f66d8951f00585e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:08:59 +0000

    Add package rmdirs/0.1.1/rmdirs-0.1.1.tar.gz

---
+++ packages/rmdirs/rmdirs-0.1.1.tar.gz/rmdirs-0.1.1/setup.py
@@ -0,0 +1,32 @@
+import setuptools
+
+with open('ReadMe.md', 'r') as f:
+    long_description = f.read()
+    
+with open('requirements.txt', 'r', encoding='UTF-16') as f:
+    required = f.readlines()
+
+
+setuptools.setup(
+    name="rmdirs",
+    version="0.1.1",
+    
+    author="A-Bak",
+    author_email="adam.bak753@gmail.com",
+    
+    description="Python utility for removing all subdirectories of a directory while preserving all the files located in the subdirectories.",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    keywords='utility util file-system file-structure remove-directories remove-dirs',
+    url='https://github.com/A-Bak/remove-dirs',
+    
+    packages=setuptools.find_packages(),
+    python_requires='>=3.6',
+    install_requires=required,
+    
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
