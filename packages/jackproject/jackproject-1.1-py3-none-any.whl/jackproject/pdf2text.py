83c3dc94725294e2db01b8a0366e8165bf4b8aed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:27:41 +0000

    Add package jackproject/1.1/jackproject-1.1-py3-none-any.whl

---
+++ packages/jackproject/jackproject-1.1-py3-none-any.whl/jackproject/pdf2text.py
@@ -0,0 +1,16 @@
+""" This module provides functions to convert a PDF to text. """
+
+class Converter:
+    """ A simple converter to converting PDFs to text. """
+
+    def convert(self, path):
+        """
+        Convert the given PDF to text.txt
+        
+        Parameters:
+        path (str): The path to a PDF file.txt
+
+        Returns:
+        str: The contents of the PDF file as text.
+        """
+        print("pdf2text")
