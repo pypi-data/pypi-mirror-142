984a725f3c0c0f7e54a859b9da0a3f08dcad26bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:53:37 +0000

    Add package janda/1.0.18/janda-1.0.18.tar.gz

---
+++ packages/janda/janda-1.0.18.tar.gz/janda-1.0.18/janda/__init__.py
@@ -0,0 +1,6 @@
+__version__ = "1.0.18"
+from .pururin import Pururin
+from .nhentai import Nhentai
+from .hentaifox import Hentaifox
+from .hentai2read import Hentai2read
+from .utils.parser import *
