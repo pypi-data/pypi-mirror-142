0e5c239d07cc951acd6791507d463cfd06deb35c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:50:58 +0000

    Add package better-cryptography/0.5.1/better_cryptography-0.5.1-cp37-cp37m-manylinux_2_5_x86_64.manylinux1_x86_64.whl

---
+++ packages/better-cryptography/better_cryptography-0.5.1-cp37-cp37m-manylinux_2_5_x86_64.manylinux1_x86_64.whl/better_cryptography/__init__.py
@@ -0,0 +1,3 @@
+#! /usr/bin/python3.9
+from .ciphers import hash_, random_choice, compare_hashes, token_generate, gen_random_password, sec_delete, delete, XOR, is_prime_number, get_prime_number, get_GCD, englishToKey, keyToEnglish, Ciphers, Cipher_Constructor
+from .log import Log
