a5962065b10e34a90d16cfbd8da314ff65562b9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:16:03 +0000

    Add package kktools/1.0/kktools-1.0.tar.gz

---
+++ packages/kktools/kktools-1.0.tar.gz/kktools-1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='kktools',
+    version='1.0',
+    author='Konstantin Khorev',
+    author_email='khorevkp@gmail.com',
+    description='Tools for finance and treasury specialists',
+    url='https://github.com/khorevkp/KK_Tools/',
+    install_requires=['pandas']
+)
