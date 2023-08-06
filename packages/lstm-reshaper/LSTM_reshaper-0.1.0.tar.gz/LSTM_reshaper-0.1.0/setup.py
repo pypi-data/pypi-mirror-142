390f0fa9d2b61f1806a4b604f6770e08cc3ecc90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:36:18 +0000

    Add package lstm-reshaper/0.1.0/LSTM_reshaper-0.1.0.tar.gz

---
+++ packages/lstm-reshaper/LSTM_reshaper-0.1.0.tar.gz/LSTM_reshaper-0.1.0/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptools.setup(
+    name="LSTM_reshaper",
+    version="0.1.0",
+    author="song111",
+    author_email="clarksoyoung@sina.com",
+    description="This package provides fundamental operation on reshaping dataframes for Keras LSTM input",
+    url="https://github.com/ysong126/lstm_reshaper",
+    project_urls={
+        "Bug Tracker": "https://github.com/ysong126/lstm_reshaper",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
