5cf3c580d623e1d9f08f5eca7ac03107063ee5a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:26:14 +0000

    Add package innocuous-api/1.1.3/innocuous-api-1.1.3.tar.gz

---
+++ packages/innocuous-api/innocuous-api-1.1.3.tar.gz/innocuous-api-1.1.3/setup.py
@@ -0,0 +1,29 @@
+import os
+import setuptools
+
+_VERSION = "1.1.3"
+
+DEPENDENCY_LINKS = [
+
+]
+
+REQUIRED_PACKAGES = [
+    "colorlog",
+    "halo",
+    "pyjwt",
+    "requests"
+]
+
+setuptools.setup(
+    name="innocuous-api",
+    version=_VERSION,
+    description="Innocuous Book API",
+    install_requires=REQUIRED_PACKAGES,
+    dependency_links=DEPENDENCY_LINKS,
+    packages = ["innocuous_api"],
+    zip_safe = False,
+    author="noam",
+    author_email="noamsrosenberg@gmail.com",
+    url="",
+    keywords=["innocuousbook"]
+)
