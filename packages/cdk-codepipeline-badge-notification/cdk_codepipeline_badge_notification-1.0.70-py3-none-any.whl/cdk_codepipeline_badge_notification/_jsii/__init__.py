c7d0f97150b902def0f10e4acbceb5090c6ab5c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:11:45 +0000

    Add package cdk-codepipeline-badge-notification/1.0.70/cdk_codepipeline_badge_notification-1.0.70-py3-none-any.whl

---
+++ packages/cdk-codepipeline-badge-notification/cdk_codepipeline_badge_notification-1.0.70-py3-none-any.whl/cdk_codepipeline_badge_notification/_jsii/__init__.py
@@ -0,0 +1,34 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import publication
+import typing_extensions
+
+import aws_cdk.aws_codebuild._jsii
+import aws_cdk.aws_codepipeline._jsii
+import aws_cdk.aws_codepipeline_actions._jsii
+import aws_cdk.aws_events_targets._jsii
+import aws_cdk.aws_iam._jsii
+import aws_cdk.aws_lambda._jsii
+import aws_cdk.aws_s3._jsii
+import aws_cdk.aws_secretsmanager._jsii
+import aws_cdk.aws_ssm._jsii
+import aws_cdk.core._jsii
+import constructs._jsii
+
+__jsii_assembly__ = jsii.JSIIAssembly.load(
+    "cdk-codepipeline-badge-notification",
+    "1.0.70",
+    __name__[0:-6],
+    "cdk-codepipeline-badge-notification@1.0.70.jsii.tgz",
+)
+
+__all__ = [
+    "__jsii_assembly__",
+]
+
+publication.publish()
