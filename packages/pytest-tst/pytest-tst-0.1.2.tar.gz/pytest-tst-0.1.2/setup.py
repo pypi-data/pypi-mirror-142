8b7dc181da373f44b96a6449d0053a584934700d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:25:39 +0000

    Add package pytest-tst/0.1.2/pytest-tst-0.1.2.tar.gz

---
+++ packages/pytest-tst/pytest-tst-0.1.2.tar.gz/pytest-tst-0.1.2/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+from pathlib import Path
+
+cwd = Path(__file__).parent
+long_description = (cwd / "README.md").read_text()
+
+setup(
+    name='pytest-tst',
+    version='0.1.2',
+    description='Customize pytest options, output and exit code to make it compatible with tst',
+    url='https://github.com/daltonserey/pytest-tst',
+    author='Dalton Serey',
+    author_email='daltonserey@gmail.com',
+    maintainer='Dalton Serey',
+    maintainer_email='daltonserey@gmail.com',
+    license='MIT',
+    long_description=long_description,
+    py_modules=['pytest_tst'],
+    python_requires='>3.6',
+    install_requires=[
+        'pytest>=5.0.0',
+        'tst>=0.15.1'
+    ],
+    entry_points={
+        'pytest11': [
+            'tst = pytest_tst',
+        ],
+    },
+)
