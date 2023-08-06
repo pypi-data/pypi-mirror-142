04e5deea83daba8f98cb0dc69ad4a73cf8185747
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:44:25 +0000

    Add package stse/0.0.6/stse-0.0.6.tar.gz

---
+++ packages/stse/stse-0.0.6.tar.gz/stse-0.0.6/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='stse',
+    version='0.0.6',
+    license='MIT',
+    author='Jacob Gerlach',
+    author_email='jwgerlach00@gmail.com',
+    url='https://github.com/jwgerlach00/stuff_that_should_exist',
+    description='Various extremely useful and commonly needed functions.',
+    packages=find_packages('stse'),
+    package_dir={'': 'stse'},
+    install_requires=[
+        'numpy',
+        'pandas',
+    ],
+)
