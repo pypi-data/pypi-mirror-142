5133a8c79455e02b09f48b6357945ec35cf8e48c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:26:00 +0000

    Add package cfgcaddy/0.1.5/cfgcaddy-0.1.5.tar.gz

---
+++ packages/cfgcaddy/cfgcaddy-0.1.5.tar.gz/cfgcaddy-0.1.5/cfgcaddy/__init__.py
@@ -0,0 +1,17 @@
+import os
+from enum import Enum
+
+__version__ = "0.1.5"
+
+
+class LinkMode(Enum):
+    SKIP = "SKIP"
+    OVERRIDE = "OVERRIDE"
+
+
+DEFAULT_CONFIG_NAME = ".cfgcaddy.yml"
+
+DEFAULT_DOTFILES_DIR = os.path.join(os.path.expanduser("~"), "dotfiles")
+
+HOME_DIR = os.path.expanduser("~")
+DEFAULT_CONFIG_PATH = os.path.join(HOME_DIR, DEFAULT_CONFIG_NAME)
