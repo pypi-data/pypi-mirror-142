614eaad02d3c86bc96b8e0c0ffc72266621162fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:02 +0000

    Add package zservices/0.0.8/zservices-0.0.8.tar.gz

---
+++ packages/zservices/zservices-0.0.8.tar.gz/zservices-0.0.8/dynamodb/queries.py
@@ -0,0 +1,18 @@
+from dynamodb.dynamo import DynamoDB
+from boto3.dynamodb.conditions import Key
+
+
+class DynamoQueries:
+    def __init__(self, table_name):
+        self.db = DynamoDB()
+        self.table = self.db.connection.Table(table_name)
+
+    def get_pk_context(self, pk, pk_value, table_name):
+        response = self.table.query(KeyConditionExpression=Key(pk).eq(pk_value))
+        model_data = response['Items']
+        return model_data
+
+    def get_index_context(self, index_key, index_value, index_name, table_value):
+        response = self.table.query(IndexName=index_name, KeyConditionExpression=Key(index_key).eq(index_value))
+        model_data = response['Items']
+        return model_data
