bf7364066c2102dfb9383afb566b0fe5ed850abe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:19:39 +0000

    Add package properly-model-python/0.5.219/properly_model_python-0.5.219-py3-none-any.whl

---
+++ packages/properly-model-python/properly_model_python-0.5.219-py3-none-any.whl/properly_model_python/alert_models/models/__init__.py
@@ -0,0 +1,6 @@
+# coding: utf-8
+
+# flake8: noqa
+from __future__ import absolute_import
+# import models into model package
+from properly_model_python.alert_models.models.users_recommendation import UsersRecommendation
