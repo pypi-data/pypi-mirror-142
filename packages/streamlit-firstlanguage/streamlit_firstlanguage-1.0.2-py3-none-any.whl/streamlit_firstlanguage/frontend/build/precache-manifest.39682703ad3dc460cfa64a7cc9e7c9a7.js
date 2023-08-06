80c7df05bb10939a44bc358b34324e0fead664d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:16:35 +0000

    Add package streamlit-firstlanguage/1.0.2/streamlit_firstlanguage-1.0.2-py3-none-any.whl

---
+++ packages/streamlit-firstlanguage/streamlit_firstlanguage-1.0.2-py3-none-any.whl/streamlit_firstlanguage/frontend/build/precache-manifest.39682703ad3dc460cfa64a7cc9e7c9a7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f0440ff4b0758f4473f53e8b4b56561",
+    "url": "./index.html"
+  },
+  {
+    "revision": "64e304302329d34b5504",
+    "url": "./static/js/2.df12b593.chunk.js"
+  },
+  {
+    "revision": "3fc7fb5bfeeec1534560a2c962e360a7",
+    "url": "./static/js/2.df12b593.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26efde78a0d3ab1a1767",
+    "url": "./static/js/main.eb315714.chunk.js"
+  },
+  {
+    "revision": "33558c5544befa24e9a0",
+    "url": "./static/js/runtime-main.9f763134.js"
+  }
+]);
