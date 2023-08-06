f863e585f9f8c976d04a91af6cb9c294aa2dc2ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:17:56 +0000

    Add package pydocstringformatter/0.5.0/pydocstringformatter-0.5.0-py3-none-any.whl

---
+++ packages/pydocstringformatter/pydocstringformatter-0.5.0-py3-none-any.whl/pydocstringformatter/formatting/base.py
@@ -0,0 +1,97 @@
+import abc
+import argparse
+import re
+import tokenize
+from typing import Literal
+
+
+class Formatter:
+    """Base class for docstring formatter."""
+
+    optional = False
+    config: argparse.Namespace
+    """Namespace object set when set_config_namespace is called."""
+
+    @property
+    @abc.abstractmethod
+    def name(self) -> str:
+        """Name of the Formatter.
+
+        This will be used to create argparse options when added to
+        'pydocstringformatter.formatting.FORMATTERS'. Therefore, it is
+        user-facing and should be chosen carefully.
+        """
+
+    @property
+    def activate_option(self) -> str:
+        """The argparse option to activate this formatter."""
+        return f"--{self.name}"
+
+    @property
+    def deactivate_option(self) -> str:
+        """The argparse option to deactivate this formatter."""
+        return f"--no-{self.name}"
+
+    @abc.abstractmethod
+    def treat_token(self, tokeninfo: tokenize.TokenInfo) -> tokenize.TokenInfo:
+        """Return a modified token."""
+
+    def set_config_namespace(self, config: argparse.Namespace) -> None:
+        """Set the config attribute for this formatter."""
+        self.config = config
+
+
+class StringFormatter(Formatter):
+    """Base class for formatter that only modifies the string content."""
+
+    @abc.abstractmethod
+    def _treat_string(self, tokeninfo: tokenize.TokenInfo, indent_length: int) -> str:
+        """Return a modified string."""
+
+    def treat_token(self, tokeninfo: tokenize.TokenInfo) -> tokenize.TokenInfo:
+        return tokenize.TokenInfo(
+            tokeninfo.type,
+            self._treat_string(tokeninfo, tokeninfo.start[1]),
+            tokeninfo.start,
+            tokeninfo.end,
+            tokeninfo.line,
+        )
+
+
+class StringAndQuotesFormatter(Formatter):
+    """Base class for string formatter that needs access to the quotes."""
+
+    quotes_regex = re.compile(r"""['"]{1,3}""")
+    """Pattern to match against opening quotes."""
+
+    @abc.abstractmethod
+    def _treat_string(
+        self,
+        tokeninfo: tokenize.TokenInfo,
+        indent_length: int,
+        quotes: str,
+        quotes_length: Literal[1, 3],
+    ) -> str:
+        """Return a modified string."""
+
+    def treat_token(self, tokeninfo: tokenize.TokenInfo) -> tokenize.TokenInfo:
+        # Get the quotes used for this docstring
+        match = re.match(self.quotes_regex, tokeninfo.string)
+        assert match
+        quotes = match.group()
+
+        quotes_length = len(quotes)
+        assert quotes_length in {1, 3}
+
+        return tokenize.TokenInfo(
+            tokeninfo.type,
+            self._treat_string(
+                tokeninfo,
+                tokeninfo.start[1],
+                quotes,
+                quotes_length,  # type: ignore[arg-type]
+            ),
+            tokeninfo.start,
+            tokeninfo.end,
+            tokeninfo.line,
+        )
---
+++ packages/pydocstringformatter/pydocstringformatter-0.5.0-py3-none-any.whl/pydocstringformatter/run.py
@@ -0,0 +1,96 @@
+# pylint: disable=too-few-public-methods, protected-access
+"""Run class."""
+
+import os
+import sys
+import tokenize
+from pathlib import Path
+from typing import List, Union
+
+from pydocstringformatter import __version__, configuration, formatting, utils
+
+
+class _Run:
+    """Main class that represent a run of the program."""
+
+    def __init__(self, argv: Union[List[str], None]) -> None:
+        # Load ArgumentsManager and set its namespace as instance's config attribute
+        self._arguments_manager = configuration.ArgumentsManager(
+            __version__,
+            formatting.FORMATTERS,
+        )
+        self.config = self._arguments_manager.namespace
+
+        if argv := argv or sys.argv[1:]:
+            self._arguments_manager.parse_options(argv)
+            for formatter in formatting.FORMATTERS:
+                formatter.set_config_namespace(self.config)
+            self._check_files(self.config.files)
+        else:
+            self._arguments_manager.print_help()
+
+    def _check_files(self, arguments: List[str]) -> None:
+        """Find all files and perform the formatting."""
+        filepaths = utils._find_python_files(arguments, self.config.exclude)
+        self._format_files(filepaths)
+
+    def _format_file(self, filename: Path) -> bool:
+        """Format a file."""
+        changed_tokens: List[tokenize.TokenInfo] = []
+        is_changed = False
+
+        with tokenize.open(filename) as file:
+            try:
+                tokens = list(tokenize.generate_tokens(file.readline))
+            except tokenize.TokenError as exc:
+                raise utils.ParsingError(
+                    f"Can't parse {os.path.relpath(filename)}. Is it valid Python code?"
+                ) from exc
+
+        for index, tokeninfo in enumerate(tokens):
+            new_tokeninfo = tokeninfo
+
+            if utils._is_docstring(new_tokeninfo, tokens[index - 1]):
+                for formatter in formatting.FORMATTERS:
+                    if getattr(self.config, formatter.name):
+                        new_tokeninfo = formatter.treat_token(new_tokeninfo)
+            changed_tokens.append(new_tokeninfo)
+
+            if tokeninfo != new_tokeninfo:
+                is_changed = True
+
+        if is_changed:
+            try:
+                filename_str = os.path.relpath(filename)
+            except ValueError:  # pragma: no cover # Covered on Windows
+                # On Windows relpath raises ValueError's when the mounts differ
+                filename_str = str(filename)
+
+            if self.config.write:
+                with open(filename, "w", encoding="utf-8") as file:
+                    file.write(tokenize.untokenize(changed_tokens))
+                    utils._print_to_console(
+                        f"Formatted {filename_str} 📖\n", self.config.quiet
+                    )
+            else:
+                sys.stdout.write(
+                    utils._generate_diff(
+                        tokenize.untokenize(tokens),
+                        tokenize.untokenize(changed_tokens),
+                        filename_str,
+                    )
+                )
+
+        return is_changed
+
+    def _format_files(self, filepaths: List[Path]) -> None:
+        """Format a list of files."""
+        is_changed = False
+
+        for file in filepaths:
+            is_changed = self._format_file(file) or is_changed
+
+        if not is_changed:
+            utils._print_to_console(
+                "Nothing to do! All docstrings are correct 🎉\n", self.config.quiet
+            )
