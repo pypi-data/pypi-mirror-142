fee851b076b36210455e8efa29b8268ae2bbb772
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:18:15 +0000

    Add package odoo12-addon-pos-name-to-sale-order/12.0.0.0.2/odoo12_addon_pos_name_to_sale_order-12.0.0.0.2-py3-none-any.whl

---
+++ packages/odoo12-addon-pos-name-to-sale-order/odoo12_addon_pos_name_to_sale_order-12.0.0.0.2-py3-none-any.whl/odoo/addons/pos_name_to_sale_order/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2021-Coopdevs Treball SCCL (<https://coopdevs.org>)
+# - konykon - <kon.tsagari@coopdevs.org>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "PoS name to sale order",
+    "version": "12.0.0.0.2",
+    "depends": ["pos_order_to_sale_order"],
+    "author": "Coopdevs Treball SCCL",
+    "category": "Project Management",
+    "website": "https://coopdevs.org",
+    "license": "AGPL-3",
+    "summary": """
+        Group sale orders by Point of sale.
+    """,
+    "data": ["views/sale_order.xml"],
+    "installable": True,
+}
