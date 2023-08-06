0c803c2f8a057ed4e2b2500330629ef4a670086d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:02 +0000

    Add package YeetsMenu/0.0.8/YeetsMenu-0.0.8-py3-none-any.whl

---
+++ packages/YeetsMenu/YeetsMenu-0.0.8-py3-none-any.whl/YeetsMenu/utils/utils.py
@@ -0,0 +1,15 @@
+import os
+from colorama import Style, Fore
+import sys
+
+
+def clear_screen():
+    if sys.platform.startswith("linux"):
+        os.system("clear")
+
+    elif sys.platform.lower().startswith("win"):
+        os.system("cls")
+
+
+def enter_confirmation():
+    input(f"{Fore.CYAN}Press enter to continue...{Style.RESET_ALL}")
