dd0dcebc52afd39cf8ae206401132a3157e3a125
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:20:38 +0000

    Add package torch-model-persistency/0.1.4/torch_model_persistency-0.1.4.tar.gz

---
+++ packages/torch-model-persistency/torch_model_persistency-0.1.4.tar.gz/torch_model_persistency-0.1.4/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['torch_persistency']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'torch-model-persistency',
+    'version': '0.1.4',
+    'description': 'Module Persistency manager for PyTorch',
+    'long_description': None,
+    'author': 'Oguz Vuruskaner',
+    'author_email': 'ovuruska@outlook.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
