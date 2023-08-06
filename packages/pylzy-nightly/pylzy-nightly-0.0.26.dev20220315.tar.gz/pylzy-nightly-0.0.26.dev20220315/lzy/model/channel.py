7afe7d2dfd8c747854b41c98af953d1aff512b3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:36:33 +0000

    Add package pylzy-nightly/0.0.26.dev20220315/pylzy-nightly-0.0.26.dev20220315.tar.gz

---
+++ packages/pylzy-nightly/pylzy-nightly-0.0.26.dev20220315.tar.gz/pylzy-nightly-0.0.26.dev20220315/lzy/model/channel.py
@@ -0,0 +1,42 @@
+from dataclasses import dataclass
+from typing import List, Optional, Union
+
+from lzy.model.slot import Slot
+
+
+@dataclass
+class SnapshotChannelSpec:
+    snapshot_id: str
+    entry_id: str
+
+
+@dataclass
+class DirectChannelSpec:
+    pass
+
+
+@dataclass
+class Channel:
+    name: str
+    spec: Union[SnapshotChannelSpec, DirectChannelSpec]
+
+
+@dataclass
+class Binding:
+    local_slot: Slot
+    remote_slot: Slot
+    channel: Channel
+
+
+class Bindings:
+    def __init__(self, bindings: List[Binding]):
+        self._bindings = list(bindings)
+        self._to_local_map = {}
+        for bind in bindings:
+            self._to_local_map[bind.remote_slot] = bind.local_slot
+
+    def bindings(self) -> List[Binding]:
+        return list(self._bindings)
+
+    def local_slot(self, remote_slot: Slot) -> Optional[Slot]:
+        return self._to_local_map.get(remote_slot, None)
