8830334a64c23779a6c0a87b4dc965e4d3f503f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:35:23 +0000

    Add package mikhailok-package/1.0.1/mikhailok_package-1.0.1.tar.gz

---
+++ packages/mikhailok-package/mikhailok_package-1.0.1.tar.gz/mikhailok_package-1.0.1/setup.py
@@ -0,0 +1,18 @@
+from io import open
+from setuptools import setup
+
+version = '1.0.1'
+
+setup(
+    name='mikhailok_package', version=version,
+
+    author='MikhailOk',
+    author_email='mikhail.okhapkin@yandex.ru',
+
+    url='https://github.com/MikhailOk-creator/Convertor',
+    download_url='https://github.com/MikhailOk-creator/Convertor/archive/refs/heads/master.zip'.format(
+        version
+    ),
+
+    packages=['mikhailok_convertor']
+)
