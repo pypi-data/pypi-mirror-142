a4456ab7211a624e7e19bc7728f49c1ca16edd44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:30:52 +0000

    Add package metadmg/0.20.3/metaDMG-0.20.3.tar.gz

---
+++ packages/metadmg/metaDMG-0.20.3.tar.gz/metaDMG-0.20.3/src/metaDMG/fit/workflow.py
@@ -0,0 +1,29 @@
+# from multiprocessing import Pool
+from concurrent.futures import ProcessPoolExecutor as Pool
+from logger_tt import logger
+from datetime import datetime
+from metaDMG.fit import serial, fit_utils
+from metaDMG.utils import Configs
+
+
+def run_workflow(configs: Configs):
+
+    cores = min(configs["cores"], len(configs))
+
+    logger.info(f"Running metaDMG on {len(configs)} files in total.")
+
+    if cores == 1 or len(configs) == 1:
+        logger.info(f"Running in seriel (1 core)")
+        for config in configs:
+            dfs = serial.run_single_config(config)
+            # df_mismatches, df_fit_results, df_results = dfs
+
+    else:
+        logger.info(f"Running with {cores} processes in parallel")
+        configs.check_number_of_jobs()
+
+        with Pool(max_workers=cores) as pool:
+            # for dfs in pool.imap_unordered(serial.run_single_config, configs):
+            for dfs in pool.map(serial.run_single_config, configs):
+                pass
+                # df_mismatches, df_fit_results, df_results = dfs
