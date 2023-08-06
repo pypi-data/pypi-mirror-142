97fb6256a6df174ee29ffc49965e9d4123595802
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:51:30 +0000

    Add package semgrep/0.85.0/semgrep-0.85.0.tar.gz

---
+++ packages/semgrep/semgrep-0.85.0.tar.gz/semgrep-0.85.0/tests/e2e/test_semgrep_core_parse_error.py
@@ -0,0 +1,21 @@
+import pytest
+
+from semgrep.constants import OutputFormat
+
+
+@pytest.mark.parametrize(
+    "filename",
+    [
+        "invalid_python.py",
+    ],
+)
+def test_rule_parser__failure__error_messages(run_semgrep_in_tmp, snapshot, filename):
+    _, stderr = run_semgrep_in_tmp(
+        config="rules/eqeq-python.yaml",
+        target_name=f"bad/{filename}",
+        options=["--verbose"],
+        output_format=OutputFormat.TEXT,
+        force_color=True,
+        fail_on_nonzero=False,
+    )
+    snapshot.assert_match(stderr, "error.txt")
