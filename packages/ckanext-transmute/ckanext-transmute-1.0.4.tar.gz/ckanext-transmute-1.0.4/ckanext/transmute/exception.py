ed079095cb62be70501e42c849f85d20cdb577ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:13:45 +0000

    Add package ckanext-transmute/1.0.4/ckanext-transmute-1.0.4.tar.gz

---
+++ packages/ckanext-transmute/ckanext-transmute-1.0.4.tar.gz/ckanext-transmute-1.0.4/ckanext/transmute/exception.py
@@ -0,0 +1,18 @@
+from ckan.logic import ValidationError
+
+
+class BaseException(Exception):
+    def __init__(self, error_message: str):
+        self.error = error_message
+
+
+class ValidationError(ValidationError):
+    pass
+
+
+class SchemaParsingError(BaseException):
+    pass
+
+
+class SchemaFieldError(BaseException):
+    pass
