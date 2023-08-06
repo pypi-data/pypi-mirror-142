4d9a366a183336f3b5f9390ebe585f9720166f66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:26:49 +0000

    Add package DecisionTreeBktUtil/1.1.1/DecisionTreeBktUtil-1.1.1.tar.gz

---
+++ packages/DecisionTreeBktUtil/DecisionTreeBktUtil-1.1.1.tar.gz/DecisionTreeBktUtil-1.1.1/setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+
+#############################################
+# File Name: setup.py
+# Author: wangyongpeng
+# Mail: 1690992651@qq.com
+# Created Time:  2022-03-15
+#############################################
+
+from setuptools import setup, find_packages            #这个包没有的可以pip一下
+
+setup(
+    name="DecisionTreeBktUtil",      #这里是pip项目发布的名称
+    version="1.1.1",  #版本号，数值大的会优先被pip
+    keywords=("pip", "wangyongpeng","tree"),
+    description="用树模型代替qcut进行分桶。",
+    long_description="用决策树来分桶",
+    license="BSD Licence",
+
+    url="https://github.com/HeiBoWang/DecisionTreeBktUtil",     # 项目相关文件地址，一般是github
+    author="wangyongpeng",
+    author_email="wangyongpeng@zhuanzhuan.com",
+
+    packages=find_packages(),
+    include_package_data=True,
+    platforms="any",
+    install_requires=["numpy"]          # 这个项目需要的第三方库
+)
+
+
