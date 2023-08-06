93b1101a590441e62af0d9ccf736ad18d40b3d02
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:06:43 +0000

    Add package commonutil-jsonstdioipc/3.0.1/commonutil-jsonstdioipc-3.0.1.tar.gz

---
+++ packages/commonutil-jsonstdioipc/commonutil-jsonstdioipc-3.0.1.tar.gz/commonutil-jsonstdioipc-3.0.1/setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+setup(
+		name="commonutil-jsonstdioipc",
+		version="3.0.1",  # REV-CONSTANT:rev 5d022db7d38f580a850cd995e26a6c2f
+		description="JSON IPC with Standard Input and Output",
+		py_modules=[
+				"commonutil_jsonstdioipc",
+		],
+		classifiers=[
+				"Development Status :: 5 - Production/Stable",
+				"Intended Audience :: Developers",
+				"License :: OSI Approved :: MIT License",
+				"Operating System :: POSIX",
+				"Programming Language :: Python :: 3.7",
+		],
+		license="MIT License",
+)
+
+# vim: ts=4 sw=4 ai nowrap
