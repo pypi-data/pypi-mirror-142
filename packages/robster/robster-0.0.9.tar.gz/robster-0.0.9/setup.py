f602f3bd8cf5f42db94fb6bdbf61c7ced33947ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:18:53 +0000

    Add package robster/0.0.9/robster-0.0.9.tar.gz

---
+++ packages/robster/robster-0.0.9.tar.gz/robster-0.0.9/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+setuptools.setup(
+    name="robster", # Replace with your own username
+    version="0.0.9",
+    author="papamoon0113",
+    author_email="papamoon0113@pusan.ac.kr",
+    description="",
+    long_description="",
+    long_description_content_type="text/markdown",
+    url="https://github.com/Ho-Jun-Moon/robster",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires = ['numpy'],
+)   
