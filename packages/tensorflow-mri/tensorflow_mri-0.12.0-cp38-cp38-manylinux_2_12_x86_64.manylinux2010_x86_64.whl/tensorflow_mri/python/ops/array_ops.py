ae0f207e4e72382fec3f19dbce9ddf5d5da98063
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:25:33 +0000

    Add package tensorflow-mri/0.12.0/tensorflow_mri-0.12.0-cp38-cp38-manylinux_2_12_x86_64.manylinux2010_x86_64.whl

---
+++ packages/tensorflow-mri/tensorflow_mri-0.12.0-cp38-cp38-manylinux_2_12_x86_64.manylinux2010_x86_64.whl/tensorflow_mri/python/ops/array_ops.py
@@ -0,0 +1,118 @@
+# Copyright 2021 University College London. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+"""Array manipulation operations."""
+
+import tensorflow as tf
+
+
+def broadcast_static_shapes(*shapes):
+  """Computes the shape of a broadcast given known shapes.
+
+  Like `tf.broadcast_static_shape`, but accepts any number of shapes.
+
+  Args:
+    *shapes: Two or more `TensorShapes`.
+
+  Returns:
+    A `TensorShape` representing the broadcasted shape.
+  """
+  bcast_shape = shapes[0]
+  for shape in shapes[1:]:
+    bcast_shape = tf.broadcast_static_shape(bcast_shape, shape)
+  return bcast_shape
+
+
+def broadcast_dynamic_shapes(*shapes):
+  """Computes the shape of a broadcast given symbolic shapes.
+
+  Like `tf.broadcast_dynamic_shape`, but accepts any number of shapes.
+
+  Args:
+    shapes: Two or more rank-1 integer `Tensors` representing the input shapes.
+
+  Returns:
+    A rank-1 integer `Tensor` representing the broadcasted shape.
+  """
+  bcast_shape = shapes[0]
+  for shape in shapes[1:]:
+    bcast_shape = tf.broadcast_dynamic_shape(bcast_shape, shape)
+  return bcast_shape
+
+
+def cartesian_product(*args):
+  """Cartesian product of input tensors.
+
+  Args:
+    *args: `Tensors` with rank 1.
+
+  Returns:
+    A `Tensor` of shape `[M, N]`, where `N` is the number of tensors in `args`
+    and `M` is the product of the sizes of all the tensors in `args`.
+  """
+  return tf.reshape(meshgrid(*args), [-1, len(args)])
+
+
+def meshgrid(*args):
+  """Return coordinate matrices from coordinate vectors.
+
+  Make N-D coordinate arrays for vectorized evaluations of N-D scalar/vector
+  fields over N-D grids, given one-dimensional coordinate arrays
+  `x1, x2, ..., xn`.
+
+  .. note::
+    Similar to `tf.meshgrid`, but uses matrix indexing and returns a stacked
+    tensor (along axis -1) instead of a list of tensors.
+
+  Args:
+    *args: `Tensors` with rank 1.
+
+  Returns:
+    A `Tensor` of shape `[M1, M2, ..., Mn, N]`, where `N` is the number of
+    tensors in `args` and `Mi = tf.size(args[i])`.
+  """
+  return tf.stack(tf.meshgrid(*args, indexing='ij'), axis=-1)
+
+
+def ravel_multi_index(multi_indices, dims):
+  """Converts an array of multi-indices into an array of flat indices.
+
+  Args:
+    multi_indices: A `Tensor` of shape `[..., N]` containing multi-indices into
+      an `N`-dimensional tensor.
+    dims: A `Tensor` of shape `[N]`. The shape of the tensor that
+      `multi_indices` indexes into.
+
+  Returns:
+    A `Tensor` of shape `[...]` containing flat indices equivalent to
+    `multi_indices`.
+  """
+  strides = tf.math.cumprod(dims, exclusive=True, reverse=True) # pylint:disable=no-value-for-parameter
+  return tf.math.reduce_sum(multi_indices * strides, axis=-1)
+
+
+def unravel_index(indices, dims):
+  """Converts an array of flat indices into an array of multi-indices.
+
+  Args:
+    indices: A `Tensor` of shape `[...]` containing flat indices into an
+      `N`-dimensional tensor.
+    dims: A `Tensor` of shape `[N]`. The shape of the tensor that
+      `indices` indexes into.
+
+  Returns:
+    A `Tensor` of shape `[..., N]` containing multi-indices equivalent to flat
+    indices.
+  """
+  return tf.transpose(tf.unravel_index(indices, dims))
