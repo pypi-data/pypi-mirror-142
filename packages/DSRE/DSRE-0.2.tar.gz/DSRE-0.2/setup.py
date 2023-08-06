7b41e5e65e0988bc12ab84fe53f55dec23c6be43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:10:58 +0000

    Add package DSRE/0.2/DSRE-0.2.tar.gz

---
+++ packages/DSRE/DSRE-0.2.tar.gz/DSRE-0.2/setup.py
@@ -0,0 +1,11 @@
+# coding=gbk
+from setuptools import setup, find_packages
+setup(
+    name = "DSRE",# 包的名称
+    version = "0.2", # 版本号
+    packages = find_packages(),# 动态获取packages
+    description = "DSRE package",
+    author = 'cml',
+    author_email = '1527449673@qq.com',
+    url = "https://gitee.com/meilincao/re.git",
+)
