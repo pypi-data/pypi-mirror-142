ec7b7605630b5e12c2529646c91063d73210f28e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:09:19 +0000

    Add package qiye-game1/0.1/qiye-game1-0.1.tar.gz

---
+++ packages/qiye-game1/qiye-game1-0.1.tar.gz/qiye-game1-0.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup,find_packages
+setup(
+    name = "qiye-game1",
+    version = "0.1",
+    author = "qiye",
+    #url = "qiyenull.github.io",
+    packages = find_packages("qiye"),
+    package_dir = {"":"qiye"},
+    package_data = {
+        "":[".txt", ".info", "*.properties", ".py"],
+        "":["data/*.*"],
+    },
+    exclude = ["*.test", "*.test.*", "test.*", "test"]
+
+)
