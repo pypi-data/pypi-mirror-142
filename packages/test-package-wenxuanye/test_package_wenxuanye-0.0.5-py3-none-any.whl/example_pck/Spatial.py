94fd7b675128646e82c3593142b8c8400d59ee4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:10:50 +0000

    Add package test-package-wenxuanye/0.0.5/test_package_wenxuanye-0.0.5-py3-none-any.whl

---
+++ packages/test-package-wenxuanye/test_package_wenxuanye-0.0.5-py3-none-any.whl/example_pck/Spatial.py
@@ -0,0 +1,24 @@
+# Fuction name: GetStateAbbreviation
+# input: StateName
+# output: StateAbbreviation
+# info: None
+def GetStateAbbreviation(StateName):
+    dict = State_Abbreviation_Dict
+    if StateName in dict:
+        return dict[StateName]
+    else:
+        return "Not Found"
+
+# Function Name: fromState2FIPS
+# input: String abbrevation of a state. For example "PA"
+# output: The FIPS value that corresponds to the state (For example the FIPS for PA is 42)
+# info:https://www.nrcs.usda.gov/wps/portal/nrcs/detail/national/home/?cid=nrcs143_013697
+from Constant import *
+def fromState2FIPS(StateString):
+    dict = State_Abbreviation_to_FIPS_Dict
+    if StateString in dict:
+        return dict[StateString]
+    else:
+        return "Not Found"
+
+
