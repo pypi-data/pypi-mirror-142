f832fa2223dd75f67e20a662ece64bf93f0412a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:26:42 +0000

    Add package doccano/1.6.1/doccano-1.6.1.tar.gz

---
+++ packages/doccano/doccano-1.6.1.tar.gz/doccano-1.6.1/projects/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+
+from .views.member import MemberDetail, MemberList
+from .views.project import ProjectDetail, ProjectList
+from .views.tag import TagDetail, TagList
+
+urlpatterns = [
+    path(route="projects", view=ProjectList.as_view(), name="project_list"),
+    path(route="projects/<int:project_id>", view=ProjectDetail.as_view(), name="project_detail"),
+    path(route="projects/<int:project_id>/tags", view=TagList.as_view(), name="tag_list"),
+    path(route="projects/<int:project_id>/tags/<int:tag_id>", view=TagDetail.as_view(), name="tag_detail"),
+    path(route="projects/<int:project_id>/members", view=MemberList.as_view(), name="member_list"),
+    path(route="projects/<int:project_id>/members/<int:member_id>", view=MemberDetail.as_view(), name="member_detail"),
+]
