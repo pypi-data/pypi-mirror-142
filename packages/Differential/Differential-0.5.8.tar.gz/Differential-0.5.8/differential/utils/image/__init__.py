aa3dd2731db028e2703feef49a9bdecce79f0bbb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:28:30 +0000

    Add package Differential/0.5.8/Differential-0.5.8.tar.gz

---
+++ packages/Differential/Differential-0.5.8.tar.gz/Differential-0.5.8/differential/utils/image/__init__.py
@@ -0,0 +1,20 @@
+
+class ImageUploaded:
+    def __init__(self, url, thumb=None):
+        self.url = url
+        self.thumb = thumb
+
+    def __str__(self):
+        if self.thumb:
+            return f"[url={self.url}][img]{self.thumb}[/img][/url]"
+        return f"[img]{self.url}[/img]"
+
+
+
+from differential.utils.image.byr import byr_upload
+from differential.utils.image.hdbits import hdbits_upload
+from differential.utils.image.imgbox import imgbox_upload
+from differential.utils.image.smms import smms_upload
+from differential.utils.image.ptpimg import ptpimg_upload
+from differential.utils.image.imgurl import imgurl_upload
+from differential.utils.image.chevereto import chevereto_api_upload, chevereto_cookie_upload, chevereto_username_upload
