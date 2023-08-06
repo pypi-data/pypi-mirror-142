2ca3d7fa951eb429196d022705723feffd628fce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:08:48 +0000

    Add package telegold-translate/0.0.1/telegold-translate-0.0.1.tar.gz

---
+++ packages/telegold-translate/telegold-translate-0.0.1.tar.gz/telegold-translate-0.0.1/setup.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+setup(name = 'telegold-translate', version = '0.0.1',author='AliTP',author_email='mikesingerali@gmail.com',url='https://t.me/aiitp', packages = ['telegold'])
