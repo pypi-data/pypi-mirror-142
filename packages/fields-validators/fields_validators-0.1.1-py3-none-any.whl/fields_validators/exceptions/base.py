78d78045532b07dafca321124bbda61997c5dace
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:20:04 +0000

    Add package fields-validators/0.1.1/fields_validators-0.1.1-py3-none-any.whl

---
+++ packages/fields-validators/fields_validators-0.1.1-py3-none-any.whl/fields_validators/exceptions/base.py
@@ -0,0 +1,14 @@
+import sys
+
+
+class BaseError(Exception):
+
+    def __init__(
+            self,
+            detail: str,
+            error_info: str,
+    ) -> None:
+        self.traceback = sys.exc_info()
+        self.detail = detail
+        self.error_info = error_info
+        super().__init__(error_info)
