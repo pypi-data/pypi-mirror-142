d4c1973f7aaa4b0de6a2501f82c674e2bce085c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:10:23 +0000

    Add package pyrinnaitouch/0.8.0/pyrinnaitouch-0.8.0-py3-none-any.whl

---
+++ packages/pyrinnaitouch/pyrinnaitouch-0.8.0-py3-none-any.whl/pyrinnaitouch/util.py
@@ -0,0 +1,14 @@
+﻿"""Utility functions"""
+import logging
+
+_LOGGER = logging.getLogger(__name__)
+
+def get_attribute(data, attribute, default_value):
+    """get json attriubte from data."""
+    return data.get(attribute) or default_value
+
+def y_n_to_bool(str_arg):
+    """Convert Rinnai YN to Bool"""
+    if str_arg == "Y":
+        return True
+    return False
