d8996bb2d42006cba1b06d08d472cd88f30a7ba2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:16 +0000

    Add package contributors-txt/0.4.1/contributors-txt-0.4.1.tar.gz

---
+++ packages/contributors-txt/contributors-txt-0.4.1.tar.gz/contributors-txt-0.4.1/contributors_txt/const.py
@@ -0,0 +1,7 @@
+from pathlib import Path
+
+HERE = Path(__file__).parent
+DEFAULT_CONTRIBUTOR_PATH = "CONTRIBUTORS.txt"
+GIT_SHORTLOG = ["git", "shortlog", "--summary", "--numbered", "--email"]
+NO_SHOW_MAIL = ["bot@noreply.github.com"]
+NO_SHOW_NAME = ["root"]
