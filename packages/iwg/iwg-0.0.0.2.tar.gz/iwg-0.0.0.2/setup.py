76f3c9e86c7ce088eed102089ff538a782a5cf8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:43 +0000

    Add package iwg/0.0.0.2/iwg-0.0.0.2.tar.gz

---
+++ packages/iwg/iwg-0.0.0.2.tar.gz/iwg-0.0.0.2/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+try:
+    with open("README.md", encoding="utf-8") as fp:
+        LONG_DESCRIPTION = fp.read()
+except IOError:
+    LONG_DESCRIPTION = ""
+
+if __name__ == "__main__":
+    setuptools.setup(
+        name="iwg",
+        version="0.0.0.2",
+        description="An in-house website generator",
+        long_description=LONG_DESCRIPTION,
+        long_description_content_type="text/markdown",
+        author="Bora Ozdogan",
+        author_email="boraozdogan99@gmail.com",
+        url="http://github.com/bozdogan/iwg",
+        license="MIT",
+        packages=setuptools.find_packages(),
+        install_requires=["pytoml"],
+        extras_require={
+            "dev": [
+                "pytest"
+            ]
+        }
+    )
