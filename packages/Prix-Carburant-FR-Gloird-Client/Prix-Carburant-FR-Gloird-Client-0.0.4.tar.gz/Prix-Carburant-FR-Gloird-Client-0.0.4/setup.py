d9151a0b9d373b33fe8b2c28780f7f7b3e61d17c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:52:21 +0000

    Add package Prix-Carburant-FR-Gloird-Client/0.0.4/Prix-Carburant-FR-Gloird-Client-0.0.4.tar.gz

---
+++ packages/Prix-Carburant-FR-Gloird-Client/Prix-Carburant-FR-Gloird-Client-0.0.4.tar.gz/Prix-Carburant-FR-Gloird-Client-0.0.4/setup.py
@@ -0,0 +1,29 @@
+import pathlib
+
+from setuptools import setup
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+# This call to setup() does all the work
+setup(
+    name="Prix-Carburant-FR-Gloird-Client",
+    version="0.0.4",
+    description="StationEssence client by Gloird",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Gloird/essence",
+    author="Maxime Wantiez, Yann RITTER, Nicolas <Gloird> DUPUIS",
+    author_email="gloird@gmail.com",
+    license="MIT",
+    packages=["prixCarburantGloirdClient"],
+    include_package_data=True,
+    entry_points={
+        "console_scripts": [
+            "realpython=reader.__main__:main",
+        ]
+    },
+)
