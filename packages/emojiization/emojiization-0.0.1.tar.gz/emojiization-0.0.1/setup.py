30868c90c9caf4b920d03f3a87fd9174b9631256
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:26:41 +0000

    Add package emojiization/0.0.1/emojiization-0.0.1.tar.gz

---
+++ packages/emojiization/emojiization-0.0.1.tar.gz/emojiization-0.0.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+    page_description = f.read()
+
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name="emojiization",
+    version="0.0.1",
+    author="Dados ao Cubo",
+    author_email="tiago@dadosaocubo.com",
+    description="O pacote emojiization foi desenvolvido pelo time do dadosaocubo.com para tratamento de textos com Emojis",
+    long_description=page_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/dadosaocubo/emojiization-package",
+    packages=find_packages(),
+    install_requires=requirements,
+    python_requires='>=3.8',
+)
