d8ac76043fd649ad34b8bbeaee6cc68017928593
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:18:57 +0000

    Add package now-info-cli/1.3.0/now-info-cli-1.3.0.tar.gz

---
+++ packages/now-info-cli/now-info-cli-1.3.0.tar.gz/now-info-cli-1.3.0/setup.py
@@ -0,0 +1,28 @@
+"""Setup config for Now CLI."""
+import pathlib
+
+from setuptools import setup
+
+here = pathlib.Path(__file__).parent.resolve()
+long_description = (here / "README.md").read_text(encoding="utf-8")
+
+with open("requirements.txt", "r") as requirements_file:
+    requirements = [line.replace("\n", "") for line in requirements_file.readlines()]
+
+with open("version.txt", "r") as version_file:
+    version = version_file.read().strip()
+
+setup(
+    name="now-info-cli",
+    version=version,
+    description="Command line utilities for information about now",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    py_modules=["now"],
+    install_requires=requirements,
+    entry_points={
+        "console_scripts": [
+            "now = now:cli",
+        ],
+    },
+)
