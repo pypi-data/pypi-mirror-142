d493df13511f09d0c74348db4e12e040878935bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:11:41 +0000

    Add package test-package-wenxuanye/0.0.7/test_package_wenxuanye-0.0.7.tar.gz

---
+++ packages/test-package-wenxuanye/test_package_wenxuanye-0.0.7.tar.gz/test_package_wenxuanye-0.0.7/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+  long_description = fh.read()
+
+setuptools.setup(
+  name="test_package_wenxuanye",
+  version="0.0.7",
+  author="WENXUAN YE",
+  author_email="lehighwenxuanye@gmail.com",
+  description="A small example package",
+  long_description=long_description,
+  long_description_content_type="text/markdown",
+  url="https://github.com/wenxuanye/testPackage.git",
+  classifiers=[
+  "Programming Language :: Python :: 3",
+  "License :: OSI Approved :: MIT License",
+  "Operating System :: OS Independent",
+  ],
+  package_dir={"": "src"},
+  packages = setuptools.find_packages(where="src"),
+  python_requires=">=3.6",
+)
