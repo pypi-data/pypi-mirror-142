6b118b7b8145ab4974184d04d20647dc421eff59
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:09:42 +0000

    Add package metabase-manager/0.1.1/metabase-manager-0.1.1.tar.gz

---
+++ packages/metabase-manager/metabase-manager-0.1.1.tar.gz/metabase-manager-0.1.1/setup.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+package_dir = \
+{'': 'src'}
+
+packages = \
+['metabase_manager', 'metabase_manager.cli']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['metabase-python>=0.3.0,<0.4.0']
+
+entry_points = \
+{'console_scripts': ['metabase-manager = metabase_manager.cli.main:cli']}
+
+setup_kwargs = {
+    'name': 'metabase-manager',
+    'version': '0.1.1',
+    'description': 'Manage your Metabase instance programmatically.',
+    'long_description': None,
+    'author': 'Charles Lariviere',
+    'author_email': None,
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'package_dir': package_dir,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
