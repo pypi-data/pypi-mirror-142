853383d37241e10dda089e55ccc06a74651483a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:24:11 +0000

    Add package prepify/0.9.3/prepify-0.9.3.tar.gz

---
+++ packages/prepify/prepify-0.9.3.tar.gz/prepify-0.9.3/prepify/py/tips.py
@@ -0,0 +1,35 @@
+
+
+SINGLE_FOLIO_WARNING = '''\
+The use of this option should be exeedingly rare.
+When the "Supplemental" checkbox is NOT ticked, it
+will result in the existence of either more 'a' side
+images or more 'b' side images. I.e., you will be
+creating an 'a' without a 'b' or vice versa.
+
+If "Supplemental" is ticked, then the single folio
+side will be treated as a continuation of the previous
+folio. E.g., "5.1a, 5.2a". A "Supplemental" single
+folio side will not increase the total number of
+distinct folia, but it will increase the number
+of images.
+
+Both options will add complexitity. Please be
+certain this is the best way to record the data.
+It is advised to check the spreadsheet to see
+how the addition of a single folio side
+will affect the imaging team.
+'''
+
+SAVE_AS = '''\
+Click with two fingers (macOS) 
+or right-click (Windows) to Save-As'''
+
+NEW = '''Clear entire form'''
+
+SUPPLEMENTAL = '''\
+Consider this a continuation of the previous folio.
+E.g., "23.1a, 23.2a"'''
+
+GA_NUMBER = '''\
+Enter only the GA designation, e.g. "P46", "032", "2882", "l1919"'''
