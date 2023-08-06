0dffc2822b6b2344917d360944cdd707b607533a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:24:10 +0000

    Add package sigma-cli/0.3.1/sigma-cli-0.3.1.tar.gz

---
+++ packages/sigma-cli/sigma-cli-0.3.1.tar.gz/sigma-cli-0.3.1/sigma/cli/check.py
@@ -0,0 +1,28 @@
+import pathlib
+import click
+
+from sigma.exceptions import SigmaError
+
+from sigma.cli.rules import load_rules
+
+@click.command()
+@click.option(
+    "--file-pattern", "-P",
+    default="*.yml",
+    show_default=True,
+    help="Pattern for file names to be included in recursion into directories.",
+)
+@click.argument(
+    "input",
+    nargs=-1,
+    type=click.Path(exists=True, path_type=pathlib.Path),
+)
+def check(input, file_pattern):
+    """Check Sigma rules for validity and best practices (not yet implemented)."""
+    try:
+        rule_collection = load_rules(input, file_pattern)
+        for rule in rule_collection.rules:
+            for error in rule.errors:
+                click.echo(error)
+    except SigmaError as e:
+        click.echo("Check error: " + str(e), err=True)
