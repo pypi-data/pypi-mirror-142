d2f2787f139559e0772e8d3e0628271e1ef53104
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:40:29 +0000

    Add package roborabbit/0.4.0/roborabbit-0.4.0-py3-none-any.whl

---
+++ packages/roborabbit/roborabbit-0.4.0-py3-none-any.whl/roborabbit/main.py
@@ -0,0 +1,36 @@
+import click
+import os
+import asyncio
+from roborabbit.rmq import create_from_config
+from pathlib import Path
+
+
+@click.command()
+@click.option('--config', default='config.yml', help='Path to rabbit config yaml file')
+@click.option('--host', help='RabbitMQ host')
+@click.option('--port', help='RabbitMQ port')
+@click.option('--virtualhost', help='RabbitMQ virtualhost')
+@click.option('--username', help='RabbitMQ username')
+@click.option('--password', help='RabbitMQ password')
+def main(config=None, host=None, port=None, virtualhost=None, username=None, password=None):
+    """import yaml config file and creates a dictionary from it"""
+    # get the path to the config file
+    path = Path(config)
+
+    if host:
+        os.environ['RABBIT_HOST'] = host
+    if port:
+        os.environ['RABBIT_PORT'] = port
+    if virtualhost:
+        os.environ['RABBIT_VHOST'] = virtualhost
+    if username:
+        os.environ['RABBIT_USER'] = username
+    if password:
+        os.environ['RABBIT_PASS'] = password
+
+    # open the config file and read it
+    asyncio.run(create_from_config(path))
+
+
+if __name__ == '__main__':
+    main()
