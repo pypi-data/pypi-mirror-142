b3322c10c34dc5d87ab857f6fefd01765359eada
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:27:12 +0000

    Add package yxytmtoolP/0.0.3/yxytmtoolP-0.0.3-py3-none-any.whl

---
+++ packages/yxytmtoolP/yxytmtoolP-0.0.3-py3-none-any.whl/tmtool/tools.py
@@ -0,0 +1,17 @@
+# @Time    : 2022/3/15 7:15 下午 
+# @Author  : yangxy
+# @File    : tools.py 
+# @Desc    :
+# @Software: PyCharm
+
+from .util import send_mail_util
+
+class Tool(object):
+    def __init__(self):
+        self.mail_from_user = ''  # 邮件发送者账号
+        self.mail_from_user_pwd = '' # 邮件发送者密码
+        self.mail_from_user_host = '' # 邮件发送者host
+        # self.qy_wechat_token = '' # 企业微信机器人token
+
+    def send_mail_msg(self, to_user, subject, content):
+        send_mail_util(self.mail_from_user, self.mail_from_user_pwd, self.mail_from_user_host, to_user, subject, content)
