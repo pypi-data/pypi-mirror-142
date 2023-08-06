5f715a9ade133e9aedf9d0f4aa9b9108f2f065a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:36:57 +0000

    Add package configflow/0.1.1/configflow-0.1.1-py3-none-any.whl

---
+++ packages/configflow/configflow-0.1.1-py3-none-any.whl/configflow/__init__.py
@@ -0,0 +1,19 @@
+"""configflow - is the configuration management library for Python."""
+
+from configflow import sources
+from configflow.configflow import Configuration
+
+
+__title__ = "configflow"
+__summary__ = "configflow - is the configuration management library for Python."
+__uri__ = "https://configflow.readthedocs.io/en/latest"
+
+__version__ = "0.1.1"
+
+__author__ = "Volodymyr Pivoshenko"
+__email__ = "volodymyr.pivoshenko@gmail.com"
+
+__license__ = "MIT"
+__copyright__ = "Copyright 2022, Volodymyr Pivoshenko"
+
+__all__ = ["sources", "Configuration"]
