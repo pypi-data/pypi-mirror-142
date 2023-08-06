9627db7ec4dff540ba181ea1b0aacfc023664d5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:41:58 +0000

    Add package ubcpdk/1.3.6/ubcpdk-1.3.6.tar.gz

---
+++ packages/ubcpdk/ubcpdk-1.3.6.tar.gz/ubcpdk-1.3.6/ubcpdk/components/grating_couplers.py
@@ -0,0 +1,48 @@
+import gdsfactory as gf
+from ubcpdk.import_gds import import_gds, add_ports_renamed_gratings
+
+
+add_ports_rotate180 = gf.compose(gf.functions.rotate180, add_ports_renamed_gratings)
+
+
+gc_te1550 = gf.partial(
+    import_gds,
+    "ebeam_gc_te1550.gds",
+    decorator=add_ports_rotate180,
+    polarization="te",
+    wavelength=1.55,
+)
+
+# FIXME: move 1nm to the left, snapping issue?
+
+gc_te1550_broadband = gf.partial(
+    import_gds,
+    "ebeam_gc_te1550_broadband.gds",
+    decorator=add_ports_rotate180,
+    polarization="te",
+    wavelength=1.55,
+)
+
+
+gc_te1310 = gf.partial(
+    import_gds,
+    "ebeam_gc_te1310.gds",
+    decorator=add_ports_rotate180,
+    polarization="te",
+    wavelength=1.31,
+)
+
+gc_tm1550 = gf.partial(
+    import_gds,
+    "ebeam_gc_tm1550.gds",
+    decorator=add_ports_rotate180,
+    polarization="tm",
+    wavelength=1.55,
+)
+
+
+if __name__ == "__main__":
+    # c = gc_te1310()
+    c = gc_tm1550()
+    # c = gc_te1550()
+    c.show()
