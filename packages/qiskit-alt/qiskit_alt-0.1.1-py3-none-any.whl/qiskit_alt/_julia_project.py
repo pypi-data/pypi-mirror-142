bda86f7f3f22f10127daded291e1ea555e60b908
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:48 +0000

    Add package qiskit-alt/0.1.1/qiskit_alt-0.1.1-py3-none-any.whl

---
+++ packages/qiskit-alt/qiskit_alt-0.1.1-py3-none-any.whl/qiskit_alt/_julia_project.py
@@ -0,0 +1,30 @@
+import logging
+import os
+from julia_project import JuliaProject
+
+qiskit_alt_path = os.path.dirname(os.path.abspath(__file__))
+
+# def _after_init_func():
+#     importlib.import_module('.hellomod', 'qiskit_alt')
+
+def new_project(calljulia="pyjulia"):
+    """Return a new `JuliaProject`.
+
+    Use this if you want to use both `pyjulia` (`julia` module) and `juliacall`
+    in a single session. For example, if are already using `pyjulia`, you can
+    do `new_proj = new_project(calljulia="juliacall")` and then
+    `new_proj.ensure_init()`.
+    """
+    return JuliaProject(
+        name="qiskit_alt",
+        package_path = qiskit_alt_path,
+        version_spec = "^1.6", # Must be at least 1.6
+        env_prefix = 'MYJULIAMOD_', # env variables prefixed with this may control JuliaProject
+        registries = {"QuantumRegistry" : "git@github.com:Qiskit-Extensions/QuantumRegistry.git"},
+        logging_level = logging.INFO, # or logging.WARN,
+        console_logging=False,
+        calljulia = calljulia
+        )
+
+
+project = new_project()
