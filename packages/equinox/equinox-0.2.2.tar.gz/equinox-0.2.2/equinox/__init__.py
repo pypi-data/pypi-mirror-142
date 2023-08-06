eeb317faa61991461bf462b5b5f007634bb1bfa8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:26:35 +0000

    Add package equinox/0.2.2/equinox-0.2.2.tar.gz

---
+++ packages/equinox/equinox-0.2.2.tar.gz/equinox-0.2.2/equinox/__init__.py
@@ -0,0 +1,26 @@
+from . import nn
+from .filters import (
+    combine,
+    filter,
+    is_array,
+    is_array_like,
+    is_inexact_array,
+    is_inexact_array_like,
+    merge,
+    partition,
+    split,
+)
+from .grad import (
+    filter_custom_vjp,
+    filter_grad,
+    filter_value_and_grad,
+    gradf,
+    value_and_grad_f,
+)
+from .jit import filter_jit, jitf
+from .module import Module, static_field
+from .tree import tree_at, tree_equal, tree_pformat
+from .update import apply_updates
+
+
+__version__ = "0.2.2"
