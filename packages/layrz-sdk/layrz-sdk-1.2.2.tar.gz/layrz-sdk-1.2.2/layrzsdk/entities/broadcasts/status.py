8e128fff2c2a73966a8fc20e0967d2c4931c06db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:19:29 +0000

    Add package layrz-sdk/1.2.2/layrz-sdk-1.2.2.tar.gz

---
+++ packages/layrz-sdk/layrz-sdk-1.2.2.tar.gz/layrz-sdk-1.2.2/layrzsdk/entities/broadcasts/status.py
@@ -0,0 +1,23 @@
+""" Broadcast result Status """
+from enum import Enum
+from http.client import UNAUTHORIZED
+
+class BroadcastStatus(Enum):
+  """ Broadcast result status """
+  OK = 'OK'
+  BADREQUEST = 'BADREQUEST'
+  INTERNALERROR = 'INTERNALERROR'
+  UNAUTHORIZED = 'UNAUTHORIZED'
+
+  @property
+  def __readable(self):
+    """ Readable """
+    return f'BroadcastStatus.{self.value}'
+
+  def __str__(self):
+    """ Readable property """
+    return self.__readable
+
+  def __repr__(self):
+    """ Readable property """
+    return self.__readable
