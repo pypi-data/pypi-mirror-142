83c35e14fefcd1b1b2da693f47c9267eadd09e4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:40:51 +0000

    Add package rgbprint/1.0.0/rgbprint-1.0.0.tar.gz

---
+++ packages/rgbprint/rgbprint-1.0.0.tar.gz/rgbprint-1.0.0/setup.py
@@ -0,0 +1,9 @@
+import setuptools
+
+setuptools.setup(
+    name="rgbprint",
+    version="1.0.0",
+    author="ddjerqq",
+    description="Print rgb colors on the console easily. Official github: https://github.com/ddjerqq/rgbprint",
+    packages=["rgbprint"]
+)
