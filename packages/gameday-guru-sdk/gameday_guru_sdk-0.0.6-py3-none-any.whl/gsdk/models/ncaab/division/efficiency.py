cea595dd1e496a67a98393380e7d4f7ebaba1e23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:09:41 +0000

    Add package gameday-guru-sdk/0.0.6/gameday_guru_sdk-0.0.6-py3-none-any.whl

---
+++ packages/gameday-guru-sdk/gameday_guru_sdk-0.0.6-py3-none-any.whl/gsdk/models/ncaab/division/efficiency.py
@@ -0,0 +1,17 @@
+import datetime
+from . import efficiencylike
+
+class Efficiency(efficiencylike.Efficiencylike):
+    division : efficiencylike.Divisionalog
+
+    def __init__(self, division: efficiencylike.Divisionalog):
+        self.division = division
+
+    def update_by_date(self, date: datetime.datetime) -> None:
+        """Updates the efficiency ratings for games on a certain date.
+
+        Args:
+            date (datetime.datetime): the date on which the games take place.
+        """
+        for game in self.division.get_games_on_date(date):
+            game.home.biupdate_and_serialize(game.away, game.home_pts, game.away_pts)
