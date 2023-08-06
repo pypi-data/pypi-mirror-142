cfefc9f663d607ec486e625b536665fa39e5d008
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:27:52 +0000

    Add package dolphindb/1.30.16.1/dolphindb-1.30.16.1-cp37-cp37m-win_amd64.whl

---
+++ packages/dolphindb/dolphindb-1.30.16.1-cp37-cp37m-win_amd64.whl/dolphindb/__init__.py
@@ -0,0 +1,11 @@
+name = "dolphindb"
+from .session import session
+from .session import DBConnectionPool
+from .session import BlockReader
+from .session import PartitionedTableAppender
+from .session import tableAppender
+from .session import BatchTableWriter
+from .table import *
+from .vector import Vector
+from .database import Database
+from .month import month
