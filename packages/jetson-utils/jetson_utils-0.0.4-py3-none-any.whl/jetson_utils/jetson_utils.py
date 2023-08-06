4533c21c9477e1d40d1e8c4f3f2f4ae49610c301
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:21:06 +0000

    Add package jetson-utils/0.0.4/jetson_utils-0.0.4-py3-none-any.whl

---
+++ packages/jetson-utils/jetson_utils-0.0.4-py3-none-any.whl/jetson_utils/jetson_utils.py
@@ -0,0 +1,18 @@
+import os 
+import subprocess
+
+class JetsonUtils:
+    def __init__(self):
+        pass
+    
+    def getJetsonEnv(self)-> dict:
+        env_jetson = subprocess.check_output("env | grep JETSON", shell=True)
+        foo = env_jetson.decode()
+        env_list = foo.split("\n")
+        env_dict = {}
+        for i in env_list:
+            if i:
+                i_items = i.split("=")
+                print(i_items)
+                env_dict[i_items[0]] = i_items[1]
+        return env_dict
