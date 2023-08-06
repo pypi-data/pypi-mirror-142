14fd304ad3d9cefe2fb122853c3e1aa9b0694b97
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:23:57 +0000

    Add package urw/0.0.7/urw-0.0.7.tar.gz

---
+++ packages/urw/urw-0.0.7.tar.gz/urw-0.0.7/urw/urw.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+
+from argparse import ArgumentParser
+import os
+import random
+import requests as r
+import sys
+
+
+def random_words(number_of_words):
+    exe_path = os.path.dirname(__file__)
+    f = open('{}/wordlist.txt'.format(exe_path), 'r', encoding='utf-16')
+    lines = f.readlines()
+
+    random_res = ''
+    while number_of_words > 0:
+        random_res += random.choice(lines).strip() + ' '
+        number_of_words = number_of_words - 1
+        
+    return random_res.strip()
+
+if __name__ == '__main__':
+    cmd_line = ArgumentParser(description='Random Urdu words and phrases generator.')
+    cmd_line.add_argument('-n', '--number_of_words', metavar='N', type=int, help='Number of words to generate for the complete output.')
+    args = cmd_line.parse_args()
+
+    ans = ''
+    if args.number_of_words:
+        ans = random_words(args.number_of_words)
+
+    if ans:
+        print('\033[1mRESULT\033[0m {}'.format(ans))
