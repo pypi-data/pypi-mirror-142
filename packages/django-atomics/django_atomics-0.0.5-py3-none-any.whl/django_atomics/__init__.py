a1dbb7ad76703a72f86dbfa7a8342d795f8e694b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:45:15 +0000

    Add package django-atomics/0.0.5/django_atomics-0.0.5-py3-none-any.whl

---
+++ packages/django-atomics/django_atomics-0.0.5-py3-none-any.whl/django_atomics/__init__.py
@@ -0,0 +1,9 @@
+from .atoms import *  # NOQA
+from .base import *  # NOQA
+from .molecules import *  # NOQA
+from .organisms import *  # NOQA
+from .version import get_version
+
+VERSION = (0, 0, 5, "final", 0)
+
+__version__ = get_version(VERSION)
