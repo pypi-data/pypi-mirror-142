0afc7c3459bbe584d037f0b6eea131ad91f8fe02
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:55:41 +0000

    Add package word-search-generator/1.1.1/word-search-generator-1.1.1.tar.gz

---
+++ packages/word-search-generator/word-search-generator-1.1.1.tar.gz/word-search-generator-1.1.1/src/word_search_generator/types.py
@@ -0,0 +1,12 @@
+from typing import Optional, TypedDict
+
+
+class KeyInfo(TypedDict):
+    start: tuple[int, int]
+    direction: str
+
+
+Puzzle = list[list[str]]
+Key = dict[str, KeyInfo]
+Fit = Optional[tuple[str, list[tuple[int, int]]]]
+Fits = dict[str, list[tuple[int, int]]]
