90d86782aad6f96a835ab0f9f8712802bc1812b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:19:49 +0000

    Add package hypixel.py/0.2.1/hypixel.py-0.2.1-py3-none-any.whl

---
+++ packages/hypixel.py/hypixel.py-0.2.1-py3-none-any.whl/hypixel/models/__init__.py
@@ -0,0 +1,18 @@
+"""
+hypixel.models
+~~~~~~~~~~~~~~~~~~~
+
+A module for modeling data.
+
+(c) 2021-present duhby
+MIT License, see LICENSE for more details.
+"""
+
+from .bans import Bans
+from .color import ColorType
+from .friend import Friend
+from .games import Game, GameCount, GameCounts, GameType
+from .guild import Guild, GuildMember, GuildRank
+from .key import Key
+from .player import Player
+from .status import Status
