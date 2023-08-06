8277fc5f4209d287ef48c5bc969ffe78fd2d5591
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:17:23 +0000

    Add package utsavpc/0.1/utsavpc-0.1-py3-none-any.whl

---
+++ packages/utsavpc/utsavpc-0.1-py3-none-any.whl/utsavpc/__init__.py
@@ -0,0 +1,15 @@
+class pckg:
+    def __init__(self):
+        print("Hello Welsomce to ustav Library.")
+    def sum(num1,num2):
+
+        return num1 + num2
+
+    def sub(num1,num2):
+        return num1 - num2
+
+    def mul(num1,num2):
+        return num1 * num2
+    
+def div(num1,num2):
+        return num1 / num2
