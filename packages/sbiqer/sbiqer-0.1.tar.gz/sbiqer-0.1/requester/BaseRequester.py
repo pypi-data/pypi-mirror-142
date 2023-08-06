bcc8600218bcb47dba15d29214793cea435705da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:38:32 +0000

    Add package sbiqer/0.1/sbiqer-0.1.tar.gz

---
+++ packages/sbiqer/sbiqer-0.1.tar.gz/sbiqer-0.1/requester/BaseRequester.py
@@ -0,0 +1,6 @@
+"""
+@ModuleName: BaseRequester
+@Description: 基础请求器，定义基础方法及相关变量提供给其下内置子类，同时暴露给用户以供用户进行二次扩展等
+@Author: Beier
+@Time: 2022/2/23 23:18
+"""
---
+++ packages/sbiqer/sbiqer-0.1.tar.gz/sbiqer-0.1/setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+# encoding: utf-8
+from setuptools import setup, find_packages
+
+setup(
+    name="sbiqer",
+    version="0.1",
+    license="MIT Licence",
+
+    url="https://github.com/srx-2000/sbiqer",
+    author="beier",
+    author_email="1601684622@qq.com",
+
+    packages=find_packages(),
+    include_package_data=True,
+    platforms="any",
+    install_requires=[]
+)
