db6af2cb5159106f1cc75bff06926bf56d9031d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:11:17 +0000

    Add package csverve/0.2.3/csverve-0.2.3.tar.gz

---
+++ packages/csverve/csverve-0.2.3.tar.gz/csverve-0.2.3/csverve/__init__.py
@@ -0,0 +1,16 @@
+"""Top-level package for csverve."""
+
+
+__author__ = """Shah Lab"""
+__email__ = 'todo@todo.com'
+__version__ = '0.2.3'
+
+
+# csverve methods
+from csverve.api import rewrite_csv_file
+from csverve.api import merge_csv
+from csverve.api import concatenate_csv
+from csverve.api import simple_annotate_csv
+from csverve.api import add_col_from_dict
+from csverve.api import write_dataframe_to_csv_and_yaml
+from csverve.api import read_csv
