fdd783df14877776a945f5068caacecff39af494
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:33:06 +0000

    Add package base-test/0.1/base-test-0.1.tar.gz

---
+++ packages/base-test/base-test-0.1.tar.gz/base-test-0.1/setup.py
@@ -0,0 +1,9 @@
+from unicodedata import name
+from setuptools import setup
+setup(name= "base-test",
+version="0.1",
+description= "This is my first package",
+long_description= "this is long description of my package",
+author= "Abhishek",
+packages=['base-test'],
+install_requires= [])
