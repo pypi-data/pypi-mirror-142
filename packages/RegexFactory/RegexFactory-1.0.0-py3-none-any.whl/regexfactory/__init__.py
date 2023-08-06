8acfd7ee0ab4aef3ce738e658758bdd630c97a51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:49 +0000

    Add package RegexFactory/1.0.0/RegexFactory-1.0.0-py3-none-any.whl

---
+++ packages/RegexFactory/RegexFactory-1.0.0-py3-none-any.whl/regexfactory/__init__.py
@@ -0,0 +1,19 @@
+"""
+API Reference
+##############
+
+The regexfactory module documentation!
+
+.. automodule:: regexfactory.pattern
+
+.. automodule:: regexfactory.patterns
+
+.. automodule:: regexfactory.chars
+
+"""
+
+from .chars import *
+from .pattern import RegexPattern, join, escape
+from .patterns import *
+
+__version__ = "1.0.0"
