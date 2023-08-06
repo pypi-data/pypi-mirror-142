bfac5a890c40da9edd3f97db42cb96d8af5c5b56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:55:40 +0000

    Add package rcmt/0.5.0/rcmt-0.5.0.tar.gz

---
+++ packages/rcmt/rcmt-0.5.0.tar.gz/rcmt-0.5.0/rcmt/cli.py
@@ -0,0 +1,36 @@
+import click
+
+import rcmt
+
+
+@click.command()
+@click.option("--config", help="Path to configuration file.", default="", type=str)
+@click.option(
+    "--packages",
+    help="Path to packages directory.",
+    multiple=True,
+    required=False,
+    type=str,
+)
+@click.argument("matcher_file")
+def run(config: str, packages: list[str], matcher_file: str):
+    opts = rcmt.options_from_config(config)
+    opts.matcher_path = matcher_file
+    opts.packages_paths = packages
+    result = rcmt.execute(opts)
+    if result is False:
+        exit(1)
+
+
+@click.command()
+def version():
+    click.echo(rcmt.__version__)
+
+
+@click.group()
+def main():
+    pass
+
+
+main.add_command(run)
+main.add_command(version)
