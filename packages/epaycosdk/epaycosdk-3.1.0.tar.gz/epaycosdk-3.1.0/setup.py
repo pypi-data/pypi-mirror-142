07383912365778c8e23252eb9cebf7a7e9df8fba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:21:22 +0000

    Add package epaycosdk/3.1.0/epaycosdk-3.1.0.tar.gz

---
+++ packages/epaycosdk/epaycosdk-3.1.0.tar.gz/epaycosdk-3.1.0/setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+from setuptools  import setup, find_packages
+
+setup(
+    name="epaycosdk",
+    version="3.1.0",
+    include_package_data=True,
+    author="ePayco Development Team",
+    author_email="ricardo.saldarriaga@epayco.co",
+    packages=find_packages(),
+    url='https://epayco.co/',
+    download_url="https://github.com/epayco/epayco-python",
+    license="MIT",
+    description="Python library for ePayco Payment API",
+    long_description="Basic python library to interact with ePayco Payment API",
+    install_requires=[
+        "requests >= 2.4.3",
+        "pycrypto >= 2.6.1",
+    ],
+)
