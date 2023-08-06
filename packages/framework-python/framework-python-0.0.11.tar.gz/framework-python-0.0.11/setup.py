f82a19f29ffb44d1dbb428cad17e65a8287c94a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:44:47 +0000

    Add package framework-python/0.0.11/framework-python-0.0.11.tar.gz

---
+++ packages/framework-python/framework-python-0.0.11.tar.gz/framework-python-0.0.11/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.11'
+DESCRIPTION = 'Macai framework for AWS lambda in Python'
+LONG_DESCRIPTION = 'Macai framework for AWS lambda in Python'
+
+setup(
+    name="framework-python",
+    version=VERSION,
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    author="Macai",
+    author_email="developers@macaiapp.com",
+    license='MIT',
+    packages=find_packages(),
+    install_requires=['sentry-sdk>=1.5.7', 'aws-xray-sdk>=2.4.3', 'boto3>=1.21.17'],
+    url='https://macaiapp.com',
+    classifiers= [
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        'License :: OSI Approved :: MIT License',
+        "Programming Language :: Python :: 3",
+    ]
+)
