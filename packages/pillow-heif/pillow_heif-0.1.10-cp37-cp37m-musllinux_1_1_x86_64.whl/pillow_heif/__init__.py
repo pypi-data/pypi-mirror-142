97a34f2503b44dbbef28a4e444e6187ecb7276fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:16:55 +0000

    Add package pillow-heif/0.1.10/pillow_heif-0.1.10-cp37-cp37m-musllinux_1_1_x86_64.whl

---
+++ packages/pillow-heif/pillow_heif-0.1.10-cp37-cp37m-musllinux_1_1_x86_64.whl/pillow_heif/__init__.py
@@ -0,0 +1,26 @@
+"""
+Import all possible stuff that can be used.
+"""
+
+# pylint: disable=unused-import
+# pylint: disable=redefined-builtin
+from .constants import *  # pylint: disable=unused-wildcard-import
+from .reader import (
+    HeifFile,
+    UndecodedHeifFile,
+    HeifThumbnail,
+    UndecodedHeifThumbnail,
+    is_supported,
+    check_heif,
+    read_heif,
+    open_heif,
+    check,
+    read,
+    open,
+)
+from .writer import write_heif
+from .error import HeifError
+from .as_opener import register_heif_opener, check_heif_magic, HeifImageFile
+from ._version import __version__
+from ._lib_info import libheif_version, have_decoder_for_format, have_encoder_for_format, libheif_info
+from ._options import options
