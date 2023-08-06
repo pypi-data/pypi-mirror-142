00078415bdbe914cf6f03b0a03604faef1dd9de2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:24:21 +0000

    Add package py-appstream/0.1.3/py-appstream-0.1.3.tar.gz

---
+++ packages/py-appstream/py-appstream-0.1.3.tar.gz/py-appstream-0.1.3/py_appstream/utils.py
@@ -0,0 +1,16 @@
+# SPDX-FileCopyrightText: 2022 Phu Hung Nguyen <phuhnguyen@outlook.com>
+# SPDX-License-Identifier: LGPL-2.1-or-later
+
+from xml.etree import ElementTree
+
+
+def localize(out_obj, node, f=None):
+    lang = node.get('{http://www.w3.org/XML/1998/namespace}lang', node.get('lang', 'C'))
+    if lang != 'x-test':
+        val = ElementTree.tostring(node, encoding='unicode', method='text').strip()
+        if val:
+            out_obj[lang] = f(val) if f else val
+
+
+def join_lines(txt: str):
+    return ' '.join(txt.replace('\n', ' ').split())
