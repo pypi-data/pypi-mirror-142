550888dfff9a353a87bc42dbfed64548c7a4c0ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:13:04 +0000

    Add package simple-rest-server/1.0.4/simple_rest_server-1.0.4-py3-none-any.whl

---
+++ packages/simple-rest-server/simple_rest_server-1.0.4-py3-none-any.whl/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='simple_rest_server',
+    version='1.0.4',
+    packages=[''],
+    url='https://gitlab.com/pavel_lavi/simple-rest-server',
+    license='MIT',
+    author='Pavel Lavi',
+    author_email='LaviPavel@outlook.com',
+    description='simple rest server based on flask, useful for api testing'
+)
