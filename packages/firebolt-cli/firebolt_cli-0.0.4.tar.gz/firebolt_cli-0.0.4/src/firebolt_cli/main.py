a2394f75f6183555bd4288e3f97525202e47b58c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:21:36 +0000

    Add package firebolt-cli/0.0.4/firebolt_cli-0.0.4.tar.gz

---
+++ packages/firebolt-cli/firebolt_cli-0.0.4.tar.gz/firebolt_cli-0.0.4/src/firebolt_cli/main.py
@@ -0,0 +1,21 @@
+from click import group, version_option
+
+from firebolt_cli import __version__
+from firebolt_cli.configure import configure
+from firebolt_cli.database import database
+from firebolt_cli.engine import engine
+from firebolt_cli.query import query
+
+
+@group()
+@version_option(__version__, "-V", "--version")
+def main() -> None:
+    """
+    Firebolt command line utility.
+    """
+
+
+main.add_command(configure)
+main.add_command(engine)
+main.add_command(query)
+main.add_command(database)
