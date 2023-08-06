320a2d3dcfc4d2c448817b496712ea372cc98c58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:58:23 +0000

    Add package gemact/0.1.0/gemact-0.1.0.tar.gz

---
+++ packages/gemact/gemact-0.1.0.tar.gz/gemact-0.1.0/gemact/__init__.py
@@ -0,0 +1,13 @@
+__version__= "0.1.0"
+__doc__ = "GemAct is a comprehensive actuarial package, based on the collective risk theory framework, that offers a comprehensive set of tools for non-life (re)insurance pricing, stochastic claims reserving, and risk aggregation.\nThe variety of available functionalities makes GemAct modeling very flexible and provides actuarial scientists and practitioners with a powerful tool that fits into the expanding community of Python programming language."
+
+import gemact.helperfunctions as hfns
+import gemact.distributions as distributions
+import gemact.gemdata as gemdata
+from gemact.lossmodel import *
+from gemact.lossreserve import *
+from gemact.lossaggregation import *
+import gemact.sobol as sbl
+
+
+
