74dec4d178c8669e5cb2ef98dab82f1681997402
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:49:21 +0000

    Add package tomoe/1.0.4/tomoe-1.0.4.tar.gz

---
+++ packages/tomoe/tomoe-1.0.4.tar.gz/tomoe-1.0.4/tomoe/tomoe.py
@@ -0,0 +1,22 @@
+import asyncio
+from pururin import get_pur, input
+
+class Tomoe():
+    def __init__(self, Pururin: str = input().pururin):
+        self.pururin = Pururin
+
+Api = Tomoe()
+
+def main():
+    async def main_pururin():
+        await asyncio.gather(get_pur())
+
+    if Api.pururin is not None:
+        asyncio.run(main_pururin())
+
+    else: 
+        print("No arguments was given")
+
+if __name__ == '__main__':
+    main()
+    
