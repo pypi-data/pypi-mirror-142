412fb58d1919bc91367748b95b1bce4a0f3f5184
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:09:19 +0000

    Add package lichess/0.1.5/lichess-0.1.5.tar.gz

---
+++ packages/lichess/lichess-0.1.5.tar.gz/lichess-0.1.5/README.rst
@@ -0,0 +1,218 @@
+
+
+.. image:: https://raw.githubusercontent.com/qe/lichess/main/docs/lichess.png
+   :target: https://lichess.org
+   :width: 160
+   :alt: Logo
+
+=============================================
+lichess: a Python wrapper for the Lichess API
+=============================================
+
+.. image:: https://img.shields.io/pypi/l/lichess?label=license
+   :alt: License
+
+.. image:: https://img.shields.io/pypi/v/lichess
+   :target: https://pypi.org/project/lichess
+   :alt: PyPI
+
+.. image:: https://img.shields.io/github/repo-size/qe/lichess?label=repo-size
+   :target: https://github.com/qe/lichess
+   :alt: GitHub Repo Size
+
+.. image:: https://img.shields.io/github/languages/code-size/qe/lichess?label=code-size
+   :target: https://github.com/qe/lichess
+   :alt: GitHub Code Size (bytes)
+
+.. image:: https://img.shields.io/tokei/lines/github/qe/lichess?label=lines-of-code
+   :target: https://github.com/qe/lichess
+   :alt: Total Lines of Code
+
+lichess_ is a Python library for interacting with the `Lichess API <https://lichess.org/api>`_ and can be used to get profile data, game data, and much more.
+
+    **This project is still in Pre-Alpha. As a result, it is still unstable and not all features have been implemented.**
+
+|
+
+Install
+=======
+You can install lichess_ on the Terminal (macOS/UNIX) or the Command Prompt (Windows) with::
+
+    pip install lichess
+
+Alternatively, you can use Git to clone the repository from GitHub with::
+
+    git clone https://github.com/qe/lichess.git
+    cd lichess
+    pip install .
+
+Or, if you already have it, upgrade to the latest version with::
+
+    pip install lichess --upgrade
+
+|
+
+Example
+=======
+For the full documentation, please check the `Lichess Python API Documentation <https://lichess.readthedocs.io>`_.
+
+Some methods, such as ``Client.get_email()``, require authorization while others, such as ``Client.get_leaderboard()``,
+do not require it. As a result, if you want to use a method/endpoint that does require authorization, you will need to
+`generate a personal access token on Lichess <https://lichess.org/account/oauth/token>`_. If you do so, this will be your API Key.
+
+To determine whether or nor you need to generate a personal access token, check
+the `Lichess API Documentation <https://lichess.org/api>`_ if the endpoint your interested in using has a OAuth2 badge.
+
+Here's an example of using lichess_ **with** an personal access token:
+::
+
+    API_KEY = "<YOUR API KEY GOES HERE>"
+
+    myclient = Client(token=API_KEY)
+
+    print(myclient.get_email())
+
+::
+
+    {'email': 'youremailwillshowuphere@gmail.com'}
+
+
+Here's an example of using lichess_ **without** a personal access token:
+::
+
+    from lichess import
+
+    myclient = Client()
+
+    print(myclient.get_data("ismodes"))
+
+::
+
+    {
+    "id": "ismodes",
+    "username": "ismodes",
+    "perfs": {
+        "blitz": {"games": 90, "rating": 1599, "rd": 109, "prog": 21},
+        "puzzle": {"games": 984, "rating": 2355, "rd": 132, "prog": 0, "prov": True},
+        "bullet": {"games": 0, "rating": 1500, "rd": 500, "prog": 0, "prov": True},
+        "correspondence": {
+            "games": 0,
+            "rating": 1500,
+            "rd": 500,
+            "prog": 0,
+            "prov": True,
+        },
+        "classical": {"games": 0, "rating": 1500, "rd": 500, "prog": 0, "prov": True},
+        "rapid": {"games": 239, "rating": 1909, "rd": 102, "prog": -79},
+        "storm": {"runs": 15, "score": 24},
+        "racer": {"runs": 2, "score": 32},
+    },
+    "createdAt": 1620502920988,
+    "online": True,
+    "profile": {
+        "country": "AR",
+        "location": "🇦🇷",
+        "bio": "🇦🇷",
+        "firstName": "Velez",
+        "links": "🇦🇷",
+    },
+    "seenAt": 1647342929853,
+    "playTime": {"total": 208321, "tv": 0},
+    "url": "https://lichess.org/@/ismodes",
+    "completionRate": 73,
+    "count": {
+        "all": 338,
+        "rated": 329,
+        "ai": 0,
+        "draw": 13,
+        "drawH": 13,
+        "loss": 148,
+        "lossH": 148,
+        "win": 177,
+        "winH": 177,
+        "bookmark": 2,
+        "playing": 0,
+        "import": 0,
+        "me": 0,
+    },
+    }
+
+
+
+For more examples, check the examples directory in the source code.
+
+|
+
+Warnings
+========
+
+    **Make sure your inputs are valid.**
+
+There is basic error handling for some invalid inputs, but there are exceptions.
+
+
+Firstly, there are many types of invalid inputs. Here is an example:
+
+- Usernames that don't exist
+    - Valid username, but no user has taken it
+    - Invalid username
+
+lichess_ does basic error handling with invalid inputs (using Regex), but it does not
+account for valid inputs that do not exist. Here's a more explicit example:
+
+``"jzq0wUnCYR"`` is a valid username (i.e. it can be registered), but at the time of writing this,
+there is no user by this name. As a result, the following code does not return everything:
+::
+
+    from lichess import get_status
+
+    myclient = Client()
+
+    print(myclient.get_status("jzq0wUnCYR", "penguingim1"))
+
+::
+
+    [{'name': 'penguingim1', 'title': 'GM', 'patron': True, 'id': 'penguingim1'}]
+
+
+Conversely, ``"jzq0 wUnCYR"`` is an invalid username, since it has invalid characters (note the whitespace!)
+However, lichess_ does catch this error, as seen below:
+::
+
+    from lichess import get_status
+
+    myclient = Client()
+
+    print(myclient.get_status("jzq0 wUnCYR", "penguingim1"))
+
+::
+
+    Traceback (most recent call last):
+      File "main.py", line 109, in <module>
+        main()
+      File "main.py", line 76, in main
+        print(myclient.get_status("jzq0 wUnCYR", "penguingim1"))
+      raise ArgumentValueError("One or more usernames are invalid.")
+    lichess.exceptions.ArgumentValueError: One or more usernames are invalid.
+
+It is your responsibility to make sure your inputs are valid, but lichess_ tries its best to catch errors <3
+
+|
+
+Links
+=====
+- `Lichess Python API Documentation <https://lichess.readthedocs.io>`_
+- `Lichess API Documentation <https://lichess.org/api>`_
+- `Lichess Website <https://lichess.org>`_
+
+|
+
+Contact
+=======
+Email me at
+
+    **helloemailmerighthere [at] gmail [dot] com**
+
+
+.. _lichess: https://pypi.org/project/lichess/
+
---
+++ packages/lichess/lichess-0.1.5.tar.gz/lichess-0.1.5/lichess/client.py
@@ -0,0 +1,296 @@
+
+
+from .enums import *
+from .utils import *
+from .exceptions import *
+import requests
+import urllib
+import logging
+
+
+logger = logging.getLogger(__name__)
+VALID_PERF_TYPES = [_.value for _ in PerfType]
+
+
+class Client:
+    def __init__(self, token=None):
+        self.url = "https://lichess.org/"
+        self.s = requests.Session()  # keep session alive to improve performance <3
+        if token:
+            self.token = token
+            # self.s.headers.update(token)
+
+    def request(self, path, oauth=False, post_data=None, *args, **kwargs):
+        full_url = urllib.parse.urljoin(self.url, path)
+        print("hitting this URL:", full_url)
+
+        try:
+            if oauth:
+                print("OAUTH status:", oauth)
+                response = self.s.get(full_url, headers={"Authorization": f"Bearer {self.token}"})
+            else:
+                response = self.s.get(full_url)
+            # res = requests.get(address, timeout=30)  # add timeout
+        except requests.exceptions.RequestException as err:
+            logger.error(err)
+            raise
+
+        # print(response.content)
+        # print(response.text)
+
+        # print("response.status_code", response.status_code)
+
+        # remove this below eventually
+        # call format.py or formats.py file to convert to JSON
+        # https://docs.python-requests.org/en/latest/user/quickstart/#json-response-content
+        # may get 204 (No Content)
+        # if the response contains invalid JSON, attempting r.json() raises requests.exceptions.JSONDecodeError
+        if response.status_code == 200:
+            response2json = response.json()
+            print(type(response2json))
+            # if isinstance(response2json, list):
+            #     return response2json[0]
+            return response2json
+        else:
+            # There is some error in response
+            print("ERROR!", response.status_code)
+            return
+
+    # -- Account --------------------------------------------------------------
+
+    def get_profile(self):
+        """Get your public profile information
+
+        :return: A dictionary with your public information
+        :rtype: dict
+        """
+        endpoint = "api/account"
+        return self.request(path=endpoint, oauth=True)
+
+    def get_email(self):
+        """Get your email address
+
+        :return: A dictionary with your email address
+        :rtype: dict
+        """
+        endpoint = "api/account/email"
+        return self.request(path=endpoint, oauth=True)
+
+    def get_preferences(self):
+        """Get your preferences
+
+        :return: A dictionary with your preferences
+        :rtype: dict
+        """
+        endpoint = "api/account/preferences"
+        return self.request(path=endpoint, oauth=True)
+
+    def get_kid_mode(self):
+        """Get your kid mode status
+
+        :return: A dictionary with your kid mode status
+        :rtype: dict
+        """
+        endpoint = "api/account/kid"
+        return self.request(path=endpoint, oauth=True)
+
+    """
+    Implement post ability in client.py
+    """
+    # def set_kid_mode(self):
+    #     """Set your kid mode status
+    #
+    #     :return:
+    #     :rtype:
+    #     """
+    #     endpoint = "api/account/kid"
+    #     # POST
+    #     pass
+
+    # -- Users ----------------------------------------------------------------
+
+    def get_status(self, *users, with_game_ids=False):
+        """Get real-time status of one or more users
+
+        :param users: User to query their real-time status
+        :type users: str
+        :param with_game_ids: Flag to know whether or not include the ID of games being played, if any, for each player
+        :type with_game_ids: bool, optional
+        :return: A list with a nested dictionary containing the real-time status of one or more users
+        :rtype: list
+        """
+        invalid_inputs = [usr for usr in users if not valid_input(usr)]
+        if invalid_inputs:
+            raise ArgumentValueError("One or more usernames are invalid.")
+
+        endpoint = "api/users/status"
+        path = endpoint + "?ids=" + ','.join(users)
+
+        if with_game_ids:
+            path += "&withGameIds=true"
+
+        return self.request(path=path)
+
+    """
+    Create function in utils.py to manually parse these two responses (JSON problems)
+    """
+    # def get_top_ten(self):
+    #     """Get the top 10 players for each speed and variant
+    #
+    #     :return:
+    #     """
+    #     endpoint = "player"
+    #     return self.request(path=endpoint)
+    #
+    # def get_leaderboard(self, perf_type, num_users):
+    #     """Get leaderboard of an individual speed or variant
+    #
+    #     :return:
+    #     """
+    #     if perf_type not in VALID_PERF_TYPES:
+    #         raise ArgumentValueError("Value of perf_type is invalid.")
+    #     if (num_users <= 0) or (200 < num_users):
+    #         raise ArgumentValueError("Value of num_users is invalid. Valid range includes any integer from 1 to 200")
+    #
+    #     endpoint = "player/top/{nb}/{perfType}"
+    #     path = endpoint.format(nb=num_users, perfType=perf_type)
+    #     return self.request(path=path)
+
+    def get_data(self, user):
+        """Get public data of an individual user
+
+        :param user: User to query their public data
+        :type user: str
+        :return: A dictionary with the public data of the user
+        :rtype: dict
+        """
+        if not valid_input(user):
+            raise ArgumentValueError("Value of user is invalid.")
+
+        endpoint = "api/user/{username}"
+        path = endpoint.format(username=user)
+        return self.request(path=path)
+
+    def get_rating_history(self, user):
+        """Get rating history of an individual user
+
+        :param user: User to query their public data
+        :type user: str
+        :return: A list with a nested dictionary containing the rating history of the user
+        :rtype: list
+        """
+        if not valid_input(user):
+            raise ArgumentValueError("Value of user is invalid.")
+
+        endpoint = "api/user/{username}/rating-history"
+        path = endpoint.format(username=user)
+        return self.request(path=path)
+
+    """
+    Possibly add Enum for PerfType (also related to the get_leaderboard() method)
+    """
+    def get_stats(self, user, perf_type):
+        """Get performance statistics of an individual user
+
+        :param user: User to query their performance statistics
+        :type user: str
+        :param perf_type: Type of speed or variant to query
+        :type user: str
+        :return: A dictionary with the performance statistics of the user
+        :rtype: dict
+        """
+        if not valid_input(user):
+            raise ArgumentValueError("Value of user is invalid.")
+        if perf_type not in VALID_PERF_TYPES:
+            raise ArgumentValueError("Value of perf_type is invalid.")
+
+        endpoint = "api/user/{username}/perf/{perf}"
+        path = endpoint.format(username=user, perf=perf_type)
+        return self.request(path=path)
+
+    def get_activity(self, user):
+        """Get the activity feed of an individual user
+
+        :param user: User to query their activity feed
+        :type user: str
+        :return: A list with a nested dictionary containing the activity feed of the user
+        :rtype: list
+        """
+        if not valid_input(user):
+            raise ArgumentValueError("Value of user is invalid.")
+
+        endpoint = "api/user/{username}/activity"
+        path = endpoint.format(username=user)
+        return self.request(path=path)
+
+    def get_by_id():
+        """
+        Get users by ID
+
+        :return:
+        """
+        endpoint = "api/users"
+        pass
+
+    # def get_team_members():
+    #     """
+    #     Get members of a team
+    #
+    #     :return:
+    #     """
+    #     endpoint = "api/team/{teamId}/users"
+    #     pass
+    #
+    # def get_live_streamers():
+    #     """
+    #     Get live streamers
+    #
+    #     :return:
+    #     """
+    #     endpoint = "streamer/live"
+    #     pass
+    #
+    # def get_crosstable():
+    #     """
+    #     Get crosstable
+    #
+    #     :return:
+    #     """
+    #     endpoint = "api/crosstable/{user1}/{user2}"
+    #     pass
+
+    # # CUSTOM
+    # def get_followers():
+    #     endpoint =
+    #     pass
+    #
+    # # CUSTOM
+    # def get_following():
+    #     endpoint =
+    #     pass
+
+    # -- Relations ------------------------------------------------------------
+
+    def relations(self):
+        endpoint = "api/rel/following"
+        return self.request(path=endpoint, oauth=True)
+
+    # -- Games ----------------------------------------------------------------
+    # -- TV -------------------------------------------------------------------
+    # -- Puzzles --------------------------------------------------------------
+    # -- Teams ----------------------------------------------------------------
+    # -- Board ----------------------------------------------------------------
+    # -- Bot ------------------------------------------------------------------
+    # -- Challenges -----------------------------------------------------------
+    # -- Bulk pairings --------------------------------------------------------
+    # -- Arena tournaments ----------------------------------------------------
+    # -- Swiss Tournaments ----------------------------------------------------
+    # -- Simuls ---------------------------------------------------------------
+    # -- Studies --------------------------------------------------------------
+    # -- Messaging ------------------------------------------------------------
+    # -- Broadcasts -----------------------------------------------------------
+    # -- Analysis -------------------------------------------------------------
+    # -- Opening Explorer -----------------------------------------------------
+    # -- Tablebase ------------------------------------------------------------
+    # -- OAuth ----------------------------------------------------------------
+
