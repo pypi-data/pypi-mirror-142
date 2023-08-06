1f3228e8bea3b5f848c5720ed4fcb58aeeb86cd0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:20:58 +0000

    Add package temppythonsample/0.1.2/temppythonsample-0.1.2-py3-none-any.whl

---
+++ packages/temppythonsample/temppythonsample-0.1.2-py3-none-any.whl/temppythonsample/test.py
@@ -0,0 +1,17 @@
+import makana
+
+# makanaService = makana.MakanaService
+
+# makanaService.registerModelService("Test Model Dev1","Test Model Dev1","43013757-56c6-4641-b46d-88ce51e4cb6d",
+#                                "main.py","requirements.txt","Y:\Working\Makana\Models\main.py","Y:\Working\Makana\Models\requirements.txt")
+
+
+
+
+makanaModel = makana.MaknaModel
+
+
+# makanaModel.registerModel("Test Model Dev1","Test Model Dev1",10,
+#                                "R","sentiment.h5","Y:\Working\Makana\Models\\requirements.txt")
+
+# data = makanaModel.getMakanaModel()
