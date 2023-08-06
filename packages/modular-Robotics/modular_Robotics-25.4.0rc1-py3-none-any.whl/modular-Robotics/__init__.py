68233e50a56f97c1bb023d8dac445b08b921adff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:12:50 +0000

    Add package modular-Robotics/25.4.0rc1/modular_Robotics-25.4.0rc1-py3-none-any.whl

---
+++ packages/modular-Robotics/modular_Robotics-25.4.0rc1-py3-none-any.whl/modular-Robotics/__init__.py
@@ -0,0 +1,2 @@
+from modularRobotics.test import test_print
+from _version import __version__
---
+++ packages/modular-Robotics/modular_Robotics-25.4.0rc1-py3-none-any.whl/modular-Robotics/movement.py
@@ -0,0 +1,11 @@
+"""
+A class that provides basic command to move the Robot.
+"""
+
+
+def drive():
+    pass
+
+
+def turn():
+    pass
