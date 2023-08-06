6d347e895cdd05fb0df3015c347e37a82673c77d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:50:18 +0000

    Add package pq-tool/0.1/pq-tool-0.1.tar.gz

---
+++ packages/pq-tool/pq-tool-0.1.tar.gz/pq-tool-0.1/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='pq-tool',
+      version='0.1',
+      description='pickle-query Tool',
+      author='Salvador Pardiñas',
+      author_email='darkfm@vera.com.uy',
+      scripts=['bin/pq']
+)
