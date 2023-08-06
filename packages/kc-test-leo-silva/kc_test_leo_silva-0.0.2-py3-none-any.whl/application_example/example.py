ab8e74ce71a86b7c33e77f58a8c72f90105ddf1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:16:21 +0000

    Add package kc-test-leo-silva/0.0.2/kc_test_leo_silva-0.0.2-py3-none-any.whl

---
+++ packages/kc-test-leo-silva/kc_test_leo_silva-0.0.2-py3-none-any.whl/application_example/example.py
@@ -0,0 +1,10 @@
+import coloredlogs, logging
+
+logger=logging.getLogger(__name__)
+coloredlogs.install(level='DEBUG',logger=logger)
+logger.debug("Hola Keepcoders")
+
+def add_one(number):
+    return number+1
+
+print(add_one(3))    
