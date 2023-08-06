b5d635784afae5909e8fad57358323ba20e4dee6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:12:34 +0000

    Add package homeassistant-api/3.0.3.post1/HomeAssistant-API-3.0.3.post1.tar.gz

---
+++ packages/homeassistant-api/HomeAssistant-API-3.0.3.post1.tar.gz/HomeAssistant-API-3.0.3.post1/homeassistant_api/_async/__init__.py
@@ -0,0 +1,3 @@
+"""Imports objects from the async sub-module for convenience."""
+from .asyncclient import RawAsyncClient
+from .models import AsyncDomain, AsyncEntity, AsyncEvent, AsyncGroup, AsyncService
