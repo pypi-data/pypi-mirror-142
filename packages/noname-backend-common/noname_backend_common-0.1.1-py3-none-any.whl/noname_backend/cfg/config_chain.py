fd53b06e9fca90f6d52366b90d8dcd4d573cca51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:35 +0000

    Add package noname-backend-common/0.1.1/noname_backend_common-0.1.1-py3-none-any.whl

---
+++ packages/noname-backend-common/noname_backend_common-0.1.1-py3-none-any.whl/noname_backend/cfg/config_chain.py
@@ -0,0 +1,28 @@
+from flask import Flask
+
+class ConfigChain:
+    def __init__(self, chain):
+        self._chain = chain
+
+    def __getitem__(self, key):
+        return ConfigChainSection(self._chain, key)
+
+class ConfigChainSection:
+    def __init__(self, chain, section):
+        self._chain = chain
+        self._section = section
+
+    def __getitem__(self, key):
+        for cfg in self._chain:
+            section = _get_or_none(cfg, self._section)
+            if section is not None:
+                value = _get_or_none(section, key)
+                if value is not None:
+                    return value
+        raise KeyError(f'{key} not found')
+
+def _get_or_none(config, key):
+    try:
+        return config[key]
+    except KeyError:
+        return None
