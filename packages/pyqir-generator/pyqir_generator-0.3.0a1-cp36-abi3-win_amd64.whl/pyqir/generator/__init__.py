5669b2f0d9f452df248bb0dfddbf44173be99365
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:53:23 +0000

    Add package pyqir-generator/0.3.0a1/pyqir_generator-0.3.0a1-cp36-abi3-win_amd64.whl

---
+++ packages/pyqir-generator/pyqir_generator-0.3.0a1-cp36-abi3-win_amd64.whl/pyqir/generator/__init__.py
@@ -0,0 +1,10 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+
+from pyqir.generator._native import (
+    BasicQisBuilder as BasicQisBuilder,
+    Builder as Builder,
+    Qubit as Qubit,
+    ResultRef as ResultRef,
+    SimpleModule as SimpleModule,
+)
