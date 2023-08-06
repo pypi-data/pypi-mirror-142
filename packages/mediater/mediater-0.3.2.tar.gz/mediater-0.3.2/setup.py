bef2af49b53389f3aabf4bb40908f7e03638f1ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:12:16 +0000

    Add package mediater/0.3.2/mediater-0.3.2.tar.gz

---
+++ packages/mediater/mediater-0.3.2.tar.gz/mediater-0.3.2/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['mediater']
+
+package_data = \
+{'': ['*']}
+
+entry_points = \
+{'console_scripts': ['mediater = mediater.console:run']}
+
+setup_kwargs = {
+    'name': 'mediater',
+    'version': '0.3.2',
+    'description': '',
+    'long_description': None,
+    'author': 'zenwalk',
+    'author_email': 'zenwalk@example.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'entry_points': entry_points,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
