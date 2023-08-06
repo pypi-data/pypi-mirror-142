7fb444d8bce4f2580032a2e950d3ade851bd4c45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:09:29 +0000

    Add package sourcetypes/0.0.2/sourcetypes-0.0.2.tar.gz

---
+++ packages/sourcetypes/sourcetypes-0.0.2.tar.gz/sourcetypes-0.0.2/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+    img_url = "https://raw.githubusercontent.com/samwillis/python-inline-source/main/sourcetypes/docs/examples.png"
+    long_description = long_description.replace("(sourcetypes/docs/examples.png)", f"({img_url})")
+
+setup(
+    name='sourcetypes',
+    version='0.0.2',
+    author="Sam Willis",
+    description="Python Source Code Types For Inline Syntax Highlighting",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/samwillis/python-inline-source/sourcetypes",
+    py_modules=['sourcetypes'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.9',
+    install_requires=[]
+)
