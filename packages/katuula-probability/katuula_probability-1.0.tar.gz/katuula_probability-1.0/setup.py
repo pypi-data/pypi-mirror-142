8c006f8930c9d09b636c2e22286ef18e3718c6af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:21 +0000

    Add package katuula-probability/1.0/katuula_probability-1.0.tar.gz

---
+++ packages/katuula-probability/katuula_probability-1.0.tar.gz/katuula_probability-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='katuula_probability',
+      version='1.0',
+      description='Gaussian and binomail distributions',
+      packages=['katuula_probability'],
+      author='Katuula Joel',
+      author_email='katuula@gmail.com',
+      zip_safe=False)
