580c448011d514796e12435369a26724dde6fcee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:53:55 +0000

    Add package pururin/0.0.8/pururin-0.0.8.tar.gz

---
+++ packages/pururin/pururin-0.0.8.tar.gz/pururin-0.0.8/setup.py
@@ -0,0 +1,54 @@
+import re
+
+from setuptools import setup
+
+version = ''
+with open('pururin/__init__.py') as f:
+    version = re.search(
+        r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]', f.read(), re.MULTILINE).group(1)
+
+
+requirements = []
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+
+if not version:
+    raise RuntimeError('version is not set')
+
+readme = ''
+with open('README.md') as f:
+    readme = f.read()
+
+
+setup(
+    name='pururin',
+    author='sinkaroid',
+    author_email='anakmancasan@gmail.com',
+    version=version,
+    long_description=readme,
+    url='https://github.com/sinkaroid/pururin',
+    packages=['pururin'],
+    license='MIT',
+    classifiers=[
+        "Framework :: AsyncIO",
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        'Programming Language :: Python :: 3.7',
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Software Development :: Libraries :: Application Frameworks",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+
+    ],
+    description='Pururin unofficial API',
+    include_package_data=True,
+    keywords=['pururin', 'doujin', 'wrapper'],
+    install_requires=requirements
+)
