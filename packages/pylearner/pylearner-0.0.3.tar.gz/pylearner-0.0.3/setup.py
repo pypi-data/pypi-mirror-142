358a8906dd929b5077626dbf30718b6c40b172ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:08:14 +0000

    Add package pylearner/0.0.3/pylearner-0.0.3.tar.gz

---
+++ packages/pylearner/pylearner-0.0.3.tar.gz/pylearner-0.0.3/setup.py
@@ -0,0 +1,30 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(name='pylearner',  # 包名
+      version='0.0.3',  # 版本号
+      description='a package which is developing',
+      long_description="",
+      author='azkidzz',
+      author_email='cszcszcsz@126.com',
+      url='',
+      install_requires=[],
+      license='GNU GPLv3',
+      packages=find_packages(),
+      platforms=["all"],
+      classifiers=[
+          'Intended Audience :: Developers',
+          'Operating System :: OS Independent',
+          'Natural Language :: English',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.5',
+          'Programming Language :: Python :: 3.6',
+          'Programming Language :: Python :: 3.7',
+          'Programming Language :: Python :: 3.8',
+          'Topic :: Software Development :: Libraries'
+      ],
+      data_files=[(r"lib\site-packages\pylearner\packages", [r"G:\pyLearner\pylearner\test.pylearner"])]
+      )
