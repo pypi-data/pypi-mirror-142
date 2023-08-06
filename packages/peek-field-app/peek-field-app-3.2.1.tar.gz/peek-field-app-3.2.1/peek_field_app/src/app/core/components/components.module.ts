c3966dc40c3d8f01ee5b6713a42c97ca73f3fd35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:27:18 +0000

    Add package peek-field-app/3.2.1/peek-field-app-3.2.1.tar.gz

---
+++ packages/peek-field-app/peek-field-app-3.2.1.tar.gz/peek-field-app-3.2.1/peek_field_app/src/app/core/components/components.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { RouterModule } from "@angular/router";
+import { FormsModule } from "@angular/forms";
+import { NzIconModule } from "ng-zorro-antd/icon";
+import { SearchModule } from "@_peek/peek_core_search/search.module";
+import { NzBadgeModule } from "ng-zorro-antd/badge";
+import { HeaderComponent } from "./header";
+import { StatusComponent } from "./status";
+
+const COMPONENTS = [HeaderComponent, StatusComponent];
+
+@NgModule({
+    declarations: COMPONENTS,
+    imports: [
+        RouterModule,
+        FormsModule,
+        BrowserModule,
+        SearchModule,
+        NzIconModule,
+        NzBadgeModule,
+    ],
+    exports: COMPONENTS,
+})
+export class ComponentsModule {}
