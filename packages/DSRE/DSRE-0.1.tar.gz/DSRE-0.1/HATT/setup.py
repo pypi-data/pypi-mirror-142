9dbc1102f30c7071c71f6b128a2bada02ac41310
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:10:46 +0000

    Add package DSRE/0.1/DSRE-0.1.tar.gz

---
+++ packages/DSRE/DSRE-0.1.tar.gz/DSRE-0.1/HATT/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+setup(
+    name = "HATT",# 包的名称
+    version = "0.1", # 版本号
+    packages = find_packages(),# 动态获取packages
+    description = "HATT package",
+    author = 'cml',
+    author_email = '1527449673@qq.com',
+    url = "None",
+)
