9ff45edbedba8b73f90212d8264d0f0437b0b9b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:18:53 +0000

    Add package robster/0.0.9/robster-0.0.9-py3-none-any.whl

---
+++ packages/robster/robster-0.0.9-py3-none-any.whl/robster/perceptron/SingleLayerPerceptron.py
@@ -0,0 +1,44 @@
+# robster/perceptron.py SingleLayerPerceptron
+from robster.model import LinearModel
+
+class SingleLayerPerceptron:
+  def __init__(self, epochs_, mini_batch_, MODEL_ = LinearModel, learning_rate_ = 0.001, report_ = 1):
+    self.epochs = epochs_
+    self.mini_batch = mini_batch_
+    self.learning_rate = learning_rate_
+    self.report=report_
+    self.MODEL = MODEL_
+
+
+  def fit(self, X, y):
+    steps = int(X.shape[0])//self.mini_batch # 미니배치가 가능한 갯수
+    self.model = self.MODEL(int(X.shape[1]), self.learning_rate)
+
+    for epoch in range(self.epochs):
+      losses, accs = [], []
+      # 매 epoch 마다 데이터를 섞어서 진행한다.
+      shuffle_map = np.arrange(X.shape[0])
+      for step in range(steps):
+        if step == 0: np.random.shuffle(shuffle_map)
+        X_train = X[shuffle_map[self.mini_batch*step : self.mini_batch(step + 1)]]
+        y_train = y[shuffle_map[self.mini_batch*step : self.mini_batch(step + 1)]]
+        
+        output, aux_nn = self.model.forward(X_train)
+        loss, aux_pp = self.model.forward_postproc(output, y_train)
+        acc = self.model.eval_accuracy(output, y_train)
+        losses.append(loss)
+        accs.append(acc)
+
+        #
+        G_loss = 1
+        G_output = self.model.back_postproc(G_loss, aux_pp)
+        self.model.back(G_output, aux_nn)
+
+      if self.report > 0 and (epoch + 1)%self.report ==0:
+        print(f"[Epoch {epoch}] loss = {np.mean(losses)} , accuracy = {np.mean(accs)}/{acc}")
+
+    print(f"\n[Final] loss = {np.mean(losses)} , accuracy = {np.mean(accs)}")
+
+  def predict(self, X):
+    return self.model.forward(X)
+  
