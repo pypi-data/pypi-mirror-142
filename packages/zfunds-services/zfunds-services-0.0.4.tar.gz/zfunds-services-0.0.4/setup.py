551d3fe6c97a4a087e885aaa351fe6fad4c48005
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:42:30 +0000

    Add package zfunds-services/0.0.4/zfunds-services-0.0.4.tar.gz

---
+++ packages/zfunds-services/zfunds-services-0.0.4.tar.gz/zfunds-services-0.0.4/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+setup(
+    name='zfunds-services',
+    packages=['dynamodb', 'mongodb', 's3'],
+    package_dir={'dynamodb': 'dynamodb', 'mongodb': 'mongodb', 's3': 's3'},
+    version='0.0.4',
+    license='MIT',
+    platforms='cross-platfom, platform-independent',
+    description='ZFunds basic services',
+    long_description='Dependencies: coming soon',
+    author='Yogesh Yadav',
+    author_email='yogesh@zfunds.in',
+    url='https://github.com/',
+    download_url='https://github.com/',
+    keywords=['dynamodb', 'mongodb', 's3'],
+    install_requires=[
+        'python-dotenv==0.19.2', 'boto3==1.21.17'
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'Programming Language :: Python :: 3.6',
+    ],
+)
