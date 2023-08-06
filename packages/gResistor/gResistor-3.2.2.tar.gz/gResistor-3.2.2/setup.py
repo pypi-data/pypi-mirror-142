81b9c055776e93d615665014a195883eba3b17dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:26 +0000

    Add package gResistor/3.2.2/gResistor-3.2.2.tar.gz

---
+++ packages/gResistor/gResistor-3.2.2.tar.gz/gResistor-3.2.2/setup.py
@@ -0,0 +1,32 @@
+from gresistor3.gresistor import app_name, app_version
+from setuptools import find_packages, setup
+import sys, os
+
+setupdir = os.path.dirname(__file__)
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+if sys.version_info < (3, 8):
+    raise RuntimeError("GResistor3 requires Python 3.8 or later")
+
+setup(
+    name=app_name,
+    version=app_version,
+    description      = 'Resistor color code calculator',
+    author           = 'Stefano Prina',
+    author_email     = 'stethewwolf@gmail.com',
+    url              = 'https://gresistor.stethewwolf.eu',
+    license          = 'GPL',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    python_requires=">=3.8",
+    packages=['gresistor3'],
+    entry_points={'console_scripts': ['gresistor3=gresistor3.gresistor:main']},
+    data_files = [
+    	('share/gresistor',['gresistor3/gresistor.glade','gresistor3/pixmaps/gresistor.png']),
+    	('share/applications', ['gresistor.desktop']),
+    	('share/icons', ['gresistor3/pixmaps/gresistor.png'])]
+    )
+
+
