1ad0d9547773c984fda90faf2007eb5b0bae49a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:19:32 +0000

    Add package rivertrace/1.1.0/rivertrace-1.1.0.tar.gz

---
+++ packages/rivertrace/rivertrace-1.1.0.tar.gz/rivertrace-1.1.0/setup.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+with open('README.md') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+    name='rivertrace',
+    version='1.1.0',
+    description='Identifies rivers in satellite images and generates a path of pixel values along its length.',
+    long_description=readme,
+    long_description_content_type='text/markdown',
+    author='James Runnalls',
+    author_email='runnalls.james@gmail.com',
+    url='https://github.com/JamesRunnalls/river-trace',
+    license=license,
+    packages=find_packages(exclude=('tests', 'docs')),
+    install_requires=[
+            'numpy',
+            'scikit-image',
+            'networkx',
+        ]
+)
