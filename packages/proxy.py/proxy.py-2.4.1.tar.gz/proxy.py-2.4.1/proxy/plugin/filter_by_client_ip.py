0bf5456265e255234e1f1782091de11a5092658f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:10:06 +0000

    Add package proxy.py/2.4.1/proxy.py-2.4.1.tar.gz

---
+++ packages/proxy.py/proxy.py-2.4.1.tar.gz/proxy.py-2.4.1/proxy/plugin/filter_by_client_ip.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+"""
+    proxy.py
+    ~~~~~~~~
+    ⚡⚡⚡ Fast, Lightweight, Pluggable, TLS interception capable proxy server focused on
+    Network monitoring, controls & Application development, testing, debugging.
+
+    :copyright: (c) 2013-present by Abhinav Singh and contributors.
+    :license: BSD, see LICENSE for more details.
+
+    .. spelling::
+
+       ip
+"""
+from typing import Optional
+
+from ..http import httpStatusCodes
+from ..http.proxy import HttpProxyBasePlugin
+from ..common.flag import flags
+from ..http.parser import HttpParser
+from ..http.exception import HttpRequestRejected
+
+
+flags.add_argument(
+    '--filtered-client-ips',
+    type=str,
+    default='127.0.0.1,::1',
+    help='Default: 127.0.0.1,::1.  Comma separated list of IPv4 and IPv6 addresses.',
+)
+
+
+class FilterByClientIpPlugin(HttpProxyBasePlugin):
+    """Drop traffic by inspecting incoming client IP address."""
+
+    def before_upstream_connection(
+            self, request: HttpParser,
+    ) -> Optional[HttpParser]:
+        assert not self.flags.unix_socket_path and self.client.addr
+        if self.client.addr[0] in self.flags.filtered_client_ips.split(','):
+            raise HttpRequestRejected(
+                status_code=httpStatusCodes.I_AM_A_TEAPOT,
+                reason=b'I\'m a tea pot',
+            )
+        return request
