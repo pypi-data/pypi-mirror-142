c96026034ad669eb2c3197f6da061d045c4b603e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:18:13 +0000

    Add package focus-xp-9588/0.0.1/focus-xp-9588-0.0.1.tar.gz

---
+++ packages/focus-xp-9588/focus-xp-9588-0.0.1.tar.gz/focus-xp-9588-0.0.1/README.md
@@ -0,0 +1,7 @@
+# focus
+前提要求：有python3的环境，有git，由于项目不够完善，目前仅支持 Linux和 Mac，仅支持本地，且功能不全
+进入到git仓库目录下，然后输入下述命令即可使用，其中focus_path是你放置focus文件包的位置,rep_path是所要管理的仓库所在的位置：
+python3 focus_path --repository rep_path &
+
+会在你的.git文件夹下生成一个.focus文件
+关注点发生变化后，点击renew即可获取关注点的更新信息
---
+++ packages/focus-xp-9588/focus-xp-9588-0.0.1.tar.gz/focus-xp-9588-0.0.1/setup.py
@@ -0,0 +1,15 @@
+import setuptools
+
+setuptools.setup(
+    name="focus-xp-9588",
+    version="0.0.1",
+    author="xp9588",
+    author_email="958876660@qq.com",
+    description="focus package",
+    url="https://github.com/pxp511/focus",
+    packages=setuptools.find_packages(),
+    platforms='any',
+    license='',
+    zip_safe=False,
+    
+)
