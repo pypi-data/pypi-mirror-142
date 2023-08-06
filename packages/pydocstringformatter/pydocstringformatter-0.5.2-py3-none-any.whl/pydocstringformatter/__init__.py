e7d8e1ee2c6e08743dab388a92f0432774c6a2be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:27:45 +0000

    Add package pydocstringformatter/0.5.2/pydocstringformatter-0.5.2-py3-none-any.whl

---
+++ packages/pydocstringformatter/pydocstringformatter-0.5.2-py3-none-any.whl/pydocstringformatter/__init__.py
@@ -0,0 +1,21 @@
+# pylint: disable = import-outside-toplevel
+
+import sys
+from typing import List, Union
+
+from pydocstringformatter.utils.exceptions import (
+    ParsingError,
+    PydocstringFormatterError,
+)
+
+__version__ = "0.5.2"
+
+
+def run_docstring_formatter(argv: Union[List[str], None] = None) -> None:
+    """Run the formatter."""
+    from pydocstringformatter.run import _Run
+
+    _Run(argv or sys.argv[1:])
+
+
+__all__ = ("run_docstring_formatter", "PydocstringFormatterError", "ParsingError")
