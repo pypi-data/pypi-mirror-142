f6872e3471dca19daabc21908c13fd1474df5ee2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:37:47 +0000

    Add package blazer/0.1.28/blazer-0.1.28.tar.gz

---
+++ packages/blazer/blazer-0.1.28.tar.gz/blazer-0.1.28/blazer/hpc/mpi/__init__.py
@@ -0,0 +1,40 @@
+from functools import partial
+
+from pipe import select, where
+from pydash import chunk
+from pydash import filter_ as filter
+from pydash import flatten, get, omit
+
+from .primitives import (
+    host,
+    map,
+    mapreduce,
+    parallel,
+    pipeline,
+    rank,
+    reduce,
+    scatter,
+    size,
+    stream,
+)
+
+__all__ = (
+    "parallel",
+    "scatter",
+    "pipeline",
+    "map",
+    "mapreduce",
+    "reduce",
+    "partial",
+    "stream",
+    "select",
+    "where",
+    "flatten",
+    "chunk",
+    "omit",
+    "get",
+    "rank",
+    "host",
+    "size",
+    "filter",
+)
