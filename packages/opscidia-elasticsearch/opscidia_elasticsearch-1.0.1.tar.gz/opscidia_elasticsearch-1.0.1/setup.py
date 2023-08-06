1e7b34db6970087951ddc8f753f586dd9d948cf3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:19:45 +0000

    Add package opscidia-elasticsearch/1.0.1/opscidia_elasticsearch-1.0.1.tar.gz

---
+++ packages/opscidia-elasticsearch/opscidia_elasticsearch-1.0.1.tar.gz/opscidia_elasticsearch-1.0.1/setup.py
@@ -0,0 +1,43 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+#import opscidia_elasticsearch
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+long_description = open(os.path.join(here, "README.md"), encoding="utf8").read()
+
+with open(os.path.join(here, "requirements.txt")) as fq:
+    requirements = list(map(lambda x:x.strip(), fq.readlines()))
+
+VERSION = '1.0.1'
+DESCRIPTION = 'Tools to manage elasticsearch indexes'
+
+setup(
+    name = "opscidia_elasticsearch",
+    version = VERSION,
+    author = "Opscidia (Tech)",
+    author_email = "tech@opscidia.com",
+    maintainer = "Fréjus Laleye",
+    maintainer_email = "frejus.Laleye@opscidia.com",
+    description = DESCRIPTION,
+    long_description_content_type = "text/markdown",
+    long_description = long_description,
+    license_files = ('LICENSE',),
+    packages = find_packages(),
+    include_package_data = True,
+    install_requires = requirements,
+    keywords = ['elasticsearch', 'ospcidia', 'index', 'create'],
+    classifiers = [
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires = '>=3.7',
+    #entry_points = {
+        #'console_scripts': [
+            #'opscidia_elasticsearch = opscidia_elasticsearch.runtime:rollet_extract'
+        #]
+    #},
+)
