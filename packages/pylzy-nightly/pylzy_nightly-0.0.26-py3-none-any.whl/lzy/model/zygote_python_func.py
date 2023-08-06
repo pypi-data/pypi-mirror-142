9e4dd94e006d7580e6859253a09ce2050c6ffd5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:36:33 +0000

    Add package pylzy-nightly/0.0.26.dev20220315/pylzy_nightly-0.0.26-py3-none-any.whl

---
+++ packages/pylzy-nightly/pylzy_nightly-0.0.26-py3-none-any.whl/lzy/model/zygote_python_func.py
@@ -0,0 +1,43 @@
+import base64
+import os
+from typing import Generic, TypeVar, Optional
+
+from lzy.model.signatures import FuncSignature
+from lzy.model.env import Env
+from lzy.model.slot import Direction
+from lzy.model.file_slots import create_slot
+from lzy.model.zygote import Zygote, Provisioning
+from lzy.api.serializer.serializer import Serializer
+
+T = TypeVar("T")  # pylint: disable=invalid-name
+
+
+class ZygotePythonFunc(Zygote, Generic[T]):
+    def __init__(
+        self,
+        sign: FuncSignature[T],
+        # lzy_mount: Path,
+        env: Env,
+        provisioning: Optional[Provisioning],
+    ):
+        # TODO: find out if lzy_mount is really needed here
+        arg_slots = [
+            create_slot(os.path.join(os.sep, sign.name, name), Direction.INPUT)
+            for name in sign.param_names
+        ]
+        return_slot = create_slot(
+            os.path.join("/", sign.name, "return"), Direction.OUTPUT
+        )
+        super().__init__(sign, arg_slots, return_slot, env, provisioning)
+
+    # just a: /
+    @property
+    def command(self) -> str:
+        _com = "".join([
+            "python ",
+            "$(python -c 'import site; print(site.getsitepackages()[0])')",
+            "/lzy/startup.py "
+        ])
+        serializer = Serializer()
+        serialized_func = base64.b64encode(serializer.serialize_to_byte_string(self.signature)).decode('ascii')
+        return _com + serialized_func
