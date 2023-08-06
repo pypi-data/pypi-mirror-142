9d230784ea4d336ac76a001004721bfb256eb059
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:10:16 +0000

    Add package py-alex-mess-server/0.1.0/py_alex_mess_server-0.1.0.tar.gz

---
+++ packages/py-alex-mess-server/py_alex_mess_server-0.1.0.tar.gz/py_alex_mess_server-0.1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(name="py_alex_mess_server",
+      version="0.1.0",
+      description="Alex Mess Server",
+      author="Alex",
+      author_email="momot-717@yandex.ru",
+      packages=find_packages(),
+      install_requires=['PyQt5', 'sqlalchemy', 'pycryptodome', 'pycryptodomex']
+      # scripts=['server/server_run']
+      )
