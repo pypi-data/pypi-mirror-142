46f39f0ef1905121ec431c5ebd4c4e56a79ea916
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:35:10 +0000

    Add package SigProfilerExtractor/1.1.6/SigProfilerExtractor-1.1.6.tar.gz

---
+++ packages/SigProfilerExtractor/SigProfilerExtractor-1.1.6.tar.gz/SigProfilerExtractor-1.1.6/SigProfilerExtractor/version.py
@@ -0,0 +1,7 @@
+
+# THIS FILE IS GENERATED FROM SIGPROFILEREXTRACTOR SETUP.PY
+short_version = '1.1.6'
+version = '1.1.6'
+Update = 'Update normalization, selection and default NMF replicates'
+    
+    
