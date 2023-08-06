3a7a2d7df89676d80079194ee6883f29071ff6cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:19:18 +0000

    Add package viztracer/0.15.1/viztracer-0.15.1-cp38-cp38-win_amd64.whl

---
+++ packages/viztracer/viztracer-0.15.1-cp38-cp38-win_amd64.whl/viztracer/__init__.py
@@ -0,0 +1,33 @@
+# Licensed under the Apache License: http://www.apache.org/licenses/LICENSE-2.0
+# For details: https://github.com/gaogaotiantian/viztracer/blob/master/NOTICE.txt
+
+__version__ = "0.15.1"
+
+
+from .cellmagic import load_ipython_extension
+from .decorator import ignore_function, trace_and_save, log_sparse
+from .main import main
+from .simulator import main as sim_main
+from .util import get_tracer
+from .viewer import viewer_main
+from .vizcounter import VizCounter
+from .vizlogging import VizLoggingHandler
+from .vizobject import VizObject
+from .viztracer import VizTracer
+
+
+__all__ = [
+    "__version__",
+    "main",
+    "sim_main",
+    "viewer_main",
+    "VizTracer",
+    "ignore_function",
+    "trace_and_save",
+    "log_sparse",
+    "get_tracer",
+    "VizCounter",
+    "VizObject",
+    "VizLoggingHandler",
+    "load_ipython_extension"
+]
