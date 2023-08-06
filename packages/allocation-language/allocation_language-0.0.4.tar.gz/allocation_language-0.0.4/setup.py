083be22b96067909de5e1e70171da3846873bf25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:06:21 +0000

    Add package allocation-language/0.0.4/allocation_language-0.0.4.tar.gz

---
+++ packages/allocation-language/allocation_language-0.0.4.tar.gz/allocation_language-0.0.4/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+setuptools.setup(
+    name="allocation_language",
+    version="0.0.4",
+    description="Language intented to construct the financial behaviour of (re)insurance contracts.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    install_requires=[
+        "rply"
+    ],
+    author="Mammen Joseph",
+    author_email="tmammenjoseph@gmail.com",
+    maintainer="Mammen Joseph",
+    maintainer_email="tmammenjoseph@gmail.com",
+    url="",
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+)
---
+++ packages/allocation-language/allocation_language-0.0.4.tar.gz/allocation_language-0.0.4/src/allocation_language/ast_nodes/alloc_lang_scope.py
@@ -0,0 +1,49 @@
+from allocation_language.alloc_lang_data_containers.event_dataclasses import EventData
+from allocation_language.ast_nodes.alloc_lang_primitives import Node
+
+# Only used for typing purposes really
+class BoolNode(Node):
+    def evaluate(self, _: EventData) -> bool:
+        return True
+
+class Condition(BoolNode):
+    def __init__(self, rhs: Node, comparator: str, lhs: Node):
+        self.rhs = rhs
+        self.lhs = lhs
+
+        self.comparator = comparator
+
+    def evaluate(self, event: EventData) -> bool:
+        rh_val: float | str = self.rhs.evaluate(event)
+        lh_val: float | str = self.lhs.evaluate(event)
+
+        return bool(eval(f"{rh_val!r} {self.comparator} {lh_val!r}"))
+
+class AndNode(BoolNode):
+    def __init__(self, rhs: BoolNode, lhs: BoolNode):
+        self.rhs = rhs
+        self.lhs = lhs
+
+    def evaluate(self, event: EventData) -> bool:
+        return self.rhs.evaluate(event) and self.lhs.evaluate(event)
+
+
+class OrNode(BoolNode):
+    def __init__(
+        self, rhs: BoolNode, lhs: BoolNode
+    ):
+        self.rhs = rhs
+        self.lhs = lhs
+
+    def evaluate(self, event: EventData) -> bool:
+        return self.rhs.evaluate(event) or self.lhs.evaluate(event)
+
+
+class Scope(Node):
+    def __init__(self, condition: BoolNode):
+        self.condition = condition
+
+    def evaluate(self, event: EventData) -> EventData:
+        if (not isinstance(event, EventData)):
+            return event
+        return EventData(data=event.data, scope_flag=self.condition.evaluate(event))
