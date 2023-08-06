4691e7a421bbe14546a930298126258fd8c666f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:19:32 +0000

    Add package epd/2.3.26/epd-2.3.26.tar.gz

---
+++ packages/epd/epd-2.3.26.tar.gz/epd-2.3.26/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+# from distutils.core import setup, Extension
+from setuptools import setup, Extension
+
+#import distutils.log
+#distutils.log.set_verbosity(distutils.log.DEBUG) # Set DEBUG level
+
+c_convert = Extension('c_convert',
+                    sources = ['epdconvert/src/lz.c',
+                               'epdconvert/src/epdconvert.c',
+                               'epdconvert/src/Compression.c',
+                               'epdconvert/src/Type0.c',
+                               'epdconvert/src/Type2.c',
+                               'epdconvert/src/Type7.c',
+                               'epdconvert/src/Invert.c',
+                               'epdconvert/src/Flip.c',
+                               'epdconvert/src/Chunks.c'],
+                    include_dirs=['epdconvert/include/',],
+                    language='c',
+                    extra_compile_args = ["-std=c99"],
+                  )
+
+
+
+setup (name = 'epd',
+       version = '2.3.26',
+       author="Paweł Musiał, MpicoSys",
+       url="https://www.mpicosys.com/",
+       author_email='pawel.musial@mpicosys.com',
+       description = 'EPD library for MpicoSys Timing Controllers (TC/TCM)',
+       ext_modules = [c_convert,],
+       packages = ['epd','epd.tcm','epd.convert'],
+       requires = ['Pillow',]
+       )
