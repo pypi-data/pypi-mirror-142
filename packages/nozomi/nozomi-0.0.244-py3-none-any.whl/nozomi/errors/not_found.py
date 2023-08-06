17c3808aacf5438fa32c5271b4d4de10b3214f59
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:10:16 +0000

    Add package nozomi/0.0.244/nozomi-0.0.244-py3-none-any.whl

---
+++ packages/nozomi/nozomi-0.0.244-py3-none-any.whl/nozomi/errors/not_found.py
@@ -0,0 +1,44 @@
+"""
+Nozomi
+NotAuthorised Error Module
+author: hugh@blinkybeach.com
+"""
+from nozomi.errors.error import NozomiError
+from nozomi.http.status_code import HTTPStatusCode
+from typing import TypeVar, Type, Union, Optional
+from nozomi.data.named import Named
+
+T = TypeVar('T', bound='NotFound')
+
+
+class NotFound(NozomiError):
+
+    def __init__(
+        self,
+        client_description: str = 'A requested resource could not be found'
+    ) -> None:
+
+        super().__init__(
+            client_description=client_description,
+            http_status_code=HTTPStatusCode.NOT_FOUND
+        )
+
+        return
+
+    @classmethod
+    def for_object(
+        cls: Type[T],
+        name: Union[str, Named, Type[Named]],
+        id_: Optional[str] = None
+    ) -> T:
+
+        derived_name = name if isinstance(name, str) else name.name
+
+        if id_ is not None:
+            raise Warning('id_ parameter deprecated')
+
+        return cls(
+            client_description='No {n} found with the supplied ID'.format(
+                n=str(derived_name)
+            )
+        )
