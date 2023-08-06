5c9a9ae648cb9ef3258b35626ac1d5c6c1b0c3b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:36:39 +0000

    Add package umbral-pre/0.5.2/umbral_pre-0.5.2.tar.gz

---
+++ packages/umbral-pre/umbral_pre-0.5.2.tar.gz/umbral_pre-0.5.2/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+from setuptools_rust import Binding, RustExtension
+
+setup(
+    name="umbral_pre",
+    description="Implementation of Umbral proxy reencryption algorithm",
+    version="0.5.2",
+    author="Bogdan Opanchuk",
+    author_email="bogdan@opanchuk.net",
+    url="https://github.com/nucypher/rust-umbral/tree/master/umbral-pre-python",
+    rust_extensions=[RustExtension("umbral_pre._umbral", binding=Binding.PyO3)],
+    packages=["umbral_pre"],
+    package_data = {
+        'umbral_pre': ['py.typed', '__init__.pyi'],
+    },
+    # rust extensions are not zip safe, just like C-extensions.
+    zip_safe=False,
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Natural Language :: English",
+        "Programming Language :: Rust",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Topic :: Security :: Cryptography",
+    ],
+)
