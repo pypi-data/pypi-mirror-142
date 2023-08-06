19800f110db019adb3abd065c0942c97e1380992
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:19:36 +0000

    Add package PF-PWeb-SourceMan/1.0.0/PF-PWeb-SourceMan-1.0.0.tar.gz

---
+++ packages/PF-PWeb-SourceMan/PF-PWeb-SourceMan-1.0.0.tar.gz/PF-PWeb-SourceMan-1.0.0/pf_pweb_sourceman/common/console.py
@@ -0,0 +1,32 @@
+import click
+
+
+class Console:
+
+    def log(self, text):
+        print(text)
+
+    def green(self, text, bold=False):
+        click.echo(click.style(text, fg='green', bold=bold))
+
+    def blue(self, text, bold=False):
+        click.echo(click.style(text, fg='blue', bold=bold))
+
+    def red(self, text, bold=False):
+        click.echo(click.style(text, fg='red', bold=bold))
+
+    def get_message_format(self, text):
+        message = ">> " + str(text)
+        return message
+
+    def error(self, message):
+        return self.red(self.get_message_format(message), True)
+
+    def success(self, message):
+        return self.green(self.get_message_format(message), True)
+
+    def info(self, message):
+        return self.blue(self.get_message_format(message), True)
+
+
+console = Console()
