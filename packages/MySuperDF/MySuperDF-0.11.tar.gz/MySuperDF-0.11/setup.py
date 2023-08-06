7c9e02fbe672b239360425bc1959bbdecbb4597e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:38 +0000

    Add package MySuperDF/0.11/MySuperDF-0.11.tar.gz

---
+++ packages/MySuperDF/MySuperDF-0.11.tar.gz/MySuperDF-0.11/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setuptools.setup(
+     name='MySuperDF',
+     version="0.011",
+     scripts=['main.py'] ,
+     author="Paul Ledesma",
+     author_email="paul.ledesma@hotmail.fr",
+     description="A package for handling dataframes",
+     long_description=long_description,
+   long_description_content_type="text/markdown",
+     url="https://github.com/Pauloledes/MySuperDataFrame",
+     packages=setuptools.find_packages(),
+     classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: MIT License",
+         "Operating System :: OS Independent",
+     ],
+ )
