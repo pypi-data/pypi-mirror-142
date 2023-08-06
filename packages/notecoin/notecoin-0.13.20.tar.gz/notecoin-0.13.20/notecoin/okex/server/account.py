5e7a5c44d25d09e9e7808b8bf43f211518616c87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:09:26 +0000

    Add package notecoin/0.13.20/notecoin-0.13.20.tar.gz

---
+++ packages/notecoin/notecoin-0.13.20.tar.gz/notecoin-0.13.20/notecoin/okex/server/account.py
@@ -0,0 +1,20 @@
+import pandas as pd
+from notebuild.tool.fastapi import add_api_routes, api_route
+from notecoin.database.connect import RedisConnect
+from notecoin.okex.client.const import account_api
+
+
+class AccountAccount(RedisConnect):
+    def __init__(self, prefix="/account", cache_prefix='okex_account_balance', *args, **kwargs):
+        super(AccountAccount, self).__init__(prefix=prefix, cache_prefix=cache_prefix, *args, **kwargs)
+        add_api_routes(self)
+
+    @api_route('/update', description="update market tickers")
+    def update_value(self, suffix=""):
+        data = account_api.get_account().data[0]['details']
+        self.put_value(self.get_key(suffix=suffix), data)
+        return {"success": len(data)}
+
+    @api_route('/read', description="read market tickers")
+    def get_value(self, suffix=""):
+        return super(AccountAccount, self).get_value(suffix=suffix)
