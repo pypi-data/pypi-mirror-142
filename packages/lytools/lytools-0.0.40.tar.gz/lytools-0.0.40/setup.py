21d986d00d2bf93343f68cfed3743fd8e39b6f3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:24:54 +0000

    Add package lytools/0.0.40/lytools-0.0.40.tar.gz

---
+++ packages/lytools/lytools-0.0.40.tar.gz/lytools-0.0.40/setup.py
@@ -0,0 +1,24 @@
+# coding='utf-8'
+
+from setuptools import setup
+
+setup(
+    name='lytools',
+    version='0.0.40',
+    author='Yang Lee',
+    author_email='leeyang1991@gmail.com',
+    packages=['lytools'],
+    url='https://github.com/leeyang1991/lytools',
+    python_requires='>=3',
+    install_requires=[
+    'matplotlib',
+    'numpy',
+    'scipy',
+    'gdal',
+    'tqdm',
+    'pandas',
+    'seaborn',
+    'sklearn',
+    'requests',
+    ],
+)
