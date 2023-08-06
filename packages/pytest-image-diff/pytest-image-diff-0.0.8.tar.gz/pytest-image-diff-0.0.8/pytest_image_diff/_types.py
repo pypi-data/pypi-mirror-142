ac01fd59c764c008339ed6b00f516f9252e8b774
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:09:56 +0000

    Add package pytest-image-diff/0.0.8/pytest-image-diff-0.0.8.tar.gz

---
+++ packages/pytest-image-diff/pytest-image-diff-0.0.8.tar.gz/pytest-image-diff-0.0.8/pytest_image_diff/_types.py
@@ -0,0 +1,35 @@
+from pathlib import Path
+from typing import BinaryIO, Union, Tuple, Optional
+
+from PIL.Image import Image
+from typing_extensions import Literal, Protocol
+
+PathOrFileType = Union[str, bytes, Path, BinaryIO]
+ImageFileType = Union[Image, PathOrFileType]
+ImageSize = Tuple[int, int]
+
+
+class ImageRegressionCallableType(Protocol):
+    def __call__(
+        self,
+        image: ImageFileType,
+        threshold: Optional[float] = None,
+        suffix: Optional[str] = None,
+    ) -> bool:
+        pass
+
+
+class ImageDiffCallableType(Protocol):
+    def __call__(
+        self,
+        image: ImageFileType,
+        image_2: ImageFileType,
+        threshold: Optional[float] = None,
+        suffix: Optional[str] = None,
+    ) -> bool:
+        pass
+
+
+# ``opts.orientation`` can be 'lr' for left-and-right,
+# 'tb' for top-and-bottom, or 'auto' for automatic.
+OrientationType = Literal["auto", "lr", "tb"]
