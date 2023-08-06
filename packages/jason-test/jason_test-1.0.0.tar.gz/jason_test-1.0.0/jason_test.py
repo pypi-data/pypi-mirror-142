b53d925346c7059b2099b4ff7bfe1518ab16ef47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:27:38 +0000

    Add package jason-test/1.0.0/jason_test-1.0.0.tar.gz

---
+++ packages/jason-test/jason_test-1.0.0.tar.gz/jason_test-1.0.0/jason_test.py
@@ -0,0 +1,2 @@
+def jason_test():
+    print("jason_test")
---
+++ packages/jason-test/jason_test-1.0.0.tar.gz/jason_test-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(
+    name ='jason_test',
+    version = '1.0.0',
+    description = 'jason_test',
+    author = 'jason',
+    author_email = None,
+    url = None,
+    py_modules = ['jason_test']
+)
