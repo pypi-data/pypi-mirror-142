bb0a50527ebf74f27c70b4c91e9d3da8125aa1c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:10:44 +0000

    Add package NEMO/3.16.0/NEMO-3.16.0.tar.gz

---
+++ packages/NEMO/NEMO-3.16.0.tar.gz/NEMO-3.16.0/setup.py
@@ -0,0 +1,42 @@
+from setuptools import find_packages, setup
+
+setup(
+	name='NEMO',
+	version='3.16.0',
+	python_requires='>=3.7',
+	packages=find_packages(exclude=['NEMO.tests','NEMO.tests.*']),
+	include_package_data=True,
+	url='https://github.com/usnistgov/NEMO',
+	license='Public domain',
+	author='Center for Nanoscale Science and Technology',
+	author_email='CNSTapplications@nist.gov',
+	description='NEMO is a laboratory logistics web application. Use it to schedule reservations, control tool access, track maintenance issues, and more.',
+	long_description='Find out more about NEMO on the GitHub project page https://github.com/usnistgov/NEMO',
+	classifiers=[
+		'Development Status :: 5 - Production/Stable',
+		'Environment :: Web Environment',
+		'Framework :: Django',
+		'Intended Audience :: Science/Research',
+		'Intended Audience :: System Administrators',
+		'License :: Public Domain',
+		'Natural Language :: English',
+		'Operating System :: OS Independent',
+		'Programming Language :: Python :: 3.6',
+	],
+	install_requires=[
+		'cryptography==36.0.1',
+		'Django==2.2.27',
+		'django-filter==21.1',
+		'drf-renderer-xlsx==0.4.5',
+		'djangorestframework==3.13.1',
+		'drf-flex-fields==0.9.7',
+		'ldap3==2.9.1',
+		'python-dateutil==2.8.2',
+		'requests==2.27.1',
+		'Pillow==9.0.1',
+		'django-mptt==0.13.4',
+	],
+	entry_points={
+		'console_scripts': ['nemo=NEMO.provisioning:entry_point'],
+	},
+)
