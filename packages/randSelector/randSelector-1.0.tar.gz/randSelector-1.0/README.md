407f1a77f4a683b2374273d797f7d0190a687877
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:10:11 +0000

    Add package randSelector/1.0/randSelector-1.0.tar.gz

---
+++ packages/randSelector/randSelector-1.0.tar.gz/randSelector-1.0/README.md
@@ -0,0 +1,3 @@
+Description: 
+RandSelector class in the rand_selector module contains a function select_rand,
+that can select a random a value from the data set and its associated probability
---
+++ packages/randSelector/randSelector-1.0.tar.gz/randSelector-1.0/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setuptools.setup(
+     name='randSelector',  
+     version='1.0',
+     author="Syed Hamza Rafique",
+     author_email="hamzah_shah@hotmail.com",
+     description="random value selector with associated probability package",
+     long_description=long_description,
+   long_description_content_type="text/markdown",
+     url="",
+     packages=setuptools.find_packages(),
+     classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: MIT License",
+         "Operating System :: OS Independent",
+     ],
+ )
+ 
