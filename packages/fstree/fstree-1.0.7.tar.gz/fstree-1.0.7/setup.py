6e23c4b00c3a8255c5ea3851c6f98752f2f8083d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:41:23 +0000

    Add package fstree/1.0.7/fstree-1.0.7.tar.gz

---
+++ packages/fstree/fstree-1.0.7.tar.gz/fstree-1.0.7/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+
+description = r'''A tree structure to store files & directories.'''
+
+setuptools.setup(
+    name='fstree',
+    packages = setuptools.find_packages(exclude=['tests*']),
+    version="1.0.7",
+    description=description,
+    author = 'Henry Weickert',
+    author_email = 'henryweickert@gmail.com',
+    url = 'https://github.com/hweickert/fstree',
+    keywords = [],
+    entry_points={},
+    install_requires=[
+        'six',
+        'anytree==2.4.3',
+        'oyaml==0.9',
+    ]
+)
