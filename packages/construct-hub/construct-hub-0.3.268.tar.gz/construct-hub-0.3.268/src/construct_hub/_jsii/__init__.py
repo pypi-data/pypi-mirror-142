1099ea2a5c3dd9bb891722f34bd3bcf08e065df4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:12:01 +0000

    Add package construct-hub/0.3.268/construct-hub-0.3.268.tar.gz

---
+++ packages/construct-hub/construct-hub-0.3.268.tar.gz/construct-hub-0.3.268/src/construct_hub/_jsii/__init__.py
@@ -0,0 +1,49 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import publication
+import typing_extensions
+
+import aws_cdk.assets._jsii
+import aws_cdk.aws_certificatemanager._jsii
+import aws_cdk.aws_cloudfront._jsii
+import aws_cdk.aws_cloudfront_origins._jsii
+import aws_cdk.aws_cloudwatch._jsii
+import aws_cdk.aws_cloudwatch_actions._jsii
+import aws_cdk.aws_codeartifact._jsii
+import aws_cdk.aws_ec2._jsii
+import aws_cdk.aws_ecs._jsii
+import aws_cdk.aws_events._jsii
+import aws_cdk.aws_events_targets._jsii
+import aws_cdk.aws_iam._jsii
+import aws_cdk.aws_lambda._jsii
+import aws_cdk.aws_lambda_event_sources._jsii
+import aws_cdk.aws_logs._jsii
+import aws_cdk.aws_route53._jsii
+import aws_cdk.aws_route53_targets._jsii
+import aws_cdk.aws_s3._jsii
+import aws_cdk.aws_s3_deployment._jsii
+import aws_cdk.aws_s3_notifications._jsii
+import aws_cdk.aws_sns._jsii
+import aws_cdk.aws_sqs._jsii
+import aws_cdk.aws_stepfunctions._jsii
+import aws_cdk.aws_stepfunctions_tasks._jsii
+import aws_cdk.core._jsii
+import aws_cdk.custom_resources._jsii
+import aws_cdk.cx_api._jsii
+import cdk_watchful._jsii
+import constructs._jsii
+
+__jsii_assembly__ = jsii.JSIIAssembly.load(
+    "construct-hub", "0.3.268", __name__[0:-6], "construct-hub@0.3.268.jsii.tgz"
+)
+
+__all__ = [
+    "__jsii_assembly__",
+]
+
+publication.publish()
