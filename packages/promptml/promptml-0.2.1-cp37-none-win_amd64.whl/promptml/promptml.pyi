76d22ff44b3512287f4f0dc89113f14418068cb1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:18:19 +0000

    Add package promptml/0.2.1/promptml-0.2.1-cp37-none-win_amd64.whl

---
+++ packages/promptml/promptml-0.2.1-cp37-none-win_amd64.whl/promptml/promptml.pyi
@@ -0,0 +1,17 @@
+from typing import List, Optional, Set
+
+
+class PromptFragment:
+    string: str
+    option: Optional[Set[str]]
+
+
+class PromptTemplate:
+    fragments: List[PromptFragment]
+
+    def __init__(self, template: str):
+        pass
+
+
+def parse(template: str) -> List[PromptFragment]:
+    pass
