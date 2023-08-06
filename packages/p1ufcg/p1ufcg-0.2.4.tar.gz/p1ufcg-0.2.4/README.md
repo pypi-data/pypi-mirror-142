c3996abc674ab6920eb8fee2e23fc4b4c53698b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:26:37 +0000

    Add package p1ufcg/0.2.4/p1ufcg-0.2.4.tar.gz

---
+++ packages/p1ufcg/p1ufcg-0.2.4.tar.gz/p1ufcg-0.2.4/README.md
@@ -0,0 +1,25 @@
+# p1 [![Português](images/BR.png)](README.pt-BR.md) [![English](images/US.png)](README.md)
+
+p1 é um pacote com ferramentas para o apoio às disciplinas de
+Programação 1 da Computação@UFCG.
+
+## Instalação
+
+```
+pip3 install --user p1
+```
+
+## Inclui...
+
+A ideia é que ao instalar o `p1ufcg`, vários pacotes e módulos
+que são úteis na disciplina serão instalados em conjunto. Segue a
+lista do que é instalado na atual versão.
+
+### packages
+
+- [tst](https://github.com/daltonserey/tst)
+- [pytest](https://docs.pytest.org)
+
+### módulos
+
+- [Array](array.md)
---
+++ packages/p1ufcg/p1ufcg-0.2.4.tar.gz/p1ufcg-0.2.4/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(name='p1ufcg',
+      version="0.2.4",
+      description='P1 UFCG',
+      url='http://github.com/daltonserey/p1',
+      author='Dalton Serey',
+      author_email='daltonserey@gmail.com',
+      license='MIT',
+      packages=find_packages(),
+      include_package_data=True,
+      scripts=[],
+      python_requires='>=3.6',
+      install_requires=[
+        'pyyaml>=5.4.1',
+        'requests>=2.6.1',
+        'cachecontrol[filecache]',
+        'tst>=0.15.1',
+        'pytest-tst>=0.1.2',
+      ],
+      entry_points = {
+        'console_scripts': [
+            'p1=p1.commands:main',
+        ]
+      },
+      zip_safe=False)
