870a56f4da8d3f47a905105afd4eb41da0cea8ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:39:51 +0000

    Add package dapla-toolbelt/1.1.0/dapla-toolbelt-1.1.0.tar.gz

---
+++ packages/dapla-toolbelt/dapla-toolbelt-1.1.0.tar.gz/dapla-toolbelt-1.1.0/dapla/__init__.py
@@ -0,0 +1,5 @@
+__version__ = "1.1.0"
+
+from .auth import AuthClient
+from .files import FileClient
+from .guardian import GuardianClient
---
+++ packages/dapla-toolbelt/dapla-toolbelt-1.1.0.tar.gz/dapla-toolbelt-1.1.0/setup.py
@@ -0,0 +1,28 @@
+import pathlib
+from setuptools import setup
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+# This call to setup() does all the work
+setup(
+    name="dapla-toolbelt",
+    version="1.1.0",
+    description="Python module for use within Jupyterlab notebooks, specifically aimed for Statistics Norway's data "
+                "platform called Dapla",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/statisticsnorway/dapla-toolbelt",
+    author="Statistics Norway",
+    author_email="old@ssb.no",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.9",
+    ],
+    packages=["dapla"],
+    include_package_data=True,
+    install_requires=["pandas", "jupyterhub", "gcsfs", "ipython", "lxml"],
+)
