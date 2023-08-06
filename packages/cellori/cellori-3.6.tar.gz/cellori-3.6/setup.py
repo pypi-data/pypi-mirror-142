b244ff7d0928456defd68fb2723b7ef652f3f9ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:11:47 +0000

    Add package cellori/3.6/cellori-3.6.tar.gz

---
+++ packages/cellori/cellori-3.6.tar.gz/cellori-3.6/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+VERSION = "3.6"
+DESCRIPTION = "Cellori"
+LONG_DESCRIPTION = "A fast and robust algorithm for clustered nuclei segmentation."
+
+setup(
+    name="cellori",
+    version=VERSION,
+    author="William Niu",
+    author_email="<wniu721@gmail.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=['dask', 'numba', 'numpy', 'opencv-python', 'scikit-image', 'scipy', 'simpleitk', 'stitchwell',
+                      'tifffile'],
+    extras_require={
+        'gui': ['matplotlib', 'pyside6']
+    },
+    keywords=["nuclei", "segmentation"],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Science/Research",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
