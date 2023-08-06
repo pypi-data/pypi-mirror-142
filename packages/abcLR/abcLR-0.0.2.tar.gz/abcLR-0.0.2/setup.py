c3994a39de55a35343e97da1cd5a98de2707a889
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:24:58 +0000

    Add package abcLR/0.0.2/abcLR-0.0.2.tar.gz

---
+++ packages/abcLR/abcLR-0.0.2.tar.gz/abcLR-0.0.2/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setup(
+    name='abcLR',
+    version='0.0.2',
+    description='ABC-LR is a classification method that combines Artificial Bee Colony algorithm with a logistic regression classification model',
+    py_modules=['abcLR'],
+    package_dir={'': 'src'},
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.10",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+    ],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/kagandedeturk/ABC-LR",
+    author="Bilge Kagan Dedeturk",
+    author_email="kagandedeturk@gmail.com",
+)
