900f5dc4619c66b0567f04d77129e1c3991ccd3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:51:44 +0000

    Add package pythonmultithreadresponse/1.0/pythonmultithreadresponse-1.0.tar.gz

---
+++ packages/pythonmultithreadresponse/pythonmultithreadresponse-1.0.tar.gz/pythonmultithreadresponse-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+setup(name='pythonmultithreadresponse',
+version='1.0',
+description='This package helps you get response from a thread',
+url='',
+author='nikhilkharade',
+author_email='nikhil.kharade@outlook.com',
+license='MIT',
+zip_safe=False)
