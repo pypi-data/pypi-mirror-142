24ce78f86d88ed9b1a0c88412f647258b72ac3fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:44:08 +0000

    Add package nonebot-plugin-blackjack/0.0.5/nonebot_plugin_blackjack-0.0.5.tar.gz

---
+++ packages/nonebot-plugin-blackjack/nonebot_plugin_blackjack-0.0.5.tar.gz/nonebot_plugin_blackjack-0.0.5/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+
+setuptools.setup(
+    name="nonebot_plugin_blackjack",
+    version="0.0.5",
+    keywords=("pip", "nonebot2", "nonebot", "nonebot_plugin"),
+    description="""nonebot2 21点插件""",
+    long_description="nonebot2 21点插件",
+    url="https://github.com/yaowan233/nonebot-plugin-blackjack",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    include_package_data=True,
+    platforms="any",
+    install_requires=['nonebot-adapter-onebot>=2.0.0-beta.1,<3.0.0', 'nonebot2>=2.0.0-beta.1,<3.0.0', 'httpx>=0.19.0', ]
+)
