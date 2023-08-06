05a7650cd51ead9f87825a067a95ab5562ac7b44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:25:03 +0000

    Add package tauk/1.0.9/tauk-1.0.9.tar.gz

---
+++ packages/tauk/tauk-1.0.9.tar.gz/tauk-1.0.9/tauk/__init__.py
@@ -0,0 +1,18 @@
+"""Helper package to facilitate reporting for webdriver-based tests on Tauk"""
+
+from tauk.tauk_appium import Tauk
+
+__project__ = "tauk"
+__version__ = "1.0.9"
+__author__ = "Nathan Krishnan"
+__url__ = "https://github.com/thetauk/tauk-webdriver-python"
+__platforms__ = "ALL"
+__classifiers__ = [
+    "Programming Language :: Python :: 3",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independent",
+]
+__requires__ = ["requests", "Appium-Python-Client", "selenium"]
+
+__extra_requires__ = {
+}
