18017481e76d2089b352584d05466b7cefbd2c9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:15:54 +0000

    Add package kenallclient/0.4/kenallclient-0.4-py3-none-any.whl

---
+++ packages/kenallclient/kenallclient-0.4-py3-none-any.whl/kenallclient/client.py
@@ -0,0 +1,203 @@
+import json
+import urllib.parse
+import urllib.request
+from typing import Dict, List, Optional, Tuple
+
+from kenallclient.model import (
+    HolidaySearchResult,
+    HoujinResult,
+    HoujinSearchResult,
+    KenAllResult,
+    KenAllSearchResult,
+)
+
+
+class KenAllClient:
+    api_url = "https://api.kenall.jp"
+
+    def __init__(self, api_key: str, api_url: Optional[str] = None) -> None:
+        self.api_key = api_key
+        if api_url is not None:
+            self.api_url = api_url
+
+    @property
+    def authorization(self) -> Dict[str, str]:
+        auth = {"Authorization": f"Token {self.api_key}"}
+        return auth
+
+    def create_request(self, postal_code: str) -> urllib.request.Request:
+        url = urllib.parse.urljoin(f"{self.api_url}/v1/postalcode/", postal_code)
+        req = urllib.request.Request(url, headers=self.authorization)
+        return req
+
+    def fetch(self, req: urllib.request.Request) -> KenAllResult:
+        with urllib.request.urlopen(req) as res:
+            if not res.headers["Content-Type"].startswith("application/json"):
+                raise ValueError("not json response", res.read())
+            d = json.load(res)
+            return KenAllResult.fromdict(d)
+
+    def get(self, postal_code: str) -> KenAllResult:
+        req = self.create_request(postal_code)
+        return self.fetch(req)
+
+    def create_search_request(
+        self,
+        q: Optional[str] = None,
+        t: Optional[str] = None,
+        offset: Optional[int] = None,
+        limit: Optional[int] = None,
+        facet: Optional[str] = None,
+    ) -> urllib.request.Request:
+        query_mapping: List[Tuple[str, Optional[str]]] = [
+            ("q", q),
+            ("t", t),
+            ("offset", str(offset) if offset is not None else None),
+            ("limit", str(limit) if limit is not None else None),
+            ("facet", facet),
+        ]
+
+        query = urllib.parse.urlencode(
+            [(k, v) for k, v in query_mapping if v is not None]
+        )
+        url = f"{self.api_url}/v1/postalcode/?{query}"
+        req = urllib.request.Request(url, headers=self.authorization)
+        return req
+
+    def fetch_search_result(self, req: urllib.request.Request) -> KenAllSearchResult:
+        with urllib.request.urlopen(req) as res:
+            if not res.headers["Content-Type"].startswith("application/json"):
+                raise ValueError("not json response", res.read())
+            d = json.load(res)
+            return KenAllSearchResult.fromdict(d)
+
+    def search(
+        self,
+        *,
+        q: Optional[str],
+        t: Optional[str],
+        offset: Optional[int] = None,
+        limit: Optional[int] = None,
+        facet: Optional[str] = None,
+    ) -> KenAllSearchResult:
+        req = self.create_search_request(q, t, offset, limit, facet)
+        return self.fetch_search_result(req)
+
+    def create_houjin_request(self, houjinbangou: str) -> urllib.request.Request:
+        url = f"{self.api_url}/v1/houjinbangou/{houjinbangou}"
+        return urllib.request.Request(url, headers=self.authorization)
+
+    def fetch_houjin_result(self, req: urllib.request.Request) -> HoujinResult:
+        with urllib.request.urlopen(req) as res:
+            if not res.headers["Content-Type"].startswith("application/json"):
+                raise ValueError("not json response", res.read())
+            d = json.load(res)
+            return HoujinResult.fromdict(d)
+
+    def get_houjin(self, houjinbangou: str) -> HoujinResult:
+        req = self.create_houjin_request(houjinbangou)
+        return self.fetch_houjin_result(req)
+
+    def create_search_houjin_request(
+        self,
+        q: str,
+        offset: Optional[int] = None,
+        limit: Optional[int] = None,
+        mode: Optional[str] = None,
+        facet_area: Optional[str] = None,
+        facet_kind: Optional[str] = None,
+        facet_process: Optional[str] = None,
+        facet_close_cause: Optional[str] = None,
+    ) -> urllib.request.Request:
+        query_mapping: List[Tuple[str, Optional[str]]] = [
+            ("q", q),
+            ("offset", str(offset) if offset is not None else None),
+            ("limit", str(limit) if limit is not None else None),
+            ("mode", mode),
+            ("facet_area", facet_area),
+            ("facet_kind", facet_kind),
+            ("facet_process", facet_process),
+            ("facet_close_cause", facet_close_cause),
+        ]
+
+        query = urllib.parse.urlencode(
+            [(k, v) for k, v in query_mapping if v is not None]
+        )
+        url = f"{self.api_url}/v1/houjinbangou?{query}"
+        req = urllib.request.Request(url, headers=self.authorization)
+        return req
+
+    def fetch_search_houjin_result(
+        self, req: urllib.request.Request
+    ) -> HoujinSearchResult:
+        with urllib.request.urlopen(req) as res:
+            if not res.headers["Content-Type"].startswith("application/json"):
+                raise ValueError("not json response", res.read())
+            d = json.load(res)
+            return HoujinSearchResult.fromdict(d)
+
+    def search_houjin(
+        self,
+        q: str,
+        offset: Optional[int] = None,
+        limit: Optional[int] = None,
+        mode: Optional[str] = None,
+        facet_area: Optional[str] = None,
+        facet_kind: Optional[str] = None,
+        facet_process: Optional[str] = None,
+        facet_close_cause: Optional[str] = None,
+    ) -> HoujinSearchResult:
+        req = self.create_search_houjin_request(
+            q,
+            offset=offset,
+            limit=limit,
+            mode=mode,
+            facet_area=facet_area,
+            facet_kind=facet_kind,
+            facet_process=facet_process,
+            facet_close_cause=facet_close_cause,
+        )
+        return self.fetch_search_houjin_result(req)
+
+    def create_search_holiday_request(self,
+        year: Optional[int] = None,
+        from_: Optional[str] = None,
+        to: Optional[str] = None) -> urllib.request.Request:
+        query_mapping: List[Tuple[str, Optional[str]]] = [
+            ("year", str(year) if year is not None else None),
+            ("from", str(from_) if from_ is not None else None),
+            ("to", str(to) if to is not None else None),
+        ]
+
+        query = urllib.parse.urlencode(
+            [(k, v) for k, v in query_mapping if v is not None]
+        )
+        url = f"{self.api_url}/v1/holidays?{query}"
+        req = urllib.request.Request(url, headers=self.authorization)
+        return req
+
+    def fetch_search_holiday_result(self, req) -> HolidaySearchResult:
+        with urllib.request.urlopen(req) as res:
+            if not res.headers["Content-Type"].startswith("application/json"):
+                raise ValueError("not json response", res.read())
+            d = json.load(res)
+            return HolidaySearchResult.fromdict(d)
+
+    def search_holiday(
+        self,
+        year: Optional[int] = None,
+        from_: Optional[str] = None,
+        to: Optional[str] = None,
+    ) -> HolidaySearchResult:
+        import urllib.error
+        req = self.create_search_holiday_request(
+            year=year,
+            from_=from_,
+            to=to,
+        )
+        try:
+            return self.fetch_search_holiday_result(req)
+        except urllib.error.HTTPError as e:
+            print(e)
+            print(json.load(e))
+            raise
---
+++ packages/kenallclient/kenallclient-0.4-py3-none-any.whl/kenallclient/model.py
@@ -0,0 +1,188 @@
+import dataclasses
+from typing import List, Optional, Any, Dict, Tuple
+
+
+HOUJIN_KIND = {
+    101: "国の機関",
+    201: "地方公共団体",
+    301: "株式会社",
+    302: "有限会社",
+    303: "合名会社",
+    304: "合資会社",
+    305: "合同会社",
+    399: "その他の設立登記法人",
+    401: "外国会社等",
+    499: "その他",
+}
+
+HOUJIN_HIHYOJI = {
+    0: "検索対象",
+    1: "検索対象から除外",
+}
+
+HOUJIN_CLOSE_CAUSE = {
+    "01": "精算の結了等",
+    "11": "合併による解散等",
+    "21": "登記官による閉鎖",
+    "31": "その他の精算の結了等",
+}
+
+CORPORATION_CODE_TYPE = {
+    0: "大口事業所",
+    1: "私書箱",
+}
+
+
+@dataclasses.dataclass()
+class HoujinResultItem:
+    sequence_number: int
+    corporate_number: str
+    process: int
+    correct: int
+    update_date: str
+    change_date: str
+    name: str
+    name_image_id: Optional[str]
+    kind: int
+    prefecture_name: str
+    city_name: str
+    published_date: str
+    hihyoji: int
+    furigana: str
+    en_address_outside: Optional[str]
+    en_address_line: Optional[str]
+    en_prefecture_name: str
+    en_name: str
+    assignment_date: str
+    change_cause: str
+    successor_corporate_number: Optional[str]
+    close_cause: Optional[str]
+    close_date: Optional[str]
+    address_outside_image_id: Optional[str]
+    address_outside: str
+    post_code: str
+    jisx0402: str
+    address_image_id: Optional[str]
+    street_number: str
+
+
+@dataclasses.dataclass()
+class HoujinResult:
+    version: str
+    data: List[HoujinResultItem]
+
+    @classmethod
+    def fromdict(cls, i: Dict[str, Any]) -> "HoujinResult":
+        return HoujinResult(**i)
+
+
+@dataclasses.dataclass()
+class HoujinSearchResult:
+    version: str
+    data: List[str]
+    query: str
+    count: int
+    offset: int
+    limit: int
+    facets: List[Tuple[str, int]]
+
+    @classmethod
+    def fromdict(cls, i: Dict[str, Any]) -> "HoujinSearchResult":
+        return HoujinSearchResult(**i)
+
+
+@dataclasses.dataclass()
+class KenAllCorporation:
+    name: str
+    name_kana: str
+    block_lot: str
+    block_lot_num: Optional[str]
+    post_office: str
+    code_type: int
+
+
+@dataclasses.dataclass()
+class KenAllResultItem:
+    jisx0402: str
+    old_code: str
+    postal_code: str
+    prefecture_kana: str
+    city_kana: str
+    town_kana: str
+    town_kana_raw: str
+    prefecture: str
+    city: str
+    town: str
+    koaza: str
+    kyoto_street: str
+    building: str
+    floor: str
+    town_partial: bool
+    town_addressed_koaza: bool
+    town_chome: bool
+    town_multi: bool
+    town_raw: str
+    corporation: Optional[KenAllCorporation]
+
+    @classmethod
+    def fromdict(cls, i: Dict[str, Any]) -> "KenAllResultItem":
+        if i["corporation"]:
+            c = i["corporation"]
+            corp = KenAllCorporation(**c)
+            i["corporation"] = corp
+        return KenAllResultItem(**i)
+
+
+@dataclasses.dataclass()
+class KenAllResult:
+    version: str
+    data: List[KenAllResultItem]
+
+    @classmethod
+    def fromdict(cls, d: Dict[str, Any]) -> "KenAllResult":
+        data = [KenAllResultItem.fromdict(i) for i in d["data"]]
+        dd = dict(**d)
+        dd["data"] = data
+        return KenAllResult(**dd)
+
+
+@dataclasses.dataclass()
+class KenAllSearchResult:
+    version: str
+    data: List[KenAllResultItem]
+    query: str
+    count: int
+    offset: Optional[int]
+    limit: Optional[int]
+    facets: Optional[List[Tuple[str, int]]]
+
+    @classmethod
+    def fromdict(cls, d: Dict[str, Any]) -> "KenAllSearchResult":
+        data = [KenAllResultItem.fromdict(i) for i in d["data"]]
+        dd = dict(**d)
+        dd["data"] = data
+        if dd["facets"] is not None:
+            dd["facets"] = [tuple(f) for f in dd["facets"]]
+        return KenAllSearchResult(**dd)
+
+
+@dataclasses.dataclass()
+class Holiday:
+    title: str
+    date: str
+    day_of_week: int
+    day_of_week_text: str
+
+    @classmethod
+    def fromdict(cls, d: Dict[str, Any]) -> "Holiday":
+        return cls(**d)
+
+
+@dataclasses.dataclass()
+class HolidaySearchResult:
+    data: List[Holiday]
+
+    @classmethod
+    def fromdict(cls, d: Dict[str, Any]) -> "HolidaySearchResult":
+        data = [Holiday.fromdict(i) for i in d["data"]]
+        return HolidaySearchResult(data=data)
