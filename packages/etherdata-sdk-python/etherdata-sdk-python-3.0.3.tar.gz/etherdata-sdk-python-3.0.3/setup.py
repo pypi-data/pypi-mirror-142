c8c9c391f5439fc791145179f554e74d0e5a43be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:15:25 +0000

    Add package etherdata-sdk-python/3.0.3/etherdata-sdk-python-3.0.3.tar.gz

---
+++ packages/etherdata-sdk-python/etherdata-sdk-python-3.0.3.tar.gz/etherdata-sdk-python-3.0.3/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+long_description = "# Etherdata BlockChain Python SDK"
+setuptools.setup(
+    name="etherdata-sdk-python",
+    version="v3.0.3",
+    author="etherdata-blockchain",
+    author_email="etherdata-blockchain@etd.com",
+    description="SDK for etherdata-blockchain",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/etherdata-blockchain/etherdata-sdk",
+    project_urls={
+        "Bug Tracker": "https://github.com/etherdata-blockchain/etherdata-sdk",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+)
