d5bbb13d625bcc541d9670b4cdc82811ee7ccdd2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:25:30 +0000

    Add package Richdata/0.1/Richdata-0.1.tar.gz

---
+++ packages/Richdata/Richdata-0.1.tar.gz/Richdata-0.1/Richdata/__init__.py
@@ -0,0 +1,7 @@
+__all__ = ['sqlserver','postgres']
+from Richdata.postgres import PostgreSQL
+from Richdata.sqlserver import SQLServer
+from Richdata.helpers import camel_to_snake
+from Richdata.helpers import snake_to_camel
+
+
---
+++ packages/Richdata/Richdata-0.1.tar.gz/Richdata-0.1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup,find_packages
+
+setup(
+    name='Richdata',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=['pandas'],  # add any additional packages that
+    extras_require={
+        'sqlserver': ['pyodbc'],
+        'postgres': ['psycopg2-binary','SQLAlchemy'],
+    },
+    url='https://github.com/rimedinaz/richdata',
+    license='Apache License 2.0',
+    author='Richard Medina',
+    author_email='rimedinaz@gmail.com',
+    description='Rich data and Data Engineering',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.3",
+)
