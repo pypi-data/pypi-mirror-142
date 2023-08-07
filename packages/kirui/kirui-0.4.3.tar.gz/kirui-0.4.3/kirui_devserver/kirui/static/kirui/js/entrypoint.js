9056c10f2f99665c5ffff3ab7fa042d70dc400cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 00:42:31 +0000

    Add package kirui/0.4.3/kirui-0.4.3.tar.gz

---
+++ packages/kirui/kirui-0.4.3.tar.gz/kirui-0.4.3/kirui_devserver/kirui/static/kirui/js/entrypoint.js
@@ -0,0 +1,12 @@
+import * as sideBar from "./components/layout/sidebar.js";
+import * as Form from "./components/forms/index.js";
+import * as Modal from "./components/modal.js";
+import * as Table from "./components/tables/index.js";
+import * as Chart from "./components/charts/index.js";
+import { html, render } from "./core/component.js";
+
+const entrypoint = (content) => {
+    render(content, document.body);
+}
+
+export { entrypoint, html };
