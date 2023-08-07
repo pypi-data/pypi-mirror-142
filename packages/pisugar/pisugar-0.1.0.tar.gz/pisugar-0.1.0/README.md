648b27be02a1e613d2f4826cafbfad75bda806bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 00:42:33 +0000

    Add package pisugar/0.1.0/pisugar-0.1.0.tar.gz

---
+++ packages/pisugar/pisugar-0.1.0.tar.gz/pisugar-0.1.0/README.md
@@ -0,0 +1,23 @@
+# pisugar-server-py
+
+Python library to communicate with pisugar-server.
+
+## Requirements
+
+python 3.5+
+
+## Usage
+
+Installation
+
+    pip3 install pisugar
+
+Python test code
+
+    from pisugar import test_via_tcp
+
+    test_via_tcp(<YOUR HOST ADDR>)
+
+## License
+
+Apache License
---
+++ packages/pisugar/pisugar-0.1.0.tar.gz/pisugar-0.1.0/setup.py
@@ -0,0 +1,29 @@
+from unicodedata import name
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pisugar",
+    version="0.1.0",
+    description="PiSugar server python api",
+    author="PiSugar Team",
+    author_email="app@pisugar.com",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/PiSugar/pisugar-server-py",
+    project_urls={
+        "Official Website": "https://www.pisugar.com",
+        "Bug Tracker": "https://github.com/PiSugar/pisugar-server-py/issues",
+    },
+    license="Apache License 2.0",
+    keywords="PiSugar Raspberry Pi",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    packages=setuptools.find_packages(),
+    python_requires=">=3.6",
+)
