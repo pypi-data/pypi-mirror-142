5aa1bfbc82b06a882323d1a392cb5a283423933a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 00:43:16 +0000

    Add package ljl-handsome/0.1/ljl-handsome-0.1.tar.gz

---
+++ packages/ljl-handsome/ljl-handsome-0.1.tar.gz/ljl-handsome-0.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup,find_packages
+setup(
+    name="ljl-handsome",
+    version="0.1",
+    author="ljl",
+    #url="ljl.handsome.com", 此网站需要存在且未被占用
+    packages = find_packages("ljl"), # 模块的保存目录
+    package_dir = {"":"ljl"}, # 告诉 setuptools 包都在 ckh 下
+    package_data = {
+        # 定义打包除了 .py 之外的文件类型，此处 .py 其实可以不写
+        "":[".txt",".info","*.properties",".py"],
+        # 包含 data 文件夹下所有的 *.dat 文件
+        "":["data/*.*"],
+    },
+    # 取消所有测试包
+    exclude = ["*.test","*.test.*","test.*","test"]
+)
