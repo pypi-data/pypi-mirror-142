0464bf4a5b6d9d5aff0c6eda0ccfb10ae5df48e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 00:44:29 +0000

    Add package mk-feature-store-postgres/0.2.7/mk-feature-store-postgres-0.2.7.tar.gz

---
+++ packages/mk-feature-store-postgres/mk-feature-store-postgres-0.2.7.tar.gz/mk-feature-store-postgres-0.2.7/setup.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+with open("README.md", "r", encoding="utf-8") as f:
+    readme = f.read()
+
+INSTALL_REQUIRE = [
+    "mk-feature-store>=0.19.0",
+    "psycopg2-binary>=2.8.3",
+    "pyarrow>=2.0.0",
+]
+
+DEV_REQUIRE = [
+    "flake8",
+    "black==21.10b0",
+    "isort>=5",
+    "mypy==0.790",
+    "build==0.7.0",
+    "twine==3.4.2",
+    "pytest>=6.0.0",
+]
+
+setup(
+    name="mk-feature-store-postgres",
+    version="0.2.7",
+    author="Gunnar Sv Sigurbjörnsson",
+    author_email="gunnar.sigurbjornsson@gmail.com",
+    description="PostgreSQL registry, and online and offline store for Feast",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    python_requires=">=3.7.0",
+    url="https://github.com/jungyuneum/feature-store/feast-postgres",
+    project_urls={
+        "Bug Tracker": "https://github.com/nossrannug/feast-postgres/issues",
+    },
+    license='Apache License, Version 2.0',
+    packages=["feast_postgres", "feast_postgres.online_stores", "feast_postgres.offline_stores"],
+    install_requires=INSTALL_REQUIRE,
+    extras_require={
+        "dev": DEV_REQUIRE,
+    },
+    keywords=("feast featurestore postgres offlinestore onlinestore"),
+    classifiers=[
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
+)
