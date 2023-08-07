7d996340cfb9892fa6703f864975c79a25dac27a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 00:41:56 +0000

    Add package markdowntable/6.0.0/markdowntable-6.0.0.tar.gz

---
+++ packages/markdowntable/markdowntable-6.0.0.tar.gz/markdowntable-6.0.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+
+setup(name='markdowntable',
+      version='6.0.0',
+      description='Easy way to make markdown code for tables',
+      url='https://github.com/PokestarFan/Python-Markdown-Table',
+      author='PythonCoderAS',
+      license='MIT',
+      packages=['markdowntable'],
+      python_requires='>=3.7') 
