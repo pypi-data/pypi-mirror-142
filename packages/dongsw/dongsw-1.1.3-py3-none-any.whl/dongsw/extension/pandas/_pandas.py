f355fa98d31f5a998f64be16f841501744b1b270
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 00:50:13 +0000

    Add package dongsw/1.1.3/dongsw-1.1.3-py3-none-any.whl

---
+++ packages/dongsw/dongsw-1.1.3-py3-none-any.whl/dongsw/extension/pandas/_pandas.py
@@ -0,0 +1,31 @@
+import pandas as pd
+
+def qcut_(data, q = None, key = None, *args, **kwargs):
+    '''
+    similar to pd.qcut, but return a list of cut result.
+
+    eg.
+    qcut_(pd.Series([1,2,3,4,5]), 2)
+    output: 
+    [0    1
+    1    2
+    2    3
+    dtype: int64, 
+    3    4
+    4    5
+    dtype: int64]
+
+    data: pd.Series or pd.DataFrame. If DataFrame, must set key param.
+    key: column name you select in data
+    return: a list of cut result by qcut.
+    '''    
+    if isinstance(data, pd.Series):
+        tmp_series = data
+    elif isinstance(data, pd.DataFrame) and key is not None:
+        tmp_series = data[key]
+    else:
+        raise Exception('The param \'data\' must be pd.Series or pd.DataFrame. If DataFrame, must set key param.')
+
+    result = pd.qcut(tmp_series, q, *args, *kwargs)
+
+    return [pair[1] for pair in data.groupby(result)]
