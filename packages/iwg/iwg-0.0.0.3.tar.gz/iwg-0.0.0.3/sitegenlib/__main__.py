3d739bfae9f90295f9fc2d1cbcc8e8cc2d7e8cfb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 09:55:22 +0000

    Add package iwg/0.0.0.3/iwg-0.0.0.3.tar.gz

---
+++ packages/iwg/iwg-0.0.0.3.tar.gz/iwg-0.0.0.3/sitegenlib/__main__.py
@@ -0,0 +1,15 @@
+"""
+In-house Site Generator
+"""
+
+from typing import List
+
+
+def main(*args: List[str]) -> int:
+    print("In-house Site Generator")
+    return 0
+
+
+if __name__ == "__main__":
+    import sys
+    sys.exit(main(*sys.argv))
