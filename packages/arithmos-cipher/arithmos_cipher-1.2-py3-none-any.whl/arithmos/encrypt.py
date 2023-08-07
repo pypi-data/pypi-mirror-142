7c07338ebbbee73241a2a906ea82352cff524e75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 00:53:25 +0000

    Add package arithmos-cipher/1.2/arithmos_cipher-1.2-py3-none-any.whl

---
+++ packages/arithmos-cipher/arithmos_cipher-1.2-py3-none-any.whl/arithmos/encrypt.py
@@ -0,0 +1,11 @@
+from .dictionary import encrypt_dict
+
+def encrypt(text:str):
+    """
+    A method to encrypt string to Arithmos
+    ----
+    Parameter :
+    - text: `str` | the string to be encrypted
+    """
+
+    return ''.join(encrypt_dict[c] for c in list(text))
