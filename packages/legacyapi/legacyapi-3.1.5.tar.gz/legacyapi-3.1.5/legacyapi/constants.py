3dcf8f824ed95f6797b3c0c40c5cec8f40d8416e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 00:39:57 +0000

    Add package legacyapi/3.1.5/legacyapi-3.1.5.tar.gz

---
+++ packages/legacyapi/legacyapi-3.1.5.tar.gz/legacyapi-3.1.5/legacyapi/constants.py
@@ -0,0 +1,13 @@
+# --------------------------------------------------------------------
+# Copyright (c) iEXBase. All rights reserved.
+# Licensed under the MIT License.
+# See License.txt in the project root for license information.
+# --------------------------------------------------------------------
+
+# Here we specify default values for the different needed urls.
+# They are verified. Don't change this unless you know what you're doing.
+DEFAULT_NODES = {
+    'full_node': 'https://apilg.lgcyscan.network/',
+    'solidity_node': 'https://apilg.lgcyscan.network/',
+    'event_server': 'https://apilg.lgcyscan.network/'
+}
