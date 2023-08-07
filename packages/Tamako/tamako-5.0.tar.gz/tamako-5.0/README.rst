222bc256ae241f2cb946fa8695faae2104caa9a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 00:43:06 +0000

    Add package Tamako/5.0/tamako-5.0.tar.gz

---
+++ packages/Tamako/tamako-5.0.tar.gz/tamako-5.0/README.rst
@@ -0,0 +1,5 @@
+This is a simple example package. You can use
+
+[联系作者](2415075961@qq.com)
+
+to write your content.
---
+++ packages/Tamako/tamako-5.0.tar.gz/tamako-5.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+        name='tamako',
+        version='5.0',
+        description='张力元',
+        author='tamako',
+        author_email='2415075961@qq.com',
+        py_modules=['debug.print']
+)
+
