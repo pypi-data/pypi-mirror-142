df5f686d960d44d0ffc0e0f461aa98787ebb70b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 00:44:30 +0000

    Add package DSPutility/0.0.15/DSPutility-0.0.15-py3-none-any.whl

---
+++ packages/DSPutility/DSPutility-0.0.15-py3-none-any.whl/dsputility/dbintf/dbintf.py
@@ -0,0 +1,20 @@
+from sqlalchemy import create_engine
+
+class dbintf:
+    """dbintf setup database connection info and establish an DB engine
+    """
+    def __init__(self, db_name:str, user:str='postgres', password:str='password', host:str='localhost', port:str='5432', vendor:str='postgresql'):
+        """Setup database connection and establish an DB engine
+
+        :param db_name: database name
+        :param user: authorized user name, defaults to 'postgres'
+        :param password: authorized user password, defaults to 'password'
+        :param host: host url of database connection, defaults to 'localhost'
+        :param port: port of database connection, defaults to '5432'
+        :param vendor: vendor of database, defaults to 'postgresql'
+        """
+        self.db_name = db_name
+        self.user, self.password = user, password
+        self.host, self.port, self.vendor = host, port, vendor
+        self.sqlalchemy_url = f"{self.vendor}://{self.user}:{self.password}@{self.host}:{self.port}/{self.db_name}"
+        self.engine = create_engine(SQLALCHEMY_DATABASE_URL)
