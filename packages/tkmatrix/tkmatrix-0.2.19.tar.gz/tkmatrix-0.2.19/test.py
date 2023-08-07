20010fa230e9aea9226a2ce5e23bf50c1bcd3749
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 09:55:23 +0000

    Add package tkmatrix/0.2.19/tkmatrix-0.2.19.tar.gz

---
+++ packages/tkmatrix/tkmatrix-0.2.19.tar.gz/tkmatrix-0.2.19/test.py
@@ -0,0 +1,7 @@
+from tkmatrix.tkmatrix_class import MATRIX
+
+
+MATRIX.plot_results("TIC 142748283", "/home/martin/Desktop/run_tests/tests/TIC142748283_ir_3", 1)
+#MATRIX.plot_diff("TIC 169285097", "/home/martin/Desktop/run_tests/tests/TIC169285097_ir_sasg",
+#                 "/home/martin/Desktop/run_tests/tests/TIC169285097_ir_none",
+#                 "./")
