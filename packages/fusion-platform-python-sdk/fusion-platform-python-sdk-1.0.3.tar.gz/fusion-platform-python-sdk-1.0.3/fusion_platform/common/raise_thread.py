f29c7911c6d81628f68544a6bad44fd1044d3ec5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 00:39:52 +0000

    Add package fusion-platform-python-sdk/1.0.3/fusion-platform-python-sdk-1.0.3.tar.gz

---
+++ packages/fusion-platform-python-sdk/fusion-platform-python-sdk-1.0.3.tar.gz/fusion-platform-python-sdk-1.0.3/fusion_platform/common/raise_thread.py
@@ -0,0 +1,40 @@
+"""
+Raise thread class file.
+
+author: Matthew Casey
+
+&copy; [Digital Content Analysis Technology Ltd](https://www.d-cat.co.uk)
+"""
+
+from threading import Thread
+
+
+class RaiseThread(Thread):
+    """
+    Custom thread class used to make sure exceptions are raised on join. Based on:
+    https://www.geeksforgeeks.org/handling-a-threads-exception-in-the-caller-thread-in-python/
+    """
+
+    def join(self, timeout=None):
+        """
+        Wait until the thread terminates or the timeout occurs.
+
+        Args:
+            timeout: The amount of time to wait until the thread has terminated. Using None will bock indefinitely.
+        """
+        super(RaiseThread, self).join(timeout)
+
+        # Make sure any exception is re-raised in the calling thread.
+        if self.__error is not None:
+            raise self.__error
+
+    def run(self):
+        """
+        Runs the thread operation, but catches any error so that it can be re-raised on join.
+        """
+        self.__error = None
+
+        try:
+            super(RaiseThread, self).run()
+        except Exception as e:
+            self.__error = e
