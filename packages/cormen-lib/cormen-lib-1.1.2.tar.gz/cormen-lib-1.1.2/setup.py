8474b7eed3f097e01e8c139fa5a8532dcfc63438
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 00:42:09 +0000

    Add package cormen-lib/1.1.2/cormen-lib-1.1.2.tar.gz

---
+++ packages/cormen-lib/cormen-lib-1.1.2.tar.gz/cormen-lib-1.1.2/setup.py
@@ -0,0 +1,17 @@
+# encoding: utf8
+
+from setuptools import setup
+
+
+setup(
+    author='Eitan Joseph, Chami Lamelas',
+    author_email="eitanjoseph@brandeis.edu",
+    description='A data structures and algorithms library based on Introduction to Algorithms Third Edition by Thomas H. Cormen.',
+    install_requires=['dalpy'],
+    long_description='''Use `DALPy <https://pypi.org/project/dalpy/>`_ instead.''',
+    name='cormen-lib',
+    platforms=['all'],
+    url='https://pypi.org/project/dalpy/',
+    version="1.1.2",
+    zip_safe=False,
+)
