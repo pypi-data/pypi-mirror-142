01e042624c06c86277c8d339a2173108d3f057ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 00:42:18 +0000

    Add package tamako2/1.0/tamako2-1.0.tar.gz

---
+++ packages/tamako2/tamako2-1.0.tar.gz/tamako2-1.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+        name='tamako2',
+        version='1.0',
+        description='张力元',
+        project_description='zly',
+        author='tamako',
+        author_email='2415075961@qq.com',
+        py_modules=['debug.print']
+)
+
