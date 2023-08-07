8ad6144b310a36d41f6c11be18f20add88c42b79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 00:40:28 +0000

    Add package streamlit-dimensions/0.0.1/streamlit_dimensions-0.0.1-py3-none-any.whl

---
+++ packages/streamlit-dimensions/streamlit_dimensions-0.0.1-py3-none-any.whl/streamlit_dimensions/frontend/build/precache-manifest.a31df42ecae412362b17080fc25dde4a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "396dc8dcc04887a17adc09164dac600d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "861e1b686a19729d4ed9",
+    "url": "./static/js/2.2d244465.chunk.js"
+  },
+  {
+    "revision": "3fc7fb5bfeeec1534560a2c962e360a7",
+    "url": "./static/js/2.2d244465.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a7e356382044c29db2ac",
+    "url": "./static/js/main.b48797e6.chunk.js"
+  },
+  {
+    "revision": "fdaba978c446e8baa1ef",
+    "url": "./static/js/runtime-main.92899dd5.js"
+  }
+]);
