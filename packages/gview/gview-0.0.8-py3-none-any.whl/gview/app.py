d9ecffa1effb46fb674e116b6f735d42d4bd16f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 00:43:08 +0000

    Add package gview/0.0.8/gview-0.0.8-py3-none-any.whl

---
+++ packages/gview/gview-0.0.8-py3-none-any.whl/gview/app.py
@@ -0,0 +1,10 @@
+from .app_celery_init import create_app
+
+# initialize app
+app = create_app()
+
+# resgister blueprints
+from . import index,host
+app.register_blueprint(index.bp)
+app.register_blueprint(host.bp)
+
