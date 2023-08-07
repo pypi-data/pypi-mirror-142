dbe80517ea8e661866df7259e9a5a89c36f0a21d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 00:50:33 +0000

    Add package ohmycloud-fib-py/0.0.2/ohmycloud_fib_py-0.0.2.tar.gz

---
+++ packages/ohmycloud-fib-py/ohmycloud_fib_py-0.0.2.tar.gz/ohmycloud_fib_py-0.0.2/README.md
@@ -0,0 +1,3 @@
+# speed_up_your_python_with_rust
+
+使用 Rust 加速 Python 代码
---
+++ packages/ohmycloud-fib-py/ohmycloud_fib_py-0.0.2.tar.gz/ohmycloud_fib_py-0.0.2/setup.py
@@ -0,0 +1,33 @@
+from setuptools import find_packages, setup
+import pathlib
+
+with open(str(pathlib.Path(__file__).parent.absolute()) + "/flitton_fib_py/version.py", "r") as fh:
+    version = fh.read().split("=")[1].replace("'", "")
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="ohmycloud_fib_py",
+    version=version,
+    author="ohmycloud",
+    author_email="ohmycloudy@gmail.com",
+    description="Calculates a Fibonacci number",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ohmycloud/speed_up_your_python_with_rust",
+    install_requires=[],
+    packages=find_packages(exclude=("tests",)),
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'fib-number = flitton_fib_py.cmd.fib_numb:fib_numb',
+        ],
+    },
+)
